21aff4424df11266d8a2ec1e712e943a706e8e0f
Greg Annandale <greg@raspberrypi.org>
2018-12-03 12:56:58 +0000

    Add basic auth to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD'] if ENV['BASIC_AUTH_PASSWORD']
 end
