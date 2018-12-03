56a17547979592feca07b6b2113992dbb525bc3d
Damien Hogan <damienhogan87@gmail.com>
2018-12-03 16:03:33 +0000

    Refactor auth to ensure it can be mocked and tested

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD'] if ENV['BASIC_AUTH_PASSWORD']
+  before_action :authenticate
+
+  def authenticate
+    return unless ENV['BASIC_AUTH_PASSWORD']
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_PASSWORD'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, :type => :controller do
+  before(:each) do
+    allow(controller).to receive(:authenticate_or_request_with_http_basic)
+  end
+
+  describe 'before_action' do
+    describe '#authenticate' do
+      context 'when BASIC_AUTH_PASSWORD is set' do
+        it 'authenticate gets called' do
+          ENV.stub(:[]).with('BASIC_AUTH_PASSWORD').and_return('password')
+          controller.send(:authenticate)
+          expect(controller).to have_received(:authenticate_or_request_with_http_basic)
+        end
+      end
+
+      context 'when BASIC_AUTH_PASSWORD is not set' do
+        it 'authenticate does not get called' do
+          controller.send(:authenticate)
+          expect(controller).not_to have_received(:authenticate_or_request_with_http_basic)
+        end
+      end
+    end
+  end
+end
