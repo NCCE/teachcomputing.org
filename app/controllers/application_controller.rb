8b5b4d24adbc22c47af866937fd1396bc78700cb
Greg Annandale <greg@raspberrypi.org>
2018-12-03 16:20:02 +0000

    Correct auth user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def authenticate
     return unless ENV['BASIC_AUTH_PASSWORD']
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_PASSWORD'] && password == ENV['BASIC_AUTH_PASSWORD']
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
