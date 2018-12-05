b0a7ba830fc95de4dc2737359400aeb1e99d1beb
MagdalenaJadach <magdalena@raspberrypi.org>
2018-12-05 14:49:13 +0000

    Add and style homepage (#11)
    * Add basic copy
    
    * Add sign-up section
    
    * Style Sign up component
    
    * Stay up to date section
    
    * Mailing section
    
    * Jobs section
    
    * Add Partners section
    
    * Split index.html into components
    
    * Remove extra links from Header
    
    * Fix indentation
    
    * Add basic test for Hero component
    
    * Test Stay up to date section
    
    * Test Mailing section
    
    * Jobs section tested
    
    * Test partners section
    
    * Remove typo from Hero test
    
    * Fix typo
    
    * Add link to Hero
    
    * Fix typo
    
    * Remove reduntant brackets
    
    * Rename component
    
    * Rename scss file
    
    * Rename classes to keep consistent naming convention
    
    * Typo
    
    * Fix test

---
+++ spec/views/home/_mailing.html_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe('home/_mailing', type: :view) do
+  before do
+    render
+  end
+
+  it 'has a title' do
+    expect(rendered).to have_css('.ncce-mailing', text: 'Stay up to date')
+  end
+
+  it 'links to the Facebook page' do
+    expect(rendered).to have_link('Like NCCE on Facebook', href: '#')
+  end
+
+  it 'links to the Twitter account' do
+    expect(rendered).to have_link('Follow @WeAreComputing on Twitter', href: '#')
+  end
+
+end
