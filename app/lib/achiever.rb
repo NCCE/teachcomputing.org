c059e3d38977233528c22ad7155154d1ab62be3f
Daniel Brierton <daniel@brierton.me>
2018-12-05 11:10:45 +0000

    Added super basic course details page (#14)
    * Added super basic course details page
    
    * Refactored based off changes on master
    
    * Changed to use path instead of url in course listings

--- app/lib/achiever.rb
+++ app/lib/achiever.rb
@@ -2,42 +2,64 @@ require('net/http')
 require('nokogiri')
 require('htmlentities')
 require_relative('course')
+require_relative('parameter')
 
 class Achiever
   def initialize
-    @WORKFLOW_ID = ENV['ACHIEVER_FUTURE_COURSES_WORKFLOW_ID']
     @DB_CONNECTION_ID = ENV['ACHIEVER_DB_CONNECTION_ID']
     @uri = URI(ENV['ACHIEVER_API_ENDPOINT'])
   end
 
-  def fetchFutureCourses
+  def runWorkflow(workflowId, params = [])
+
     builder = Nokogiri::XML::Builder.new do |xml|
       xml.Achiever {
         xml.Command {
           xml.CommandType 'ExternalRunWorkflow'
-          xml.WorkflowId @WORKFLOW_ID
+          xml.WorkflowId workflowId
           xml.ReturnSchemaWithXmlData 1
           xml.Identity {
             xml.DbConnectionId @DB_CONNECTION_ID
           }
-          xml.Parameters {
-            xml.Programme 'NCCE'
-          }
+          xml.Parameters
         }
       }
+    end
+
+    parameters = builder.doc.xpath('//Parameters')
+    params.each do |param|
+      parameters[0].add_child(param.to_node)
     end
 
     @uri.query = URI.encode_www_form({ :sXmlParams => builder.to_xml })
 
     res = Net::HTTP.get_response(@uri)
     doc = parseStringFromXml(Nokogiri::XML(res.body))
-    results = doc.xpath('//AchieverDataResult_1/Detail')
+    doc.xpath('//AchieverDataResult_1/Detail')
+  end
+
+  def fetchFutureCourses
+    programme = Parameter.new('Programme', 'NCCE')
+
+    results = self.runWorkflow(ENV['ACHIEVER_FUTURE_COURSES_WORKFLOW_ID'], [programme])
 
     courses = Array.new
     results.each do |result|
       courses << Course.new(result)
     end
     courses
+  end
+
+  def fetchCourseTemplate(id)
+    courseTemplateNo = Parameter.new('CourseTemplateNo', id)
+
+    self.runWorkflow(ENV['ACHIEVER_COURSE_TEMPLATE_WORKFLOW_ID'], [courseTemplateNo])
+  end
+
+  def fetchCourseOccurrence(id)
+    courseOccurrenceNo = Parameter.new('CourseOccurrenceNo', id)
+
+    self.runWorkflow(ENV['ACHIEVER_COURSE_OCCURRENCE_WORKFLOW_ID'], [courseOccurrenceNo])
   end
 
   def parseStringFromXml(doc)
