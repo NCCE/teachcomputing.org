cc5ec5a7f299269aaef41a3d825a34a33e54b683
Magda <magdalena@raspberrypi.org>
2018-12-05 15:06:42 +0000

    Keep the same name convention

--- spec/views/home/_stay_updated_spec.rb
+++ spec/views/home/_stay_updated_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe('home/_stay_updated', type: :view) do
   end
 
   it 'has a title' do
-    expect(rendered).to have_css('.ncce-stay_updated', text: 'Stay up to date with the NCCE')
+    expect(rendered).to have_css('.ncce-stay-updated', text: 'Stay up to date with the NCCE')
   end
 
   it('renders the correct number of cards') do
-    expect(rendered).to(have_css('.ncce-stay_updated__panel', count: 3))
+    expect(rendered).to(have_css('.ncce-stay-updated__panel', count: 3))
   end
 
 end
