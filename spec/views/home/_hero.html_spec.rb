90e45c8570055bbd4c77cd87454ce78f876a06d8
Magda <magdalena@raspberrypi.org>
2018-12-05 11:10:39 +0000

    Test Stay up to date section

--- spec/views/home/_hero.html_spec.rb
+++ spec/views/home/_hero.html_spec.rb
@@ -5,7 +5,15 @@ RSpec.describe('home/_hero', type: :view) do
     render
   end
 
+  it 'has a title' do
+    expect(rendered).to have_css('.c-hero', text: 'Helping you teach computing')
+  end
+
   it 'button to find out more about NCCE' do
     expect(rendered).to have_css('.c-hero--button', text: 'About the NCCE')
+  end
+
+  it 'links to the About section' do
+    expect(rendered).to have_link('About the NCCE', href: '#'))
   end
 end
