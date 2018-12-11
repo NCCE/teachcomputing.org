94e744002f2a0331e7f447bff76789768e76a8ec
Daniel Brierton <daniel@brierton.me>
2018-12-11 11:34:48 +0000

    Added final test for Acheiver API (#30)

--- spec/lib/achiever_spec.rb
+++ spec/lib/achiever_spec.rb
@@ -3,6 +3,23 @@ require('htmlentities')
 
 RSpec.describe Achiever do
   describe 'Achiever' do
+    describe 'runWorkflow' do
+      it 'runs the given workflow with the given params' do
+        # ARRANGE
+        achiever = Achiever.new
+        raw_future_courses_xml = File.new('spec/support/achiever_api/future_courses.xml')
+        stub_request(:get, "https://bookingsystem-dev.stem.org.uk:8080/WorkflowAPI/Dev/_services/workflowservice.asmx/ExecuteWorkflow?sXmlParams=%3C?xml%20version=%221.0%22?%3E%0A%3CAchiever%3E%0A%20%20%3CCommand%3E%0A%20%20%20%20%3CCommandType%3EExternalRunWorkflow%3C/CommandType%3E%0A%20%20%20%20%3CWorkflowId%3ESOME_FUTURE_COURSES_WORKFLOW_ID%3C/WorkflowId%3E%0A%20%20%20%20%3CReturnSchemaWithXmlData%3E1%3C/ReturnSchemaWithXmlData%3E%0A%20%20%20%20%3CIdentity%3E%0A%20%20%20%20%20%20%3CDbConnectionId%3ESOME_DB_CONNECTION_ID%3C/DbConnectionId%3E%0A%20%20%20%20%3C/Identity%3E%0A%20%20%20%20%3CParameters%3E%0A%20%20%20%20%20%20%3CProgramme%3ENCCE%3C/Programme%3E%0A%20%20%20%20%3C/Parameters%3E%0A%20%20%3C/Command%3E%0A%3C/Achiever%3E%0A")
+          .to_return(raw_future_courses_xml)
+        programme = Parameter.new('Programme', 'NCCE')
+
+        # ACT
+        res = achiever.runWorkflow('SOME_FUTURE_COURSES_WORKFLOW_ID', [programme])
+
+        # ASSERT
+        expect(res[0].xpath('.//Activity.Activity_Code')[0].content).to eq('CP202')
+      end
+    end
+
     describe 'fetchCourseTemplate' do
       it 'runs the course occurrence workflow with the correct param' do
         # ARRANGE
