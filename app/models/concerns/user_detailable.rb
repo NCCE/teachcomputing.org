6809b34892d4936ae228e3f7429a9a884f224dfa
Damien Hogan <damienhogan87@gmail.com>
2019-01-03 14:22:09 +0000

    Tweak to details method to ensure we dont see rollbacks due to validation failing

--- app/models/concerns/user_detailable.rb
+++ app/models/concerns/user_detailable.rb
@@ -4,11 +4,6 @@ module UserDetailable
   extend ActiveSupport::Concern
 
   def details
-    UserDetail.find_or_create_by(stem_user_id: id,
-                                 stem_achiever_contact_no: stem_achiever_contact_no,
-                                 stem_credentials_access_token: stem_credentials_access_token,
-                                 stem_credentials_refresh_token: stem_credentials_refresh_token,
-                                 stem_credentials_expires_at: stem_credentials_expires_at)
+    UserDetail.find_or_create_by(stem_user_id: id)
   end
-
 end
