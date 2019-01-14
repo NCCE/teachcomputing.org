e2dc917e7536a11d89d9708f8b73def743c7ed58
Daniel Brierton <daniel@brierton.me>
2019-01-14 14:38:35 +0000

    Block logged out users from accessing dashboard and courses

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,13 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
+  let(:user) { create(:user) }
+
   describe 'GET #show' do
-    before do
-      get :show
+    describe 'while logged in' do
+      before do
+        allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
+        get :show
+      end
+
+      it 'renders the correct template' do
+        expect(response).to render_template('show')
+      end
     end
 
-    it 'renders the correct template' do
-      expect(response).to render_template('show')
+    describe 'while logged out' do
+      before do
+        get :show
+      end
+
+      it 'should redirect to login' do
+        expect(response).to redirect_to(login_path)
+      end
     end
   end
 end
