bbd8672c8c85e7b93862ab121cbb8798e77f3810
Magda <magdalena@raspberrypi.org>
2019-01-15 12:16:40 +0000

    Test Feedback component

---
+++ spec/views/components/_feedback_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe('components/_feedback', type: :view) do
+  before do
+    render
+  end
+
+  it('renders the feedback banner') do
+    expect(rendered).to(have_css('.ncce-feedback'))
+  end
+
+  it 'text to send feedback note' do
+    expect(rendered).to have_css('.ncce-fedback__link', text: 'feedback')
+  end
+
+  it 'links to the feedback mail' do
+    expect(rendered).to have_link('feedback', href: 'mailto:info@teachcomputing.org')
+  end
+
+end
