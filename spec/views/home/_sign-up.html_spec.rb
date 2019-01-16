d34a108a0c7f83387e62001be10173d7a8218899
MagdalenaJadach <magdalena@raspberrypi.org>
2019-01-16 15:56:15 +0000

    Check and update Footer links (#68)
    * Sign up link fix
    
    * Update About aside title
    
    * Fix test
    
    * Remove Jobs from the Footer as it's dead link

--- spec/views/home/_sign-up.html_spec.rb
+++ spec/views/home/_sign-up.html_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe('home/_sign-up', type: :view) do
   end
 
   it 'links to the Sign up section' do
-    expect(rendered).to have_link('Sign up', href: '#')
+    expect(rendered).to have_link('Sign up', href: '/login')
   end
 
   it('renders the correct number of bullets') do
