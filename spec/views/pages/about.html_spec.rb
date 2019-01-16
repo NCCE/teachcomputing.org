37c5567d1aaa4e2545a760a8f6e4d563039b12d6
Magda <magdalena@raspberrypi.org>
2019-01-16 09:13:24 +0000

    Swap the order of Footer links

---
+++ spec/views/pages/about.html_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe('pages/about', type: :view) do
+  before do
+    render
+  end
+
+  it 'has a title' do
+    expect(rendered).to have_css('.ncce-login__title', text: 'Sign up')
+  end
+
+  it 'button to sign up' do
+    expect(rendered).to have_css('.ncce-login__button', text: 'Sign up')
+  end
+
+  it 'links to the Stem account' do
+    expect(rendered).to have_link('Sign up', href: signup_stem_path)
+  end
+end
