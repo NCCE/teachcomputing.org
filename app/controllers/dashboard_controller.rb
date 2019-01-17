8416b45bf3e5502cf6aa9e59ddf0c30479809bb3
Daniel Brierton <daniel@brierton.me>
2019-01-17 16:46:34 +0000

    Added filter for courses and mock data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,10 @@ class DashboardController < ApplicationController
 
   def show
     @achievements = current_user.achievements
+    t = Time.now
+    current_date = t.iso8601
     @delegate_course_list = DelegateCourseList.fromAchiever(current_user.stem_achiever_contact_no)
+      .select { |delegate_course| delegate_course.enrolment_status != 'Cancelled' && delegate_course.end_date < current_date }
 
     render :show
   end
