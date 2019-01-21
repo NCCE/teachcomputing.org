ee0e3953399f080464cbc55af64779d9a5e34346
Glen Pike <github@glenpike.co.uk>
2019-01-21 14:10:43 +0000

    Change copy on 'Login' page for 'Create an account' section.

--- spec/views/pages/login.html_spec.rb
+++ spec/views/pages/login.html_spec.rb
@@ -10,10 +10,10 @@ RSpec.describe('pages/login', type: :view) do
   end
 
   it 'button to sign up' do
-    expect(rendered).to have_css('.ncce-login__button', text: 'Sign up')
+    expect(rendered).to have_css('.ncce-login__button', text: 'Create an account')
   end
 
   it 'links to the Stem account' do
-    expect(rendered).to have_link('Sign up', href: signup_stem_path)
+    expect(rendered).to have_link('Create an account', href: signup_stem_path)
   end
 end
