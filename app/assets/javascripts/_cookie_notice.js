b46b6f86e1ac68a879666a13af1fd31f0bee015d
Daniel Brierton <daniel@brierton.me>
2019-01-22 14:44:00 +0000

    Refactor ncce-link class and update views for consistent focus styles

--- app/assets/javascripts/_cookie_notice.js
+++ app/assets/javascripts/_cookie_notice.js
@@ -4,7 +4,7 @@ window.addEventListener('load', function() {
       showLink: false,
       autoOpen: true,
       overrideHTML:
-        "<div id='global-cookie-message'><div class='cookie-inner'><p class='govuk-body'>We use cookies to make the site simpler. <a class='govuk-link ncce-link__on-light' href='/privacy'>Learn more</a></p> <a class='cc-btn cc-dismiss govuk-link ncce-link__on-light' href=''>OK</a></div></div>",
+        "<div id='global-cookie-message'><div class='cookie-inner'><p class='govuk-body'>We use cookies to make the site simpler. <a class='ncce-link ncce-link--on-dark' href='/privacy'>Learn more</a></p> <a class='cc-btn cc-dismiss ncce-link ncce-link--on-dark' href=''>OK</a></div></div>",
       position: 'top',
     })
   })
