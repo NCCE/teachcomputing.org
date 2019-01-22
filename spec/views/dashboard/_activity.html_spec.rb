251386ec24d908bffc910c284a42029993b07b6a
Magda <magdalena@raspberrypi.org>
2019-01-22 17:00:05 +0000

    Fix test

--- spec/views/dashboard/_activity.html_spec.rb
+++ spec/views/dashboard/_activity.html_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe('dashboard/_activity', type: :view) do
   end
 
   it 'has the signed up item' do
-    expect(rendered).to have_css('.ncce-activity-list__item', text: 'Signed up to the National Centre for Computing Education')
+    expect(rendered).to have_css('.ncce-activity-list__item', text: 'Signed up to the NCCE')
   end
 
   it 'has the mock course' do
