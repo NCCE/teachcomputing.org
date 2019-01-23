daf7873e0ef8321db6f18dc15f067e3e10758ec1
Glen Pike <github@glenpike.co.uk>
2019-01-23 12:30:25 +0000

    Add social media links/icons for Twitter & Facebook. (#129)
    To home page 'Mailing List' section and Footer.
    Add tests.

--- spec/views/pages/home/_mailing.html_spec.rb
+++ spec/views/pages/home/_mailing.html_spec.rb
@@ -17,4 +17,20 @@ RSpec.describe('pages/home/_mailing', type: :view) do
     expect(rendered).to have_css('.ncce-stay-in-touch__input', count: 2)
   end
 
+  it 'has a twitter link' do
+    expect(rendered).to have_link('Follow us on Twitter', href: /twitter.com\/WeAreComputing/)
+  end
+
+  it 'has a facebook link' do
+    expect(rendered).to have_link('Like us on Facebook', href: /facebook.com\/WeAreComputing/)
+  end
+
+  it 'has a twitter icon' do
+    expect(rendered).to have_xpath('//img[contains(@class, "ncce-link__icon")][contains(@alt, "Twitter logo")]')
+  end
+  
+  it 'has a facebook icon' do
+    expect(rendered).to have_xpath('//img[contains(@class, "ncce-link__icon")][contains(@alt, "Facebook logo")]')
+  end
+
 end
