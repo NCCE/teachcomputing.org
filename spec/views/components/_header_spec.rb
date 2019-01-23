d1cc9a1583607a4edf4050e4e070fbb65f12d91f
Glen Pike <github@glenpike.co.uk>
2019-01-23 16:54:17 +0000

    Replace <a> tags with link_to: header component

--- spec/views/components/_header_spec.rb
+++ spec/views/components/_header_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe('components/_header', type: :view) do
   let(:user) { create(:user) }
 
+  it 'has a link to the home page ' do
+    render
+    expect(rendered).to have_link('National Centre forComputing Education', href: '/')
+  end
+
   context 'when a user is signed in' do
     before do
       allow(view).to receive(:current_user).and_return(user)
@@ -11,6 +16,10 @@ RSpec.describe('components/_header', type: :view) do
 
     it 'shows a link to your dashboard' do
       expect(rendered).to have_css('.govuk-header__navigation-item', text: 'Your dashboard')
+    end
+
+    it 'shows a link to your dashboard' do
+      expect(rendered).to have_css('.govuk-header__navigation-item', text: 'Edit profile')
     end
 
     it 'shows a link to logout' do
