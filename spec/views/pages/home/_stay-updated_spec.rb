eea9136457c8f2c11d27297647a7c515458f39ef
Glen Pike <github@glenpike.co.uk>
2019-01-24 11:19:17 +0000

    Replace <a> tags with link_to: home page 'news'

--- spec/views/pages/home/_stay-updated_spec.rb
+++ spec/views/pages/home/_stay-updated_spec.rb
@@ -9,4 +9,7 @@ RSpec.describe('pages/home/_stay-updated', type: :view) do
     expect(rendered).to(have_css('.ncce-stay-updated__panel', count: 2))
   end
 
+  it 'has 2 news links' do
+    expect(rendered).to have_xpath('//a[contains(@class, "ncce-link")][contains(@href, "/news")]', count: 2)
+  end
 end
