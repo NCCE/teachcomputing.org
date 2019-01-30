b4d4c319bcca6e8538a658b2fb83117622dccc6f
Damien Hogan <damienhogan87@gmail.com>
2019-01-30 14:30:52 +0000

    ensure pool size is set for puma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,8 @@ Rails.application.configure do
                          failover: true,
                          socket_timeout: 1.5,
                          socket_failure_delay: 0.2,
-                         down_retry_delay: 60 }
+                         down_retry_delay: 60,
+                         pool_size: 5 }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
