5a3ea41bf064060f48d3d7d7df29f8bcc5b13612
Glen Pike <github@glenpike.co.uk>
2019-02-08 16:04:02 +0000

    Deploy sitemap to S3 when app is released to staging & production
    * Change release task to generate the sitemap. https://devcenter.heroku.com/articles/release-phase#specifying-release-phase-tasks
    * Change the SitemapGenerator config to use an S3 bucket based on this wiki article: https://github.com/kjvarga/sitemap_generator/wiki/Generate-Sitemaps-on-read-only-filesystems-like-Heroku
    * Add AWS variable names to app.json as required

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec puma -C config/puma.rb
-release: bin/rails db:migrate
+release: bash ./release-tasks.sh
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,13 @@
 SitemapGenerator::Sitemap.default_host = 'https://teachcomputing.org'
-SitemapGenerator::Sitemap.create_index = true
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
+                                                                    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                                                    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+                                                                    fog_directory: ENV['AWS_S3_BUCKET'],
+                                                                    fog_region: ENV['AWS_S3_REGION'])
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3-#{ENV['AWS_S3_REGION']}.amazonaws.com/#{ENV['AWS_S3_BUCKET']}/"
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+SitemapGenerator::Sitemap.create_index = false
 SitemapGenerator::Sitemap.create do
   add '/', changefreq: 'daily'
   add '/about', changefreq: 'monthly'
