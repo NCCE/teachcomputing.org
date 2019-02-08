eaffd15269d6f1889c70cddbca144b929bb6a1d0
Damien Hogan <damienhogan87@gmail.com>
2019-02-08 10:33:00 +0000

    rubocop fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
-Sidekiq.default_worker_options = { :retry => false }
+Sidekiq.default_worker_options = { retry: false }
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => ENV['REDIS_URL'], :size => 2 }
+  config.redis = { url: ENV['REDIS_URL'], size: 2 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { :url => ENV['REDIS_URL'], :size => 20 }
+  config.redis = { url: ENV['REDIS_URL'], size: 20 }
 end
