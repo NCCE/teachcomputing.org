d23377efaa00b3088a170a1f575dd0b8aab41d80
Magdalena Jadach <magdalena@raspberrypi.org>
2019-02-08 15:29:58 +0000

    update homepage tests

--- spec/views/pages/home/_sign-up.html_spec.rb
+++ spec/views/pages/home/_sign-up.html_spec.rb
@@ -1,20 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe('pages/home/_sign-up', type: :view) do
+  let(:user) { create(:user) }
+
   before do
     render
   end
 
-  it 'has a title' do
-    expect(rendered).to have_css('.ncce-sign-up', text: 'Create an account')
-  end
-
-  it 'links to the Sign up section' do
-    expect(rendered).to have_link('Create an account', href: '/login')
-  end
-
   it('renders the correct number of bullets') do
     expect(rendered).to(have_css('.ncce-sign-up__list--item', count: 3))
+  end
+
+  context 'when a user is signed in' do
+    before do
+      allow(view).to receive(:current_user).and_return(user)
+      render
+    end
+
+    it 'has a title' do
+      expect(rendered).to have_css('.ncce-sign-up', text: 'Visit your dashboard')
+    end
+
+    it 'links to the Sign up section' do
+      expect(rendered).to have_link('Visit your dashboard', href: '/dashboard')
+    end
+  end
+
+  context 'when there is not signed in user' do
+    before do
+      allow(view).to receive(:current_user).and_return(nil)
+      render
+    end
+    it 'has a title' do
+      expect(rendered).to have_css('.ncce-sign-up', text: 'Create an account')
+    end
+
+    it 'links to the Sign up section' do
+      expect(rendered).to have_link('Create an account', href: '/login')
+    end
+
   end
 
 end
