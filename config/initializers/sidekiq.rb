96386a0756cfa76baaea2bf5588eb828616fd31a
Damien Hogan <damienhogan87@gmail.com>
2019-02-08 10:30:43 +0000

    remove production conditional from sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,9 @@
-if Rails.env.production?
+Sidekiq.default_worker_options = { :retry => false }
 
-  Sidekiq.default_worker_options = { :retry => false }
+Sidekiq.configure_client do |config|
+  config.redis = { :url => ENV['REDIS_URL'], :size => 2 }
+end
 
-  Sidekiq.configure_client do |config|
-    config.redis = { :url => ENV['REDIS_URL'], :size => 2 }
-  end
-
-  Sidekiq.configure_server do |config|
-    config.redis = { :url => ENV['REDIS_URL'], :size => 20 }
-  end
+Sidekiq.configure_server do |config|
+  config.redis = { :url => ENV['REDIS_URL'], :size => 20 }
 end
