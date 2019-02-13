9e4f4477df4d4c82b0731c6e23f82034d8495f35
Magdalena Jadach <magdalena@raspberrypi.org>
2019-02-13 15:53:09 +0000

    Update test

--- spec/views/pages/home/_jobs.html_spec.rb
+++ spec/views/pages/home/_jobs.html_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe('pages/home/_jobs', type: :view) do
   end
 
   it('renders the correct number of links') do
-    expect(rendered).to(have_css('.ncce-link', count: 3))
+    expect(rendered).to(have_css('.ncce-link', count: 4))
   end
 
   it('has hub link') do
