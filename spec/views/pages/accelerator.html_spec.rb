28edbf796b9a2ded185c6c85f9c8a52ba3bebb1d
Magdalena Jadach <magdalena@raspberrypi.org>
2019-02-21 17:29:00 +0000

    Fix tests by passing correct CTA copy

--- spec/views/pages/accelerator.html_spec.rb
+++ spec/views/pages/accelerator.html_spec.rb
@@ -13,27 +13,10 @@ RSpec.describe('pages/accelerator', type: :view) do
     expect(rendered).to have_link('Read about our bursaries', href: '/bursary')
   end
 
-  context 'when a user is signed in' do
-    before do
-      allow(view).to receive(:current_user).and_return(user)
-      render
-    end
 
-    it 'has no link' do
-      expect(rendered).to have_text('Log in or create an account to see our face to face courses', count: 0)
-    end
-
+  it 'has a link to the courses' do
+    expect(rendered).to have_text('Browse available courses', href: '/courses')
   end
 
-  context 'when there is not signed in user' do
-    before do
-      allow(view).to receive(:current_user).and_return(nil)
-      render
-    end
-
-    it 'has a login link' do
-      expect(rendered).to have_link('Log in or create an account to see our face to face courses', href: '/login')
-    end
-  end
 
 end
