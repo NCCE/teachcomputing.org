cf55589f6a1e0e5a26ba721cf05422462cd3c072
Greg Annandale <greg@gregannandale.com>
2019-02-27 10:27:35 +0000

    Add fonts.gstatic.com to font-src (#180)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,7 +6,7 @@
 
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :self, :https
-  policy.font_src    :self, :https, :data
+  policy.font_src    :self, :https, :data, 'fonts.gstatic.com'
   policy.img_src     :self, :https, :data
   policy.object_src  :none
   policy.script_src  :self, :https, 'www.google-analytics.com', 'www.googletagmanager.com', 'tagmanager.google.com'
