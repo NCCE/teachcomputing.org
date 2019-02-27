330ff2d58fea02505f2e7bdd07afa6f52c47d724
Glen Pike <github@glenpike.co.uk>
2019-02-27 11:02:40 +0000

    Revert optimisation of loops

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,15 +6,12 @@ class CoursesController < ApplicationController
   def index
     @courses = @achiever.approved_course_templates
     course_occurrences = @achiever.future_courses
-    course_occurrence_hash = {}
-    course_occurrences.each do |course_occurrence|
-      if course_occurrence_hash[course_occurrence.course_template_no].nil?
-        course_occurrence_hash[course_occurrence.course_template_no] = []
-      end
-      course_occurrence_hash[course_occurrence.course_template_no].push(course_occurrence)
-    end
     @courses.each do |course|
-      course.course_occurrences = course_occurrence_hash[course.course_template_no] || []
+      course_occurrences.each do |course_occurrence|
+        if course_occurrence.course_template_no == course.course_template_no
+          course.course_occurrences.push(course_occurrence)
+        end
+      end
     end
     render :index
   end
