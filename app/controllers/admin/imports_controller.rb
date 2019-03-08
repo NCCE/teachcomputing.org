f3d781357f2f04b67d4284fc38b7d5348abb87bf
Damien Hogan <damienhogan87@gmail.com>
2019-03-08 15:15:45 +0000

    Ensure that the import route checks for CF_Authorization token

--- app/controllers/admin/imports_controller.rb
+++ app/controllers/admin/imports_controller.rb
@@ -1,22 +1,42 @@
 class Admin::ImportsController < ApplicationController
   before_action :set_activity, only: %i[new create]
+  before_action :set_admin, only: %i[new create]
+
+  def index
+    @activities = Activity.future_learn
+  end
 
   def new; end
 
   def create
     import = Import.new(activity_id: params[:activity_id],
                         provider: params[:provider],
-                        triggered_by: 'Damien')
+                        triggered_by: params[:triggered_by])
+                        
     if import.save
       ProcessFutureLearnCsvExportJob.perform_later(@activity, params[:csv_file].read.to_s, import)
       flash[:notice] = "CSV Import for #{@activity.title} has been scheduled"
     else
-      flash[:error] = import.errors.inspect.to_s
+      flash[:error] = 'Whoops something went wrong'
     end
     redirect_to admin_imports_path
   end
 
   private
+
+  def set_admin
+    if cookies[:CF_Authorization].nil?
+      flash[:error] = 'Whoops something went wrong'
+      redirect_to root_path
+    else
+      decoded_token = decode_cookie(cookies[:CF_Authorization])
+      @admin_email = decoded_token.first['email']
+    end
+  end
+
+  def decode_cookie(token)
+    JWT.decode token, nil, false
+  end
 
   def set_activity
     @activity = Activity.find_by(id: params[:activity_id])
