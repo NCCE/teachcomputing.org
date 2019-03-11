a7df5f4fe79bc2b26e6458d09e9d231ee4ffeffe
Damien Hogan <damienhogan87@gmail.com>
2019-03-11 15:40:14 +0000

    update content

--- spec/views/pages/home/_news-and-updates_spec.rb
+++ spec/views/pages/home/_news-and-updates_spec.rb
@@ -8,8 +8,4 @@ RSpec.describe('pages/home/_news-and-updates', type: :view) do
   it('renders the correct number of cards') do
     expect(rendered).to(have_css('.ncce-news-and-updates__panel', count: 2))
   end
-
-  it 'has 2 news links' do
-    expect(rendered).to have_xpath('//a[contains(@class, "ncce-link")][contains(@href, "/news")]', count: 2)
-  end
 end
