ab45c6b69e5a6f74c8e69981da4443d7319ad34c
Magdalena Jadach <magdalena@raspberrypi.org>
2019-03-14 16:48:50 +0000

    Update the form styling, remove extra css, clean up the codebase

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -2,12 +2,12 @@ function addAcourse() {
   const activity = document.querySelectorAll(
     'p.ncce-activity-list__item-text-completed');
   Array.prototype.forEach.call(activity, h => {
-    let btn = h.querySelector('button');
+    let button = h.querySelector('button');
     let target = h.nextElementSibling;
 
-    btn.onclick = () => {
-      let expanded = btn.getAttribute('aria-expanded') === 'true';
-      btn.setAttribute('aria-expanded', !expanded);
+    button.onclick = () => {
+      let expanded = button.getAttribute('aria-expanded') === 'true';
+      button.setAttribute('aria-expanded', !expanded);
       target.hidden = expanded;
     }
   });
