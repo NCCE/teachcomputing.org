d54e1567906f338b7c879f7403481239321c49d0
Damien Hogan <damienh@users.noreply.github.com>
2019-03-14 09:22:58 +0000

    ensure connection size cannot go over 20 connection size for hobby plan (#202)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,5 +5,5 @@ Sidekiq.configure_client do |config|
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], size: 20 }
+  config.redis = { url: ENV['REDIS_URL'], size: 15 }
 end
