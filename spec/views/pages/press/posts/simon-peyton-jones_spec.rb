49bd8d23b7bf1c01a9e64e3e4864922047e17cce
Glen Pike <github@glenpike.co.uk>
2019-03-15 17:05:50 +0000

    264: Add press release

---
+++ spec/views/pages/press/posts/simon-peyton-jones_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe('pages/press/posts/simon-peyton-jones', type: :view) do
+  before do
+    render
+  end
+
+  it 'has a title' do
+    expect(rendered).to have_css('.govuk-heading-l', text: 'Top computer scientist chosen to lead National Centre for Computing Education')
+  end
+
+  it 'notes to editors' do
+    expect(rendered).to have_css('.govuk-heading-s', text: 'Notes to editors:')
+  end
+end
