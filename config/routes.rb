b3bf01ab3a5c05bec585f955912196d6b220f935
Glen Pike <github@glenpike.co.uk>
2019-03-19 10:01:30 +0000

    83 employers info page (#204)
    Add employers info page - '/get-involved' - and test
    Change link on About Us page to point to this and fix tests

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   get '/bursary', to: 'pages#page', as: :bursary, defaults: { page_slug: 'bursary' }
   get '/certification', to: 'pages#page', as: :certification, defaults: { page_slug: 'certification' }
   get '/contact', to: 'pages#page', as: :contact, defaults: { page_slug: 'contact' }
+  get '/get-involved', to: 'pages#page', as: :get_involved, defaults: { page_slug: 'get-involved' }
   get '/login', to: 'pages#login', as: :login
   get '/logout', to: 'auth#logout', as: :logout
   get '/offer', to: 'pages#page', as: :offer, defaults: { page_slug: 'offer' }
