50f84adcf1146d46d58a9d6ede7e3f37f9b5076b
Glen Pike <github@glenpike.co.uk>
2019-03-20 19:56:21 +0000

    204: Tweak collapsible section HTML/CSS/JS
    Add a title 'button' to the collapsible activity and allow it to close the box.
    Tweak HTML and classnames.
    Make the JavaScript treat the collapsible activity as a single item rather than multiple.
    Nest SCSS classes for the components.
    Add SCSS for responsive behaviour.
    Show the collapsible activity box by default - if JS, then hide.
    Fix / add tests.

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,18 +1,27 @@
-function addAcourse() {
-  const activity = document.querySelectorAll(
-    'p.ncce-activity-list__item-text-completed');
-  Array.prototype.forEach.call(activity, h => {
-    let button = h.querySelector('button');
-    let target = h.nextElementSibling;
+function initialiseCollapsibleActivity() {
+  const activity = document.querySelector(
+    '.ncce-activity-links--add')
 
-    button.onclick = () => {
-      let expanded = button.getAttribute('aria-expanded') === 'true';
-      button.setAttribute('aria-expanded', !expanded);
-      target.hidden = expanded;
-    }
-  });
+  const button = activity.querySelector('button')
+  const target = activity.nextElementSibling
+  const hideButton = target.querySelector('.ncce-collapsible-activity__title-button')
+
+  const wrapper = document.querySelector(
+    '.ncce-activity-links')
+  wrapper.classList.toggle('ncce-activity-links--progressive')
+
+  target.classList.toggle('ncce-collapsible-activity--progressive')
+
+  const listener = () => {
+    const expanded = button.getAttribute('aria-expanded') === 'true'
+    button.setAttribute('aria-expanded', !expanded)
+    target.hidden = expanded
+  }
+  button.onclick = listener
+  hideButton.onclick = listener
+  listener()
 }
 
 ready(function () {
-  addAcourse()
+  initialiseCollapsibleActivity()
 })
