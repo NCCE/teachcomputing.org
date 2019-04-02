7459ccee5edcebe1a04d1896b4e63d4f0ac9214b
Glen Pike <github@glenpike.co.uk>
2019-04-02 16:57:59 +0100

    299: Refactor Ghost API stuff and test.
    Handle errors - TODO: remove debug &  use Raven

---
+++ app/services/ghost.rb
@@ -0,0 +1,36 @@
+require('rest-client')
+
+class Ghost
+  def initialize
+    @ghost_api_endpoint = ENV['GHOST_API_ENDPOINT']
+    @ghost_api_path = '/ghost/api/v2/'
+    @ghost_api_key = ENV['GHOST_CONTENT_API_KEY']
+  end
+
+  def get_featured_posts(how_many = 2)
+    request = api_url_prefix(:posts)
+    params = {
+      key: @ghost_api_key,
+      filter: 'featured:true',
+      limit: how_many,
+      fields: 'title,url,feature_image,custom_excerpt,published_at'
+    }
+
+    begin
+      result = RestClient.get(request, params: params).body
+
+      featured_posts = ActiveSupport::JSON.decode(result)
+
+      return featured_posts['posts']
+    rescue RestClient::Exception => err
+      puts "Rest Error from Ghost API #{err.inspect}"
+    rescue ActiveSupport::JSON.parse_error
+      puts "JSON Parse error for string: #{result}"
+    end
+    []
+  end
+
+  def api_url_prefix(endpoint)
+    "#{@ghost_api_endpoint}/content/#{endpoint}/"
+  end
+end
--- spec/views/pages/home/_news-and-updates_spec.rb
+++ spec/views/pages/home/_news-and-updates_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe('pages/home/_news-and-updates', type: :view) do
-  before do
-    render
+  context 'has featured posts' do
+    before do
+      stub_featured_posts
+      @featured_posts = Ghost.new.get_featured_posts
+      render
+    end
+
+    it('renders the correct number of cards') do
+      expect(rendered).to(have_css('.ncce-news-and-updates__panel', count: 2))
+    end
   end
 
-  it('renders the correct number of cards') do
-    expect(rendered).to(have_css('.ncce-news-and-updates__panel', count: 2))
+  context 'has 0 posts' do
+    before do
+      stub_featured_posts_error
+      @featured_posts = Ghost.new.get_featured_posts
+      render
+    end
+
+    it('renders the correct number of cards') do
+      expect(rendered).to(have_css('.ncce-news-and-updates__panel', count: 0))
+    end
   end
 end
