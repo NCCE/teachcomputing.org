c6c1f18060363110ace4a0a59154e49d71e34c51
Damien Hogan <damienhogan87@gmail.com>
2019-04-02 16:07:58 +0100

    remove cors from individual environments

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -37,10 +37,6 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-  config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => 'https://blog.teachcomputing.org',
-    'Access-Control-Request-Method' => %w{GET OPTIONS}.join(",")
-  }
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
