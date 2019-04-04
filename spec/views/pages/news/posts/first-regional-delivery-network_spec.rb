b8562bbf6ef912042cef07725e0e57bf9a93a362
Magdalena Jadach <magdalena@raspberrypi.org>
2019-04-04 11:19:11 +0100

    Fix test

--- spec/views/pages/news/posts/first-regional-delivery-network_spec.rb
+++ spec/views/pages/news/posts/first-regional-delivery-network_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe('pages/news/posts/first-regional-delivery-network', type: :view) 
     expect(rendered).to have_css('.govuk-heading-l', text: 'Boost in computing support for local schools')
   end
 
-  it 'has a link to the courses page' do
-    expect(rendered).to have_link('https://teachcomputing.org/courses', href: 'https://teachcomputing.org/courses')
+  it 'has a link to the courses' do
+    expect(rendered).to have_link('https://teachcomputing.org/courses', href: '/courses')
   end
 end
