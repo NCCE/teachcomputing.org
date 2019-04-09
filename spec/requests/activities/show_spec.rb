99e8adf9c168d17cf2909fa447b6737469a7a0ce
Glen Pike <github@glenpike.co.uk>
2019-04-09 17:02:19 +0100

    308: Don't allow access to activity downoads unless logged in, with tests.

--- spec/requests/activities/show_spec.rb
+++ spec/requests/activities/show_spec.rb
@@ -5,13 +5,29 @@ RSpec.describe Activities::DownloadsController do
   let(:activity) { create(:activity, :diagnostic_tool) }
 
   describe 'GET show' do
-    before do
-      allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
-      get activities_download_path(activity.id, :file => { name: 'NCCE.Diagnostic.pdf'})
+    describe 'while logged in' do
+      before do
+        allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
+        get activities_download_path(activity.id, :file => { name: 'NCCE.Diagnostic.pdf'})
+      end
+
+      it 'creates an Achievement if one does not exist already' do
+        expect(Achievement.count).to eq 1
+      end
     end
 
-    it 'creates an Achievement if one does not exist already' do
-      expect(Achievement.count).to eq 1
+    describe 'while logged out' do
+      before do
+        get activities_download_path(activity.id, :file => { name: 'NCCE.Diagnostic.pdf'})
+      end
+
+      it 'should redirect to login' do
+        expect(response).to redirect_to(login_path)
+      end
+
+      it 'creates an Achievement if one does not exist already' do
+        expect(Achievement.count).to eq 0
+      end
     end
   end
 end
