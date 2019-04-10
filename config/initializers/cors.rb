e5533c6233e5a30b9cd1ead1d9b771002b9c6344
Damien Hogan <damienhogan87@gmail.com>
2019-04-10 13:36:39 +0100

    add head method

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource(
       '*',
       headers: :any,
-      methods: %i[get options]
+      methods: %i[get options head]
     )
   end
 end
