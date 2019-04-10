a3a43135cc3dae8f6ee9a646204e0223be09131e
Damien Hogan <damienhogan87@gmail.com>
2019-04-10 11:39:39 +0100

    Set blog instead of wildcard

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://*.teachcomputing.org'
+    origins 'https://blog.teachcomputing.org'
 
     resource(
       '*',
