de7c15ce16332c1488dca7f35c31d7138fde9c2c
Damien Hogan <damienhogan87@gmail.com>
2019-04-10 13:41:40 +0100

    revert back to allowing all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource(
       '*',
       headers: :any,
-      methods: %i[get options head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
     )
   end
 end
