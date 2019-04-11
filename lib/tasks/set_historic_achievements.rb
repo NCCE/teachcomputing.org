5293eef01fe52a0953e1f2e3d1a0c09bc5ec4a15
Damien Hogan <damienhogan87@gmail.com>
2019-04-11 15:40:07 +0100

    Ensure that the created_at timestamp matches the timestamp when the user record was created.

--- lib/tasks/set_historic_achievements.rb
+++ lib/tasks/set_historic_achievements.rb
@@ -2,7 +2,8 @@ namespace :set_historic_achievement do
   task registered_with_ncce: :environment do
     User.all.each do |user|
       Achievement.create(user_id: user.id,
-                         activity_id: Activity.registered_with_the_national_centre.id)
+                         activity_id: Activity.registered_with_the_national_centre.id,
+                         created_at: user.created_at)
     end
   end
 end
