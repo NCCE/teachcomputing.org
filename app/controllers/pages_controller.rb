e27558c0fe7e847cef307421718a2c8ab6f65807
Damien Hogan <damienh@users.noreply.github.com>
2019-04-26 09:45:46 +0100

    remove feature flag and static press and news content (#252)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,8 +11,7 @@ class PagesController < ApplicationController
   end
 
   def home
-    @ghost_enabled = ghost_enabled?
-    @featured_posts = Ghost.new.get_featured_posts(ENV['GHOST_LIMIT_FEATURED_POSTS']) if @ghost_enabled
+    @featured_posts = Ghost.new.get_featured_posts(ENV['GHOST_LIMIT_FEATURED_POSTS'])
     render template: 'pages/home/index'
   end
 
