5a8a43eeb31945811c1fe54d432d4d65932cb604
Glen Pike <glen.pike@raspberrypi.org>
2019-04-29 14:02:20 +0100

    358: Fix issue with removing achievements. (#256)
    When an Achievement had transitions associated with it, trying to remove this failed.
    This seems to occur when achievements are created by importing from Future Learn - the online
    courses are still self-certifiable, so a user may have tried to remove this, but failed.
    Added a migration to fix the foreign keys on AchievementTransition and a test covering the error.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_29_080853) do
+ActiveRecord::Schema.define(version: 2019_04_29_110311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -105,5 +105,5 @@ ActiveRecord::Schema.define(version: 2019_04_29_080853) do
     t.index ["stem_user_id"], name: "index_users_on_stem_user_id", unique: true
   end
 
-  add_foreign_key "achievement_transitions", "achievements"
+  add_foreign_key "achievement_transitions", "achievements", on_delete: :cascade
 end
