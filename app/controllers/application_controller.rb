cf75c1e53ab28ddc4ffc4db0691398fd2a65aa0d
Damien Hogan <damienh@users.noreply.github.com>
2019-04-30 13:58:14 +0100

    Add certification enabled feature flag and method (#258)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,18 @@ class ApplicationController < ActionController::Base
   include AuthenticationHelper
 
   before_action :authenticate
+  before_action :certification_enabled?
 
   def authenticate
     return unless ENV['BASIC_AUTH_PASSWORD']
+
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
+  end
+
+  def certification_enabled?
+    ActiveModel::Type::Boolean.new.cast(ENV.fetch('CERTIFICATION_ENABLED'))
   end
 
   def authenticate_user!
