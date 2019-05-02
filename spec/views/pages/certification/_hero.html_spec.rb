94bf7f9239781f04137d0c6144a4b50ae58d352e
Magdalena Jadach <magdalena@raspberrypi.org>
2019-05-02 16:31:44 +0100

    Test hero component

---
+++ spec/views/pages/certification/_hero.html_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe('pages/certification/hero', type: :view) do
+  before do
+    render
+  end
+
+  it 'has a title' do
+    expect(rendered).to have_css('.govuk-heading-m', text: 'GCSE computer science subject knowledge')
+  end
+
+  it 'has two subheadings' do
+    expect(rendered).to have_css('.govuk-heading-m', count: 2)
+  end
+
+  it 'has icon' do
+    expect(rendered).to have_css('.certification-hero__image', count: 1)
+  end
+
+end
