dac1a2be4da516aa9b43ecc4e982a90d3c34fce3
Damien Hogan <damienhogan87@gmail.com>
2019-05-03 09:16:21 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_131914) do
+ActiveRecord::Schema.define(version: 2019_04_29_110311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -110,6 +110,15 @@ ActiveRecord::Schema.define(version: 2019_04_23_131914) do
     t.index ["slug"], name: "index_programmes_on_slug", unique: true
   end
 
+  create_table "user_programme_enrolments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.uuid "programme_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["programme_id"], name: "index_user_programme_enrolments_on_programme_id"
+    t.index ["user_id"], name: "index_user_programme_enrolments_on_user_id"
+  end
+
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -127,6 +136,6 @@ ActiveRecord::Schema.define(version: 2019_04_23_131914) do
     t.index ["stem_user_id"], name: "index_users_on_stem_user_id", unique: true
   end
 
-  add_foreign_key "achievement_transitions", "achievements"
+  add_foreign_key "achievement_transitions", "achievements", on_delete: :cascade
   add_foreign_key "assessment_attempt_transitions", "assessment_attempts"
 end
