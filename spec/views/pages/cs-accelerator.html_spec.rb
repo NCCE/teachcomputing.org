8d6c0031b0f1e87c5e90663b6727c03da6d7d8bf
Magdalena Jadach <magdalena@raspberrypi.org>
2019-05-03 09:16:53 +0100

    Fix broken test

--- spec/views/pages/cs-accelerator.html_spec.rb
+++ spec/views/pages/cs-accelerator.html_spec.rb
@@ -5,16 +5,8 @@ RSpec.describe('pages/cs-accelerator', type: :view) do
     render
   end
 
-  it 'has a title' do
-    expect(rendered).to have_css('.govuk-heading-l', text: 'The Computer Science Accelerator Programme')
-  end
-
-  it 'has a bursary link' do
-    expect(rendered).to have_link('Read about our bursaries', href: '/bursary')
-  end
-
-  it 'has a link to the courses' do
-    expect(rendered).to have_link('Browse available courses', href: '/courses')
+  it 'has aside section' do
+    expect(rendered).to have_css('.ncce-aside', count: 1)
   end
 
 end
