613f4856db014c2c760c4bcc1f99b5f526f94cfc
Damien Hogan <damienhogan87@gmail.com>
2019-05-09 15:51:58 +0100

    remove byebug

--- spec/helpers/activities_helper_spec.rb
+++ spec/helpers/activities_helper_spec.rb
@@ -5,7 +5,6 @@ describe ActivitiesHelper, type: :helper do
 
   describe('#class_marker_diagnostic_url') do
     it 'returns a url with the user id and email address' do
-      byebug
       url = ENV.fetch('CLASS_MARKER_DIAGNOSTIC_URL').to_s + "&cm_e=#{user.email}&cm_user_id=#{user.id}"
       expect(helper.class_marker_diagnostic_url(user)).to eq url
     end
