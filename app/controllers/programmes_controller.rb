c74c07c150e62d002646a21c2b9ebac9b33680e4
Glen Pike <github@glenpike.co.uk>
2019-05-13 16:33:48 +0100

    386: Add 'complete' page for Certificate and tests.
    Create route and complete action in controller.
    Add view
    Add tests for view and controller.
    TODO: check if the programme is complete and behave accordingly

--- app/controllers/programmes_controller.rb
+++ app/controllers/programmes_controller.rb
@@ -2,13 +2,24 @@ class ProgrammesController < ApplicationController
   layout 'full-width'
   before_action :enabled?
   before_action :authenticate_user!
-  before_action :find_programme!, only: [:show]
+  before_action :find_programme!, only: [:show, :complete]
 
   def show
     if @programme.user_enrolled?(current_user)
       render :show
     else
       redirect_to certification_path
+    end
+  end
+  
+  def complete
+    if !@programme.user_enrolled?(current_user)
+      redirect_to certification_path
+    # TODO
+    # elsif !@programme.complete(current_user)
+    #   redirect_to programme_path(@programme.slug)
+    else
+      render :complete
     end
   end
 
---
+++ spec/requests/programmes/complete_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+
+RSpec.describe ProgrammesController do
+  let(:user) { create(:user) }
+  let(:programme) { create(:programme, slug: 'cs-accelerator') }
+  let(:user_programme_enrolment) {
+                                    create( :user_programme_enrolment,
+                                            user_id: user.id,
+                                            programme_id: programme.id)
+                                  }
+
+  describe '#complete' do
+    describe 'while certification is not enabled' do
+      it 'redirects to home page' do
+        get programme_complete_path('cs-accelerator')
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    describe 'while certification is enabled' do
+      before do
+        allow_any_instance_of(ProgrammesController)
+          .to receive(:certification_enabled?).and_return(true)
+      end
+
+      describe 'while logged in' do
+        before do
+          programme
+          allow_any_instance_of(AuthenticationHelper)
+            .to receive(:current_user).and_return(user)
+        end
+
+        it 'handles missing programmes' do
+          expect {
+            get programme_complete_path('programme-missing')
+          }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
+        it 'redirects if not enrolled' do
+          get programme_complete_path('cs-accelerator')
+          expect(response).to redirect_to(certification_path)
+        end
+
+        # TODO
+        # describe 'and enrolled' do
+        #   before do
+        #     user_programme_enrolment
+        #     get programme_complete_path('cs-accelerator')
+        #   end
+
+        #   it 'redirects if not complete' do
+        #     expect(response).to redirect_to(programme_path('cs-accelerator'))
+        #   end
+        # end
+
+        describe 'and complete' do
+          before do
+            user_programme_enrolment
+            get programme_complete_path('cs-accelerator')
+          end
+
+          it 'shows the page if complete' do
+            expect(response.status).to eq(200)
+          end
+
+          it 'assigns the programme' do
+            expect(assigns(:programme)).to eq(programme)
+          end
+        end
+      end
+
+      describe 'while logged out' do
+        before do
+          get programme_complete_path('cs-accelerator')
+        end
+
+        it 'redirects to login' do
+          expect(response).to redirect_to(login_path)
+        end
+      end
+    end
+  end
+end
