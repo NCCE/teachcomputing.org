ff55b59506085a56a3edd8441a7c94e980398e6f
Magdalena Jadach <magdalena@raspberrypi.org>
2019-05-15 12:12:51 +0100

    Remove redundant parameters from the controller

--- app/controllers/programmes_controller.rb
+++ app/controllers/programmes_controller.rb
@@ -3,7 +3,7 @@ class ProgrammesController < ApplicationController
   before_action :enabled?
   before_action :authenticate_user!
 
-  before_action :find_programme!, :set_achievements_complete, only: [:show]
+  before_action :find_programme!, :list_achievements_by_category, only: [:show]
 
 
   def show
@@ -24,11 +24,9 @@ class ProgrammesController < ApplicationController
       @programme = Programme.find_by!(slug: params[:slug])
     end
 
-    def set_achievements_complete
-      programme = Programme.find_by!(slug: params[:slug])
-      @achievements = current_user.achievements.for_programme(programme).joins(:activity)
-      @online_achievements = current_user.achievements.for_programme(programme).joins(:activity).with_category('online')
-      @face_to_face_achievements = current_user.achievements.for_programme(programme).joins(:activity).with_category('face-to-face')
-      @action_achievements = current_user.achievements.for_programme(programme).joins(:activity).with_category('action')
+    def list_achievements_by_category
+      @online_achievements = current_user.achievements.for_programme(@programme).with_category('online')
+      @face_to_face_achievements = current_user.achievements.for_programme(@programme).with_category('face-to-face')
+      @action_achievements = current_user.achievements.for_programme(@programme).with_category('action')
     end
 end
