84a44edb31b01e54b4b85e7898ce155d1e2354b7
Magdalena Jadach <magdalena@raspberrypi.org>
2019-05-15 17:45:05 +0100

    Add partials tests

---
+++ spec/views/programmes/_online_activity.html_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe('programmes/_online', type: :view) do
+  let!(:user) { create(:user) }
+  let(:programme) { create(:programme, slug: 'cs-accelerator') }
+
+  before do
+    allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
+    @programme = programme
+    render
+  end
+
+  it 'has activities in the list' do
+    expect(rendered).to have_css('.ncce-activity-list__item', count: 7)
+  end
+
+  it 'some of the activities are incomplete' do
+    expect(rendered).to have_css('.ncce-activity-list__item--incomplete', count: 6)
+  end
+
+  it 'incomplete activities have buttons' do
+    expect(rendered).to have_css('.ncce-button__pink--rounded', count: 5)
+  end
+end
