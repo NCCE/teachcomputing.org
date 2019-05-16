8859529601778c3c2c744c507b5891523bde736a
MagdalenaJadach <magdalena@raspberrypi.org>
2019-05-16 19:48:49 +0100

    Add functionality to Programmes Controller to populate data needed for view (#267)
    * Populate first completed activity
    
    * Add Action category
    
    * Move funcionality from helper to the controller
    
    * Add Face to face courses
    
    * Clean up the code
    
    * Add Action courses/activities
    
    * clean up the action activities
    
    * Remove code I used to debug the view
    
    * Reverse wrongly removed category
    
    * Fix the indendation
    
    * Fix the indentation once more
    
    * Remove redundant parameters from the controller
    
    * Fix indentation and update the controller
    
    * Fix dignostic tool link
    
    * Fix typo: replace f2f with online
    
    * Fix typo
    
    * Add partials tests
    
    * Style the header
    
    * Fix background image size, copy for Action partial
    
    * Fix the tests
    
    * Refactor the method to pull out the courses
    
    * Fix broken tests
    
    * Reverse the test
    
    * refactor of face to face courses
    
    * Refactor Online courese to match f2f
    
    * Remove duplicated code

--- app/controllers/programmes_controller.rb
+++ app/controllers/programmes_controller.rb
@@ -4,12 +4,14 @@ class ProgrammesController < ApplicationController
   before_action :authenticate_user!
   before_action :find_programme!, only: [:show, :complete]
   before_action :user_enrolled?, only: [:show, :complete]
+  before_action :list_achievements_by_category, only: [:show]
   before_action :passed_programme_assessment?, only: [:complete]
+
 
   def show
     render :show
   end
-  
+
   def complete
     render :complete
   end
@@ -22,6 +24,13 @@ class ProgrammesController < ApplicationController
 
     def find_programme!
       @programme = Programme.find_by!(slug: params[:slug])
+    end
+
+    def list_achievements_by_category
+      achievements = current_user.achievements.for_programme(@programme)
+      @online_achievements = achievements.with_category('online').take(2)
+      @face_to_face_achievements = achievements.with_category('face-to-face').take(2)
+      @downloaded_diagnostic = achievements.with_category('action').where(activities: {slug: 'downloaded-diagnostic-tool'}).any?
     end
 
     def user_enrolled?
--- spec/views/programmes/show.html_spec.rb
+++ spec/views/programmes/show.html_spec.rb
@@ -10,11 +10,4 @@ RSpec.describe('programmes/show', type: :view) do
     render
   end
 
-  it 'has a title' do
-    expect(rendered).to have_css('h1', text: @programme.title)
-  end
-
-  it 'has progress section' do
-    expect(rendered).to have_css('h2', text: 'Your progress')
-  end
 end
