663c4b0e0de3ef1a5fa4a2dc1eb4db095401e914
Damien Hogan <damienhogan87@gmail.com>
2019-05-17 16:19:38 +0100

    add skip auth for staging

--- app/controllers/class_marker/webhooks_controller.rb
+++ app/controllers/class_marker/webhooks_controller.rb
@@ -1,5 +1,5 @@
 class ClassMarker::WebhooksController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, :authenticate
 
   before_action :verify_hmac_signature
 
