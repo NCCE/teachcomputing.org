adca6e6e0990c6ea4cdd1a96f0020d5fd8eb367e
Glen Pike <github@glenpike.co.uk>
2019-07-05 17:04:12 +0100

    511: rename function to make more explicit

--- app/controllers/programmes_controller.rb
+++ app/controllers/programmes_controller.rb
@@ -31,14 +31,14 @@ class ProgrammesController < ApplicationController
     end
 
     def achievements_by_category
-      sort_by_state = lambda do |a, b|
+      completed_first = lambda do |a, b|
         return -1 if a.current_state == 'complete'
         return 1 if b.current_state == 'complete'
         0
       end
       achievements = current_user.achievements.for_programme(@programme)
-      @online_achievements = achievements.with_category('online').sort(&sort_by_state).take(2)
-      @face_to_face_achievements = achievements.with_category('face-to-face').sort(&sort_by_state).take(2)
+      @online_achievements = achievements.with_category('online').sort(&completed_first).take(2)
+      @face_to_face_achievements = achievements.with_category('face-to-face').sort(&completed_first).take(2)
       @downloaded_diagnostic = achievements.with_category('action').where(activities: { slug: 'diagnostic-tool' }).any?
     end
 
