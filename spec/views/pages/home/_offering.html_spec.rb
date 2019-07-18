1fc5c33005ebeab73f5b4b503d28459ea6d2e19c
Glen Pike <github@glenpike.co.uk>
2019-07-18 16:08:00 +0100

    Fix broken test

--- spec/views/pages/home/_offering.html_spec.rb
+++ spec/views/pages/home/_offering.html_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe('pages/home/_offering', type: :view) do
   end
 
   it('contains the offering text') do
-    expect(rendered).to have_css('.ncce-journey__aside-text', text: 'Teachers can transform lives, so we are giving them the power to change computing education across England.')
+    expect(rendered).to have_css('.ncce-offering__aside-text', text: 'Teachers can transform lives, so we are giving them the power to change computing education across England.')
   end
 end
