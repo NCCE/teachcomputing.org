01e1c501b6beb6ea6cbb1d7e1460e6100d4b8841
Damien Hogan <damienh@users.noreply.github.com>
2019-07-18 10:41:00 +0100

    Removal of sign up component and add offering component (#364)
    * Removal of sign up component and add offering component
    
    * remove div
    
    * Tidy svg's
    
    * add remaining alt tags
    
    * add commas

---
+++ spec/views/pages/home/_offering.html_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe('pages/home/_offering', type: :view) do
+  before do
+    render
+  end
+
+  it('contains the offering text') do
+    expect(rendered).to have_css('.ncce-journey__aside-text', text: 'Teachers can transform lives, so we are giving them the power to change computing education across England.')
+  end
+end
