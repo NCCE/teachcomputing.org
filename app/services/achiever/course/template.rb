57a7b1302bd0fa15f875685ca899081289f0dbc1
Damien Hogan <damienhogan87@gmail.com>
2019-07-30 16:53:27 +0100

    Tweaks to Template

--- app/services/achiever/course/template.rb
+++ app/services/achiever/course/template.rb
@@ -1,28 +1,34 @@
 class Achiever::Course::Template
   attr_accessor :course_template_no,
+                :title,
                 :summary,
+                :booking_url,
                 :activity_code,
                 :meta_description,
                 :activity_code,
-                :subject,
-                :workstream
+                :subjects,
+                :workstream,
+                :occurrences
 
-  RESOURCE_PATH = 'Get?cmd=CourseTemplatesListing'.freeze
+  RESOURCE_PATH = 'Get?cmd=CourseTemplatesListingByProgramme'.freeze
   QUERY_STRINGS = { 'Page': '1',
                     'RecordCount': '1000',
                     'ProgrammeID': ENV.fetch('ACHIEVER_V2_NCCE_PROGRAMME_ID') }.freeze
 
   def self.all
     templates = Achiever::Request.resource(RESOURCE_PATH, QUERY_STRINGS)
-    templates.map { |course| Achiever::Template.new(course) }
+    templates.map { |course| Achiever::Course::Template.new(course) }
   end
 
   def initialize(template)
     @course_template_no = template.send('Template.COURSETEMPLATENO')
+    @title = template.send('Template.TemplateTitle')
     @summary = template.send('Template.Summary')
+    @booking_url = template.send('Template.Booking_URL')
     @meta_description = template.send('Template.MetaDescription')
     @activity_code = template.send('Template.ActivityCode')
-    @subject = template.send('Template.Subject').split(';')
+    @subjects = template.send('Template.Subject').split(';')
     @workstream = template.send('Template.Workstream')
+    @occurrences = []
   end
 end
