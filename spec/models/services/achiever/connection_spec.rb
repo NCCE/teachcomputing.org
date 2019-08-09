2267d87817df50af3f63cb9486c2932805ee8f8b
Damien Hogan <damienhogan87@gmail.com>
2019-08-09 14:13:20 +0100

    connection spec

---
+++ spec/models/services/achiever/connection_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Achiever::Connection do
+  let(:connection)  { described_class.new }
+
+  describe '#api' do
+    it 'is a Faraday::Connection class' do
+      expect(described_class.api.class).to eq Faraday::Connection
+    end
+
+    it 'has a proxy set' do
+      expect(described_class.api.proxy.host).not_to eq nil
+    end
+  end
+end
