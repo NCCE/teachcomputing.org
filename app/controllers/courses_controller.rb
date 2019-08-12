a84be74a290cc0ab91a1e872a2949c92b33778a1
Damien Hogan <damienhogan87@gmail.com>
2019-08-12 15:43:55 +0100

    format file

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -29,75 +29,75 @@ class CoursesController < ApplicationController
 
   private
 
-  def init_filters
-    @current_location = nil
-    @current_level = nil
-    @current_topic = nil
-    @current_workstream = nil
-    @course_occurrences = nil
-  end
-
-  def filter_courses(courses)
-    courses.select do |c|
-      has_level = true
-      has_location = true
-      has_topic = true
-      has_workstream = true
-
-      if params[:level].present?
-        @current_level = params[:level]
-        key = @age_groups[params[:level]].to_s
-        has_level = c.age_groups.any?(key)
-      end
-      if params[:location].present?
-        @current_location = params[:location]
-        has_location = compare_location(c, @current_location)
-      end
-      if params[:topic].present?
-        @current_topic = params[:topic]
-        key = @subjects[params[:topic]].to_s
-        has_topic = c.subjects.any?(key)
-      end
-      if params[:workstream].present?
-        @current_workstream = params[:workstream]
-        has_workstream = c.workstream == @current_workstream
-      end
-      has_level && has_location && has_topic && has_workstream
+    def init_filters
+      @current_location = nil
+      @current_level = nil
+      @current_topic = nil
+      @current_workstream = nil
+      @course_occurrences = nil
     end
-  end
 
-  def alert_filter_params
-    filter_strings = []
-    filter_strings.push("<strong>Level</strong>: #{@current_level}") if @current_level
-    filter_strings.push("<strong>Topic</strong>: #{@current_topic}") if @current_topic
-    filter_strings.push("<strong>Location</strong>: #{@current_location}") if @current_location
-    filter_strings.push("<strong>Programme</strong>: #{@current_workstream}") if @current_workstream
+    def filter_courses(courses)
+      courses.select do |c|
+        has_level = true
+        has_location = true
+        has_topic = true
+        has_workstream = true
 
-    return if filter_strings.empty?
-
-    notice = "<span>You are filtering with #{filter_strings.join('; ')}</span>"
-    notice += " #{view_context.link_to('Remove filter', url_for(controller: 'courses', anchor: 'filter-results'))}"
-    flash.now[:notice] = notice
-  end
-
-  def compare_location(course, location)
-    case location
-    when 'Online'
-      course.online_cpd
-    when 'Face to face'
-      !course.online_cpd
-    else
-      course.occurrences.any? { |oc| oc.address_town == location }
+        if params[:level].present?
+          @current_level = params[:level]
+          key = @age_groups[params[:level]].to_s
+          has_level = c.age_groups.any?(key)
+        end
+        if params[:location].present?
+          @current_location = params[:location]
+          has_location = compare_location(c, @current_location)
+        end
+        if params[:topic].present?
+          @current_topic = params[:topic]
+          key = @subjects[params[:topic]].to_s
+          has_topic = c.subjects.any?(key)
+        end
+        if params[:workstream].present?
+          @current_workstream = params[:workstream]
+          has_workstream = c.workstream == @current_workstream
+        end
+        has_level && has_location && has_topic && has_workstream
+      end
     end
-  end
 
-  def course_tags(courses)
-    used_subjects = courses.reduce([]) { |tags, c| tags + c.subjects }.uniq.sort
-    @subjects.reject { |k,v| !used_subjects.include?(v.to_s) }
-  end
+    def alert_filter_params
+      filter_strings = []
+      filter_strings.push("<strong>Level</strong>: #{@current_level}") if @current_level
+      filter_strings.push("<strong>Topic</strong>: #{@current_topic}") if @current_topic
+      filter_strings.push("<strong>Location</strong>: #{@current_location}") if @current_location
+      filter_strings.push("<strong>Programme</strong>: #{@current_workstream}") if @current_workstream
 
-  def course_locations(course_occurrences)
-    towns = course_occurrences.reduce([]) { |acc, oc| !oc.online_cpd ? acc.push(oc.address_town) : acc }
-    towns.uniq.sort.unshift('Face to face').unshift('Online')
-  end
+      return if filter_strings.empty?
+
+      notice = "<span>You are filtering with #{filter_strings.join('; ')}</span>"
+      notice += " #{view_context.link_to('Remove filter', url_for(controller: 'courses', anchor: 'filter-results'))}"
+      flash.now[:notice] = notice
+    end
+
+    def compare_location(course, location)
+      case location
+      when 'Online'
+        course.online_cpd
+      when 'Face to face'
+        !course.online_cpd
+      else
+        course.occurrences.any? { |oc| oc.address_town == location }
+      end
+    end
+
+    def course_tags(courses)
+      used_subjects = courses.reduce([]) { |tags, c| tags + c.subjects }.uniq.sort
+      @subjects.select { |_k, v| used_subjects.include?(v.to_s) }
+    end
+
+    def course_locations(course_occurrences)
+      towns = course_occurrences.reduce([]) { |acc, oc| !oc.online_cpd ? acc.push(oc.address_town) : acc }
+      towns.uniq.sort.unshift('Face to face').unshift('Online')
+    end
 end
