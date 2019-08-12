801e2507d362fe4e756145267cbe10a4ec3caa4e
Glen Pike <github@glenpike.co.uk>
2019-08-12 14:20:30 +0100

    561: Fix dashboard 'enrol' hero and add '...enrolled...' to normal hero.
    Combine the _enrol partial for dashboard into single re-usable partial (may
    benefit from further combination with hero later?).
    Add the 'You are enrolled' text to the _hero partial with tests.

--- spec/views/pages/certification/_hero.html_spec.rb
+++ spec/views/pages/certification/_hero.html_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe('pages/certification/_hero', type: :view) do
+  let(:user) { create(:user) }
   let(:programme) { create(:programme) }
+  let(:user_programme_enrolment) do
+    create(:user_programme_enrolment,
+           user_id: user.id,
+           programme_id: programme.id)
+  end
+
   before do
     @programme = programme
     render
@@ -15,4 +22,30 @@ RSpec.describe('pages/certification/_hero', type: :view) do
     expect(rendered).to have_css('.certification-hero__image', count: 1)
   end
 
+  it 'doesn\'t show the enrolled tag' do
+    expect(rendered).not_to have_css('.certification-hero__enrolled', count: 1)
+  end
+
+  context 'when the user logged in but not enrolled' do
+    before do
+      allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
+      render
+    end
+
+    it 'doesn\'t show the enrolled tag' do
+      expect(rendered).not_to have_css('.certification-hero__enrolled', count: 1)
+    end
+  end
+
+  context 'when the user logged in but not enrolled' do
+    before do
+      user_programme_enrolment
+      allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
+      render
+    end
+
+    it 'shows the enrolled tag' do
+      expect(rendered).to have_css('.certification-hero__enrolled', count: 1)
+    end
+  end
 end
