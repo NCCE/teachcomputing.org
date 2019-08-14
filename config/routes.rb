e7304eec77963fc9d71f11fa4a0a75b882b2bab7
Damien Hogan <damienh@users.noreply.github.com>
2019-08-14 11:06:51 +0100

    remove contact as we dont use it (#389)

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,6 @@ Rails.application.routes.draw do
   get '/accelerator', to: redirect('/cs-accelerator')
   get '/bursary', to: 'pages#page', as: :bursary, defaults: { page_slug: 'bursary' }
   get '/certification', to: 'pages#page', as: :certification, defaults: { page_slug: 'certification' }
-  get '/contact', to: 'pages#page', as: :contact, defaults: { page_slug: 'contact' }
   get '/get-involved', to: 'pages#page', as: :get_involved, defaults: { page_slug: 'get-involved' }
   get '/login', to: 'pages#login', as: :login
   get '/logout', to: 'auth#logout', as: :logout
