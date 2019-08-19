fd7d1cdfce0e3579788bd4ce956cdd8a8d18e9bb
Damien Hogan <damienhogan87@gmail.com>
2019-08-19 14:33:29 +0100

    revert change

--- app/services/achiever/course/age_group.rb
+++ app/services/achiever/course/age_group.rb
@@ -4,7 +4,7 @@ class Achiever::Course::AgeGroup
                     'RecordCount': '1000' }.freeze
 
   def self.all
-    age_groups = Achiever::Request.resource(RESOURCE_PATH, QUERY_STRINGS)
+    age_groups = Achiever::Request.option_sets(RESOURCE_PATH, QUERY_STRINGS)
     parsed_response = JSON.parse(age_groups)
     parsed_response.reduce({}, :merge)
                    .select { |k| k.starts_with?('Key') }
