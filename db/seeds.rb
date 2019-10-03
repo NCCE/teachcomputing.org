271c1cc49b1a77bd0628ac21ed8fef0c1e753f44
Glen Pike <glen.pike@raspberrypi.org>
2019-10-03 09:52:25 +0100

    643: Add missing FutureLearn course and report missing ids to Raven (#432)
    Throw an error when a FutureLearn course id is missing a matching Activity and
    then send a message to Raven with details.  Try to only do this once for each
    course id.
    Add spec's to test this.
    Comment out seeding of Primary & Secondary programmes along with incomplete
    'community' activities temporarily until these are complete so we can
    still run 'db:seed' in production without them for now.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,12 @@ require File.expand_path('../seeds/activities/action', __FILE__)
 require File.expand_path('../seeds/activities/face-to-face', __FILE__)
 require File.expand_path('../seeds/activities/online', __FILE__)
 require File.expand_path('../seeds/activities/assessment', __FILE__)
-require File.expand_path('../seeds/activities/community', __FILE__)
+# Glen commented out until these activities have finalised info
+# require File.expand_path('../seeds/activities/community', __FILE__)
 Activity.all.each do |a|
   puts "Created activity #{a.title} (#{a})"
 end
 require File.expand_path('../seeds/programmes/cs_accelerator', __FILE__)
-require File.expand_path('../seeds/programmes/primary-certificate', __FILE__)
-require File.expand_path('../seeds/programmes/secondary-certificate', __FILE__)
+# Glen commented out until these programmes have finalised info
+# require File.expand_path('../seeds/programmes/primary-certificate', __FILE__)
+# require File.expand_path('../seeds/programmes/secondary-certificate', __FILE__)
