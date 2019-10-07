5ba45d987fe483694d4df7234c9f4db7961b3d50
Glen Pike <glen.pike@raspberrypi.org>
2019-10-07 09:44:44 +0100

    636: Update login page and update Create Account / Log in links (#435)
    - Change all 'Create account' links to point at register page.
    - Change all 'Log in' links to initiate the OAuth flow.
    - Remove the 'signup-stem' action & views.
    - Remove signup CSS
    - Remove stem_login_enabled method and STEM_LOGIN_ENABLED env var
    - Fixing sitemap & routes
    - Fix tests
    - Update the login page to remove the 'Create Account' button, but still be able to handle 'redirects' from STEM sites (on account verify and booking course without being logged in).

--- config/routes.rb
+++ config/routes.rb
@@ -47,7 +47,6 @@ Rails.application.routes.draw do
   get '/privacy', to: 'pages#page', as: :privacy, defaults: { page_slug: 'privacy' }
   get '/resources', to: 'pages#page', as: :resources, defaults: { page_slug: 'resources' }
   get '/signup-confirmation', to: 'pages#page', as: :signup_confirmation, defaults: { page_slug: 'signup-confirmation' }
-  get '/signup-stem', to: 'pages#signup_stem', as: :signup_stem
   get '/competition-terms-and-conditions', to: 'pages#page', as: :competition_terms_and_conditions, defaults: { page_slug: 'competition-terms-and-conditions' }
   get '/terms-conditions', to: 'pages#page', as: :terms_conditions, defaults: { page_slug: 'terms-conditions' }
   get '/hubs', to: 'pages#page', as: :hubs, defaults: { page_slug: 'hubs' }
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -14,11 +14,9 @@ SitemapGenerator::Sitemap.create do
   add '/accelerator', changefreq: 'monthly'
   add '/certification', changefreq: 'monthly'
   add '/contact', changefreq: 'monthly'
-  add '/login', changefreq: 'monthly'
   add '/news', changefreq: 'daily'
   add '/news/a-level', changefreq: 'monthly'
   add '/offer', changefreq: 'monthly'
   add '/privacy', changefreq: 'monthly'
-  add '/signup-stem', changefreq: 'monthly'
   add '/terms-conditions', changefreq: 'monthly'
 end
