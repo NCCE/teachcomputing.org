61d96e099734e25388aac85001ccb80df04ddd14
Glen Pike <github@glenpike.co.uk>
2019-10-07 17:02:08 +0100

    642: Tidy up the routes file.
    Rearrange to be alphabetical per-type.
    Remove redirects as these have had time to take effect.
    Fix tests

--- spec/requests/pages/news_spec.rb
+++ spec/requests/pages/news_spec.rb
@@ -15,19 +15,5 @@ RSpec.describe PagesController do
         expect(response).to redirect_to('https://blog.teachcomputing.org/tag/news')
       end
     end
-
-    context 'first regional delivery network' do
-      before do
-        get '/news/first-regional-delivery-network'
-      end
-
-      it 'has a status code of 301 - redirected' do
-        expect(response).to have_http_status(:moved_permanently)
-      end
-
-      it 'renders the view' do
-        expect(response).to redirect_to('https://blog.teachcomputing.org/first-regional-delivery-network')
-      end
-    end
   end
 end
--- spec/requests/pages/press_spec.rb
+++ spec/requests/pages/press_spec.rb
@@ -15,19 +15,5 @@ RSpec.describe PagesController do
         expect(response).to redirect_to('https://blog.teachcomputing.org/tag/press')
       end
     end
-
-    context 'simon-peyton-jones' do
-      before do
-        get '/press/simon-peyton-jones-chair-ncce'
-      end
-
-      it 'has a status code of 301 - redirected' do
-        expect(response).to have_http_status(:moved_permanently)
-      end
-
-      it 'renders the view' do
-        expect(response).to redirect_to('https://blog.teachcomputing.org/simon-peyton-jones-chair-ncce')
-      end
-    end
   end
 end
