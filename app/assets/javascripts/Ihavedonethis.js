836c303d3e7c81357efed8e6b05922d1b062d5d2
Magdalena Jadach <magdalena@raspberrypi.org>
2019-10-07 11:57:22 +0100

    Fix the button

--- app/assets/javascripts/Ihavedonethis.js
+++ app/assets/javascripts/Ihavedonethis.js
@@ -1,22 +1,21 @@
 function initialiseselfActivity() {
-  const wrapper = document.querySelector(
+  const wrappers = document.querySelectorAll(
     '.ihavedonethis')
-  if (!wrapper) {
-    return
-  }
-  const button = wrapper.querySelector('button')
-  const target = button.nextElementSibling
 
+  Array.prototype.forEach.call(wrappers, function iterateWrappers(wrapper) {
+    const button = wrapper.querySelector('button')
+    const target = button.nextElementSibling
 
-  wrapper.classList.toggle('ihavedonethis--progressive')
+    wrapper.classList.toggle('ihavedonethis--progressive')
 
-  const listener = function () {
-    const expanded = button.getAttribute('aria-expanded') === 'true'
-    button.setAttribute('aria-expanded', !expanded)
-    target.hidden = expanded
-  }
-  button.onclick = listener
-  listener()
+    const listener = function () {
+      const expanded = button.getAttribute('aria-expanded') === 'true'
+      button.setAttribute('aria-expanded', !expanded)
+      target.hidden = expanded
+    }
+    button.onclick = listener
+    listener()
+  })
 }
 
 ready(function () {
