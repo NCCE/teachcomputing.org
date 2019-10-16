1b6ea7a21a6701f88edc4ffd30c2324459b30e3b
Magdalena Jadach <magdalena@raspberrypi.org>
2019-10-16 12:11:51 +0100

    Update the copy and add some tests

--- spec/views/pages/bursary.html_spec.rb
+++ spec/views/pages/bursary.html_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe('pages/bursary', type: :view) do
   it 'has a title' do
     expect(rendered).to have_css('.govuk-heading-l', text: 'Bursary entitlement')
   end
+
+  it 'has 8 subtitles' do
+    expect(rendered).to have_css('.govuk-heading-s', count: 8)
+  end
+
+  it 'has a price table' do
+    expect(rendered).to have_css('.govuk-table', count: 1)
+  end
+
+  it 'has a bullet list' do
+    expect(rendered).to have_css('.govuk-list--bullet', count: 1)
+  end
+
 end
