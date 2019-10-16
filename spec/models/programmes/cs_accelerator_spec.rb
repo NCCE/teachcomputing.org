1cc33546c8bee2b13c333b9fb50d946e9e655b53
Damien Hogan <damienhogan87@gmail.com>
2019-10-16 13:20:54 +0100

    update context description

--- spec/models/programmes/cs_accelerator_spec.rb
+++ spec/models/programmes/cs_accelerator_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Programmes::CSAccelerator do
       end
     end
 
-    context 'when an associated diagnostic activity exists' do
+    context 'when an associated diagnostic activity doesn\'t exists' do
       it 'returns raises an RecordNotFound' do
         expect {
           programme.diagnostic
