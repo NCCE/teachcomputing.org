b17db5fa9bfad0895dd406fc04fe182150e4a93d
Glen Pike <github@glenpike.co.uk>
2019-10-18 16:02:47 +0100

    683: Whitelist url's for use in login redirection.
    Only allow redirection to teachcomputing and stem sites - may be extended late if
    we want to do other redirects.
    TODO: tests, etc.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,18 @@ module ApplicationHelper
   def press_url
     'https://blog.teachcomputing.org/tag/press/'
   end
+
+  def whitelist_redirect_url(url)
+    allowed_redirect_urls = [
+      /^https:\/\/teachcomputing.org/,
+      /^https:\/\/staging-teachcomputing.org/,
+      /^https:\/\/www.stem.org.uk/,
+      /^https:\/\/www-stage.stem.org.uk/,
+      /^https:\/\/teachcomputing-staging-pr-([0-9]+).herokuapp.com/
+    ]
+    allowed_redirect_urls.each do |regex|
+      return url if url =~ regex
+    end
+    return nil
+  end
 end
