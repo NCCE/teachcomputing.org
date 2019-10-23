891ecb421d878f2deb93153371edf432c026faaf
Magdalena Jadach <magdalena@raspberrypi.org>
2019-10-23 17:22:35 +0100

    Add recommendation scss file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,11 @@ require File.expand_path('../seeds/activities/face-to-face', __FILE__)
 require File.expand_path('../seeds/activities/online', __FILE__)
 require File.expand_path('../seeds/activities/assessment', __FILE__)
 # Glen commented out until these activities have finalised info
-# require File.expand_path('../seeds/activities/community', __FILE__)
+require File.expand_path('../seeds/activities/community', __FILE__)
 Activity.all.each do |a|
   puts "Created activity #{a.title} (#{a})"
 end
 require File.expand_path('../seeds/programmes/cs_accelerator', __FILE__)
 # Glen commented out until these programmes have finalised info
-# require File.expand_path('../seeds/programmes/primary-certificate', __FILE__)
+require File.expand_path('../seeds/programmes/primary-certificate', __FILE__)
 # require File.expand_path('../seeds/programmes/secondary-certificate', __FILE__)
