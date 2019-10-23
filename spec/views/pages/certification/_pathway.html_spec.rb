c1ca894d4e559359e2a59c2f193901eeb3f309bc
Magdalena Jadach <magdalena@raspberrypi.org>
2019-10-23 09:46:03 +0100

    Fix broken test

--- spec/views/pages/certification/_pathway.html_spec.rb
+++ spec/views/pages/certification/_pathway.html_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe('pages/certification/_pathway', type: :view) do
   end
 
   it 'has the title' do
-    expect(rendered).to have_css('.govuk-heading-s', text: 'You'll be supported at every step along the way')
+    expect(rendered).to have_css('.govuk-heading-s', text: "You'll be supported at every step along the way")
   end
 
   it 'has all steps' do
