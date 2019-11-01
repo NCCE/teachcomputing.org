558a47f7dcbe37cf428b79ac15c96f342d824a56
Magdalena Jadach <magdalena@raspberrypi.org>
2019-11-01 15:24:29 +0000

    Write more tests and add vertical line

--- spec/views/pages/bursary.html_spec.rb
+++ spec/views/pages/bursary.html_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe('pages/bursary', type: :view) do
   it 'has a title' do
     expect(rendered).to have_css('.govuk-heading-xl', text: 'Bursary entitlement')
   end
+
+  it 'has two subtitles' do
+    expect(rendered).to have_css('.govuk-heading-l', count: 2)
+  end
+
+  it 'has price table' do
+    expect(rendered).to have_css('.govuk-table', count: 1)
+  end
+
 end
