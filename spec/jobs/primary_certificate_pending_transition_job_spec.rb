8c59c73b136489c2246bd9d613478242be069f8f
Glen Pike <github@glenpike.co.uk>
2019-11-07 11:23:45 +0000

    734: Implement job to transition Primary Certificate users to 'pending'
    Create Job, add spec.
    Add/re-implement checking user has completed all activities in Programme.

---
+++ spec/jobs/primary_certificate_pending_transition_job_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe PrimaryCertificatePendingTransitionJob, type: :job do
+  let(:primary_certificate) { create(:primary_certificate) }
+  let(:user) { create(:user) }
+  let(:user_programme_enrolment) { create(:user_programme_enrolment, programme_id: primary_certificate.id, user_id: user.id) }
+
+  describe '#perform' do
+    context 'when user is valid' do
+      before do
+        allow_any_instance_of(Programmes::PrimaryCertificate).to receive(:user_meets_completion_requirement?).and_return(true)
+        user_programme_enrolment
+        PrimaryCertificatePendingTransitionJob.perform_now(user.id, some_value: '10')
+      end
+
+      it 'transitions to pending' do
+        expect(user_programme_enrolment.current_state).to eq 'pending'
+      end
+
+      it 'metadata is stored' do
+        expect(user_programme_enrolment.last_transition.metadata['some_value']).to eq '10'
+      end
+    end
+  end
+
+  context 'when user is invalid' do
+    it 'raises error if user is not found' do
+      expect {
+        PrimaryCertificatePendingTransitionJob.perform_now('123', some_value: '10')
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
