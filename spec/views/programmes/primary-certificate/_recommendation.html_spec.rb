b925aca9f882bd063528327e6137da54f56e5620
Glen Pike <github@glenpike.co.uk>
2019-11-15 20:54:42 +0000

    775: Add in copy for course recommendations and refactor templates.
    Move the text / links for recommendations into a template and select correct one
    using small amount of logic.
    Change the Programme model course_recommendation method to diagnostic_result and
    just return the score for Primary Certificate.
    Guard against uninitialised vars in the templates as not all courses have
    a diagnostic.
    Add/update spec's

---
+++ spec/views/programmes/primary-certificate/_recommendation.html_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe('programmes/primary-certificate/_recommendation', type: :view) do
+  it 'shows correct online course for low score' do
+    render  partial: 'programmes/primary-certificate/recommendation',
+            locals: {
+              category: Activity::ONLINE_CATEGORY,
+              diagnostic_result: 5
+            }
+    expect(rendered).to have_css('.recommendation__text', text: /Teaching Programming in Primary Schools/)
+  end
+
+  it 'shows correct f2f course for low score' do
+    render  partial: 'programmes/primary-certificate/recommendation',
+            locals: {
+              category: Activity::FACE_TO_FACE_CATEGORY,
+              diagnostic_result: 5
+            }
+    expect(rendered).to have_css('.recommendation__text', text: /Primary programming and algorithms/)
+  end
+
+  it 'shows 2 online courses for low score' do
+    render  partial: 'programmes/primary-certificate/recommendation',
+            locals: {
+              category: Activity::ONLINE_CATEGORY,
+              diagnostic_result: 15
+            }
+    expect(rendered).to have_css('.recommendation__text', count: 2)
+  end
+
+  it 'shows no f2f courses for low score' do
+    render  partial: 'programmes/primary-certificate/recommendation',
+            locals: {
+              category: Activity::FACE_TO_FACE_CATEGORY,
+              diagnostic_result: 15
+            }
+    expect(rendered).to eq('')
+  end
+
+end
