cf6ba99c0e23edf7398a7c52c1dd1d39b8c2fa94
Glen Pike <glen.pike@raspberrypi.org>
2019-11-15 11:52:37 +0000

    765: Refactor dashboard with simple new brand/design. (#508)
    Update the hero colour and flip the glyph to match design.
    Remove the intro and courses sections.
    Update the activity titles.
    Remove the self-certification section.
    Remove unused JS and CSS

--- spec/views/dashboard/show.html_spec.rb
+++ spec/views/dashboard/show.html_spec.rb
@@ -23,19 +23,7 @@ RSpec.describe('dashboard/show', type: :view) do
   end
 
   it 'has progress section' do
-    expect(rendered).to have_css('h2', text: 'Your record of achievement')
-  end
-
-  it 'has a link to certification' do
-    expect(rendered).to have_link('certification', href: '/certification')
-  end
-
-  it 'has an aside' do
-    expect(rendered).to have_css('.ncce-aside', count: 1)
-  end
-
-  it 'has a link to download the diagnostic tool' do
-    expect(rendered).to have_css('a', text: 'diagnostic tool')
+    expect(rendered).to have_css('h2', text: 'Your activity')
   end
 
   it 'does not show the certificate progress section' do
