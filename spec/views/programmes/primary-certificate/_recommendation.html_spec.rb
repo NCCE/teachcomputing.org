b8708ccf90f200f1506098b7500f8026d453e9b6
Glen Pike <github@glenpike.co.uk>
2019-11-27 12:50:27 +0000

    775: Update recommended courses

--- spec/views/programmes/primary-certificate/_recommendation.html_spec.rb
+++ spec/views/programmes/primary-certificate/_recommendation.html_spec.rb
@@ -19,19 +19,19 @@ RSpec.describe('programmes/primary-certificate/_recommendation', type: :view) do
     expect(rendered).to have_css('.recommendation__text', text: /Primary programming and algorithms/)
   end
 
-  it 'shows 2 online courses for low score' do
-    render  partial: 'programmes/primary-certificate/recommendation',
-            locals: {
-              category: Activity::ONLINE_CATEGORY,
-              diagnostic_result: 15
-            }
-    expect(rendered).to have_css('.recommendation__text', count: 2)
-  end
-
-  it 'shows no f2f courses for low score' do
+  it 'shows 2 f2f courses for low score' do
     render  partial: 'programmes/primary-certificate/recommendation',
             locals: {
               category: Activity::FACE_TO_FACE_CATEGORY,
+              diagnostic_result: 15
+            }
+    expect(rendered).to have_css('.recommendation__text', count: 3)
+  end
+
+  it 'shows no online courses for low score' do
+    render  partial: 'programmes/primary-certificate/recommendation',
+            locals: {
+              category: Activity::ONLINE_CATEGORY,
               diagnostic_result: 15
             }
     expect(rendered).to eq('')
