61a2f12d7446f81d9ec464552db0e5b69af3035f
Glen Pike <glen.pike@raspberrypi.org>
2019-11-28 13:51:03 +0000

    778 hubs page revamp (#526)
    Add the 'locations' structure for hubs to the page and styling.
    Change the hero colour (fix issue with glyph size)
    Fix/add tests

--- spec/views/pages/hubs.html_spec.rb
+++ spec/views/pages/hubs.html_spec.rb
@@ -8,31 +8,13 @@ RSpec.describe('pages/hubs', type: :view) do
     expect(rendered).to have_css('.govuk-heading-xl', text: 'Network of Computing Hubs')
   end
 
-  it 'has a contact link' do
+  it 'has the hubs areas' do
     render
-    expect(rendered).to have_link('C.taylor@stem.org.uk', href: 'mailto:C.taylor@stem.org.uk')
+    expect(rendered).to have_css('.hubs-area', count: 11)
   end
 
-  context 'when a user is signed in' do
-    before do
-      allow(view).to receive(:current_user).and_return(user)
-      render
-    end
-
-    it 'has no aside panel' do
-      expect(rendered).to have_css('.ncce-aside', count: 0)
-    end
+  it 'has the hubs locations' do
+    render
+    expect(rendered).to have_css('.hubs-location', count: 34)
   end
-
-  context 'when there is not signed in user' do
-    before do
-      allow(view).to receive(:current_user).and_return(nil)
-      render
-    end
-
-    it 'has aside panel' do
-      expect(rendered).to have_css('.ncce-aside', count: 1)
-    end
-  end
-
 end
