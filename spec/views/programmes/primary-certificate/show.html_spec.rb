1a497dd292f857aed385768ec0e0a92960517f11
Glen Pike <github@glenpike.co.uk>
2019-12-04 11:38:40 +0000

    819: Fixing failing spec - another weird/random one

--- spec/views/programmes/primary-certificate/show.html_spec.rb
+++ spec/views/programmes/primary-certificate/show.html_spec.rb
@@ -2,21 +2,18 @@ require 'rails_helper'
 
 RSpec.describe('programmes/primary-certificate/show', type: :view) do
   let(:user) { create(:user) }
-  let(:programme) { create(:programme, slug: 'primary-certificate') }
-  let(:assessment) { create(:assessment, programme_id: programme.id) }
+  let(:programme) { create(:primary_certificate) }
 
   before do
-    assessment
     @current_user = user
     @programme = programme
     allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
+    allow_any_instance_of(Programmes::PrimaryCertificate).to receive(:diagnostic_result).and_return(15)
     @user_programme_achievements = instance_double('UserProgrammeAchievements')
     allow(@user_programme_achievements).to receive_messages(online_achievements: [],
                                                             face_to_face_achievements: [],
                                                             diagnostic_achievements: [],
                                                             community_activities: [])
-    @user_programme_assessment = instance_double('UserProgrammeAssessment')
-    allow(@user_programme_assessment).to receive_messages(enough_credits_for_test?: false)
     render
   end
 
