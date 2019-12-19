f0d4ca57c9a651b0362fea2fdeefc7e0a83e0d3d
Damien Hogan <damienhogan87@gmail.com>
2019-12-19 15:34:49 +0000

    ensure check for job

--- spec/requests/resources/show_spec.rb
+++ spec/requests/resources/show_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ResourcesController do
+  let(:redirect_url) { URI.encode_www_form_component('https://ncce.io/Year1') }
+
   describe '#show' do
     it 'renders the index template' do
-      redirect_url = URI.encode_www_form_component('https://ncce.io/Year1')
       get resources_redirect_path(redirect_url: redirect_url, year: 1)
       expect(response).to redirect_to('https://ncce.io/Year1')
+    end
+
+    it 'queues CsAcceleratorEnrolmentTransitionJob job' do
+      expect do
+        get resources_redirect_path(redirect_url: redirect_url, year: 1)
+      end.to have_enqueued_job(ScheduleUserResourcesFeedbackJob)
     end
   end
 end
