ab6373803abaa591aa646b859457a1494b6e0da5
Damien Hogan <damienhogan87@gmail.com>
2019-12-19 15:32:45 +0000

    mailer spec

---
+++ spec/mailers/resources_feedback_mailer_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe ResourcesFeedbackMailer, type: :mailer do
+  let(:user) { create(:user) }
+  let(:mail) { ResourcesFeedbackMailer.with(user: user, year: 1).feedback_request }
+
+  describe 'email' do
+    it 'renders the headers' do
+      expect(mail.subject).to eq('How are you finding the Resource Repository content?')
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(['noreply@teachcomputing.org'])
+    end
+
+    it 'contains the users name' do
+      expect(mail.body.encoded).to include(user.first_name.to_s)
+    end
+
+    it 'contains the correct feedback link' do
+      expect(mail.body.encoded).to include('http://ncce.io/year1capture')
+    end
+  end
+end
