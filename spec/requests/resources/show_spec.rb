3cba84e7accd660ea97dc318c2a353ac8ae02d97
Damien Hogan <damienh@users.noreply.github.com>
2020-01-06 15:54:39 +0000

    add before action to ensure user for resources feedback job (#571)

--- spec/requests/resources/show_spec.rb
+++ spec/requests/resources/show_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ResourcesController do
   let(:redirect_url) { URI.encode_www_form_component('https://ncce.io/Year1') }
+  let(:user) { create(:user) }
+
 
   describe '#show' do
+    before do
+      allow_any_instance_of(AuthenticationHelper)
+        .to receive(:current_user).and_return(user)
+    end
+
     it 'renders the index template' do
       get resources_redirect_path(redirect_url: redirect_url, year: 1)
       expect(response).to redirect_to('https://ncce.io/Year1')
