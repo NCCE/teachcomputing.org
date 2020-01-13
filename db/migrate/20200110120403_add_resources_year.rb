024f5897eb8ece114bb6f2aa886a0eb814334115
Magdalena Jadach <magdalena@raspberrypi.org>
2020-01-13 12:26:26 +0000

    Add migration

--- db/migrate/20200110120403_add_resources_year.rb
+++ db/migrate/20200110120403_add_resources_year.rb
@@ -1,4 +1,9 @@
 class AddResourcesYear < ActiveRecord::Migration[5.2]
   def change
+    create_table :resources, id: :uuid do |t|
+      t.references :user_id
+      t.string :resource_year, null: false
+      t.integer :counter, :default => 0
+      t.timestamps
   end
 end
