d7c1f5075874eb53fa5171146a375c6c43b9cf1c
Damien Hogan <damienhogan87@gmail.com>
2020-01-14 14:43:54 +0000

    update spec

--- spec/mailers/certificate_mailer_spec.rb
+++ spec/mailers/certificate_mailer_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe CertificateMailer, type: :mailer do
 
   describe 'email' do
     it 'renders the headers' do
-      expect(mail.subject).to include('Congratulations you have completed the National Centre for Computing Education Certificate in Primary Computing Teaching')
+      expect(mail.subject).to include('Congratulations you have completed the National Centre for Computing Education Certificate in GCSE Computing Subject Knowledge')
       expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(['noreply@teachcomputing.org'])
     end
