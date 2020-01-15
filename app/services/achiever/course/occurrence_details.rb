27ec5715e7ca0d948e2cb3b92df7999216800452
Damien Hogan <damienhogan87@gmail.com>
2020-01-15 09:51:15 +0000

    spec

--- app/services/achiever/course/occurrence_details.rb
+++ app/services/achiever/course/occurrence_details.rb
@@ -1,17 +1,17 @@
 class Achiever::Course::OccurrenceDetails
-  attr_accessor :id,
-                :activity_code,
+  attr_accessor :activity_code,
                 :activity_title,
-                :start_date,
-                :end_date,
-                :summary,
-                :address_post_code,
-                :address_city,
-                :address_line_1,
-                :address_line_2,
-                :address_name,
-                :title,
+                :address_city
+                :address_line_1
+                :address_line_2
+                :address_name
+                :address_post_code
                 :duration
+                :end_date
+                :id
+                :start_date,
+                :summary,
+                :title,
 
   RESOURCE_PATH = 'Get?cmd=CourseDetails'.freeze
   PROGRAMME_NAME = 'ncce'.freeze
@@ -28,18 +28,17 @@ class Achiever::Course::OccurrenceDetails
   end
 
   def initialize(occurrence)
-    @id = occurrence.send('Activity.ID')
-    @activity_code = occurrence.send('Activity.ID')
     @activity_code = occurrence.send('Activity.ActivityCode')
     @activity_title = occurrence.send('Activity.ActivityTitle')
-    @start_date = occurrence.send('Activity.StartDate')
-    @end_date = occurrence.send('Activity.EndDate')
-    @summary = occurrence.send('Activity.Summary')
-    @address_post_code = occurrence.send('Venueaddress.PostCode')
     @address_city = occurrence.send('Venueaddress.City')
     @address_line_2 = occurrence.send('Venueaddress.Line1')
     @address_line_2 = occurrence.send('Venueaddress.Line2')
-    @title = occurrence.send('Template.Title')
+    @address_post_code = occurrence.send('Venueaddress.PostCode')
     @duration = occurrence.send('Template.Duration')
+    @end_date = occurrence.send('Activity.EndDate')
+    @id = occurrence.send('Activity.ID')
+    @start_date = occurrence.send('Activity.StartDate')
+    @summary = occurrence.send('Activity.Summary')
+    @title = occurrence.send('Template.Title')
   end
 end
