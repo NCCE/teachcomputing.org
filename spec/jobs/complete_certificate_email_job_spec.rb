4b86dae07467e9ccfc56e3bd872b445c0d437544
Damien Hogan <damienhogan87@gmail.com>
2020-01-17 13:37:33 +0000

    update spec

--- spec/jobs/complete_certificate_email_job_spec.rb
+++ spec/jobs/complete_certificate_email_job_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe CompleteCertificateEmailJob, type: :job do
-  let(:enrolment) { create(:user_programme_enrolment) }
+  let(:cs_accelerator) { create(:cs_accelerator) }
+  let(:primary) { create(:primary_certificate) }
+  let(:cs_accelerator_enrolment) { create(:user_programme_enrolment, programme_id: cs_accelerator.id) }
+  let(:primary_enrolment) { create(:user_programme_enrolment, programme_id: primary.id) }
 
   describe '#perform' do
-    it 'sends an email' do
-      expect { described_class.perform_now(enrolment.user, enrolment.programme) }
-        .to change { ActionMailer::Base.deliveries.count }.by(1)
+    context 'when the programme is cs accelerator' do
+      it 'sends an email' do
+        expect { described_class.perform_now(cs_accelerator_enrolment.user, cs_accelerator_enrolment.programme) }
+          .to change { ActionMailer::Base.deliveries.count }.by(1)
+      end
+    end
+
+    context 'when the programme is primary' do
+      it 'sends an email' do
+        expect { described_class.perform_now(primary_enrolment.user, primary_enrolment.programme) }
+          .to change { ActionMailer::Base.deliveries.count }.by(1)
+      end
     end
   end
 end
