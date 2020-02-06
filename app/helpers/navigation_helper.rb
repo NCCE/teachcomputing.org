20e7019c2a2d0695a267da0fbab97f4c136b5f52
Damien Hogan <damienhogan87@gmail.com>
2020-02-06 14:04:38 +0000

    ensure it links to the programme

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -6,7 +6,7 @@ module NavigationHelper
     achieved_credits = programme.credits_achieved_for_certificate(current_user)
     max_credits = programme.max_credits_for_certificate
 
-    link_to(dashboard_path, class: 'govuk-header__link') do
+    link_to(programme_path(programme.slug), class: 'govuk-header__link') do
       "<div class='ncce-header__navigation-certification-progress-bar-container'>Your certificate<progress class='ncce-header__navigation-certification-progress-bar' aria-label='You have #{achieved_credits} out of #{max_credits} credits' value='#{achieved_credits}' max='#{max_credits}'></progress></div>".html_safe
     end
   end
