b7580f179ac0508623dc1d928a0f01cf26a06ff4
Glen Pike <glen.pike@raspberrypi.org>
2020-02-24 14:55:18 +0000

    956 email styling (#642)
    Trying to remedy issues with Gmail styling of emails.
    
    - Gmail doesn't render SVGs - add logo as a PNG.
    - Gmail renames CSS #id and .class attributes on elements so styling needs to be done inline - set BG colour on footer.
    - Set the asset host for emails in prod & staging environments - so emails don't use root-relative URLs.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.asset_host  = 'https://teachcomputing.org'
   config.action_mailer.default_url_options = { host: 'https://teachcomputing.org' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.mandrillapp.com',
