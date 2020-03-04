b9a123a5ccacbe3d582c4a93a0e59009c28c7910
Glen Pike <glen.pike@raspberrypi.org>
2020-03-04 15:44:11 +0000

    899: Proof Of Concept for using Ghost as a CMS (#633)
    Add method to Ghost to fetch page content with cache-ing.
    Create a CmsController with a generic action for handling CMS pages.
    Make a generic route, currently under /cms/ that will allow us to
    request the cms page action with the right slug - constrain it to hubs & bursary.
    Add a similar generic route for busting the cache.
    (Created pages on ghost: https://teachcomputing.ghost.io/ghost/#/editor/page/5e453b6dfe73020038d5b9e7)
    Create generic view for CMS pages.
    Add stubs for Ghost requests
    Add specs for request and view.
    Mute brakeman warning about page_slug param in CMS controller

---
+++ spec/requests/cms/cms_page_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe CmsController do
+  describe 'GET #cms_page' do
+    context 'with a valid page' do
+      before do
+        stub_cms_page
+        get '/cms/bursary'
+      end
+
+      it 'assigns @page' do
+        expect(assigns(:page)).to be_a(Object)
+      end
+
+      it '@page has a title' do
+        expect(assigns(:page)['title']).to eq('Test')
+      end
+
+      it 'renders the template' do
+        expect(response).to render_template('cms_page')
+      end
+    end
+
+    context 'with a missing page' do
+      before do
+        stub_missing_cms_page
+      end
+
+      it 'raises an error' do
+        expect { get '/cms/bursary' }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+end
