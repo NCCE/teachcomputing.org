2811a3602232d95ac614791088333dff7e8b644c
Damien Hogan <damienhogan87@gmail.com>
2020-03-13 12:50:30 +0000

    Add Attendance Service class to get the attendance sets

---
+++ app/services/achiever/course/attendance.rb
@@ -0,0 +1,18 @@
+class Achiever::Course::Attendance
+  RESOURCE_PATH = 'Get?cmd=OptionsetAttendanceStatus'.freeze
+  QUERY_STRINGS = { 'Page': '1',
+                    'RecordCount': '1000' }.freeze
+
+  def self.all
+    options = Achiever::Request.option_sets(RESOURCE_PATH, QUERY_STRINGS)
+    JSON.parse(options).reduce({}, :merge).transform_keys(&:downcase)
+  end
+
+  def self.cancelled
+    all['cancelled'].to_s
+  end
+
+  def self.pending
+    all['cancelled'].to_s
+  end
+end
---
+++ spec/models/services/achiever/courses/attendance_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Achiever::Course::Attendance do
+  describe 'constants' do
+    describe 'RESOURCE_PATH' do
+      it 'is not nil' do
+        expect(Achiever::Course::Attendance::RESOURCE_PATH).not_to eq nil
+      end
+    end
+
+    describe 'QUERY_STRINGS' do
+      it 'contains page' do
+        expect(Achiever::Course::Attendance::QUERY_STRINGS).to have_key(:Page)
+      end
+
+      it 'contains record count' do
+        expect(Achiever::Course::Attendance::QUERY_STRINGS).to have_key(:RecordCount)
+      end
+    end
+  end
+
+  describe 'class methods' do
+    before do
+      stub_attendance_sets
+    end
+
+    describe '#all' do
+      it 'returns a Hash' do
+        stub_subjects
+        expect(described_class.all).to be_an Hash
+      end
+    end
+
+    describe '#cancelled' do
+      it 'returns a Hash' do
+        stub_subjects
+        expect(described_class.cancelled).to be_an String
+      end
+    end
+
+    describe '#pending' do
+      it 'returns a Hash' do
+        stub_subjects
+        expect(described_class.pending).to be_an String
+      end
+    end
+  end
+end
