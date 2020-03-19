73ef1076f55a51a8483cb7666f96af1930af75d6
Magdalena Jadach <magdalena@raspberrypi.org>
2020-03-19 09:30:52 +0000

    Changes to the new CS path

--- app/controllers/cs_changes_controller.rb
+++ app/controllers/cs_changes_controller.rb
@@ -1,8 +1,14 @@
 class CsChangesController < ApplicationController
   layout 'full-width'
+  before_action :authenticate_user!
 
   def show
 		@programme = Programme.cs_accelerator
+
+		@online_achievements = current_user.achievements.for_programme(@programme).with_category(Activity::ONLINE_CATEGORY)
+
+		@face_to_face_achievements = current_user.achievements.for_programme(@programme).with_category(Activity::FACE_TO_FACE_CATEGORY)
+
 		render :show
  	end
 
