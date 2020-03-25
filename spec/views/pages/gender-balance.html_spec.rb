cbb409f21e498be4e5b6e05de4df383fa260d1a2
Magdalena Jadach <magdalena@raspberrypi.org>
2020-03-25 09:52:32 +0000

    Test the Gender Balance page

---
+++ spec/views/pages/gender-balance.html_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe('pages/gender-balance', type: :view) do
+  before do
+    render
+  end
+
+  it 'has a title' do
+    expect(rendered).to have_css('.govuk-heading-l', text: 'Inspiring the computing experts of the future')
+  end
+
+  it 'has a intro section' do
+    expect(rendered).to have_css('.govuk-body-l', count: 1)
+  end
+
+  it 'has images' do
+    expect(rendered).to have_css('img', count: 5)
+  end
+
+  it 'has an aside section' do
+    expect(rendered).to have_css('.ncce-aside', text: 'Get involved in this landmark programme')
+  end
+end
