d73c07b6f914be7edbd39a535eb950201bfea6ff
Damien Hogan <damienhogan87@gmail.com>
2020-03-25 10:46:12 +0000

    tweak callback_url to avoid issues in development and staging

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -19,7 +19,7 @@ module OmniAuth::Strategies
         achiever_contact_no: 'achieverContactNo',
         achiever_organisation_no: 'achieverOrganisationNo'
       }.each_pair do |key, stem_key|
-        our_info[key] = user_info['attributes'][stem_key][0] if user_info['attributes'].has_key?(stem_key)
+        our_info[key] = user_info['attributes'][stem_key][0] if user_info['attributes'].key?(stem_key)
       end
       our_info
     end
@@ -34,7 +34,7 @@ module OmniAuth::Strategies
     def callback_url
       return super if ENV['BYPASS_OAUTH'].present?
 
-      ENV.fetch('STEM_OAUTH_CALLBACK_URL')
+      full_host + script_name + callback_path
     end
 
     def raven_context(response)
@@ -51,6 +51,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 end
 
 OmniAuth.config.on_failure = AuthController.action(:failure)
+OmniAuth.config.logger = Rails.logger if Rails.env.development?
 
 if ActiveModel::Type::Boolean.new.cast(ENV.fetch('BYPASS_OAUTH', false))
   puts 'Faking OAuth login for review apps'
