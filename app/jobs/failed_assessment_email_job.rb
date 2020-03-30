97e2ee52b54ac31c678d775cfeebb1adda0c45e9
Dan Halson <dan.halson@raspberrypi.org>
2020-03-30 15:47:53 +0100

    1025: Comment out sending, and associated test

--- app/jobs/failed_assessment_email_job.rb
+++ app/jobs/failed_assessment_email_job.rb
@@ -2,6 +2,7 @@ class FailedAssessmentEmailJob < ApplicationJob
   queue_as :default
 
   def perform(user_id)
-    AssessmentMailer.with(user_id: user_id).failed.deliver_now
+    # To be enabled once email content received
+    # AssessmentMailer.with(user_id: user_id).failed.deliver_now
   end
 end
