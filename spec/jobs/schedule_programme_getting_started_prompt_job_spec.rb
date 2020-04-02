8dcacfb476055ef35d1857040ace77e34ce0ca22
Glen Pike <github@glenpike.co.uk>
2020-04-02 15:11:21 +0100

    1028: Add job to trigger the sending of the Getting Started prompt email.
    This will not send if the user has any achievements in the programme.

---
+++ spec/jobs/schedule_programme_getting_started_prompt_job_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe ScheduleProgrammeGettingStartedPromptJob, type: :job do
+  let(:user) { create(:user) }
+  let(:programme) { create(:primary_certificate) }
+  let(:achievement) { create(:achievement, programme: programme, user: user) }
+
+
+  describe '#perform' do
+    it 'sends an email' do
+      expect { ScheduleProgrammeGettingStartedPromptJob.perform_now(user.id, programme.id) }
+        .to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+
+    it 'doesn\'t send an email if they have an achievement' do
+      achievement
+      expect { ScheduleProgrammeGettingStartedPromptJob.perform_now(user.id, programme.id) }
+        .not_to change { ActionMailer::Base.deliveries.count }
+    end
+  end
+end
