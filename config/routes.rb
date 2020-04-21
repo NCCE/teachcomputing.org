6fed322fac7187ad15d51c432b44f0f38a318158
Glen Pike <github@glenpike.co.uk>
2020-04-21 13:42:05 +0100

    Add page / route for testing the FL video trailers markup

--- config/routes.rb
+++ config/routes.rb
@@ -47,6 +47,7 @@ Rails.application.routes.draw do
   get '/competition-terms-and-conditions', to: 'pages#page', as: :competition_terms_and_conditions, defaults: { page_slug: 'competition-terms-and-conditions' }
   get '/cs-accelerator', to: 'pages#static_programme_page', as: :cs_accelerator, defaults: { page_slug: 'cs-accelerator' }
   get '/external/assets/ncce.css', to: 'asset_endpoint#css_endpoint', as: :css_endpoint
+  get '/fl-trailers', to: 'pages#page', as: :trailers, defaults: { page_slug: 'fl-trailers' }
   get '/get-involved', to: 'pages#page', as: :get_involved, defaults: { page_slug: 'get-involved' }
   get '/hero-demo', to: 'pages#page', as: :hero_demo, defaults: { page_slug: 'hero-demo' }
   get '/home-teaching/:page_slug', to: 'cms#cms_page', as: :cms_home_teaching_page, defaults: { page_slug: 'key-stage-1' }, constraints: { page_slug: /(key-stage-1|key-stage-2|key-stage-3|key-stage-4)/ }
