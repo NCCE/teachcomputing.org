ae2b6f158716ca215c4bb4a001f0714c89f4ca59
Magdalena Jadach <magdalena@raspberrypi.org>
2020-04-23 16:53:26 +0100

    resize the text

--- spec/views/pages/gender-balance.html_spec.rb
+++ spec/views/pages/gender-balance.html_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe('pages/gender-balance', type: :view) do
   end
 
   it 'has a intro section' do
-    expect(rendered).to have_css('.govuk-body-l', count: 2)
+    expect(rendered).to have_css('.govuk-body-l', count: 1)
   end
 
   it 'has 4 images' do
