9e531f520ded201408d03b336eab4aa36e35c24b
Damien Hogan <damienhogan87@gmail.com>
2020-04-23 10:58:39 +0100

    revert change to wrong spec

--- spec/system/landing_pages/teacher_pages_spec.rb
+++ spec/system/landing_pages/teacher_pages_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe('Teacher Landing pages', type: :system) do
     end
 
     it 'is the correct page' do
-      expect(page).to have_css('.govuk-heading-l', text: 'Home Teaching')
+      expect(page).to have_css('.govuk-heading-l', text: 'Inspiration and support for teaching primary computing')
     end
 
     it 'main is accessible' do
@@ -26,8 +26,13 @@ RSpec.describe('Teacher Landing pages', type: :system) do
       visit secondary_teachers_path
     end
 
+    it 'is the correct page' do
+      expect(page).to have_css('.govuk-heading-l', text: 'The essential toolkit for secondary computing teachers')
+    end
+
     it 'main is accessible' do
       expect(page).to be_accessible.within('#main-content').excluding('.axe-skip-a11y-test')
     end
   end
+
 end
