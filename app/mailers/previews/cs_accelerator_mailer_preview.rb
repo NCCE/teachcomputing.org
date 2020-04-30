b226197d2a221abd7c00432877138d9d28c7dcd6
Glen Pike <glen.pike@raspberrypi.org>
2020-04-30 10:48:51 +0100

    1099/1100: Eligibility mailers for CSA users (#738)
    * 1099: Add mailer to send email to 'newly eligible' CSA users
    
    Add mailer func, preview and templates to CS Accelerator mailer and spec.
    Amend Programme model to allow us to check old vs new eligibility.
    Add job to check eligibility of user and send email if so (with specs)
    Add Rake task to trigger.
    
    * 1099: Fix spec
    
    * 1099: Fix missing link.  1100: Add copy for automated mailer.
    
    Add spec's for automated mailer.
    
    * 1100: Enable Automated eligibility mailer and fix spec.
    
    * 1099/1100 Fix spelling of assessment
    
    * 1099/1100: Remove complexity from Rake task.
    
    We don't need to delay the task as it's emailing people, this will queue anyway.
    
    * 1099/1100: Intro para wrong way round in emails - swap over.
    
    * 1099/1100 - revert swap of first para
    
    * 1099/1100: Revert changes for checking old vs new CSA pathway
    
    * 1099/1100: Allow mailer previews in staging.
    
    * 1099/1100: Use scopes for rake task and simplify job
    
    * 1099/1100: Final copy for eligibility emails.
    
    Tweak spacing/size in layout / template and contrast for footer text.
    
    * 1099/1100: Remove the handbook info because it's not ready.
    
    * 1099/1100: Buttons and links in email
    
    * 1099/1100 fix error in template
    
    Co-authored-by: Damien Hogan <damienh@users.noreply.github.com>

--- app/mailers/previews/cs_accelerator_mailer_preview.rb
+++ app/mailers/previews/cs_accelerator_mailer_preview.rb
@@ -2,4 +2,12 @@ class CsAcceleratorMailerPreview < ActionMailer::Preview
   def completed
     CsAcceleratorMailer.with(user: User.first).completed
   end
+
+  def assessment_eligibility
+    CsAcceleratorMailer.with(user: User.first).assessment_eligibility
+  end
+
+  def new_assessment_eligibility
+    CsAcceleratorMailer.with(user: User.first).new_assessment_eligibility
+  end
 end
--- spec/mailers/cs_accelerator_mailer_spec.rb
+++ spec/mailers/cs_accelerator_mailer_spec.rb
@@ -3,22 +3,58 @@ require 'rails_helper'
 RSpec.describe CsAcceleratorMailer, type: :mailer do
   let(:user) { create(:user) }
   let(:programme) { create(:cs_accelerator) }
-  let(:mail) { CsAcceleratorMailer.with(user: user, programme: programme).completed }
-  let(:subject) { 'Congratulations you have completed the National Centre for Computing Education Certificate in GCSE Computing Subject Knowledge' }
+  let(:completed_mail) { CsAcceleratorMailer.with(user: user, programme: programme).completed }
+  let(:completed_subject) { 'Congratulations you have completed the National Centre for Computing Education Certificate in GCSE Computing Subject Knowledge' }
+  let(:eligible_mail) { CsAcceleratorMailer.with(user: user, programme: programme).assessment_eligibility }
+  let(:newly_eligible_mail) { CsAcceleratorMailer.with(user: user, programme: programme).new_assessment_eligibility }
+  let(:eligible_subject) { "#{user.first_name.to_s} your CS Accelerator test is ready." }
 
-  describe 'email' do
+
+  describe '#completed' do
     it 'renders the headers' do
-      expect(mail.subject).to include(subject)
-      expect(mail.to).to eq([user.email])
-      expect(mail.from).to eq(['noreply@teachcomputing.org'])
+      expect(completed_mail.subject).to include(completed_subject)
+      expect(completed_mail.to).to eq([user.email])
+      expect(completed_mail.from).to eq(['noreply@teachcomputing.org'])
     end
 
     it 'renders the body' do
-      expect(mail.body.encoded).to include(user.first_name.to_s)
+      expect(completed_mail.body.encoded).to include(user.first_name.to_s)
     end
 
     it 'includes the subject in the email' do
-      expect(mail.body.encoded).to include("<title>#{subject}</title>")
+      expect(completed_mail.body.encoded).to include("<title>#{completed_subject}</title>")
+    end
+  end
+
+  describe '#assessment_eligibility' do
+    it 'renders the headers' do
+      expect(eligible_mail.subject).to include(eligible_subject)
+      expect(eligible_mail.to).to eq([user.email])
+      expect(eligible_mail.from).to eq(['noreply@teachcomputing.org'])
+    end
+
+    it 'renders the body' do
+      expect(eligible_mail.body.encoded).to include(user.first_name.to_s)
+    end
+
+    it 'includes the subject in the email' do
+      expect(eligible_mail.body.encoded).to include("<title>#{eligible_subject}</title>")
+    end
+  end
+
+  describe '#new_assessment_eligibility' do
+    it 'renders the headers' do
+      expect(newly_eligible_mail.subject).to include(eligible_subject)
+      expect(newly_eligible_mail.to).to eq([user.email])
+      expect(newly_eligible_mail.from).to eq(['noreply@teachcomputing.org'])
+    end
+
+    it 'renders the body' do
+      expect(newly_eligible_mail.body.encoded).to include(user.first_name.to_s)
+    end
+
+    it 'includes the subject in the email' do
+      expect(newly_eligible_mail.body.encoded).to include("<title>#{eligible_subject}</title>")
     end
   end
 end
