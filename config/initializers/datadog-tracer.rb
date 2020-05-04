6d9c69a9c5218303c8619476375f171ff93d91cd
Dan Halson <danhalson@users.noreply.github.com>
2020-05-04 09:25:09 +0100

    706: Add standalone chrome webdriver (#747)
    * 706: Add standalone chrome webdriver
    
    * 706: Remove selenium container, and tidy up handling
    
    * 706: Add comment
    
    Co-authored-by: Glen Pike <glen.pike@raspberrypi.org>

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,3 +1,3 @@
 Datadog.configure do |c|
-  c.use :rails, service_name: "teachcomputing_#{Rails.env}"
+  c.use :rails, service_name: "teachcomputing_#{Rails.env}" unless ENV['ENV_TYPE'] == "development"
 end
