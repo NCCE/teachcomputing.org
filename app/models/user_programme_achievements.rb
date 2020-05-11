d0bbde201e498b667cc7bfbaa08b4f7e1a8009c8
Magdalena Jadach <magdalena@raspberrypi.org>
2020-05-11 16:20:29 +0100

    Re-add removed method

--- app/models/user_programme_achievements.rb
+++ app/models/user_programme_achievements.rb
@@ -2,7 +2,7 @@ class UserProgrammeAchievements
   def initialize(programme, user)
     @user = user
     @achievements = user.achievements.without_category('action')
-                                     .for_programme(programme)
+                                     .for_programme(programme).sort_complete_first
   end
  
   def online_achievements(to_show = 1)
