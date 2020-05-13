1cc3576fc0989319cd5c74d0135ce086be63355a
Glen Pike <github@glenpike.co.uk>
2020-05-13 16:54:54 +0100

    1137: Refactor seeds so that we are no longer using slugs
    Create Programmes first.
    Add each activity to relevant programme(s) after it is created.
    Print out programmes / activities at end a bit more intuitively.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,18 @@
+require File.expand_path('../seeds/programmes/cs_accelerator', __FILE__)
+require File.expand_path('../seeds/programmes/primary-certificate', __FILE__)
+# require File.expand_path('../seeds/programmes/secondary-certificate', __FILE__)
+
 require File.expand_path('../seeds/activities/action', __FILE__)
 require File.expand_path('../seeds/activities/diagnostic', __FILE__)
 require File.expand_path('../seeds/activities/face-to-face', __FILE__)
 require File.expand_path('../seeds/activities/online', __FILE__)
 require File.expand_path('../seeds/activities/assessment', __FILE__)
 require File.expand_path('../seeds/activities/community', __FILE__)
-Activity.all.each do |a|
-  puts "Created activity #{a.title} (#{a})"
+
+Programme.all.each do |p|
+  puts "\nProgramme: #{p.title} activities:\n\n"
+  p.activities.each do |a|
+    puts "#{a.title} (category: #{a.category})"
+  end
+  puts "\n"
 end
-require File.expand_path('../seeds/programmes/cs_accelerator', __FILE__)
-require File.expand_path('../seeds/programmes/primary-certificate', __FILE__)
-# require File.expand_path('../seeds/programmes/secondary-certificate', __FILE__)
