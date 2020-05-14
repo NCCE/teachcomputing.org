5f89d8ae7e43cb7a332b579a169bfdcb8e9901c4
Magdalena Jadach <magdalena@raspberrypi.org>
2020-05-14 15:57:46 +0100

    Improve and fix test

--- spec/views/programmes/cs-accelerator/show.html_spec.rb
+++ spec/views/programmes/cs-accelerator/show.html_spec.rb
@@ -2,24 +2,22 @@ require 'rails_helper'
 
 RSpec.describe('programmes/cs-accelerator/show', type: :view) do
   let(:user) { create(:user) }
-  let(:programme) { create(:programme, slug: 'cs-accelerator') }
-  let(:diagnostic_tool_activity) { create(:activity, :cs_accelerator_diagnostic_tool) }
-  let(:assessment) { create(:assessment, programme_id: programme.id) }
+  let(:programme) { create(:cs_accelerator) }
 
   before do
-    assessment
     @current_user = user
     @programme = programme
-		diagnostic_tool_activity
-    allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
-    @user_programme_achievements = instance_double('UserProgrammeAchievements')
-    allow(@user_programme_achievements).to receive_messages(online_achievements: [], face_to_face_achievements: [], diagnostic_achievements: [])
-    @user_programme_assessment = instance_double('UserProgrammeAssessment')
-    allow(@user_programme_assessment).to receive_messages(enough_credits_for_test?: false)
+		stub_template 'programmes/cs-accelerator/_activity-list.html.erb' => 'stub activity list'
+		stub_template 'programmes/cs-accelerator/_aside.html.erb' => 'stub aside'
     render
   end
 
   it 'has one heading' do
-    expect(rendered).to have_css('.govuk-heading-s', count: 1)
+    expect(rendered).to have_css('.govuk-heading-s', count: 2)
   end
+
+	it 'has one courses button' do
+    expect(rendered).to have_link('Book or join a course today', href: /cs-accelerator/)
+  end
+
 end
