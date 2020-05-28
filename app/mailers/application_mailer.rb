433a3e3274d49cbeca00d482e6e2ebc7d7fb8cdd
Glen Pike <glen.pike@raspberrypi.org>
2020-05-28 09:57:55 +0100

    1147: Prevent repeat emails being sent to people eligible for CSA test. (#780)
    Add a model to record emails sent out to a user.
    Add constants for the 'type' of email in CS Accelerator email.
    When sending mail, record if is sent if requested.
    In the job to send emails to eligible users check if they've already been sent one.
    Add spec's

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,20 @@
 class ApplicationMailer < ActionMailer::Base
   default from: '"Teach Computing" <noreply@teachcomputing.org>'
   layout 'mailer'
+
+  def mail(**args)
+    record_sent_mail(args)
+    super(**args)
+  end
+
+  def record_sent_mail(record_sent_mail: false, to:, mailer_type: nil, subject:)
+    if record_sent_mail == true
+      user = User.find_by!(email: to)
+
+      sent_email = SentEmail.new(user: user,
+                                mailer_type: mailer_type,
+                                subject: subject)
+      sent_email.save!
+    end
+  end
 end
---
+++ spec/models/sent_email_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe SentEmail, type: :model do
+  let(:sent_email) { create(:sent_email) }
+
+  describe 'associations' do
+    it 'belongs to user' do
+      expect(sent_email).to belong_to(:user)
+    end
+  end
+
+  describe 'validations' do
+    before do
+      sent_email
+    end
+
+    it { is_expected.to validate_presence_of(:user) }
+    it { is_expected.to validate_presence_of(:mailer_type) }
+    it { is_expected.to validate_uniqueness_of(:user).case_insensitive.scoped_to(:mailer_type) }
+  end
+end
