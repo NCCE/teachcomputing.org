be2a36db2a91a9994954473399db579996e98781
Magdalena Jadach <magdalena@raspberrypi.org>
2020-06-02 08:06:36 +0100

    Fix broken test

--- spec/views/pages/certification/_pathway.html_spec.rb
+++ spec/views/pages/certification/_pathway.html_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe('pages/certification/_pathway', type: :view) do
+	let(:programme) { create(:cs_accelerator) }
+
   before do
+		@programme = programme
     render
   end
 
   it 'has the title' do
-    expect(rendered).to have_css('.govuk-heading-s', text: "Learning pathways for teachers")
+    expect(rendered).to have_css('.govuk-heading-s', text: 'Learning pathways for teachers')
   end
 
   it 'has all items' do
     expect(rendered).to have_css('.pathway__item', count: 5)
   end
 
-  it 'has all PDFs' do
-    expect(rendered).to have_css('.pathway__item', count: 5)
-  end
-
-  it 'has coursed button' do
+  it 'has a courses link' do
     expect(rendered).to have_css('.button', text: 'Browse our courses')
   end
 
