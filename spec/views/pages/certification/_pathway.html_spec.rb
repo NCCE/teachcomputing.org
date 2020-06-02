6708e0e1f467a4bd92004605c0cdd43e88b40873
Magdalena Jadach <magdalena@raspberrypi.org>
2020-06-02 07:45:51 +0100

    Fix tests

--- spec/views/pages/certification/_pathway.html_spec.rb
+++ spec/views/pages/certification/_pathway.html_spec.rb
@@ -6,23 +6,19 @@ RSpec.describe('pages/certification/_pathway', type: :view) do
   end
 
   it 'has the title' do
-    expect(rendered).to have_css('.govuk-heading-s', text: "You'll be supported at every step along the way")
+    expect(rendered).to have_css('.govuk-heading-s', text: "Learning pathways for teachers")
   end
 
-  it 'has all steps' do
-    expect(rendered).to have_css('.pathway__step', count: 4)
+  it 'has all items' do
+    expect(rendered).to have_css('.pathway__item', count: 5)
   end
 
-  it 'has all titles' do
-    expect(rendered).to have_css('.pathway__step-title', count: 4)
+  it 'has all PDFs' do
+    expect(rendered).to have_css('.pathway__item', count: 5)
   end
 
-  it 'has all paras' do
-    expect(rendered).to have_css('.govuk-body', count: 4)
-  end
-
-  it 'has all sub-paras' do
-    expect(rendered).to have_css('.govuk-body-s', count: 2)
+  it 'has coursed button' do
+    expect(rendered).to have_css('.button', text: 'Browse our courses')
   end
 
 end
--- spec/views/pages/cs-accelerator.html_spec.rb
+++ spec/views/pages/cs-accelerator.html_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe('pages/cs-accelerator', type: :view) do
   end
 
   it 'has a heading' do
-    expect(rendered).to have_css('h1.govuk-heading-m', text: 'Welcome to the Computer Science Accelerator Programme')
+    expect(rendered).to have_css('h1.govuk-heading-m', text: 'Welcome to the Computer Science Accelerator')
   end
 
   it 'has diagram' do
@@ -43,15 +43,11 @@ RSpec.describe('pages/cs-accelerator', type: :view) do
     end
 
     it 'has How to enrol title' do
-      expect(rendered).to have_css('.ncce-aside__title', text: 'How to enrol')
+      expect(rendered).to have_css('.ncce-aside__title', text: 'Enrol on the programme')
     end
 
     it 'has Account button' do
-      expect(rendered).to have_css('.button--aside', text: 'Create an account')
-    end
-
-    it 'has a Log in link' do
-      expect(rendered).to have_css('.ncce-link', text: 'log in')
+      expect(rendered).to have_css('.button--aside', text: 'Enrol now')
     end
   end
 end
