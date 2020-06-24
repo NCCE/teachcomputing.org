81c8e81fad8a5a7aaee79d99ddc4897260da4832
Damien Hogan <damienhogan87@gmail.com>
2020-06-24 08:30:48 +0100

    remove need for basic auth on sidekiq route

--- config/routes.rb
+++ config/routes.rb
@@ -77,11 +77,5 @@ Rails.application.routes.draw do
 
 
   require 'sidekiq/web'
-  if Rails.env.production?
-    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
-        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
-    end
-  end
   mount Sidekiq::Web, at: 'admin/sidekiq'
 end
