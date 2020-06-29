fd7bf448b66659605f4a30d05b6ac71e255a2bd0
Damien Hogan <damienhogan87@gmail.com>
2020-06-29 15:49:07 +0100

    feature flag for curriculum

--- app/controllers/curriculum/key_stages_controller.rb
+++ app/controllers/curriculum/key_stages_controller.rb
@@ -1,4 +1,12 @@
 class Curriculum::KeyStagesController < ApplicationController
+  before_action :enabled?
+
   def index
+  end
+
+  private
+
+  def enabled?
+    redirect_to root_path unless curriculum_enabled?
   end
 end
--- spec/requests/curriculum/key_stages/index_spec.rb
+++ spec/requests/curriculum/key_stages/index_spec.rb
@@ -2,12 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Curriculum::KeyStagesController do
   describe 'GET #index' do
-    before do
-      get curriculum_key_stages_path
+    context 'when curriculum is enabled' do
+      it 'renders the index template' do
+        get curriculum_key_stages_path
+        expect(response).to render_template(:index)
+      end
     end
 
-    it 'renders the index template' do
-      expect(response).to render_template(:index)
+    context 'when curriculum is not enabled' do
+      before do
+        allow_any_instance_of(Curriculum::KeyStagesController)
+          .to receive(:curriculum_enabled?).and_return(true)
+      end
+
+      it 'redirects to the root path' do
+        get curriculum_key_stages_path
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 end
