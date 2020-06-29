90266d07ba94303ab7809bc927e9185eadfe6482
Damien Hogan <damienhogan87@gmail.com>
2020-06-29 15:58:13 +0100

    fix ordering

--- spec/requests/curriculum/key_stages/index_spec.rb
+++ spec/requests/curriculum/key_stages/index_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe Curriculum::KeyStagesController do
   describe 'GET #index' do
     context 'when curriculum is enabled' do
+      before do
+        allow_any_instance_of(Curriculum::KeyStagesController)
+          .to receive(:curriculum_enabled?).and_return(true)
+      end
+
       it 'renders the index template' do
         get curriculum_key_stages_path
         expect(response).to render_template(:index)
@@ -10,11 +15,6 @@ RSpec.describe Curriculum::KeyStagesController do
     end
 
     context 'when curriculum is not enabled' do
-      before do
-        allow_any_instance_of(Curriculum::KeyStagesController)
-          .to receive(:curriculum_enabled?).and_return(true)
-      end
-
       it 'redirects to the root path' do
         get curriculum_key_stages_path
         expect(response).to redirect_to(root_path)
