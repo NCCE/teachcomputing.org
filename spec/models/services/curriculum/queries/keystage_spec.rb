fb0ce3fad5c3bd6986857fc2b902859c7dca8ced
Dan Halson <dan.halson@raspberrypi.org>
2020-06-30 10:56:28 +0100

    1242: Add test to ensure query is well defined

--- spec/models/services/curriculum/queries/keystage_spec.rb
+++ spec/models/services/curriculum/queries/keystage_spec.rb
@@ -1,15 +1,22 @@
 require 'spec_helper'
 
 RSpec.describe Curriculum::KeyStage do
-  it 'creates a request with the expected params' do
-    url = Curriculum::Connection::CURRICULUM_API_URL
+  let(:url) {Curriculum::Connection::CURRICULUM_API_URL}
 
+  before :each do
     stub_request(:post, url)
-      .to_return(
-        status: 200,
-        body: {}.to_json
-      )
+    .to_return(
+      status: 200,
+      body: {}.to_json
+    )
+  end
 
+  it 'creates valid queries' do
+    expect{described_class.all}.not_to raise_error
+    expect{described_class.one('some_id')}.not_to raise_error
+  end
+
+  it 'creates a request with the expected params' do
     expect(described_class.one('some_id')).to have_requested(:post, url)
       .with(body: hash_including({'variables': {'id': 'some_id'}}))
   end
