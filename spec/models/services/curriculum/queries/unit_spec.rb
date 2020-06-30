e6c7e1c9843e5b488681a7793156ccdef53bb253
Damien Hogan <damienhogan87@gmail.com>
2020-06-30 16:33:35 +0100

    update spec

--- spec/models/services/curriculum/queries/unit_spec.rb
+++ spec/models/services/curriculum/queries/unit_spec.rb
@@ -1,16 +1,19 @@
+
 require 'spec_helper'
 
 RSpec.describe Curriculum::Queries::Unit do
-  it 'creates a request with the expected params' do
-    url = Curriculum::Connection::CURRICULUM_API_URL
+  let(:url) {Curriculum::Connection::CURRICULUM_API_URL}
 
+  before :each do
     stub_request(:post, url)
-      .to_return(
-        status: 200,
-        body: {}.to_json
-      )
+    .to_return(
+      status: 200,
+      body: {}.to_json
+    )
+  end
 
-    expect(described_class.one('some_id')).to have_requested(:post, url)
-      .with(body: hash_including({'variables': {'id': 'some_id'}}))
+  it 'creates valid queries' do
+    expect{described_class.all}.not_to raise_error
+    expect{described_class.one('some_id')}.not_to raise_error
   end
 end
