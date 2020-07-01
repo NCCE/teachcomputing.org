179a87edce35545d63b47b36eac703dfc9292f49
Dan Halson <danhalson@users.noreply.github.com>
2020-07-01 12:20:02 +0100

    1242: Add yearGroup query (#819)
    * 1241: Add yearGroup query
    
    * 1242: Add test to ensure query is well defined
    
    * 1242: Update schema and add test
    
    * 1242: Update namespace

--- app/services/curriculum/queries/year_group.rb
+++ app/services/curriculum/queries/year_group.rb
@@ -1,10 +1,9 @@
-class Curriculum::Queries::KeyStage
-  # TODO: Could this use the schema? (would need an instance of schema in)
-  FIELDS = ['id', 'title', 'shortTitle', 'level', 'ages', 'description', 'yearGroups', 'teacherGuide']
+class Curriculum::Queries::YearGroup
+  FIELDS = ['id', 'keyStage', 'yearNumber', 'description', 'units']
 
   ALL = <<~GRAPHQL
     query {
-      keyStages {
+      yearGroups {
         #{FIELDS.join(' ')}
       }
     }
@@ -12,7 +11,7 @@ class Curriculum::Queries::KeyStage
 
   ONE = <<~GRAPHQL
     query($id: ID!) {
-      keyStage(id: $id) {
+      yearGroup(id: $id) {
         #{FIELDS.join(' ')}
       }
     }
--- spec/models/services/curriculum/queries/year_group_spec.rb
+++ spec/models/services/curriculum/queries/year_group_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe Curriculum::Queries::KeyStage do
+RSpec.describe Curriculum::Queries::YearGroup do
   let(:url) {Curriculum::Connection::CURRICULUM_API_URL}
 
   before :each do
@@ -14,10 +14,5 @@ RSpec.describe Curriculum::Queries::KeyStage do
   it 'creates valid queries' do
     expect{described_class.all}.not_to raise_error
     expect{described_class.one('some_id')}.not_to raise_error
-  end
-
-  it 'creates a request with the expected params' do
-    expect(described_class.one('some_id')).to have_requested(:post, url)
-      .with(body: hash_including({'variables': {'id': 'some_id'}}))
   end
 end
