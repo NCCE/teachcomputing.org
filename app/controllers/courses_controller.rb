fac4b9731a55a2a98d2dc97cb191c3ea45f68039
Damien Hogan <damienh@users.noreply.github.com>
2020-08-11 09:12:45 +0100

    Add Remote option to dropdown and handle filtering at the controller (#899)
    * Add Remote option to dropdown and handle filtering at the controller
    
    * tweak

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -94,6 +94,8 @@ class CoursesController < ApplicationController
         course.online_cpd
       when 'Face to face'
         !course.online_cpd
+      when 'Remote'
+        course.remote_delivered_cpd
       else
         course.occurrences.any? { |oc| oc.address_town == location }
       end
@@ -106,7 +108,7 @@ class CoursesController < ApplicationController
 
     def course_locations(course_occurrences)
       towns = course_occurrences.reduce([]) { |acc, occurrence| !occurrence.online_cpd ? acc.push(occurrence.address_town) : acc }
-      towns.uniq.sort.unshift('Face to face').unshift('Online')
+      towns.reject! { |location| location == 'Remote delivered CPD' }.uniq.sort.unshift('Face to face', 'Online', 'Remote')
     end
 
     def course_programme
