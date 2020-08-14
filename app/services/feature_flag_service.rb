9f8f962114e94132a49e6e5a39bcf1a6a729bb50
Ant Barnes <ant@arayb.com>
2020-08-14 12:12:28 +0100

    Refactor feature flag service
    Make flags easier to test

--- app/services/feature_flag_service.rb
+++ app/services/feature_flag_service.rb
@@ -1,17 +1,25 @@
 class FeatureFlagService
-  class << self
-    def flags
-      @flags ||= {
-        fl_lti_enabled: cast_boolean(ENV['FLAG_FL_LTI_ENABLED'])
-      }
+  FLAGS = {
+    fl_lti_enabled: 'FLAG_FL_LTI_ENABLED'
+  }.freeze
+
+  def initialize(dependencies = {})
+    @flags_to_define = dependencies.fetch(:flags) do
+      FLAGS
     end
-
-    private
-
-      def cast_boolean(string_value)
-        return false unless string_value.present?
-
-        ActiveModel::Type::Boolean.new.cast(string_value)
-      end
   end
+
+  def flags
+    @flags ||= begin
+                 @flags_to_define.map { |k, v| [k, cast_boolean(ENV[v])] }.to_h
+               end
+  end
+
+  private
+
+    def cast_boolean(string_value)
+      return false unless string_value.present?
+
+      ActiveModel::Type::Boolean.new.cast(string_value)
+    end
 end
--- spec/services/feature_flag_service_spec.rb
+++ spec/services/feature_flag_service_spec.rb
@@ -2,15 +2,57 @@ require 'rails_helper'
 
 RSpec.describe FeatureFlagService do
   describe '.flags' do
-    subject(:flags) { described_class.flags }
+    subject(:flags) { described_class.new(flags: { test_flag: 'TEST_FLAG_VAR' }).flags }
 
-    it 'sets boolean values for flags' do
-      expect(flags.values.all? { |x| [TrueClass, FalseClass].include?(x.class) })
-        .to eq(true)
+    context('when env var is "true"') do
+      before do
+        ENV['TEST_FLAG_VAR'] = 'true'
+      end
+
+      it 'sets flag correctly' do
+        expect(flags[:test_flag]).to eq(true)
+      end
     end
 
-    it 'sets the future learn lti flag' do
-      expect(flags[:fl_lti_enabled]).not_to eq(nil)
+    context('when env var is "false"') do
+      before do
+        ENV['TEST_FLAG_VAR'] = 'false'
+      end
+
+      it 'sets flag correctly' do
+        expect(flags[:test_flag]).to eq(false)
+      end
+    end
+
+    context('when env var is empty') do
+      before do
+        ENV['TEST_FLAG_VAR'] = ''
+      end
+
+      it 'sets flag correctly' do
+        expect(flags[:test_flag]).to eq(false)
+      end
+    end
+
+    context('when env var is not set') do
+      it 'sets flag correctly' do
+        expect(flags[:test_flag]).to eq(false)
+      end
+    end
+
+    context('with app defined flags') do
+      subject(:flags) { described_class.new.flags }
+
+      it 'sets the future learn lti flag' do
+        expect(flags[:fl_lti_enabled]).not_to eq(nil)
+      end
+    end
+  end
+
+  describe 'FLAGS' do
+    it 'defines hash of flags correctly' do
+      flag_hash = { fl_lti_enabled: 'FLAG_FL_LTI_ENABLED' }
+      expect(FeatureFlagService::FLAGS).to eq(flag_hash)
     end
   end
 end
