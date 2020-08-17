3694d7d2b25d3abcbc1962710456dbcccf7a9bb3
Dan Halson <dan.halson@raspberrypi.org>
2020-08-17 17:21:57 +0100

    1363: Refactor to use faraday raiseerror middleware

--- app/services/future_learn/request.rb
+++ app/services/future_learn/request.rb
@@ -1,48 +1,16 @@
 module FutureLearn
-  JWT_ALGORITHM = 'HS256'.freeze
   CACHE_EXPIRY = 5.hours
-  BASE_URL = ENV.fetch('FL_PARTNERS_URL').freeze
 
   class Request
     def self.run(endpoint, params = {})
-      @jwt = create_jwt unless @expiry && @expiry > Time.now
-
-      url = "#{BASE_URL}/#{endpoint}"
-      headers = {
-        :Authorization => "Bearer #{@jwt}",
-        'Accept' => 'application/json',
-        'Content-Type' => 'application/json'
-      }
+      @connection = Connection.connect
 
       params_str = params.map { |_, v| v }.join('-')
       res = Rails.cache.fetch("#{endpoint}-#{params_str}", expires_in: CACHE_EXPIRY) do
-        res = Faraday.get(url, params, headers)
+        res = @connection.get(endpoint, params)
       end
 
-      handle_response(res)
-    end
-
-    def self.handle_response(res)
-      case res.status
-      when 302
-        JSON.parse(res.body)
-      when 401
-        raise FutureLearn::Errors::AuthorizationError, "Failed to connect to #{BASE_URL}"
-      when 404
-        Rails.logger.warn "#{Request.name} Record not found"
-        {}
-      end
-    end
-
-    def self.create_jwt
-      now = Time.now
-      @expiry = now + 10.minutes
-      payload = {
-        iss: ENV.fetch('FL_PARTNERS_CONSUMER_KEY'),
-        iat: now.to_i,
-        exp: @expiry.to_i
-      }
-      JWT.encode payload, ENV.fetch('FL_PARTNERS_CONSUMER_SECRET'), JWT_ALGORITHM
+      JSON.parse(res.body)
     end
   end
 end
--- spec/services/future_learn/request_spec.rb
+++ spec/services/future_learn/request_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe FutureLearn::Request do
   let(:vcr_id_base) { "future_learn/#{endpoint}" }
   let(:endpoint) { 'partners/organisation_memberships/find' }
   let(:user_id) { '87e2b457-4bcd-4630-970e-01672b4e0365' }
+  let(:full_endpoint) { "#{ENV.fetch('FL_PARTNERS_URL')}/#{endpoint}?external_learner_id=#{user_id}" }
 
-  before(:each) do |test|
-    VCR.turn_on! unless test.metadata[:no_vcr]
+  before(:each) do
+    VCR.turn_on!
   end
 
-  after(:each) do |test|
-    VCR.turn_off! unless test.metadata[:no_vcr]
+  after(:each) do
+    VCR.turn_off!
   end
 
   describe 'with a valid id' do
@@ -18,60 +19,34 @@ RSpec.describe FutureLearn::Request do
       VCR.use_cassette("#{vcr_id_base}/302") do
         resp = described_class.run(endpoint, { external_learner_id: user_id })
         expect(resp).to eq(
-          { 'uuid' => '7f64ee87-5e77-47bb-8736-3fec909ab1a1', 'href' => 'https://lticourses-api.sandbox.futurelearn.com/partners/organisation_memberships/7f64ee87-5e77-47bb-8736-3fec909ab1a1' }
+          {
+            'uuid' => '7f64ee87-5e77-47bb-8736-3fec909ab1a1',
+            'href' => 'https://lticourses-api.sandbox.futurelearn.com/partners/organisation_memberships/7f64ee87-5e77-47bb-8736-3fec909ab1a1'
+          }
         )
-      end
-    end
-  end
-
-  describe 'when run successively' do
-    before do
-      stub_request(:get, "#{ENV.fetch('FL_PARTNERS_URL')}/#{endpoint}?external_learner_id=87e2b457-4bcd-4630-970e-01672b4e0365")
-    end
-
-    context 'when within the expiry time', no_vcr: true do
-      it 're-uses the jwt' do
-        described_class.run(endpoint, { external_learner_id: user_id })
-
-        allow(described_class).to receive(:create_jwt).and_call_original
-        described_class.run(endpoint, { external_learner_id: user_id })
-
-        expect(described_class).not_to have_received(:create_jwt)
-      end
-    end
-
-    context 'when outside the expiry time' do
-      it 'uses a new jwt', no_vcr: true do
-        described_class.run(endpoint, { external_learner_id: user_id })
-        allow(described_class).to receive(:create_jwt)
-
-        travel_to Time.now + 15.minutes
-        described_class.run(endpoint, { external_learner_id: user_id })
-
-        expect(described_class).to have_received(:create_jwt)
-        travel_back
       end
     end
   end
 
   describe 'with an invalid jwt' do
     before do
-      stub_request(:get, "#{ENV.fetch('FL_PARTNERS_URL')}/#{endpoint}?external_learner_id=87e2b457-4bcd-4630-970e-01672b4e0365")
+      stub_request(:get, full_endpoint)
         .to_return(status: [401, 'Some unauthorized msg'])
     end
 
-    it 'raises an error' do
+    it 'raises the expected error' do
       expect {
         described_class.run(endpoint, { external_learner_id: user_id })
-      }.to raise_error(FutureLearn::Errors::AuthorizationError, 'Failed to connect to https://lticourses-api.sandbox.futurelearn.com')
+      }.to raise_error(Faraday::UnauthorizedError)
     end
   end
 
   describe 'with an invalid id' do
-    it 'returns an empty hash' do
+    it 'raises the expected error' do
       VCR.use_cassette("#{vcr_id_base}/404") do
-        resp = described_class.run(endpoint, { external_learner_id: 'wrong' })
-        expect(resp).to eq({})
+        expect {
+          described_class.run(endpoint, { external_learner_id: 'wrong' })
+        }.to raise_error(Faraday::ResourceNotFound)
       end
     end
   end
