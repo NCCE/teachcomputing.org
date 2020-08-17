3bff2f1715a313ebf0364e998f4f5516498c9fdc
Dan Halson <dan.halson@raspberrypi.org>
2020-08-17 14:55:25 +0100

    1363: Add more tests

--- spec/services/future_learn/request_spec.rb
+++ spec/services/future_learn/request_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe FutureLearn::Request do
   let(:endpoint) { 'partners/organisation_memberships/find' }
   let(:user_id) { '87e2b457-4bcd-4630-970e-01672b4e0365' }
 
-  before(:all) do
-    VCR.turn_on!
+  before(:each) do |test|
+    VCR.turn_on! unless test.metadata[:no_vcr]
   end
 
-  after(:all) do
-    VCR.turn_off!
+  after(:each) do |test|
+    VCR.turn_off! unless test.metadata[:no_vcr]
   end
 
-  describe 'a request with a valid id' do
+  describe 'with a valid id' do
     it 'returns the expected data for a valid learner' do
       VCR.use_cassette("#{vcr_id_base}/302") do
         resp = described_class.run(endpoint, { external_learner_id: user_id })
@@ -24,23 +24,50 @@ RSpec.describe FutureLearn::Request do
     end
   end
 
-  describe 'multiple subsequent requests' do
-    it 're-use the same jwt' do
-      jwt_matcher = lambda do |r1, r2|
-        r1.headers['Authorization'] == r2.headers['Authorization']
-      end
+  describe 'when run successively' do
+    before do
+      stub_request(:get, "#{ENV.fetch('FL_PARTNERS_URL')}/#{endpoint}?external_learner_id=87e2b457-4bcd-4630-970e-01672b4e0365")
+    end
 
-      VCR.use_cassette("#{vcr_id_base}/jwt_reuse_1", :match_requests_on => [:method, jwt_matcher]) do
+    context 'when within the expiry time', no_vcr: true do
+      it 're-uses the jwt' do
         described_class.run(endpoint, { external_learner_id: user_id })
-      end
 
-      VCR.use_cassette("#{vcr_id_base}/jwt_reuse_2", :match_requests_on => [:method, jwt_matcher]) do
+        allow(described_class).to receive(:create_jwt).and_call_original
         described_class.run(endpoint, { external_learner_id: user_id })
+
+        expect(described_class).not_to have_received(:create_jwt)
+      end
+    end
+
+    context 'when outside the expiry time' do
+      it 'uses a new jwt', no_vcr: true do
+        described_class.run(endpoint, { external_learner_id: user_id })
+        allow(described_class).to receive(:create_jwt)
+
+        travel_to Time.now + 15.minutes
+        described_class.run(endpoint, { external_learner_id: user_id })
+
+        expect(described_class).to have_received(:create_jwt)
+        travel_back
       end
     end
   end
 
-  describe 'a request with an invalid id' do
+  describe 'with an invalid jwt' do
+    before do
+      stub_request(:get, "#{ENV.fetch('FL_PARTNERS_URL')}/#{endpoint}?external_learner_id=87e2b457-4bcd-4630-970e-01672b4e0365")
+        .to_return(status: [401, 'Some unauthorized msg'])
+    end
+
+    it 'raises an error' do
+      expect {
+        described_class.run(endpoint, { external_learner_id: user_id })
+      }.to raise_error(FutureLearn::Errors::AuthorizationError, 'Failed to connect to https://lticourses-api.sandbox.futurelearn.com')
+    end
+  end
+
+  describe 'with an invalid id' do
     it 'returns an empty hash' do
       VCR.use_cassette("#{vcr_id_base}/404") do
         resp = described_class.run(endpoint, { external_learner_id: 'wrong' })
