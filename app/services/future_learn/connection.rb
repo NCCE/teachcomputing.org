f47f2c3495cd00822982d226e58b7b7429ace241
Dan Halson <danhalson@users.noreply.github.com>
2020-08-18 08:36:49 +0100

    1363: Add service to integrate fl api (#902)
    * 1363: Add service to integrate fl api
    
    * Update env var names
    
    * 1363: Add vars
    
    * 1363: Update envs
    
    * 1363: Add more tests
    
    * 1363: Refactor to use faraday raiseerror middleware

---
+++ app/services/future_learn/connection.rb
@@ -0,0 +1,39 @@
+module FutureLearn
+  BASE_URL = ENV.fetch('FL_PARTNERS_URL').freeze
+  KEY = ENV.fetch('FL_PARTNERS_CONSUMER_KEY').freeze
+  SECRET = ENV.fetch('FL_PARTNERS_CONSUMER_SECRET').freeze
+  JWT_ALGORITHM = 'HS256'.freeze
+
+  class Connection
+    # Don't memoize this as the jwt will expire, if unavoidable use it conditionally
+    # with `jwt_valid?`
+    def self.connect
+      @jwt = create_jwt unless jwt_valid?
+      Faraday.new(
+        url: BASE_URL,
+        headers: {
+          :Authorization => "Bearer #{@jwt}",
+          'Accept' => 'application/json',
+          'Content-Type' => 'application/json'
+        }
+      ) do |c|
+        c.use Faraday::Response::RaiseError
+      end
+    end
+
+    def self.create_jwt
+      now = Time.now
+      @expiry = now + 10.minutes
+      payload = {
+        iss: KEY,
+        iat: now.to_i,
+        exp: @expiry.to_i
+      }
+      JWT.encode payload, SECRET, JWT_ALGORITHM
+    end
+
+    def self.jwt_valid?
+      @expiry && @expiry > Time.now
+    end
+  end
+end
