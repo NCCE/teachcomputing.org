af0e8035ec8d5e57a39565d93405491a0382a174
Ant Barnes <e1daf3a5@opayq.com>
2020-08-20 08:25:58 +0100

    Remove curriculum flag (#907)
    Make all behaviour default to curriculum enabled

--- spec/requests/curriculum/key_stages/index_spec.rb
+++ spec/requests/curriculum/key_stages/index_spec.rb
@@ -7,21 +7,11 @@ RSpec.describe Curriculum::KeyStagesController do
     context 'when curriculum is enabled' do
       before do
         stub_a_valid_request(key_stages_json_response)
-
-        allow_any_instance_of(described_class)
-          .to receive(:curriculum_enabled?).and_return(true)
       end
 
       it 'renders the index template' do
         get curriculum_key_stages_path
         expect(response).to render_template(:index)
-      end
-    end
-
-    context 'when curriculum is not enabled' do
-      it 'redirects to the root path' do
-        get curriculum_key_stages_path
-        expect(response).to redirect_to(root_path)
       end
     end
   end
