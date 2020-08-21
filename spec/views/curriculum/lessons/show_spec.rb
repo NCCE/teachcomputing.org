3b2679e79fb1a5e8a378eaaa7f0b280ad574853e
Magdalena Jadach <magdalena@raspberrypi.org>
2020-08-21 13:38:31 +0100

    Fix lesson tests

--- spec/views/curriculum/lessons/show_spec.rb
+++ spec/views/curriculum/lessons/show_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe('curriculum/lessons/show', type: :view) do
+	let(:lesson_json) { File.new('spec/support/curriculum/views/lesson.json').read }
+
+	let(:unit_json) { File.new('spec/support/curriculum/views/unit.json').read }
+
   before do
-    render
+		#stub_a_valid_request(unit_json_response)
+		json = JSON.parse(unit_json, object_class: OpenStruct).data
+		assign(:unit, json.unit)
+
+		json = JSON.parse(lesson_json, object_class: OpenStruct).data
+		assign(:lesson, json.lesson)
+		assign(:id, 'id')
+		render :template => "curriculum/lessons/show.html.erb"
   end
 
   it 'has a title' do
-    expect(rendered).to have_css('.hero__heading', text: 'L6: Using a computer responsibly')
+    expect(rendered).to have_css('.hero__heading', text: 'Lesson 1')
   end
 
   it 'has a lesson label' do
@@ -14,7 +25,7 @@ RSpec.describe('curriculum/lessons/show', type: :view) do
   end
 
   it 'has a download button' do
-    expect(rendered).to have_link('Download all lesson files', href: '')
+    expect(rendered).to have_link('Download all lesson files', href: 'https://teachcomputing.org')
   end
 
 end
