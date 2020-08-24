de8eb36c5af83292708e31510a1bac78103254d8
MagdalenaJadach <magdalena@raspberrypi.org>
2020-08-24 14:54:31 +0100

    Test templates (#846)
    * Add Lesson and Unit mobile versions, tweak Unit
    
    * Empty test files
    
    * Test components
    
    * Fix broken test path
    
    * Revert
    
    * Revert changes - wrong branch
    
    * Revert icon change
    
    * Re-add removed icon
    
    * Fix tests
    
    * Tweak test copy
    
    * Finish tests
    
    * Fixing some tests
    
    * Remove duplicated file
    
    * Fix hero component
    
    * Fix lesson tests
    
    * Fix unit tests
    
    * Remove post debugging entry
    
    * Terminalowa kluseczka
    
    * Remove tests for code that don't exist anymore
    
    * Tidy up a typo
    
    * Fix lessons test
    
    Co-authored-by: Dan Halson <danhalson@users.noreply.github.com>

---
+++ spec/views/curriculum/units/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe('curriculum/units/show', type: :view) do
+	let(:unit_json) { File.new('spec/support/curriculum/views/unit.json').read }
+
+  before do
+		json = JSON.parse(unit_json, object_class: OpenStruct).data
+		assign(:unit, json.unit)
+		assign(:id, 'id')
+		render
+  end
+
+  it 'has a title' do
+    expect(rendered).to have_css('.hero__heading', text: 'Unit 1')
+  end
+
+  it 'links to feedback form' do
+    expect(rendered).to have_link('Provide your feedback', href: 'https://forms.gle/qT2XqoCecYjLLohC6')
+  end
+end
