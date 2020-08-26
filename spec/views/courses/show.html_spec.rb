6af8ca391ce468f3450d9febbfe178302409166a
Dan Halson <dan.halson@raspberrypi.org>
2020-08-26 16:50:02 +0100

    1370: Add tests and refactor

--- spec/views/courses/show.html_spec.rb
+++ spec/views/courses/show.html_spec.rb
@@ -4,49 +4,57 @@ RSpec.describe('courses/show', type: :view) do
   let(:programme) { create(:cs_accelerator) }
 
   before do
-    stub_age_groups
     stub_course_templates
-    stub_face_to_face_occurrences
-    stub_online_occurrences
-    stub_subjects
-
     courses = Achiever::Course::Template.all
-    course_occurrences = Achiever::Course::Occurrence.face_to_face + Achiever::Course::Occurrence.online
-
     @course = courses[0]
-    assign(:other_courses, courses)
-    assign(:age_groups, Achiever::Course::AgeGroup.all)
-    assign(:occurrences, course_occurrences)
 
+    assign(:course, @course)
+    assign(:other_courses, [])
+    assign(:age_groups, {})
+    assign(:occurrences, [])
+  end
+
+  describe 'renders' do
+    before do
+      render
+    end
+
+    it 'the hero partial' do
+      expect(rendered).to render_template(:partial => '_hero')
+    end
+
+    it 'the asides partial' do
+      expect(rendered).to render_template(:partial => '_aside-section')
+    end
+
+    it 'a course summary' do
+      expect(rendered).to have_css('h2.govuk-body-m', text: @course.meta_description)
+    end
+
+    it 'the courses details partial' do
+      expect(rendered).to render_template(:partial => '_courses-details')
+    end
+
+    it 'a course description' do
+      expect(rendered).to have_css('.external-content')
+      expect(rendered).to have_text(strip_tags(sanitize_stem_html(@course.summary)))
+      expect(rendered).to have_text(strip_tags(sanitize_stem_html(@course.outcomes)))
+    end
+
+    it 'the courses card partial' do
+      expect(rendered).to render_template(:partial => '_courses-card')
+    end
+
+    it 'the courses list partial' do
+      expect(rendered).to render_template(:partial => '_courses-list')
+    end
+  end
+
+  it 'has attempted to sanitize the description html' do
+    allow(view).to receive(:sanitize_stem_html)
     render
-  end
 
-  it 'renders the hero component' do
-    expect(rendered).to render_template(:partial => '_hero')
-  end
-
-  it 'renders the asides partial' do
-    expect(rendered).to render_template(:partial => '_aside-section')
-  end
-
-  it 'has a course summary' do
-    expect(rendered).to have_css('h2.govuk-body-m', text: @course.meta_description)
-  end
-
-  it 'renders the courses details partial' do
-    expect(rendered).to render_template(:partial => '_courses-details')
-  end
-
-  it 'has a course description' do
-    expect(rendered).to have_text(strip_tags(sanitize_stem_html(@course.summary)))
-    expect(rendered).to have_text(strip_tags(sanitize_stem_html(@course.outcomes)))
-  end
-
-  it 'renders the courses card partial' do
-    expect(rendered).to render_template(:partial => '_courses-card')
-  end
-
-  it 'renders the courses list partial' do
-    expect(rendered).to render_template(:partial => '_courses-list')
+    expect(view).to have_received(:sanitize_stem_html).with(@course.summary)
+    expect(view).to have_received(:sanitize_stem_html).with(@course.outcomes)
   end
 end
