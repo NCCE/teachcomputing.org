1590266758958672bccf1e8ba51b68ed0b702684
MagdalenaJadach <magdalena@raspberrypi.org>
2020-08-27 11:50:27 +0100

    Remove old resources mailers and jobs (#918)

--- app/mailers/previews/resources_feedback_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-class ResourcesFeedbackMailerPreview < ActionMailer::Preview
-  def feedback_request
-    user = User.last
-    ResourcesFeedbackMailer.with(user: user, year: '3').feedback_request
-  end
-end
