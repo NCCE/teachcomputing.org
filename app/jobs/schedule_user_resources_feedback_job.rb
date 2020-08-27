655431cb8204d5b5d819c14e1079603b9688ad26
MagdalenaJadach <magdalena@raspberrypi.org>
2020-08-27 11:50:27 +0100

    Remove old resources mailers and jobs (#918)

--- app/jobs/schedule_user_resources_feedback_job.rb
+++
@@ -1,7 +0,0 @@
-class ScheduleUserResourcesFeedbackJob < ApplicationJob
-  queue_as :default
-  
-  def perform(user, year)
-    ResourcesFeedbackMailer.with(user: user, year: year).feedback_request.deliver_now
-  end
-end
