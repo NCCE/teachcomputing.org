19027166d3e0619fdaeab8bd0a0ff5ea705b0baf
Ant Barnes <e1daf3a5@opayq.com>
2020-09-07 14:25:56 +0100

    1397 ratings missing user (#931)
    * Send user_id with rating requests

--- app/controllers/concerns/curriculum/rateable.rb
+++ app/controllers/concerns/curriculum/rateable.rb
@@ -4,18 +4,26 @@ module Curriculum
 
     NEW_FEEDBACK = 'Thank you for your feedback!'.freeze
     EXISTING_FEEDBACK = 'You have already provided a rating, thanks!'.freeze
+    NO_USER_FEEDBACK = 'You must be logged in to leave a rating.'.freeze
 
     def rate
       raise NoMethodError unless respond_to?(:client, true)
 
       id = params[:id]
       polarity = params[:polarity]
+      user_id = params[:user_id]
       message = ''
 
-      if helpers.user_has_rated?(id)
+      user = current_user.present? ? current_user : User.find_by(id: user_id)
+
+      if user.nil?
+        message = NO_USER_FEEDBACK
+        # It shouldn't be possible to make this request without being logged in.
+        Raven.capture_message("Information missing when adding rating, current_user: #{current_user&.id}, user_id param: #{user_id}")
+      elsif helpers.user_has_rated?(id)
         message = EXISTING_FEEDBACK
       else
-        response = add_rating(id, polarity)
+        response = add_rating(id, polarity, user)
         store_rating(id)
         message = NEW_FEEDBACK
       end
@@ -27,8 +35,11 @@ module Curriculum
       }, status: :ok
     end
 
-    def add_rating(id, polarity)
-      achiever_contact_no = current_user&.stem_achiever_contact_no
+    def add_rating(id, polarity, user)
+      achiever_contact_no = user.stem_achiever_contact_no
+
+      Raven.capture_message("User has no stem_achiever_contact_no: #{user.id}") unless achiever_contact_no.present?
+
       case polarity.to_sym
       when :positive
         client.add_positive_rating(
