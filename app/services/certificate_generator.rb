b3fd860894cc9938858feaa1f989958780eb42ca
Ant Barnes <ant@arayb.com>
2020-09-10 15:06:13 +0100

    Add spec for certificate generator

--- app/services/certificate_generator.rb
+++ app/services/certificate_generator.rb
@@ -2,16 +2,21 @@ class CertificateGenerator
   CSA_PROGRAMME_TITLE = 'GCSE Computer Science Subject Knowledge'.freeze
   PRIMARY_PROGRAMME_TITLE = 'Primary Computing Teaching'.freeze
 
-  def initialize(user:, programme:, transition:)
+  def initialize(user:, programme:, transition:, dependencies: {})
+    @src_path = dependencies.fetch(:src_path) do
+      File.join(Rails.root, 'app', 'assets', 'pdf', 'ncce-cert-src.pdf')
+    end
+
+    @output_path = dependencies.fetch(:output_path) do
+      File.join(Rails.root, 'tmp', "#{SecureRandom.uuid}.pdf")
+    end
+
     @user = user
     @programme = programme
     @transition = transition
   end
 
   def generate_pdf
-    temp_id = SecureRandom.uuid
-    temp_path = File.join(Rails.root, 'tmp', "#{temp_id}.pdf")
-
     user_name = "#{@user.first_name} #{@user.last_name}"
     programme_title = select_programme_title
     date_awarded = @transition.created_at.strftime('%d/%m/%Y')
@@ -19,7 +24,7 @@ class CertificateGenerator
     slug = @programme.slug
     teacher_ref_no = @user.teacher_reference_number
 
-    Prawn::Document.generate(temp_path) do
+    Prawn::Document.generate(@output_path) do
       font_families.update('Roboto' => {
                              normal: Rails.root.join('app', 'assets', 'fonts', 'Roboto-Regular.ttf').to_s,
                              bold: Rails.root.join('app', 'assets', 'fonts', 'Roboto-Bold.ttf').to_s
@@ -67,15 +72,15 @@ class CertificateGenerator
       end
     end
 
-    src_pdf = CombinePDF.load(File.join(Rails.root, 'app', 'assets', 'pdf', 'ncce-cert-src.pdf'))
-    text_pdf = CombinePDF.load(temp_path).pages[0]
+    src_pdf = CombinePDF.load(@src_path)
+    text_pdf = CombinePDF.load(@output_path).pages[0]
     src_pdf.pages[0] << text_pdf
 
-    src_pdf.save(temp_path)
+    src_pdf.save(@output_path)
 
     filename = "#{programme_title.parameterize}-certificate-#{cert_number}.pdf"
 
-    { path: temp_path, filename: filename }
+    { path: @output_path, filename: filename }
   end
 
   private
---
+++ spec/services/certificate_generator_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe CertificateGenerator do
+  let(:output_path) { 'tmp/test_generated_certificate.pdf' }
+  let(:user) { create(:user) }
+  let(:programme) { create(:cs_accelerator) }
+  let(:user_programme_enrolment) do
+    create(:user_programme_enrolment,
+           user_id: user.id,
+           programme_id: programme.id)
+  end
+
+  let(:transition) do
+    user_programme_enrolment.transition_to(:complete, certificate_number: 20)
+    user_programme_enrolment.reload.last_transition
+  end
+
+  let(:generator) do
+    described_class.new(
+      user: user,
+      programme: programme,
+      transition: transition,
+      dependencies: { output_path: output_path }
+    )
+  end
+
+  describe '#generate_pdf' do
+    subject { generator.generate_pdf }
+
+    before do
+      File.delete(output_path) if File.exist?(output_path)
+    end
+
+    after do
+      File.delete(output_path)
+    end
+
+    it 'creates a pdf file' do
+      expect(File.exist?(output_path)).to eq(false)
+      generator.generate_pdf
+      expect(File.exist?(output_path)).to eq(true)
+    end
+
+    it 'returns filepath and filename' do
+      expect(generator.generate_pdf)
+        .to eq(
+          {
+            filename: 'gcse-computer-science-subject-knowledge-certificate-202009-020.pdf',
+            path: 'tmp/test_generated_certificate.pdf'
+          }
+        )
+    end
+  end
+end
