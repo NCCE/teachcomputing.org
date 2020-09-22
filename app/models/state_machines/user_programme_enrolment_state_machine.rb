4c9460961b7e00ff88cd25a1e55bfd0e673d8171
Damien Hogan <damienh@users.noreply.github.com>
2020-09-22 15:58:27 +0100

    Add flagged to user programme enolments and ensure records cannot transition to complete if its true (#942)

--- app/models/state_machines/user_programme_enrolment_state_machine.rb
+++ app/models/state_machines/user_programme_enrolment_state_machine.rb
@@ -10,6 +10,10 @@ class StateMachines::UserProgrammeEnrolmentStateMachine
   transition from: :pending, to: %i[complete enrolled]
   transition from: :complete, to: %i[pending enrolled]
 
+  guard_transition(to: :complete) do |programme_enrolment|
+    !programme_enrolment.flagged?
+  end
+
   after_transition(to: :complete) do |programme_enrolment|
     CompleteCertificateEmailJob.perform_later(programme_enrolment.user,
                                                programme_enrolment.programme)
