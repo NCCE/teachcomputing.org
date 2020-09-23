29f74e2e42952074d830c5ee0a4449bf48cc94cd
Ant Barnes <ant@arayb.com>
2020-09-23 10:39:15 +0100

    Add cache invalidator class and call from controller

---
+++ app/services/cache_invalidator.rb
@@ -0,0 +1,26 @@
+class CacheInvalidator
+  def initialize(resource:, identifier:)
+    @resource = resource
+    @identifier = identifier
+    @namespace = 'curriculum'
+  end
+
+  def run
+    Rails.cache.delete(resource_single_key, namespace: @namespace)
+    Rails.cache.delete(resource_list_key, namespace: @namespace) if resource_list_cached?
+  end
+
+  private
+
+    def resource_single_key
+      "#{@resource}--#{@identifier}"
+    end
+
+    def resource_list_key
+      "#{@resource}--all"
+    end
+
+    def resource_list_cached?
+      @resource == 'key_stage'
+    end
+end
