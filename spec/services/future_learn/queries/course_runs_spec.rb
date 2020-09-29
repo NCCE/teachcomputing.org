8e73168ef0e1bf0f399454c6fd189f71555478d9
Damien Hogan <damienh@users.noreply.github.com>
2020-09-29 09:53:22 +0100

    Class for fetching course runs (#952)
    * WIP class for fetching course runs
    
    * Add spec
    
    * remove extra slash
    
    Co-authored-by: Dan Halson <danhalson@users.noreply.github.com>

---
+++ spec/services/future_learn/queries/course_runs_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe FutureLearn::Queries::CourseRuns do
+  let(:vcr_id_base) { "/future_learn//partners/course_runs/" }
+  let(:full_endpoint) { "#{ENV.fetch('FL_PARTNERS_URL')}/future_learn/partners/course_runs" }
+
+  before(:each) do
+    VCR.turn_on!
+  end
+
+  after(:each) do
+    VCR.turn_off!
+  end
+
+  describe 'all' do
+    it 'returns all the course runs and their data associated with the NCCE organisation' do
+      VCR.use_cassette("#{vcr_id_base}200") do
+        expect(described_class.all.first).to respond_to(:uuid, :href, :title, :start_time, :weeks_count)
+      end
+    end
+  end
+end
