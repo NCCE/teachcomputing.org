68df6923b3c19e75022486b03382edb64db6d90a
Dan Halson <dan.halson@raspberrypi.org>
2020-10-01 11:34:38 +0100

    1402: Add test

--- spec/views/curriculum/_rating.html_spec.rb
+++ spec/views/curriculum/_rating.html_spec.rb
@@ -28,4 +28,17 @@ RSpec.describe('curriculum/_rating', type: :view) do
       expect(rendered).to have_link(nil, href: '/curriculum/rating/units/negative/an_id/user_id')
     end
   end
+
+  describe 'a user who has previously done a rating' do
+    before do
+      allow_any_instance_of(CurriculumHelper)
+        .to receive(:user_has_rated?).and_return(true)
+
+      render partial: 'curriculum/rating', locals: { path: :create_curriculum_unit_rating_path, comment_path: :update_curriculum_unit_rating, id: 'an_id', user_id: 'user_id' }
+    end
+
+    it 'renders with the expected message' do
+      expect(rendered).to have_text('You have already provided a rating, thanks!')
+    end
+  end
 end
