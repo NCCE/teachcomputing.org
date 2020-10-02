9d5bc642c142bb3f87056e828ba5f5d4930e1121
Dan Halson <dan.halson@raspberrypi.org>
2020-10-02 12:17:21 +0100

    1434: Small tweak

--- app/services/future_learn/request.rb
+++ app/services/future_learn/request.rb
@@ -5,8 +5,7 @@ module FutureLearn
     def self.run(endpoint, params = [])
       @connection = Connection.connect
 
-      params_str = params.join('/')
-      endpoint_with_params = "#{endpoint}/#{params_str}"
+      endpoint_with_params = "#{endpoint}/#{params.join('/')}"
       res = Rails.cache.fetch(endpoint_with_params, expires_in: CACHE_EXPIRY) do
         res = @connection.get(endpoint_with_params)
       end
