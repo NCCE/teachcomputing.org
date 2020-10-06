3e2f672fed8e1118574a1491c8b3ce794f3ecc76
Dan Halson <danhalson@users.noreply.github.com>
2020-10-06 10:36:57 +0100

    1434: Add org memberships (#957)
    * 1434: Add org memberships
    
    * 1434: Small tweak
    
    * 1434: Add tests

--- spec/services/future_learn/connection_spec.rb
+++ spec/services/future_learn/connection_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe FutureLearn::Connection do
-  let(:endpoint) { 'partners/organisation_memberships/find' }
+  let(:endpoint) { 'partners/organisation_memberships' }
   let(:user_id) { '87e2b457-4bcd-4630-970e-01672b4e0365' }
-  let(:full_endpoint) { "#{ENV.fetch('FL_PARTNERS_URL')}/#{endpoint}?external_learner_id=#{user_id}" }
+  let(:full_endpoint) { "#{ENV.fetch('FL_PARTNERS_URL')}/#{endpoint}/#{user_id}" }
 
   describe 'when run successively' do
     before do
@@ -11,25 +11,25 @@ RSpec.describe FutureLearn::Connection do
         .to_return(status: 200, body: {}.to_json)
     end
 
-    context 'when within the expiry time', no_vcr: true do
+    context 'when within the expiry time' do
       it 're-uses the jwt' do
-        FutureLearn::Request.run(endpoint, { external_learner_id: user_id })
+        FutureLearn::Request.run(endpoint, user_id)
 
         allow(described_class).to receive(:create_jwt).and_call_original
-        FutureLearn::Request.run(endpoint, { external_learner_id: user_id })
+        FutureLearn::Request.run(endpoint, user_id)
 
         expect(described_class).not_to have_received(:create_jwt)
       end
     end
 
     context 'when outside the expiry time' do
-      it 'uses a new jwt', no_vcr: true do
-        FutureLearn::Request.run(endpoint, { external_learner_id: user_id })
+      it 'uses a new jwt' do
+        FutureLearn::Request.run(endpoint, user_id)
 
         travel_to Time.now + 20.minutes
 
         allow(described_class).to receive(:create_jwt).and_call_original
-        FutureLearn::Request.run(endpoint, { external_learner_id: user_id })
+        FutureLearn::Request.run(endpoint, user_id)
 
         expect(described_class).to have_received(:create_jwt)
 
