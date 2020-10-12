0b8e3c204d31a6505c8778866a343d803c5926a1
Magdalena Jadach <magdalena@raspberrypi.org>
2020-10-12 13:18:48 +0100

    Contributing partners

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2020_09_21_144008) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
