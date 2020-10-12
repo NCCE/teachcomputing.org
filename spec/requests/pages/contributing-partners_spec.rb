bad9dfb4fb84ebe317550e0dd24fd14bcf638c75
Magdalena Jadach <magdalena@raspberrypi.org>
2020-10-12 12:52:33 +0100

    Fix merge conflict

---
+++ spec/requests/pages/contributing-partners_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe PagesController do
+
+  describe 'GET #contributing-partners' do
+		before do
+      get contributing_partners_path
+    end
+
+		it 'shows the page' do
+			expect(response).to render_template('pages/contributing-partners')
+		end
+  end
+
+end
