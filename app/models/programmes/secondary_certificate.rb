4b98da7c43b4e158b4e1f05b2d4c6a154a323a6a
Dan Halson <dan.halson@raspberrypi.org>
2020-10-23 09:42:16 +0100

    1461: Add method to check enrolment status

--- app/models/programmes/secondary_certificate.rb
+++ app/models/programmes/secondary_certificate.rb
@@ -8,5 +8,11 @@ module Programmes
     def user_completed_diagnostic?(user)
       false
     end
+
+    def user_is_eligible?(user)
+      programme = Programme.find_by(slug: 'cs-accelerator')
+      enrolment = UserProgrammeEnrolment.find_by(user_id: user.id, programme_id: programme.id)
+      enrolment.current_state == :complete.to_s
+    end
   end
 end
