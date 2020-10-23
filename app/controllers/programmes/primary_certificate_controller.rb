a42ae9b0af0c697028ab4cb433e9152a0ecefa5f
Dan Halson <dan.halson@raspberrypi.org>
2020-10-23 14:35:51 +0100

    1421: Review comments

---
+++ app/controllers/programmes/primary_certificate_controller.rb
@@ -0,0 +1,92 @@
+module Programmes
+  class PrimaryCertificateController < ApplicationController
+    layout 'full-width'
+    before_action :authenticate_user!
+    before_action :find_programme, only: %i[show complete certificate pending]
+    before_action :user_enrolled?, only: %i[show complete certificate pending]
+    before_action :user_completed_diagnostic?, only: %i[show], if: -> { @programme.slug == 'primary-certificate' }
+    before_action :user_programme_enrolment_pending?, only: %i[show complete certificate], if: -> { @programme.slug == 'primary-certificate' }
+
+    def show
+      @user_programme_achievements = UserProgrammeAchievements.new(@programme, current_user)
+      render "programmes/#{@programme.slug}/show"
+    end
+
+    def complete
+      return redirect_to programme_path(@programme.slug) unless @programme.user_completed?(current_user)
+
+      @user_programme_achievements = UserProgrammeAchievements.new(@programme, current_user)
+
+      @complete_achievements = current_user.achievements.without_category('action')
+                                          .without_category('diagnostic')
+                                          .for_programme(@programme).sort_complete_first
+
+      render "programmes/#{@programme.slug}/complete"
+    end
+
+    def certificate
+      return redirect_to programme_path(@programme.slug) unless @programme.user_completed?(current_user)
+
+      get_certificate_details
+
+      generator = CertificateGenerator.new(
+        user: current_user,
+        programme: @programme,
+        transition: @transition
+      )
+
+      pdf_details = generator.generate_pdf
+
+      send_file(
+        pdf_details[:path],
+        filename: pdf_details[:filename],
+        type: 'application/pdf',
+        disposition: 'inline'
+      )
+    end
+
+    def pending
+      return redirect_to programme_complete_path(@programme.slug) if enrolment.current_state == 'complete'
+
+      @complete_achievements = current_user.achievements.without_category('action')
+                                          .without_category('diagnostic')
+                                          .for_programme(@programme).sort_complete_first
+
+      @enrolment = current_user.user_programme_enrolments.find_by(programme_id: @programme.id)
+
+      render "programmes/#{@programme.slug}/pending"
+    end
+
+    private
+
+      def enrolment
+        current_user.user_programme_enrolments.find_by(programme_id: @programme.id)
+      end
+
+      def find_programme
+        @programme = Programme.enrollable.find_by!(slug: params[:slug])
+      end
+
+      def get_certificate_details
+        @transition = enrolment.last_transition
+      end
+
+      def user_completed_diagnostic?
+        questionnaire = Questionnaire.find_by(slug: 'primary-certificate-enrolment-questionnaire')
+        response = QuestionnaireResponse.find_by(user: current_user, questionnaire: questionnaire)
+        return true if response && response.current_state == 'complete'
+
+        # Navigate directly to the last question reached, or question_1.
+        question = response&.current_question ? "question_#{response.current_question}" : 'question_1'
+        redirect_to primary_certificate_diagnostic_path(question.to_sym)
+      end
+
+      def user_enrolled?
+        redirect_to "/#{@programme.slug}" unless @programme.user_enrolled?(current_user)
+      end
+
+      def user_programme_enrolment_pending?
+        redirect_to programme_pending_path(@programme.slug) if enrolment.current_state == 'pending'
+      end
+  end
+end
