59d03dd97013b4abbc1148f1470909b9b197dd47
Ant Barnes <ant@arayb.com>
2020-10-27 12:19:54 +0000

    Add path to programme models for navigation helper

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -6,10 +6,7 @@ module NavigationHelper
     achieved_credits = programme.credits_achieved_for_certificate(current_user)
     max_credits = programme.max_credits_for_certificate
 
-    path = programme.slug == :primary_certificate.to_s ? primary_certificate_path : programme_path(programme.slug)
-
-
-    link_to(path, class: 'govuk-header__link ncce-header__navigation-certification-progress-bar-container') do
+    link_to(programme.path, class: 'govuk-header__link ncce-header__navigation-certification-progress-bar-container') do
       "Your certificate <progress class='ncce-header__navigation-certification-progress-bar' aria-label='You have #{achieved_credits} out of #{max_credits} credits' value='#{achieved_credits}' max='#{max_credits}'></progress>".html_safe
     end
   end
