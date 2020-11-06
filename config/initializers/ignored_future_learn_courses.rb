5896d93d4bfd7d70cdde67af94995cbc04fe651a
Ant Barnes <ant@arayb.com>
2020-11-06 16:56:42 +0000

    Fix initializer

--- config/initializers/ignored_future_learn_courses.rb
+++ config/initializers/ignored_future_learn_courses.rb
@@ -3,6 +3,6 @@
 # IDs of these courses will be added to an environment variable and we create a
 # constant here to access them in jobs
 
-str = ENV['FL_PARTNERS_IGNORED_COURSE_UUIDS']
+str = ENV['FL_PARTNERS_IGNORED_COURSE_UUIDS'] || ''
 
 FL_PARTNERS_IGNORED_COURSE_UUIDS = str.split(',')
