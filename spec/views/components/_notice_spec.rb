daf9fdbda79df878136424fcacdcfcd4679ee3c1
MagdalenaJadach <magdalena@raspberrypi.org>
2020-11-10 17:31:15 +0000

    Re-add covid banner (#1001)
    * Re-add covid banner
    
    * Fix the test
    
    * Tweak the size of the heading
    
    * Make title bold
    
    * Rename test
    
    * Rename test

---
+++ spec/views/components/_notice_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe('components/_notice', type: :view) do
+  before do
+    render
+  end
+
+  it 'has a tag' do
+    expect(rendered).to have_css('.ncce-courses__tag', text: 'ANNOUNCEMENT')
+  end
+
+  it 'national restrictions' do
+    expect(rendered).to have_link('here for more information', href: 'https://blog.teachcomputing.org/national-restrictions/')
+  end
+end
