1aae3d3362dec84864b293b516f0a558ff7b68a9
Magdalena Jadach <magdalena@raspberrypi.org>
2020-11-10 16:23:42 +0000

    Fix the test

--- spec/views/components/_notice_spec.rb
+++ spec/views/components/_notice_spec.rb
@@ -10,6 +10,6 @@ RSpec.describe('components/_notice', type: :view) do
   end
 
   it 'read our full statement' do
-    expect(rendered).to have_link('Explore home learning:', href: 'https://blog.teachcomputing.org/computing-resources-for-home-learning/')
+    expect(rendered).to have_link(' here for more information', href: 'https://blog.teachcomputing.org/national-restrictions/')
   end
 end
