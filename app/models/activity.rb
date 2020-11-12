dd9458c44c987fe152b2c7d650652ab38d323eee
Damien Hogan <damienhogan87@gmail.com>
2020-11-12 16:08:36 +0000

    update providers

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -14,7 +14,7 @@ class Activity < ApplicationRecord
 
   validates :title, :slug, :category, presence: true
   validates :category, inclusion: { in: [ACTION_CATEGORY, ASSESSMENT_CATEGORY, COMMUNITY_CATEGORY, DIAGNOSTIC_CATEGORY, FACE_TO_FACE_CATEGORY, ONLINE_CATEGORY] }
-  validates :provider, inclusion: { in: %w[barefoot cas classmarker code-club future-learn isaac ncce stem-learning system] }
+  validates :provider, inclusion: { in: %w[barefoot cas classmarker future-learn isaac ncce raspberrypi stem-learning system] }
   validates :future_learn_course_uuid, uniqueness: true, unless: Proc.new { |a| a.future_learn_course_uuid.blank? }
   validates :stem_course_template_no, uniqueness: true, unless: Proc.new { |a| a.stem_course_template_no.blank? }
 
