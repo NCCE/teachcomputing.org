42402ac2ca97037e5ca271c82726c94bc4dcf9e8
Damien Hogan <damienhogan87@gmail.com>
2020-11-16 15:04:55 +0000

    remove old file

--- spec/views/certificates/secondary-certificate/show.html_spec.rb
+++ spec/views/certificates/secondary-certificate/show.html_spec.rb
@@ -4,23 +4,21 @@ RSpec.describe('certificates/secondary_certificate/show', type: :view) do
   let(:user) { create(:user) }
   let(:programme) { create(:programme, slug: 'secondary-certificate') }
   let(:assessment) { create(:assessment, programme_id: programme.id) }
+  let(:programme_activity_groupings) { create_list(:programme_activity_grouping, 3, :with_activities, programme_id: programme.id) }
 
   before do
     assessment
     @current_user = user
     @programme = programme
+    @programme_activity_groupings = programme_activity_groupings
     allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
     @user_programme_achievements = instance_double('UserProgrammeAchievements')
     allow(@user_programme_achievements).to receive_messages(online_achievements: [],
-                                                            face_to_face_achievements: [],
-                                                            diagnostic_achievements: [],
-                                                            community_activities: [])
-    @user_programme_assessment = instance_double('UserProgrammeAssessment')
-    allow(@user_programme_assessment).to receive_messages(enough_credits_for_test?: false)
-    render
+                                                            face_to_face_achievements: [])
   end
 
   it 'has the hero' do
+    byebug
     expect(rendered).to have_css('.hero__heading', text: programme.title)
   end
 
