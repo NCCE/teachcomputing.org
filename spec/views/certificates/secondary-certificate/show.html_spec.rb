48709e3513af9fbb9b8ffca1a208ca4ee051bfdf
Damien Hogan <damienhogan87@gmail.com>
2020-11-17 14:26:11 +0000

    speak tweak to ensure sequence resets

--- spec/views/certificates/secondary-certificate/show.html_spec.rb
+++ spec/views/certificates/secondary-certificate/show.html_spec.rb
@@ -7,29 +7,27 @@ RSpec.describe('certificates/secondary_certificate/show', type: :view) do
   let(:programme_activity_groupings) { create_list(:programme_activity_grouping, 5, programme_id: secondary_certificate.id) }
 
   before do
+    FactoryBot.rewind_sequences
     cs_accelerator
     @current_user = user
     @programme = secondary_certificate
-    programme_activity_groupings
     programme_activity_groupings.each do |grouping|
       create_list(:programme_activity, 3, programme_id: @programme.id, programme_activity_grouping_id: grouping.id)
     end
     @programme_activity_groupings = @programme.programme_activity_groupings
     @user_programme_achievements = UserProgrammeAchievements.new(@programme, @current_user)
+    render
   end
 
   it 'has the hero' do
-    render
     expect(rendered).to have_css('.hero__heading', text: @programme.title)
   end
 
   it 'has correct list setup' do
-    render
     expect(rendered).to have_css('.ncce-activity-list--programme', count: 4)
   end
 
   it 'has support information' do
-    render
     expect(rendered).to have_css('.ncce-aside__title', text: 'Support')
   end
 end
