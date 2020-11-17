072afd283f109faf093225cdaa884eaece6ab377
Magdalena Jadach <magdalena@raspberrypi.org>
2020-11-17 16:36:07 +0000

    Use model method instead of passing info between models. Law of Demeter rule

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -23,4 +23,17 @@ module NavigationHelper
       "<progress class='ncce-header__navigation-certification-progress-bar' aria-label='You have #{achieved_credits} out of #{max_credits} credits' value='#{achieved_credits}' max='#{max_credits}'></progress>".html_safe
     end
   end
+
+  def certificate_progress_for_user_primary(user)
+    return nil if user.programmes.empty?
+
+		cs = Programme.find_by(slug: 'primary-certificate')
+    programme = user.programmes.first
+    achieved_credits = programme.credits_achieved_for_certificate(user)
+    max_credits = programme.max_credits_for_certificate
+
+    link_to(programme.path, class: 'govuk-header__link ncce-header__navigation-certification-progress-bar-container') do
+      "<progress class='ncce-header__navigation-certification-progress-bar' aria-label='You have #{achieved_credits} out of #{max_credits} credits' value='#{achieved_credits}' max='#{max_credits}'></progress>".html_safe
+    end
+  end
 end
