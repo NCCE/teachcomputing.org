0855922738d1c5880ada4ba7ab4fd05deb289370
Magdalena Jadach <magdalena@raspberrypi.org>
2020-11-18 15:43:44 +0000

    Progress bar update

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -12,23 +12,25 @@ module NavigationHelper
   end
 
   def certificate_progress_for_user_cs_accelerator(user)
-    return nil if user.programmes.empty?
+		programme = user.programmes.cs_accelerator
 
-		cs = Programme.find_by(slug: 'cs-accelerator')
-    programme = user.programmes.first
-    achieved_credits = programme.credits_achieved_for_certificate(user)
-    max_credits = programme.max_credits_for_certificate
+  	if programme.nil?
+			"<span class='ncce-header__navigation-certification-progress-bar' aria-label='Not yet progress'></span>".html_safe
+		else
+			programme = user.programmes.cs_accelerator
+			achieved_credits = programme.credits_achieved_for_certificate(user)
+			max_credits = programme.max_credits_for_certificate
 
-    link_to(programme.path, class: 'govuk-header__link ncce-header__navigation-certification-progress-bar-container') do
-      "<progress class='ncce-header__navigation-certification-progress-bar' aria-label='You have #{achieved_credits} out of #{max_credits} credits' value='#{achieved_credits}' max='#{max_credits}'></progress>".html_safe
-    end
+			link_to(programme.path, class: 'govuk-header__link ncce-header__navigation-certification-progress-bar-container') do
+				"<progress class='ncce-header__navigation-certification-progress-bar' aria-label='You have #{achieved_credits} out of #{max_credits} credits' value='#{achieved_credits}' max='#{max_credits}'></progress>".html_safe
+    	end
+		end
   end
 
   def certificate_progress_for_user_primary(user)
-    return nil if user.programmes.empty?
+    return nil if user.programmes.primary_certificate.nil?
 
-		cs = Programme.find_by(slug: 'primary-certificate')
-    programme = user.programmes.first
+    programme = user.programmes.primary_certificate
     achieved_credits = programme.credits_achieved_for_certificate(user)
     max_credits = programme.max_credits_for_certificate
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2020_11_03_150940) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
