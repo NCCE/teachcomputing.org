09727e8129faff05a42696d529a239a99b8e00fe
Damien Hogan <damienhogan87@gmail.com>
2020-11-19 09:49:56 +0000

    tweaks to view and controller

--- app/controllers/certificates/secondary_certificate_controller.rb
+++ app/controllers/certificates/secondary_certificate_controller.rb
@@ -4,13 +4,12 @@ module Certificates
     before_action :authenticate_user!
     before_action :find_programme, only: %i[show complete pending]
     before_action :user_enrolled?, only: %i[show complete pending]
+    before_action :user_programme_enrolment_pending?, only: %i[show complete]
 
     def show
       return redirect_to complete_secondary_certificate_path if @programme.user_completed?(current_user)
 
-      @programme_activity_groupings = @programme.programme_activity_groupings
       @user_programme_achievements = UserProgrammeAchievements.new(@programme, current_user)
-
       render :show
     end
 
@@ -28,7 +27,7 @@ module Certificates
     def pending
       return redirect_to complete_secondary_certificate_path if enrolment.current_state == 'complete'
 
-      @enrolment = current_user.user_programme_enrolments.find_by(programme_id: @programme.id)
+      @complete_achievements = complete_achievements
 
       render :pending
     end
@@ -39,12 +38,21 @@ module Certificates
         current_user.user_programme_enrolments.find_by(programme_id: @programme.id)
       end
 
+      def complete_achievements
+        current_user.achievements.without_category('action')
+                    .for_programme(@programme).sort_complete_first
+      end
+
       def find_programme
         @programme = Programme.secondary_certificate
       end
 
       def user_enrolled?
         redirect_to secondary_path unless @programme.user_enrolled?(current_user)
+      end
+
+      def user_programme_enrolment_pending?
+        redirect_to pending_secondary_certificate_path if enrolment.in_state?(:pending)
       end
   end
 end
