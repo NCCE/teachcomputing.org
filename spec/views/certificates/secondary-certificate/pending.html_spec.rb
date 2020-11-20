8f6cb3515b74f177ff6a8083f3e42fb2012b4000
Damien Hogan <damienh@users.noreply.github.com>
2020-11-20 09:03:35 +0000

    Add copy to complete page and update its specs (#1017)

--- spec/views/certificates/secondary-certificate/pending.html_spec.rb
+++ spec/views/certificates/secondary-certificate/pending.html_spec.rb
@@ -3,13 +3,10 @@ require 'rails_helper'
 RSpec.describe('certificates/secondary_certificate/pending', type: :view) do
   let(:user) { create(:user) }
   let(:secondary_certificate) { create(:secondary_certificate) }
-  let(:enrolment) { create(:user_programme_enrolment, programme_id: secondary_certificate.id, user_id: user.id) }
 
   before do
     @programme = secondary_certificate
-    allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
     assign(:complete_achievements, user.achievements.for_programme(secondary_certificate).sort_complete_first)
-    enrolment.transition_to(:pending)
     render
   end
 
