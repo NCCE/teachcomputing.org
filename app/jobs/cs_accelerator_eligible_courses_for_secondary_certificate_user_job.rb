56bbf52ac084ae4e6b4c9333a7f6ecedb8b36b27
Damien Hogan <damienhogan87@gmail.com>
2020-11-20 09:50:39 +0000

    Current WIP

---
+++ app/jobs/cs_accelerator_eligible_courses_for_secondary_certificate_user_job.rb
@@ -0,0 +1,17 @@
+class CsAcceleratorEligibleCoursesForSecondaryCertificateUserJob
+  queue_as :default
+
+  def perform(user_id)
+    user = User.find(user_id)
+    programme = Programme.secondary_certificate
+    additional_csa_course_activity = Activity.find_by(slug: 'complete-a-cs-accelerator-course')
+
+    return if user.achievements.for_programme(programme).include(additional_csa_course_activity)
+
+    eligible_courses = programme.csa_eligible_courses(user)
+    if eligible_courses.any?
+      achievement = Achievement.create(activity_id: additional_csa_course_activity, user_id: user.id, programme_id: programme.id)
+      achievement.transition_to(:complete, eligible_courses.map { |achievement| achievement.activity.title } )
+    end
+  end
+end
