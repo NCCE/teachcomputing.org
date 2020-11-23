170e40114a31123ed9a2beebf12d98817d5b82ea
Ant Barnes <ant@arayb.com>
2020-11-23 08:58:13 +0000

    Ensure email previews work
    If email is recording sent then previews break.

--- app/mailers/previews/cs_accelerator_mailer_preview.rb
+++ app/mailers/previews/cs_accelerator_mailer_preview.rb
@@ -4,6 +4,9 @@ class CsAcceleratorMailerPreview < ActionMailer::Preview
   end
 
   def assessment_eligibility
+    SentEmail.where(user_id: User.first.id,
+                    mailer_type: CsAcceleratorMailer::CSA_ASSESSMENT_ELIGIBILITY_EMAIL)
+             .destroy_all
     CsAcceleratorMailer.with(user: User.first).assessment_eligibility
   end
 
@@ -12,10 +15,16 @@ class CsAcceleratorMailerPreview < ActionMailer::Preview
   end
 
   def non_enrolled_csa_user
+    SentEmail.where(user_id: User.first.id,
+                    mailer_type: CsAcceleratorMailer::CSA_NON_ENROLLED_USER_EMAIL)
+             .destroy_all
     CsAcceleratorMailer.with(user: User.first).non_enrolled_csa_user
   end
 
   def auto_enrolled_welcome
+    SentEmail.where(user_id: User.first.id,
+                    mailer_type: CsAcceleratorMailer::CSA_AUTO_ENROLLED_WELCOME)
+             .destroy_all
     CsAcceleratorMailer.with(user: User.first).auto_enrolled_welcome
   end
 end
