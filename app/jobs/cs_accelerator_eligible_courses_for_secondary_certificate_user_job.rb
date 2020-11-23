75cf74d1c0cbffaf52ae1158681ea8bca2b1fde4
Damien Hogan <damienhogan87@gmail.com>
2020-11-23 11:41:49 +0000

    fix

--- app/jobs/cs_accelerator_eligible_courses_for_secondary_certificate_user_job.rb
+++ app/jobs/cs_accelerator_eligible_courses_for_secondary_certificate_user_job.rb
@@ -10,7 +10,7 @@ class CsAcceleratorEligibleCoursesForSecondaryCertificateUserJob < ApplicationJo
 
     eligible_courses = programme.csa_eligible_courses(user)
 
-    if programme.csa_eligible_courses(user).any?
+    if eligible_courses.any?
       achievement = Achievement.create(activity_id: additional_csa_course_activity.id, user_id: user.id, programme_id: programme.id)
       achievement.transition_to(:complete, eligible_courses.map { |achievement| achievement.activity.title } )
       CertificatePendingTransitionJob.perform_later(programme, user.id, source: 'ProcessFutureLearnCsvExportJob')
