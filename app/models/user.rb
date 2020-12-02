eb5381964d0c12ad0c700e6ef028dacc3b29048d
Magdalena Jadach <magdalena@raspberrypi.org>
2020-12-02 10:45:03 +0000

    Fix user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ApplicationRecord
     self.user_programme_enrolments
   end
 
-	def programme_enrolment_state(programme.id)
-		self.user_programme_enrolments.find_by(programme_id: programme.id).current_state
+	def programme_enrolment_state(programme_id)
+		self.user_programme_enrolments.find_by(programme_id: programme_id).current_state
 	end
 end
