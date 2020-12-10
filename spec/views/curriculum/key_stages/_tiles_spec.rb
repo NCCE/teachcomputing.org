1c80df4c77e86a7ed8ba6d9744b5138adc03ac59
Dan Halson <danhalson@users.noreply.github.com>
2020-12-10 13:18:57 +0000

    1530: Add curriculum map ui (#1047)
    * 1530: Add curriculum map support
    
    * 1530: Update data
    
    * 1530: Ensure partial not rendered when no curriculum maps
    
    * 1530: Uncomment teacher guide and add test
    
    * 1530: Remove var

---
+++ spec/views/curriculum/key_stages/_tiles_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe('curriculum/key_stages/_tiles', type: :view) do
+	let(:key_stage_json) { File.new('spec/support/curriculum/views/key_stage.json').read }
+
+  before do
+		key_stage = JSON.parse(key_stage_json, object_class: OpenStruct).data.key_stage
+		render partial: 'tiles', locals: { key_stage: key_stage }
+  end
+
+  it 'shows the expected curriculum maps' do
+    expect(rendered).to have_link('Map 1', href: 'something.somewhere')
+    expect(rendered).to have_link('Map 2', href: 'something_else.somewhere_else')
+  end
+
+  it 'shows the expected teacher guide' do
+    expect(rendered).to have_link('KS1 teacher guide', href: "https://teachcomputing.org")
+  end
+end
