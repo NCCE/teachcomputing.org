b88b32db86d6e476c259b15c3f1feaa29d938ca4
Damien Hogan <damienhogan87@gmail.com>
2020-12-10 11:40:41 +0000

    Just show complete for now

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @achievements = current_user.achievements.not_in_state(:dropped)
+    @achievements = current_user.achievements.in_state(:complete)
                                 .with_courses.order('created_at ASC')
     render :show
   end
