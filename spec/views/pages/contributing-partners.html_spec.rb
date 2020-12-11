6d07bc086b46abc9b4e53b2843c9622daa0731d2
Ant Barnes <e1daf3a5@opayq.com>
2020-12-11 16:35:09 +0000

    Add new contributing partners (#1059)
    * Add new contributing partners
    
    * Updates view spec

--- spec/views/pages/contributing-partners.html_spec.rb
+++ spec/views/pages/contributing-partners.html_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe('pages/contributing-partners', type: :view) do
     expect(rendered).to have_css('.govuk-body-l', count: 1)
   end
 
-  it 'has six cards' do
-    expect(rendered).to have_css('.dark_card', count: 6)
+  it 'has 8 cards' do
+    expect(rendered).to have_css('.dark_card', count: 8)
   end
 
 end
