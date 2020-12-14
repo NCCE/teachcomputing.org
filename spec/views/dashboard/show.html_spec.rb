5200fd7a52090a87d7d72279c2b62b0c15443644
Dan Halson <dan.halson@raspberrypi.org>
2020-12-14 15:44:09 +0000

    1562: Check for link

--- spec/views/dashboard/show.html_spec.rb
+++ spec/views/dashboard/show.html_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe('dashboard/show', type: :view) do
     end
 
     it 'shows the placeholder text' do
-      expect(rendered).to have_link('Find a course', courses_path)
+      expect(rendered).to have_link('Find a course', href: '/courses')
     end
   end
 
