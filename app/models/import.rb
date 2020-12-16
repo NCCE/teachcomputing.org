1080afa758e00a420d7340b58f13a2bba9148558
Ant Barnes <ant@arayb.com>
2020-12-16 14:41:41 +0000

    Remove Imports class and helpers

--- app/models/import.rb
+++
@@ -1,6 +0,0 @@
-class Import < ApplicationRecord
-  validates :provider, :triggered_by, presence: true
-  validates :provider, inclusion: { in: %w[future-learn] }
-
-  scope :by_completed_at, -> { order(completed_at: 'desc') }
-end
