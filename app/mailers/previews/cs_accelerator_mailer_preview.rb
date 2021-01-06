6b7b30824470d30ca0c8eba3ad140f6cadc66aa3
Damien Hogan <damienhogan87@gmail.com>
2021-01-06 08:58:38 +0000

    tweak to how mailer preview is used

--- app/mailers/previews/cs_accelerator_mailer_preview.rb
+++ app/mailers/previews/cs_accelerator_mailer_preview.rb
@@ -11,10 +11,12 @@ class CsAcceleratorMailerPreview < ActionMailer::Preview
   end
 
   def getting_started_prompt
+    user = User.first
+    enrolment = UserProgrammeEnrolment.find_or_create_by(user: user, programme: Programme.cs_accelerator)
     SentEmail.where(user_id: User.first.id,
                     mailer_type: CsAcceleratorMailer::CSA_GETTING_STARTED_PROMPT_EMAIL)
              .destroy_all
-    CsAcceleratorMailer.with(user: User.first).getting_started_prompt
+    CsAcceleratorMailer.with(user: user, enrolment_id: enrolment.id).getting_started_prompt
   end
 
   def new_assessment_eligibility
