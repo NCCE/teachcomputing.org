62b4669d3421c63585ef4a32aad6d09597dcc52e
Dan Halson <danhalson@users.noreply.github.com>
2021-01-07 08:56:25 +0000

    Get rubocopy working (#1081)

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -3,17 +3,17 @@ class CmsController < ApplicationController
 
   def cms_page
     @page = Ghost.new.get_single_page(params[:page_slug])
-    @parent_slug = get_parent_slug
+    @parent_slug = parent_slug
     render :cms_page
   end
 
-  def get_parent_slug
-    route = request.env['PATH_INFO'].split('/').reject { |el| el.empty? }
-    parent = route[0] || ''
+  def parent_slug
+    route = request.env['PATH_INFO'].split('/').reject(&:empty?)
+    route[0] || ''
   end
 
   def clear_page_cache
     Ghost.new.clear_page_cache(params[:page_slug])
-    return redirect_to request.fullpath.sub(/\/refresh$/, '')
+    redirect_to request.fullpath.sub(%r{/refresh$}, '')
   end
 end
