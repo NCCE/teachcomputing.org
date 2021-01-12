086634a7b66fde9c14d1c1641ba511ec666dd0d0
Dan Halson <dan.halson@raspberrypi.org>
2021-01-12 11:21:18 +0000

    596: Update new references to CsAccelerator

--- app/jobs/cs_accelerator_eligible_courses_for_secondary_certificate_user_job.rb
+++ app/jobs/cs_accelerator_eligible_courses_for_secondary_certificate_user_job.rb
@@ -13,7 +13,7 @@ class CSAcceleratorEligibleCoursesForSecondaryCertificateUserJob < ApplicationJo
     if eligible_courses.any?
       achievement = Achievement.create(activity_id: additional_csa_course_activity.id, user_id: user.id, programme_id: programme.id)
       achievement.transition_to(:complete, eligible_courses.map { |achievement| achievement.activity.title } )
-      CertificatePendingTransitionJob.perform_later(programme, user.id, source: 'CsAcceleratorEligibleCoursesForSecondaryCertificateUserJob')
+      CertificatePendingTransitionJob.perform_later(programme, user.id, source: 'CSAcceleratorEligibleCoursesForSecondaryCertificateUserJob')
     end
   end
 end
