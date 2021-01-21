a1215b45c2abd999173980008783975d1994e581
Damien Hogan <damienh@users.noreply.github.com>
2021-01-21 16:18:10 +0000

    Ensure using self_verification_info key (#1105)
    * Ensure using self_verification_info key
    
    * tweak
    
    * spec amendment

--- app/jobs/cs_accelerator_eligible_courses_for_secondary_certificate_user_job.rb
+++ app/jobs/cs_accelerator_eligible_courses_for_secondary_certificate_user_job.rb
@@ -5,6 +5,7 @@ class CSAcceleratorEligibleCoursesForSecondaryCertificateUserJob < ApplicationJo
     user = User.find(user_id)
     programme = Programme.secondary_certificate
     additional_csa_course_activity = Activity.find_by(slug: 'complete-a-cs-accelerator-course')
+    metadata = {}
 
     return if user.achievements.for_programme(programme).where(activity_id: additional_csa_course_activity.id).any?
 
@@ -13,7 +14,9 @@ class CSAcceleratorEligibleCoursesForSecondaryCertificateUserJob < ApplicationJo
     if eligible_courses.any?
       achievement = Achievement.create(activity_id: additional_csa_course_activity.id, user_id: user.id,
                                        programme_id: programme.id)
-      achievement.transition_to(:complete, eligible_courses.map { |achievement| achievement.activity.title })
+
+      metadata[:self_verification_info] = eligible_courses.map { |achievement| achievement.activity.title }
+      achievement.transition_to(:complete, metadata)
       CertificatePendingTransitionJob.perform_later(programme, user.id,
                                                     source: 'CSAcceleratorEligibleCoursesForSecondaryCertificateUserJob')
     end
