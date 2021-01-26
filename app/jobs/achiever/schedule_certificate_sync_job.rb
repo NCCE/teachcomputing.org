af1a419b2a5de96401e10ae286a46c020dd416a9
Damien Hogan <damienhogan87@gmail.com>
2021-01-26 09:16:03 +0000

    Add feature flag

--- app/jobs/achiever/schedule_certificate_sync_job.rb
+++ app/jobs/achiever/schedule_certificate_sync_job.rb
@@ -3,9 +3,8 @@ module Achiever
     queue_as :achiever
 
     def perform(enrolment_id)
-      # for now we dont want this to run until the initial sync has been done
-      # enrolment = UserProgrammeEnrolment.find(enrolment_id)
-      # Achiever::User::Enrolment.new(enrolment).sync
+      enrolment = UserProgrammeEnrolment.find(enrolment_id)
+      Achiever::User::Enrolment.new(enrolment).sync if FeatureFlagService.new.flags[:certification_sync_enabled]
     end
   end
 end
