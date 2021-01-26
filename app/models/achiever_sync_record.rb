a9fc4f87d168da4a54ca87814d169a09ea8023dd
Damien Hogan <damienhogan87@gmail.com>
2021-01-26 13:40:38 +0000

    add spec

--- app/models/achiever_sync_record.rb
+++ app/models/achiever_sync_record.rb
@@ -2,5 +2,5 @@ class AchieverSyncRecord < ApplicationRecord
   belongs_to :user_programme_enrolment
 
   validates :user_programme_enrolment_id, uniqueness: { scope: [:state] }
-  validates :state, inclusion: { in: %w[enrolled pending complete] }
+  validates :state, inclusion: { in: %w[enrolled pending complete unenrolled] }
 end
