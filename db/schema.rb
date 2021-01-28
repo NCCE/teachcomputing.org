8f6c4d5e2e8c6ddb16433acd3cce0870b27f8e42
Damien Hogan <damienhogan87@gmail.com>
2021-01-28 09:36:27 +0000

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_092130) do
+ActiveRecord::Schema.define(version: 2021_01_27_085220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_092130) do
     t.text "description"
     t.text "self_verification_info"
     t.boolean "uploadable", default: false
+    t.string "stem_activity_code"
     t.index ["category"], name: "index_activities_on_category"
     t.index ["future_learn_course_uuid"], name: "index_activities_on_future_learn_course_uuid", unique: true
     t.index ["self_certifiable"], name: "index_activities_on_self_certifiable"
