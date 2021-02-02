5c899a5ee14de455a9ed7ab9514557d0d9186050
Damien Hogan <damienhogan87@gmail.com>
2021-02-02 15:54:43 +0000

    tweak

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
-  # config.sass.style = :compressed
+  config.sass.preferred_syntax = :sass
   # config.assets.css_compressor = :nil
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
