aa40d1cee75def3bf6020697ae281c1f0543249e
Damien Hogan <damienhogan87@gmail.com>
2021-02-02 15:59:51 +0000

    follow approach outlined https://github.com/alphagov/govuk-account-manager-prototype/pull/555/

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -24,8 +24,9 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
-  config.sass.preferred_syntax = :sass
-  # config.assets.css_compressor = :nil
+  config.assets.css_compressor = nil
+  config.sass.style = :compressed
+  config.sass.line_comments = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
