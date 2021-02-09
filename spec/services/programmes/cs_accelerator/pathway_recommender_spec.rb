e322388f196b15e01c34b2f238b7ca24da84f0f3
Ant Barnes <e1daf3a5@opayq.com>
2021-02-09 11:21:12 +0000

    CSA pathway recommender (#1131)
    Recommends a csa pathway based on questionnaire response
    If the user answers questions then goes back and changes answer 1 to be low confidence ensure they end up on correct path

---
+++ spec/services/programmes/cs_accelerator/pathway_recommender_spec.rb
@@ -0,0 +1,155 @@
+require 'rails_helper'
+
+RSpec.describe Programmes::CSAccelerator::PathwayRecommender do
+  let(:cs_accelerator) { create(:cs_accelerator) }
+  let(:recommender) { described_class.new(questionnaire_response: questionnaire_response) }
+
+  let(:questionnaire_response) { create(:questionnaire_response, programme: cs_accelerator, answers: answers) }
+
+  let!(:new_to_computing_pathway) { create(:pathway, slug: 'new_to_computing') }
+  let!(:preparing_to_teach_pathway) { create(:pathway, slug: 'preparing_to_teach') }
+  let!(:algorithms_and_programming_pathway) { create(:pathway, slug: 'algorithms_and_programming') }
+  let!(:systems_pathway) { create(:pathway, slug: 'systems') }
+  let!(:advanced_pathway) { create(:pathway, slug: 'advanced') }
+
+  describe '#recommended_pathway' do
+    context 'when user answered "A" to question 1' do
+      let(:answers) { { '1': '1' } }
+
+      it 'recommends "New to computing" pathway' do
+        expect(recommender.recommended_pathway).to eq(new_to_computing_pathway)
+      end
+    end
+
+    context 'when user answered "A" to question 1 and has further answers' do
+      let(:answers) { { '1': '1', '2': '2', '3': '3', '4': '2', '5': '2' } }
+
+      it 'recommends "New to computing" pathway' do
+        expect(recommender.recommended_pathway).to eq(new_to_computing_pathway)
+      end
+    end
+
+    context 'when score is 10 or less' do
+      let(:questionnaire_response) { instance_double(QuestionnaireResponse) }
+
+      before do
+        allow(questionnaire_response).to receive(:answers).and_return({})
+      end
+
+      (2..10).each do |val|
+        context "when score is #{val}" do
+          it 'recommends "Preparing to teach GCSE Computer Science" pathway' do
+            allow(questionnaire_response).to receive(:score).and_return(val)
+            expect(recommender.recommended_pathway).to eq(preparing_to_teach_pathway)
+          end
+        end
+      end
+    end
+
+    context 'when score is between 11 and 14' do
+      let(:answers) { { '1': '3', '2': '2', '3': '2', '4': '2', '5': '2' } }
+
+      it 'recommends "Preparing to teach GCSE Computer Science" pathway' do
+        expect(recommender.recommended_pathway).to eq(preparing_to_teach_pathway)
+      end
+    end
+
+    context 'when score is between 11 and 14 and there was only one "A" or "B" response' do
+      context 'when Q1 has a "B" response' do
+        let(:answers) { { '1': '2', '2': '3', '3': '3', '4': '3', '5': '3' } }
+
+        it 'recommends "Preparing to teach GCSE Computer Science" pathway' do
+          expect(recommender.recommended_pathway).to eq(preparing_to_teach_pathway)
+        end
+      end
+
+      context 'when "A" or "B" relates to Q2' do
+        let(:answers) { { '1': '3', '2': '2', '3': '3', '4': '3', '5': '3' } }
+
+        it 'recommends "Algorithms & Programming" pathway' do
+          expect(recommender.recommended_pathway).to eq(algorithms_and_programming_pathway)
+        end
+      end
+
+      context 'when "A" or "B" relates to Q4' do
+        let(:answers) { { '1': '3', '2': '3', '3': '3', '4': '2', '5': '3' } }
+
+        it 'recommends "Algorithms & Programming" pathway' do
+          expect(recommender.recommended_pathway).to eq(algorithms_and_programming_pathway)
+        end
+      end
+
+      context 'when "A" or "B" relates to Q3' do
+        let(:answers) { { '1': '3', '2': '3', '3': '2', '4': '3', '5': '3' } }
+
+        it 'recommends "Systems" pathway' do
+          expect(recommender.recommended_pathway).to eq(systems_pathway)
+        end
+      end
+
+      context 'when "A" or "B" relates to Q5' do
+        let(:answers) { { '1': '3', '2': '3', '3': '3', '4': '3', '5': '2' } }
+
+        it 'recommends "Systems" pathway' do
+          expect(recommender.recommended_pathway).to eq(systems_pathway)
+        end
+      end
+    end
+
+    context 'when score is between 15 and 20' do
+      let(:answers) { { '1': '3', '2': '3', '3': '3', '4': '3', '5': '3' } }
+
+      it 'recommends "Advanced" pathway' do
+        expect(recommender.recommended_pathway).to eq(advanced_pathway)
+      end
+
+      context 'when there is more than 1 "A" or "B" response' do
+        let(:answers) { { '1': '4', '2': '2', '3': '1', '4': '4', '5': '4' } }
+
+        it 'recommends "Preparing to teach GCSE Computer Science" pathway' do
+          expect(recommender.recommended_pathway).to eq(preparing_to_teach_pathway)
+        end
+      end
+
+      context 'When question 1 has a "B" response' do
+        let(:answers) { { '1': '2', '2': '4', '3': '3', '4': '3', '5': '3' } }
+
+        it 'recommends "Preparing to teach GCSE Computer Science" pathway' do
+          expect(recommender.recommended_pathway).to eq(preparing_to_teach_pathway)
+        end
+      end
+
+      context 'when there is one "A" or "B" relating to Q2' do
+        let(:answers) { { '1': '3', '2': '2', '3': '3', '4': '3', '5': '4' } }
+
+        it 'recommends "Algorithms & Programming" pathway' do
+          expect(recommender.recommended_pathway).to eq(algorithms_and_programming_pathway)
+        end
+      end
+
+      context 'when there is one "A" or "B" relating to Q4' do
+        let(:answers) { { '1': '3', '2': '3', '3': '4', '4': '1', '5': '4' } }
+
+        it 'recommends "Algorithms & Programming" pathway' do
+          expect(recommender.recommended_pathway).to eq(algorithms_and_programming_pathway)
+        end
+      end
+
+      context 'when there is one "A" or "B" relating to Q3' do
+        let(:answers) { { '1': '3', '2': '3', '3': '2', '4': '3', '5': '4' } }
+
+        it 'recommends "Systems" pathway' do
+          expect(recommender.recommended_pathway).to eq(systems_pathway)
+        end
+      end
+
+      context 'when there is one "A" or "B" relating to Q5' do
+        let(:answers) { { '1': '3', '2': '3', '3': '4', '4': '4', '5': '1' } }
+
+        it 'recommends "Systems" pathway' do
+          expect(recommender.recommended_pathway).to eq(systems_pathway)
+        end
+      end
+    end
+  end
+end
