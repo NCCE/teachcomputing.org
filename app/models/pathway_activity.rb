74325c3e7b26b20478e091bec6b8092cdf813957
Damien Hogan <damienhogan87@gmail.com>
2021-02-11 08:59:57 +0000

    remove method

--- app/models/pathway_activity.rb
+++ app/models/pathway_activity.rb
@@ -1,7 +1,4 @@
 class PathwayActivity < ApplicationRecord
-  before_save :update_activity_type
-  before_update :update_activity_type
-
   belongs_to :pathway
   belongs_to :activity, autosave: true, validate: true
 
