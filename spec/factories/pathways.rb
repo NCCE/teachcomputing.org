64f27a9f582e01f52fb4d2932f984ba04d8ba0f3
Dan Halson <danhalson@users.noreply.github.com>
2021-02-16 11:13:08 +0000

    1635: Hookup pathway to diagnostic (#1137)
    * 1635: Hookup pathway to diagnostic
    
    * 1635: Uncomment recommend_pathway
    
    * 1635 Further tweaks and test fixes
    
    * 1635: Switch order of method calls
    
    * 1635: Tidy up setter
    
    * 1635: Better name
    
    * 1635: Fix debase version

--- spec/factories/pathways.rb
+++ spec/factories/pathways.rb
@@ -4,6 +4,31 @@ FactoryBot.define do
     title { 'Pathway' }
     description { 'Pathway description' }
     pdf_link { 'https://example.com/pdf-link.pdf' }
-    sequence(:slug) { |n| "slug-#{n}" }
+    slug { 'slug' }
+
+    trait :new_to_computing do
+      slug { 'new-to-computing' }
+    end
+    factory :new_to_computing, traits: %i[new_to_computing]
+
+    trait :prepare_to_teach_gcse_computer_science do
+      slug { 'prepare-to-teach-gcse-computer-science' }
+    end
+    factory :prepare_to_teach_gcse_computer_science, traits: %i[prepare_to_teach_gcse_computer_science]
+
+    trait :new_to_algorithms_and_programming do
+      slug { 'new-to-algorithms-and-programming' }
+    end
+    factory :new_to_algorithms_and_programming, traits: %i[new_to_algorithms_and_programming]
+
+    trait :new_to_computer_systems do
+      slug { 'new-to-computer-systems' }
+    end
+    factory :new_to_computer_systems, traits: %i[new_to_computer_systems]
+
+    trait :advanced_gcse_computer_science do
+      slug { 'advanced-gcse-computer-science' }
+    end
+    factory :advanced_gcse_computer_science, traits: %i[advanced_gcse_computer_science]
   end
 end
--- spec/factories/user_programme_enrolments.rb
+++ spec/factories/user_programme_enrolments.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
   factory :user_programme_enrolment do
     user
     programme
-    pathway
   end
 end
