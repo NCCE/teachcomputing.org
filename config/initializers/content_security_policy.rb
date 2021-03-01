3b6e92e827d13967b0de731f069ffbfae0b62da1
Dan Halson <danhalson@users.noreply.github.com>
2021-03-01 13:36:04 +0000

    1606: Migrate remaining assets to webpacker (#1152)
    * 1606: Initial migration
    
    * 1606: Fix tests
    
    * 1606: Speed up test by preventing compile check
    
    * 1606: Add back uglifier as we have some dependency on sprockets still
    
    * 1606: Remove profiler as it's adding some weird markup even on review apps
    
    * 1606: Fix asset path
    
    * 1606: Move profiler to dev only
    
    * 1606: Further tweaks
    
    * 1606: Review changes
    
    * 1606: Some performance tweaks

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,7 +4,7 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
+Rails.application.config.content_security_policy do |policy|
 #   policy.default_src :self, :https, 'youtube.com', 'www.youtube.com'
 #   policy.font_src    :self, :https, :data, 'fonts.gstatic.com'
 #   policy.img_src     :self, :https, :data, 'view.vzaar.com', 'blog.teachcomputing.org', 'futurelearn-assets.raspberrypi.org'
@@ -12,9 +12,8 @@
 #   policy.object_src  :none
 #   policy.script_src  :self, :https, :data, 'www.google-analytics.com', 'www.googletagmanager.com', 'tagmanager.google.com'
 #   policy.style_src   :self, :https, :unsafe_inline
-
-#   policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035' if Rails.env.development?
-# end
+  policy.connect_src :self, :https, 'https://localhost:3035', 'wss://localhost:3035' if Rails.env.development?
+end
 
 # If you are using UJS then enable automatic nonce generation
 # This overrides 'unsafe-inline', if specified
