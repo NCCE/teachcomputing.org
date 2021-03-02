d78220c1e3e5d630df571563e4ae171f81f70eba
Damien Hogan <damienhogan87@gmail.com>
2021-03-02 10:55:17 +0000

    remove html default

--- config/routes.rb
+++ config/routes.rb
@@ -97,7 +97,7 @@ Rails.application.routes.draw do
   patch '/users/:id/teacher-reference-number', action: :teacher_reference_number, controller: 'user',
                                                as: :user_teacher_reference_number
 
-  get '/404', to: 'pages#exception', defaults: { format: 'html', status: 404 }
+  get '/404', to: 'pages#exception', defaults: { status: 404 }
   get '/422', to: 'pages#exception', defaults: { status: 422 }
   get '/500', to: 'pages#exception', defaults: { status: 500 }
   get '/about', to: 'pages#page', as: :about, defaults: { page_slug: 'about' }
