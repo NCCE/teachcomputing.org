77c2c9d73552808e044019d4061ec484102cc587
Ant Barnes <ant@arayb.com>
2021-03-08 11:58:15 +0000

    Reinstate commented out tests

--- spec/views/programmes/cs-accelerator/show.html_spec.rb
+++ spec/views/programmes/cs-accelerator/show.html_spec.rb
@@ -2,33 +2,28 @@ require 'rails_helper'
 
 RSpec.describe('certificates/cs_accelerator/show', type: :view) do
   let(:user) { create(:user) }
-  let(:programme) { create(:cs_accelerator) }
+  let!(:programme) { create(:cs_accelerator) }
 
   before do
+    @csa_dash = CSADash.new(user: user)
     @current_user = user
     @programme = programme
-    stub_template 'certificates/cs_accelerator/v1/_activity-list.html.erb' => 'stub activity list'
-    stub_template 'certificates/cs_accelerator/v1/_aside.html.erb' => 'stub aside'
     render
   end
 
-  # it 'has the hero' do
-  #   expect(rendered).to have_css('.hero__heading', text: programme.title)
-  # end
-  #
-  # it 'has a heading' do
-  #   expect(rendered).to have_css('.govuk-heading-m')
-  # end
-  #
-  # it 'has an activity heading' do
-  #   expect(rendered).to have_css('.govuk-heading-s')
-  # end
-  #
-  # it 'has one courses button' do
-  #   expect(rendered).to have_link('Book or join a course today', href: /cs-accelerator/)
-  # end
-  #
-  # it 'has the expected bursary info' do
-  #   expect(rendered).to have_css('.bursary-info', text: /secondary teacher/)
-  # end
+  it 'has the hero' do
+    expect(rendered).to have_css('.hero__heading', text: programme.title)
+  end
+
+  it 'has a heading' do
+    expect(rendered).to have_css('.govuk-heading-m')
+  end
+
+  it 'has an activity heading' do
+    expect(rendered).to have_css('.govuk-heading-s')
+  end
+
+  it 'has the expected bursary info' do
+    expect(rendered).to have_css('.bursary-aside', text: /secondary teacher/)
+  end
 end
