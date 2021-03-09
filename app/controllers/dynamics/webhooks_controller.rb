1d0cfdffbbe183284852eaa0229cc0d209f7f4c9
Damien Hogan <damienhogan87@gmail.com>
2021-03-09 15:22:13 +0000

    update logging

--- app/controllers/dynamics/webhooks_controller.rb
+++ app/controllers/dynamics/webhooks_controller.rb
@@ -5,7 +5,7 @@ module Dynamics
 
     def user
       user = User.find_by(stem_achiever_contact_no: request[:stem_achiever_contact_no])
-      Rails.logger.warn request
+      Rails.logger.warn request[:stem_achiever_contact_no]
       Achiever::FetchUsersCompletedCoursesFromAchieverJob.perform_later(user) if user
 
       head :ok
