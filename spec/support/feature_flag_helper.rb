f23872f5c5c6e4e0dc6ac344a9ea23346094a06a
Ant Barnes <ant@arayb.com>
2021-03-09 09:26:47 +0000

    Add feature flag stub before callback test

--- spec/support/feature_flag_helper.rb
+++ spec/support/feature_flag_helper.rb
@@ -1,6 +1,17 @@
+# To use this add a before hook stubbing the appropriate flag with a hash.
+# Then unstub in an after hook e.g:
+
+# before do
+#   stub_feature_flags({ flag_key: true })
+# end
+#
+# after do
+#   unstub_feature_flags
+# end
+
 module FeatureFlagHelper
   def stub_feature_flags(flag_hash = {})
-    dbl = double(FeatureFlagService)
+    dbl = instance_double(FeatureFlagService)
     allow(FeatureFlagService).to receive(:new) { dbl }
     allow(dbl).to receive(:flags).and_return(flag_hash)
   end
