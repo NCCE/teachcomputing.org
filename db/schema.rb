cf0ec438d514c99bd5a5b97af469b07b161061c0
Magdalena Jadach <magdalena@raspberrypi.org>
2021-03-16 09:58:59 +0000

    Empty line

--- db/schema.rb
+++ db/schema.rb
@@ -55,8 +55,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_090132) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
-  
-  
+
   create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
