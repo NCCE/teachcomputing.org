fea756a543496787d8e1ade1b743470b916f32fe
Damien Hogan <damienhogan87@gmail.com>
2021-03-23 16:00:27 +0000

    update config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,8 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials[ENV.fetch('SENTRY_DSN')]
   config.rails.report_rescued_exceptions = true
+  config.breadcrumbs_logger = [:active_support_logger]
+  config.traces_sample_rate = 0.5
   config.enabled_environments = %w[staging]
 
   filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
