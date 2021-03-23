e86fe607e3c671cb5b8d98e8238e2c5a36fd83a7
Damien Hogan <damienhogan87@gmail.com>
2021-03-23 08:43:37 +0000

    update feature flag spec count

--- spec/services/feature_flag_service_spec.rb
+++ spec/services/feature_flag_service_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe FeatureFlagService do
 
   describe 'FLAGS' do
     it 'defines hash of flags correctly' do
-      expect(FeatureFlagService::FLAGS.count).to eq 2
+      expect(FeatureFlagService::FLAGS.count).to eq 1
     end
 
     it 'sets the csa_questionnaire_enabled flag' do
