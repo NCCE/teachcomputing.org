1054119f1f4b79b1ffa16549ec97b6e2b2848618
Damien Hogan <damienh@users.noreply.github.com>
2021-03-24 09:22:51 +0000

    Update Sentry libraries and enable APM (#1187)
    * Progress on migrating to new Sentry library
    
    * update
    
    * remove dup config
    
    * remove logger setting
    
    * update config
    
    * Add production
    
    * update config
    
    * Update to 50% of traces
    
    * Set sample rate to 0.25

--- app/models/programme_complete_counter.rb
+++ app/models/programme_complete_counter.rb
@@ -2,15 +2,13 @@ class ProgrammeCompleteCounter < ApplicationRecord
   belongs_to :programme
 
   def get_next_number
-    begin
-      ActiveRecord::Base.transaction do
-        next_number = counter + 1
-        update!(counter: next_number)
-        next_number
-      end
-    rescue ActiveRecord::ActiveRecordError => error
-      Raven.tags_context programme: self.programme
-      Raven.capture_exception(error)
+    ActiveRecord::Base.transaction do
+      next_number = counter + 1
+      update!(counter: next_number)
+      next_number
     end
+  rescue ActiveRecord::ActiveRecordError => e
+    Sentry.set_tags programme: programme
+    Sentry.capture_exception(e)
   end
 end
