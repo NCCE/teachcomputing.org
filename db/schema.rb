30fd3ab31e03c91044b918c44510af1b7eb74daf
MagdalenaJadach <magdalena@raspberrypi.org>
2021-03-24 12:03:43 +0000

    Navigation - new site navigation (#1110)
    * Button
    
    * Initial approach to nav
    
    * Style and position icons
    
    * Introduce helper method to move data out of view file
    
    * Move away from courses class
    
    * Additional data, pass class to dropdown container
    
    * Update labels and links, labels etc.
    
    * Style dropwond kids
    
    * Clean up
    
    * Clean up classes
    
    * Nav - no js
    
    * Restore js file, re-add missing button
    
    * Close a class
    
    * Navigation position
    
    * Tweak mobile
    
    * Fix dropdown class nesting
    
    * Icon on hover
    
    * Fix click on mobile
    
    * Clean up hover
    
    * Remove old classes
    
    * Rename classes
    
    * Raname class
    
    * Fix padding issues
    
    * Clean up classes
    
    * Clean up css
    
    * Fix desktop/mobile
    
    * Tiny tweaks
    
    * Copy tweaks
    
    * Desktop tweaks
    
    * Desktop tweaks
    
    * Mobile tweaks
    
    * Mobile changes
    
    * Mobile tweaks
    
    * Desktop icon positon
    
    * Local changes to make webpacker work
    
    * Click behaviour update
    
    * 1579: Get tabbing mostly working
    
    * 1579: Fix mobile, and a style tweak
    
    * 1579: More fixes and tweaks
    
    * 1579: Fix test failure
    
    * 1579: Fix menu issue
    
    * Mobile tweaks - remove extra color when hover/click
    
    * Clean up comments
    
    * Fix icon position
    
    * Desktop tweaks
    
    * Mobile tweak
    
    * Friday changes
    
    * Re-add missing element
    
    * Fix mobile issue
    
    * Keep the nav on top
    
    * Mobile icon change
    
    * Mobile tweaks
    
    * tweak mobile behaviour
    
    * Positin icons evently
    
    * Padding, remove nav white space
    
    * Tweak font sizes
    
    * Deskop hover tweaks
    
    * Mobile tweaks
    
    * Mobile fix - different height
    
    * Expand text, reposition image
    
    * Sort out chunkiness of the column labels
    
    * Adjust line
    
    * Fix tests
    
    * Update paddings
    
    * JS tweak - Ants work
    
    * Mobile behaviour
    
    * Remove debugger
    
    * Reposition cookie banner
    
    * Move icon to the separate class to make sure we can position it properly
    
    * Make work on tablets at desktop sizes
    
    Co-authored-by: Dan Halson <dan.halson@raspberrypi.org>
    Co-authored-by: Ant Barnes <ant@arayb.com>

--- db/schema.rb
+++ db/schema.rb
@@ -46,16 +46,6 @@ ActiveRecord::Schema.define(version: 2021_02_25_090132) do
     t.index ["user_programme_enrolment_id"], name: "index_achiever_sync_records_on_user_programme_enrolment_id"
   end
 
-  create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "body"
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
-  end
-
   create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
