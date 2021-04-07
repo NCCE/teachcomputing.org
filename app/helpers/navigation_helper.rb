512fb98b608689d5a490456368d4e13ac6c14cd4
Ant Barnes <e1daf3a5@opayq.com>
2021-04-07 10:36:02 +0100

    1670 secondary teachers page (#1193)
    * Add Secondary landing page & view model
    * Add Primary landing page & view model
    * Fix typo in navigation
    * Add Google Tag Manager events

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,41 +1,39 @@
 module NavigationHelper
-
   def header_navigation
-		[
-			{text: "Primary teachers",
-			children: [
-				{text: "Primary teacher toolkit", link: primary_teachers_path, label: "Primary teachers" },
-				{text: "Primary certificate", link: primary_path, label: "Primary certificate" }
-			]},
-			{text: "Secondary teacher", children:
-				[{text: "Secondary teacher toolkit",
-				link: secondary_teachers_path, label: "Secondary teachers" },
-				{text: "Subject knowledge certificate",
-				link: cs_accelerator_path, label: "Subject knowledge certificate" },
-				{text: "Secondary certificate",
-				link: secondary_path, label: "Secondary certificate" },
-				{text: "A level computer science",
-				link: a_level_computer_science_path, label: "A level computer science" }
-			]},
-			{text: "Training and support",
-			children: [
-				{text: "Courses", link: courses_path, label: "Courses" },
-				{text: "Bursaries", link: '/bursary', label: "Bursaries" },
-				{text: "Computing Hubs", link: '/hubs', label: "Computing hubs" }
-			]},
-			{text: "Teaching resources",
-			children: [
-				{text: "Teaching resources", link: curriculum_key_stages_path, label: "Teaching resources" },
-				{text: "Pedagogy", link: '/pedagogy', label: "Pedagogy" }
-			]},
-			{text: "About us",
-			children: [
-				{text: "About the NCCE", link: about_path, label: "About the NCCE" },
-				{text: "News", link: 'https://blog.teachcomputing.org/', label: "News" },
-				{text: "Get involved", link: get_involved_path, label: "Get involved" },
-				{text: "Gender Balance research", link: gender_balance_path, label: "Gender Balance research" },
-				{text: "Subject practitioners", link: '/subject-practitioners', label: "Subject practitioners" }
-			]}
-		]
+    [
+      { text: 'Primary teachers',
+        children: [
+          { text: 'Primary teacher toolkit', link: primary_teachers_path, label: 'Primary teachers' },
+          { text: 'Primary certificate', link: primary_path, label: 'Primary certificate' }
+        ] },
+      { text: 'Secondary teachers', children:
+        [{ text: 'Secondary teacher toolkit',
+           link: secondary_teachers_path, label: 'Secondary teachers' },
+         { text: 'Subject knowledge certificate',
+           link: cs_accelerator_path, label: 'Subject knowledge certificate' },
+         { text: 'Secondary certificate',
+           link: secondary_path, label: 'Secondary certificate' },
+         { text: 'A level computer science',
+           link: a_level_computer_science_path, label: 'A level computer science' }] },
+      { text: 'Training and support',
+        children: [
+          { text: 'Courses', link: courses_path, label: 'Courses' },
+          { text: 'Bursaries', link: '/bursary', label: 'Bursaries' },
+          { text: 'Computing Hubs', link: '/hubs', label: 'Computing hubs' }
+        ] },
+      { text: 'Teaching resources',
+        children: [
+          { text: 'Teaching resources', link: curriculum_key_stages_path, label: 'Teaching resources' },
+          { text: 'Pedagogy', link: '/pedagogy', label: 'Pedagogy' }
+        ] },
+      { text: 'About us',
+        children: [
+          { text: 'About the NCCE', link: about_path, label: 'About the NCCE' },
+          { text: 'News', link: 'https://blog.teachcomputing.org/', label: 'News' },
+          { text: 'Get involved', link: get_involved_path, label: 'Get involved' },
+          { text: 'Gender Balance research', link: gender_balance_path, label: 'Gender Balance research' },
+          { text: 'Subject practitioners', link: '/subject-practitioners', label: 'Subject practitioners' }
+        ] }
+    ]
   end
 end
--- spec/requests/landing_pages/teacher_pages_spec.rb
+++ spec/requests/landing_pages/teacher_pages_spec.rb
@@ -1,42 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe LandingPagesController do
-  let(:cs_accelerator) { create(:cs_accelerator) }
-  let(:primary_certificate) { create(:primary_certificate) }
-  let(:secondary_certificate) { create(:secondary_certificate) }
+  let!(:cs_accelerator) { create(:cs_accelerator) }
+  let!(:primary_certificate) { create(:primary_certificate) }
+  let!(:secondary_certificate) { create(:secondary_certificate) }
 
   describe '#primary_teachers' do
     before do
-      primary_certificate
       get primary_teachers_path
     end
 
-    it 'assigns programme' do
-      expect(assigns(:programme)).to eq(primary_certificate)
+    it 'assigns PrimaryLandingPage' do
+      expect(assigns(:landing_page)).to be_a(PrimaryLandingPage)
     end
 
     it 'renders the correct template' do
-      expect(response).to render_template('landing_pages/primary_teachers')
+      expect(response).to render_template('landing_pages/show')
     end
   end
 
   describe '#secondary_teachers' do
     before do
-      secondary_certificate
-      cs_accelerator
       get secondary_teachers_path
     end
 
-    it 'assigns cs accelerator' do
-      expect(assigns(:cs_accelerator)).to eq(cs_accelerator)
-    end
-
-    it 'assigns secondary certifcate' do
-      expect(assigns(:secondary_certificate)).to eq(secondary_certificate)
+    it 'assigns SecondaryLandingPage' do
+      expect(assigns(:landing_page)).to be_a(SecondaryLandingPage)
     end
 
     it 'renders the correct template' do
-      expect(response).to render_template('landing_pages/secondary_teachers')
+      expect(response).to render_template('landing_pages/show')
     end
   end
 end
