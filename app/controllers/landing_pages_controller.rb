f094026e535cb4731cbb9af5ec44a913a6132817
Ant Barnes <ant@arayb.com>
2021-04-07 09:44:35 +0100

    Remove comment, move logic checks inline with render

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -2,7 +2,6 @@ class LandingPagesController < ApplicationController
   layout 'full-width'
 
   def primary_teachers
-    # @programme = Programme.primary_certificate
     @landing_page = PrimaryLandingPage.new(current_user: current_user)
     render :show
   end
