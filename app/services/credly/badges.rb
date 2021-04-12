b7168d28dd3e6c73e775a77e373f69b7da205419
Damien Hogan <damienhogan87@gmail.com>
2021-04-12 10:43:23 +0100

    remove /

--- app/services/credly/badges.rb
+++ app/services/credly/badges.rb
@@ -1,7 +1,7 @@
 module Credly
   class Badges
     ORG_ID = ENV['CREDLY_ORGANISATION_ID'].freeze
-    RESOURCE_PATH = "/organizations/#{ORG_ID}/badge_templates".freeze
+    RESOURCE_PATH = "organizations/#{ORG_ID}/badge_templates".freeze
 
     def self.all
       Credly::Request.run(RESOURCE_PATH, nil)
