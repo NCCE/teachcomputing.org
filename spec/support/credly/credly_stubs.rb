b55a852d4b82e3e76738de97e3419e08e936b60f
Damien Hogan <damienhogan87@gmail.com>
2021-04-16 11:26:20 +0100

    WIP stub file

--- spec/support/credly/credly_stubs.rb
+++ spec/support/credly/credly_stubs.rb
@@ -1,3 +1,15 @@
+module CredlyStubs
+  def stub_badge_templates
+    json_response = File.new('spec/support/credly/badge_templates.json')
+    stub_request(:get, 'https://api.credly.com/v1/organizations/e52b9e79-9ddb-4110-9883-ae2c44a7440e/badge_templates').to_return(body: json_response)
+  end
+
+  def stub_issue_badge(user_id, template_id)
+    json_response = File.new('spec/support/credly/issue_badge.json')
+    stub_request(:post, 'https://api.credly.com/v1/organizations/e52b9e79-9ddb-4110-9883-ae2c44a7440e/badges').to_return(body: json_response)
+  end
+
   def stub_issued_badges(user_id)
 
   end
+end
