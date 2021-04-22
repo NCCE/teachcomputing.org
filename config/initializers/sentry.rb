12700e63b651a3e079527a556393bcc712beee31
Damien Hogan <damienhogan87@gmail.com>
2021-04-22 08:31:20 +0100

    Tweak how we filter https://github.com/getsentry/sentry-ruby/issues/1383

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,10 +6,6 @@ Sentry.init do |config|
 
   filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
   config.before_send = lambda do |event, _hint|
-    request = event.request
-
-    request.data = filter.filter(request.data) if request
-
-    event
+    filter.filter(event.to_hash)
   end
 end
