d90eeb56b426e53a22cffad3e5a2f3c80dd69e11
Damien Hogan <damienhogan87@gmail.com>
2021-05-27 15:34:15 +0100

    remove VCR call

--- spec/requests/courses/show_spec.rb
+++ spec/requests/courses/show_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe CoursesController do
 
   describe 'GET #show' do
     before do
-      VCR.turn_off!
       stub_duration_units
       stub_face_to_face_occurrences
       stub_online_occurrences
