720b6a25576fe867573e2664d01414d2763f9108
Damien Hogan <damienhogan87@gmail.com>
2021-06-04 09:11:24 +0100

    spec for dash badge component

--- spec/components/dash_badge_component_spec.rb
+++ spec/components/dash_badge_component_spec.rb
@@ -1,13 +1,51 @@
 require "rails_helper"
 
 RSpec.describe DashBadgeComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user, email: 'web@raspberrypi.org') }
+  let(:achievement) { create(:achievement, user: user) }
+  let(:dash_badge_component) { described_class.new(achievement: achievement, badge_template_id: '00cd7d3b-baca-442b-bce5-f20666ed591b', user_id: user.id)}
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  context 'when the badges feature is disabled' do
+    before do
+      stub_issued_badges(user.id)
+      render_inline(dash_badge_component)
+    end
+
+    it 'does not render' do
+      expect(rendered_component).to eq ''
+    end
+  end
+
+  context 'when the badging feature is enabled' do
+    before do
+      stub_issued_badges(user.id)
+      stub_feature_flags({ badges_enabled: true })
+    end
+
+    context 'but the achievement is not in a state of complete' do
+      it 'does not render' do
+        render_inline(dash_badge_component)
+        expect(rendered_component).to eq ''
+      end
+    end
+
+    context 'when the achievement is complete' do
+      before do
+        achievement.transition_to(:complete)
+        render_inline(dash_badge_component)
+      end
+
+      it "the badges image" do
+        expect(rendered_component).to have_css('.dash-badge-component__badge')
+      end
+
+      it "a link to the badge" do
+        expect(rendered_component).to have_css('.ncce-link')
+      end
+
+      it "the congratulatory copy" do
+        expect(rendered_component).to have_css('.dash-badge-component__content')
+      end
+    end
+  end
 end
