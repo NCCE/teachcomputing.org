c1e642166461a67720d22b22f417c8168f4fc47f
Damien Hogan <damienhogan87@gmail.com>
2021-06-08 11:14:26 +0100

    rename badge component to secondary to make way for new primary dash badge component

--- spec/components/secondary_dash_badge_component_spec.rb
+++ spec/components/secondary_dash_badge_component_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
-RSpec.describe DashBadgeComponent, type: :component do
+RSpec.describe SecondaryDashBadgeComponent, type: :component do
   let(:user) { create(:user, email: 'web@raspberrypi.org') }
   let(:achievement) { create(:achievement, user: user) }
-  let(:dash_badge_component) { described_class.new(achievement: achievement, badge_template_id: '00cd7d3b-baca-442b-bce5-f20666ed591b', user_id: user.id, tracking_event_category: 'category', tracking_event_label: 'label')}
+  let(:dash_badge_component) { described_class.new(achievement: achievement, badge_template_id: '00cd7d3b-baca-442b-bce5-f20666ed591b', fixed_width: true, user_id: user.id, tracking_event_category: 'category', tracking_event_label: 'label')}
 
   context 'when the badges feature is disabled' do
     before do
