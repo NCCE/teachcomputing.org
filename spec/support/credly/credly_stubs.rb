66440372326fe90859ae46b82195aab0d05d7eee
Damien Hogan <damienhogan87@gmail.com>
2021-06-14 09:52:51 +0100

    add back empty stub

--- spec/support/credly/credly_stubs.rb
+++ spec/support/credly/credly_stubs.rb
@@ -18,4 +18,9 @@ module CredlyStubs
     json_response = File.new('spec/support/credly/issued_badges.json')
     stub_request(:get, 'https://api.credly.com/v1/organizations/e52b9e79-9ddb-4110-9883-ae2c44a7440e/badges?filter=recipient_email::web@raspberrypi.org').to_return(body: json_response)
   end
+
+  def stub_issued_badges_empty(user_id)
+    json_response = File.new('spec/support/credly/issued_badges_empty.json')
+    stub_request(:get, 'https://api.credly.com/v1/organizations/e52b9e79-9ddb-4110-9883-ae2c44a7440e/badges?filter=recipient_email::web@raspberrypi.org').to_return(body: json_response)
+  end
 end
