65b60f7e4735d647e36444d5e3caa4c272787259
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2021-06-18 09:03:15 +0100

    WIP get involved page

---
+++ app/view_objects/get_involved.rb
@@ -0,0 +1,36 @@
+class GetInvolved
+  class << self
+    def tracking_page
+      'Get ivolved'
+    end
+    def partnership_cards
+      {
+        class_name: 'support-cards',
+        cards: [
+          {
+            class_name: 'supporting-partners-card',
+            title: I18n.t('pages.partnerships.supporting_partner_card.title'),
+            text: I18n.t('pages.partnerships.supporting_partner_card.text'),
+            link: {
+              link_title: I18n.t('pages.partnerships.supporting_partner_card.link_title'),
+              link_url: '/supporting-partners',
+              tracking_page: tracking_page,
+              tracking_label: I18n.t('pages.about.supporting_partners_card.title')
+            }
+          },
+          {
+            class_name: 'contributing-partners-card',
+            title: I18n.t('pages.partnerships.contributing_partner_card.title'),
+            text: I18n.t('pages.partnerships.contributing_partner_card.text'),
+            link: {
+              link_title: I18n.t('pages.partnerships.contributing_partner_card.link_title'),
+              link_url: '/contributing-partners',
+              tracking_page: tracking_page,
+              tracking_label: I18n.t('pages.partnerships.contributing_partner_card.title')
+            }
+          }
+        ]
+      }
+    end
+  end
+end
