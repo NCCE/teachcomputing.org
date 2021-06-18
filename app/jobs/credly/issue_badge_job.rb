dd3e3c9431c4936cfd58be44d02c3e43c20e5ff0
Damien Hogan <damienhogan87@gmail.com>
2021-06-18 09:02:38 +0100

    refactor

--- app/jobs/credly/issue_badge_job.rb
+++ app/jobs/credly/issue_badge_job.rb
@@ -6,13 +6,13 @@ module Credly
       return unless FeatureFlagService.new.flags[:badges_enabled]
 
       programme = Programme.find(programme_id)
-      badge_template_id = programme.credly_badge_template_id
+      badge = programme.badges.active.first
       user = User.find(user_id)
       issued_badges = Credly::Badge.issued(user.id)
 
-      return if issued_badges.map { |badge| badge[:badge_template][:id] == badge_template_id }.any?
+      return if issued_badges.map { |badge| badge[:badge_template][:id] == badge.credly_badge_template_id }.any?
 
-      Credly::Badge.issue(user.id, badge_template_id)
+      Credly::Badge.issue(user.id, badge.credly_badge_template_id)
       NewBadgeMailer.new_badge_email(user, programme).deliver_now
     end
   end
