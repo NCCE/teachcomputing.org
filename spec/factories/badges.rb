bacd50b70029493762d6476d3792a0638297477a
Damien Hogan <damienhogan87@gmail.com>
2021-06-22 09:40:35 +0100

    update factory

--- spec/factories/badges.rb
+++ spec/factories/badges.rb
@@ -1,10 +1,12 @@
 FactoryBot.define do
   factory :badge do
     credly_badge_template_id { '00cd7d3b-baca-442b-bce5-f20666ed591b' }
+    academic_year { '2019-20' }
     active { false }
     programme
 
     trait :active do
+      academic_year { '2020-21' }
       active { true }
     end
   end
