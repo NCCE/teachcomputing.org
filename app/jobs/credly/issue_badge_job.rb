4e81c0f3c967ecf66e04b1ec57ebeb103dd58134
Damien Hogan <damienhogan87@gmail.com>
2021-06-22 08:55:15 +0100

    update specs

--- app/jobs/credly/issue_badge_job.rb
+++ app/jobs/credly/issue_badge_job.rb
@@ -5,12 +5,15 @@ module Credly
     def perform(user_id, programme_id)
       return unless FeatureFlagService.new.flags[:badges_enabled]
 
+      user = User.find(user_id)
       programme = Programme.find(programme_id)
       badge = programme.badges.active.first
-      user = User.find(user_id)
+
+      return unless badge
+
       issued_badges = Credly::Badge.issued(user.id)
 
-      return if issued_badges.map { |badge| badge[:badge_template][:id] == badge.credly_badge_template_id }.any?
+      return if issued_badges.map { |b| b[:badge_template][:id] == badge.credly_badge_template_id }.any?
 
       Credly::Badge.issue(user.id, badge.credly_badge_template_id)
       NewBadgeMailer.new_badge_email(user, programme).deliver_now
--- spec/jobs/credly/issue_badge_job_spec.rb
+++ spec/jobs/credly/issue_badge_job_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Credly::IssueBadgeJob, type: :job do
   let(:user) { create(:user, email: 'web@raspberrypi.org') }
   let(:programme) { create(:cs_accelerator) }
+  let(:badge) { create(:badge, :active, programme_id: programme.id) }
 
 
   describe '#perform' do
@@ -21,6 +22,7 @@ RSpec.describe Credly::IssueBadgeJob, type: :job do
 
       context 'when the user already has the badged issued' do
         before do
+          badge
           stub_issued_badges(user.id)
         end
         it 'does not issue a badge' do
@@ -31,6 +33,7 @@ RSpec.describe Credly::IssueBadgeJob, type: :job do
 
       context 'when the user does not already have the badge' do
         before do
+          badge
           stub_issued_badges_empty(user.id)
         end
 
