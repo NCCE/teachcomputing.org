e1bbf232be0d84fa5462841bca5667a6b5cda9ed
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2021-06-24 13:43:49 +0100

    adds images to cards

--- app/view_objects/get_involved.rb
+++ app/view_objects/get_involved.rb
@@ -39,40 +39,85 @@ class GetInvolved
         class_name: 'support-cards',
         cards: [
           {
-            class_name: 'supporting-partners-card',
-            title: I18n.t('pages.partnerships.supporting_partner_card.title'),
-            text: I18n.t('pages.partnerships.supporting_partner_card.text'),
-            list_items:[],
+            class_name: 'code-club-card',
+            title: I18n.t('pages.other_ways_to_get_involved.code_club.title'),
+            text:'',
+            list_items: I18n.t('pages.other_ways_to_get_involved.code_club.list_items'),
+            image_url: '',
             link: {
-              link_title: I18n.t('pages.partnerships.supporting_partner_card.link_title'),
-              link_url: '/supporting-partners',
+              link_title: I18n.t('pages.other_ways_to_get_involved.code_club.link_title'),
+              link_url: 'https://codeclub.org/en/volunteer',
               tracking_page: tracking_page,
               tracking_label: I18n.t('pages.about.supporting_partners_card.title')
             }
           },
           {
-            class_name: 'contributing-partners-card',
-            title: I18n.t('pages.partnerships.contributing_partner_card.title'),
-            text: I18n.t('pages.partnerships.contributing_partner_card.text'),
-            list_items:[],
+            class_name: 'stem-card',
+            title: I18n.t('pages.other_ways_to_get_involved.stem_ambassador.title'),
+            text: '',
+            list_items: I18n.t('pages.other_ways_to_get_involved.stem_ambassador.list_items'),
+            image_url: '',
             link: {
-              link_title: I18n.t('pages.partnerships.contributing_partner_card.link_title'),
-              link_url: '/contributing-partners',
+              link_title: I18n.t('pages.other_ways_to_get_involved.stem_ambassador.link_title'),
+              link_url: 'https://www.stem.org.uk/stem-ambassadors/join-stem-ambassador-programme',
               tracking_page: tracking_page,
-              tracking_label: I18n.t('pages.partnerships.contributing_partner_card.title'),
+              tracking_label: I18n.t('pages.other_ways_to_get_involved.stem_ambassador.title'),
               class_name: "button"
             }
           },
           {
-            class_name: 'contributing-partners-card',
-            title: I18n.t('pages.partnerships.contributing_partner_card.title'),
-            text: I18n.t('pages.partnerships.contributing_partner_card.text'),
-            list_items:["jkajssjjdfsj",'sdhhffhdkdfhjk','skjhffhjjhfksd'],
+            class_name: 'isac-computer-card',
+            title: I18n.t('pages.other_ways_to_get_involved.isac_computing.title'),
+            text: '',
+            list_items:I18n.t('pages.other_ways_to_get_involved.isac_computing.list_items'),
+            image_url: 'get-involved/isac.svg',
             link: {
-              link_title: I18n.t('pages.partnerships.contributing_partner_card.link_title'),
-              link_url: '/contributing-partners',
+              link_title: I18n.t('pages.other_ways_to_get_involved.isac_computing.link_title'),
+              link_url: 'https://isaaccomputerscience.org/pages/getintouch_events',
               tracking_page: tracking_page,
-              tracking_label: I18n.t('pages.partnerships.contributing_partner_card.title'),
+              tracking_label: I18n.t('pages.other_ways_to_get_involved.isac_computing.title'),
+              class_name: "button"
+            }
+          },
+          {
+            class_name: 'barefoot-computing-card',
+            title: I18n.t('pages.other_ways_to_get_involved.barefoot_computing.title'),
+            text: I18n.t('pages.other_ways_to_get_involved.barefoot_computing.text'),
+            list_items: I18n.t('pages.other_ways_to_get_involved.barefoot_computing.list_items'),
+            image_url: '',
+            link: {
+              link_title: I18n.t('pages.other_ways_to_get_involved.barefoot_computing.link_title'),
+              link_url: 'https://www.barefootcomputing.org/',
+              tracking_page: tracking_page,
+              tracking_label: I18n.t('pages.other_ways_to_get_involved.barefoot_computing.title'),
+              class_name: "button"
+            }
+          },
+          {
+            class_name: 'cas-card',
+            title: I18n.t('pages.other_ways_to_get_involved.cas.title'),
+            text: I18n.t('pages.other_ways_to_get_involved.cas.text'),
+            list_items: I18n.t('pages.other_ways_to_get_involved.cas.list_items'),
+            image_url: 'get-involved/cas.svg',
+            link: {
+              link_title: I18n.t('pages.other_ways_to_get_involved.cas.link_title'),
+              link_url: 'https://www.computingatschool.org.uk/',
+              tracking_page: tracking_page,
+              tracking_label: I18n.t('pages.other_ways_to_get_involved.cas.title'),
+              class_name: "button"
+            }
+          },
+          {
+            class_name: 'school-governors-card',
+            title: I18n.t('pages.other_ways_to_get_involved.school_governors.title'),
+            text: I18n.t('pages.other_ways_to_get_involved.school_governors.text'),
+            list_items:[],
+            image_url: 'get-involved/school_governors.svg',
+            link: {
+              link_title: I18n.t('pages.other_ways_to_get_involved.school_governors.link_title'),
+              link_url: 'https://teachcomputing.org/governors-and-trustees/',
+              tracking_page: tracking_page,
+              tracking_label: I18n.t('pages.other_ways_to_get_involved.school_governors.title'),
               class_name: "button"
             }
           }
