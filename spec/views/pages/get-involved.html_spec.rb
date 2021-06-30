f2cf376085f98898423b95cb5fb0c4687fdeefe0
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2021-06-30 10:47:21 +0100

    all tests passing

--- spec/views/pages/get-involved.html_spec.rb
+++ spec/views/pages/get-involved.html_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe('pages/get-involved', type: :view) do
   end
 
   it 'has a title' do
-    expect(rendered).to have_css('.hero__heading', text: 'Help us inspire the computing experts of the future')
+    expect(rendered).to have_css('.hero__text', text: 'Help us inspire the computing experts of the future')
   end
 
   it 'has a get involved section' do
-    expect(rendered).to have_css('.govuk-heading-m', text: 'Get involved')
+    expect(rendered).to have_css('.govuk-heading-l', text: 'Get involved')
   end
 
   it 'has a how you can help section' do
@@ -19,9 +19,5 @@ RSpec.describe('pages/get-involved', type: :view) do
 
   it 'has a list of things you can do' do
     expect(rendered).to have_css('.govuk-list li', count: 4)
-  end
-
-  it 'list has headings' do
-    expect(rendered).to have_css('.govuk-list h3', count: 5)
   end
 end
