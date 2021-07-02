70572530a47c83efe99f4c3188853e807730b73e
Ant Barnes <ant@arayb.com>
2021-07-02 09:38:53 +0100

    Add lighthouse

---
+++ lighthouserc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'bundle exec rails server -e production',
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
