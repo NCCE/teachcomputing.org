d68165cd34be68faa838678da4bb81e63d131cd1
Dan Halson <dan.halson@raspberrypi.org>
2021-07-02 16:30:41 +0100

    1705: Reinstate changes only

---
+++ scripts/check_coverage.rb
@@ -0,0 +1,24 @@
+output = `cat /home/circleci/project/index.html | grep Changed -A 2 | grep "[0-9\.]*%"`
+percentage_match = output.match(/([0-9.]+)%/)
+
+if percentage_match.nil?
+  puts 'No changes to check'
+  exit 0
+end
+
+raise 'Unable to determine test coverage change' unless percentage_match
+
+percentage = percentage_match[0].to_f
+if percentage < 90
+  warn '\n'
+  warn 'INSUFFICIENT TEST COVERAGE'
+  warn '\n'
+  warn "New and changed files need a test coverage of >= 90% -- Simplecov only detected #{percentage}"
+  warn '\n'
+  warn 'Changed files:'
+  warn " * #{`git diff --name-only origin/master`.split("\n").join("\n * ")}"
+  warn '\n'
+  exit 1
+end
+
+puts "Test coverage for changed files: #{percentage}"
