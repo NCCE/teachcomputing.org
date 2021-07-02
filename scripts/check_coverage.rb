1bf2d8235449fe4a5bd80ad5a57fc9b1738bc896
Dan Halson <dan.halson@raspberrypi.org>
2021-07-02 16:58:42 +0100

    1705: Test with full coverage again

--- scripts/check_coverage.rb
+++ scripts/check_coverage.rb
@@ -1,4 +1,4 @@
-output = `cat #{ENV['CIRCLE_ARTIFACTS']}~/coverage/index.html | grep Changed -A 2 | grep "[0-9\.]*%"`
+output = `cat #{ENV['CIRCLE_ARTIFACTS']}~/coverage/index.html | grep "[0-9\.]*%"`
 percentage_match = output.match(/([0-9.]+)%/)
 
 if percentage_match.nil?
