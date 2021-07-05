fd8bb2d3a6d8df1ef9326f088a7fd1961efe981f
Ant Barnes <ant@arayb.com>
2021-07-05 13:49:07 +0100

    Amend server command

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: ['http://localhost:3000/', 'http://localhost:3000/primary-teachers', 'http://localhost:3000/secondary-teachers'],
-      startServerCommand: 'bundle exec rails server',
+      startServerCommand: 'bundle exec rails server -e production',
       settings: {
         onlyCategories: ['performance', 'accessibility', 'best-practices', 'seo'],
         skipAudits: ['cumulative-layout-shift'],
