6ca1b6492cd8f4cfa18334f46bf0e83dbb75353d
Ant Barnes <ant@arayb.com>
2021-07-05 08:57:30 +0100

    Try and remove PWA category

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,11 @@ module.exports = {
     collect: {
       url: ['http://localhost:3000/'],
       startServerCommand: 'bundle exec rails server -e production',
-      settings: {skipAudits: ['cumulative-layout-shift']},
+      settings: {
+        onlyCategories: ['performance', 'accessibility', 'best-practices', 'seo'],
+        skipAudits: ['cumulative-layout-shift'],
+
+      },
     },
     // assert: {
       // preset: 'lighthouse:no-pwa'
