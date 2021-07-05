ab5f9651bc96ace4d8d5055ed986b4288a1395a1
Ant Barnes <ant@arayb.com>
2021-07-05 08:17:03 +0100

    Amend settings

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,14 +3,10 @@ module.exports = {
     collect: {
       url: ['http://localhost:3000/'],
       startServerCommand: 'bundle exec rails server -e production',
+      settings: {skipAudits: ['cumulative-layout-shift']},
     },
     upload: {
       target: 'temporary-public-storage',
     },
-    settings: {
-      skipAudits: [
-        'cumulative-layout-shift'
-      ]
-    }
   },
 };
