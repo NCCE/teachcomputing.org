1d6e513d19b3cadf1693e867f14f6e4967bde60d
Ant Barnes <ant@arayb.com>
2021-07-05 08:27:10 +0100

    Remove PWA assertion

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,6 +5,9 @@ module.exports = {
       startServerCommand: 'bundle exec rails server -e production',
       settings: {skipAudits: ['cumulative-layout-shift']},
     },
+    assert: {
+      preset: 'lighthouse:no-pwa'
+    },
     upload: {
       target: 'temporary-public-storage',
     },
