b847c8685d88304286645401dac3c5c4e24ff3b9
Ant Barnes <ant@arayb.com>
2021-08-02 09:17:37 +0100

    Update previews to use test data
    Add some locales and an image to use in tests instead of relying on prod data that may change

--- previews/components/image_link_card_component_preview.rb
+++ previews/components/image_link_card_component_preview.rb
@@ -1,13 +1,13 @@
 class ImageLinkCardComponentPreview < ViewComponent::Preview
   def default
     # Component takes paths to locales and renders the text.
-    data = {
-      title_locale: 'pages.supporting_partners.arm.title',
-      link_url: 'https://www.arm.com/',
-      image_path: 'media/images/partners/arm.svg',
-      img_alt_locale: 'pages.supporting_partners.arm.img_alt',
-      text_locale: 'pages.supporting_partners.arm.text_html'
+    card = {
+      title_locale: 'test.image_link_card.title',
+      link_url: 'https://www.example.com/',
+      image_path: 'media/images/test/example.svg',
+      img_alt_locale: 'test.image_link_card.img_alt',
+      text_locale: 'test.image_link_card.text_html'
     }
-    render(ImageLinkCardComponent.new(image_link_card: data))
+    render(ImageLinkCardComponent.new(image_link_card: card))
   end
 end
--- previews/components/image_link_cards_component_preview.rb
+++ previews/components/image_link_cards_component_preview.rb
@@ -2,18 +2,18 @@ class ImageLinkCardsComponentPreview < ViewComponent::Preview
   def default
     # Component takes a list of cards and renders them in grid
     cards = [{
-      title_locale: 'pages.supporting_partners.arm.title',
-      link_url: 'https://www.arm.com/',
-      image_path: 'media/images/partners/arm.svg',
-      img_alt_locale: 'pages.supporting_partners.arm.img_alt',
-      text_locale: 'pages.supporting_partners.arm.text_html'
+      title_locale: 'test.image_link_card.title',
+      link_url: 'https://www.example.com/',
+      image_path: 'media/images/test/example.svg',
+      img_alt_locale: 'test.image_link_card.img_alt',
+      text_locale: 'test.image_link_card.text_html'
     },
     {
-      title_locale: 'pages.supporting_partners.arm.title',
-      link_url: 'https://www.arm.com/',
-      image_path: 'media/images/partners/arm.svg',
-      img_alt_locale: 'pages.supporting_partners.arm.img_alt',
-      text_locale: 'pages.supporting_partners.arm.text_html'
+      title_locale: 'test.image_link_card.title',
+      link_url: 'https://www.example.com/',
+      image_path: 'media/images/test/example.svg',
+      img_alt_locale: 'test.image_link_card.img_alt',
+      text_locale: 'test.image_link_card.text_html'
     }]
     render(ImageLinkCardsComponent.new(cards: cards))
   end
