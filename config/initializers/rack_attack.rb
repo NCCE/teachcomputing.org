9cc8cc6e102f5edb917c2d9f3852d2fd05af3639
Ant Barnes <e1daf3a5@opayq.com>
2021-08-04 09:17:55 +0100

    Amend rack attack rejecting unproxied requests (#1339)
    * Amend rack attack rejecting unproxied requests

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,17 +1,35 @@
 # When hosted on Heroku set ENV["REJECT_UNPROXIED_REQUESTS"] to TRUE to prevent responding to requests via *.herokuapp.com domain
-class Rack::Attack
-  if ENV['REJECT_UNPROXIED_REQUESTS']
-    blocklist('block non-proxied requests in staging & production') do |request|
-      raw_ip = request.get_header('HTTP_X_FORWARDED_FOR')
-      ip_addresses = raw_ip ? raw_ip.strip.split(/[,\s]+/) : []
-      proxy_ip = ip_addresses.last
+module Rack
+  class Attack
+    blocklist('reject unproxied requests') do |request|
+      next false unless ENV.fetch('REJECT_UNPROXIED_REQUESTS', 'FALSE').upcase == 'TRUE'
 
-      if !(request.host =~ /heroku/) && ::Rails.application.config.cloudflare.ips.any?{ |proxy| proxy === proxy_ip }
-        false
-      else
-        ::Rails.logger.warn "Rack Attack IP Filtering: blocked request from #{proxy_ip} to #{request.url}"
-        true
+      # Block requests that go straight to heroku.
+      if request.host.include?('herokuapp')
+        ::Rails.logger.warn "Rack Attack filtering: rejected request to #{request.url} as it went direct to #{request.host}"
+        next true
       end
+
+      # Cloudflare IPs are fetched dynamically and cached.  If the list is
+      # empty then don't reject anything.
+      next false if ::Rails.application.config.cloudflare.ips.empty?
+
+      forwarded_for_header = request.get_header('HTTP_X_FORWARDED_FOR').to_s
+      last_forwarded_for_ip = forwarded_for_header.split(/[,\s]+/).last
+
+      # If we've not got a forwarded IP, reject
+      unless last_forwarded_for_ip
+        ::Rails.logger.warn "Rack Attack filtering: rejected request to #{request.url} with missing/malformed X-Forwarded-For header #{forwarded_for_header.inspect}"
+        next true
+      end
+
+      # If the IP doesn't appear in any of the ranges, reject
+      unless ::Rails.application.config.cloudflare.ips.any? { |ip_range| ip_range.include?(last_forwarded_for_ip) }
+        ::Rails.logger.warn "Rack Attack filtering: rejected request to #{request.url} from a non Cloudflare IP #{last_forwarded_for_ip}"
+        next true
+      end
+
+      false
     end
   end
 end
---
+++ spec/requests/reject_unproxied_requests_spec.rb
@@ -0,0 +1,93 @@
+require 'rails_helper'
+
+RSpec.describe Rack::Attack do
+  subject { get '/primary-teachers', headers: headers }
+
+  let(:reject_uproxied_requests) { 'false' }
+  let(:headers) { {} }
+  let(:cloudflare_ips) { [] }
+
+  around do |example|
+    ClimateControl.modify REJECT_UNPROXIED_REQUESTS: reject_uproxied_requests do
+      example.run
+    end
+  end
+
+  before do
+    Rails.application.config.cloudflare.ips = cloudflare_ips
+  end
+
+  shared_examples 'a successful request' do
+    it { is_expected.to eq 200 }
+  end
+
+  shared_examples 'a rejected request' do
+    it { is_expected.to eq 403 }
+  end
+
+  context 'when REJECT_UNPROXIED_REQUESTS is false' do
+    it_behaves_like 'a successful request'
+
+    describe 'when the last forwarded IP is a non-cloudflare IP' do
+      let(:cloudflare_ips) { %w[192.168.0.0/22 fd80::/80].map { |i| IPAddr.new(i) } }
+      let(:x_forwarded_for) { '192.168.0.1' }
+      let(:headers) { { 'X-Forwarded-For': x_forwarded_for } }
+
+      it_behaves_like 'a successful request'
+    end
+
+    describe 'when the request comes in direct to heroku' do
+      let(:headers) { { Host: 'foobar.herokuapp.com' } }
+
+      it_behaves_like 'a successful request'
+    end
+  end
+
+  context 'when REJECT_UNPROXIED_REQUESTS is TRUE' do
+    let(:reject_uproxied_requests) { 'TRUE' }
+
+    describe 'when no cloudflare IPs are set' do
+      it_behaves_like 'a successful request'
+    end
+
+    describe 'when the request comes in direct to heroku' do
+      let(:headers) { { Host: 'foobar.herokuapp.com' } }
+
+      it_behaves_like 'a rejected request'
+    end
+
+    describe 'when cloudflare IPs are set' do
+      let(:cloudflare_ips) { %w[192.168.0.0/22 fd80::1/80].map { |i| IPAddr.new(i) } }
+
+      it_behaves_like 'a rejected request'
+
+      context 'when x-forwarded-for is set' do
+        let(:headers) { { 'X-Forwarded-For': x_forwarded_for } }
+
+        describe 'when X-Forwarded-For is empty' do
+          let(:x_forwarded_for) { '' }
+
+          it_behaves_like 'a rejected request'
+        end
+
+        describe 'when X-Forwarded-For is set to a cloudflare IP' do
+          let(:x_forwarded_for) { '1.2.3.4 4.5.6.7 192.168.0.1' }
+
+          it_behaves_like 'a successful request'
+
+          describe 'when X-Forwarded-For has extra spaces, commas' do
+            let(:x_forwarded_for) { ' , 192.168.0.1 , ' }
+
+            it_behaves_like 'a successful request'
+          end
+        end
+
+        describe 'when X-Forwarded-For is set to a non-cloudflare IP' do
+          let(:x_forwarded_for) { '1.2.3.4 4.5.6.7' }
+
+          it_behaves_like 'a rejected request'
+        end
+      end
+    end
+  end
+end
