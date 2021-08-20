3386b8970bd733d081cefaa6b7ef21531508e0e7
Damien Hogan <damienhogan87@gmail.com>
2021-08-20 11:06:59 +0100

    refactor to use a locale

--- app/components/feedback_component.rb
+++ app/components/feedback_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class FeedbackComponent < ViewComponent::Base
+  include ViewComponent::Translatable
+
   def initialize(heading:, area:)
     @heading = heading
     @area = area
