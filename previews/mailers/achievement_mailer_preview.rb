83bf76500543f92f6c1459f8e6a4e58a06d4f59f
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2021-09-28 18:16:20 +0100

    adds button to email adds email preview

---
+++ previews/mailers/achievement_mailer_preview.rb
@@ -0,0 +1,10 @@
+class AchievementMailerPreview < ActionMailer::Preview
+
+  def completed_face_to_face_course
+    AchievementMailer.with(user_id: User.first.id).completed_face_to_face_course
+  end
+
+  def completed_online_course
+    AchievementMailer.with(user_id: User.first.id).completed_online_course
+  end
+end
