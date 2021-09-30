412e55444ff4aaafb956c13eb14e77080971a9f7
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2021-09-30 13:12:56 +0100

    adds email query string

--- previews/mailers/achievement_mailer_preview.rb
+++ previews/mailers/achievement_mailer_preview.rb
@@ -1,5 +1,4 @@
 class AchievementMailerPreview < ActionMailer::Preview
-
   def completed_face_to_face_course
     AchievementMailer.with(user_id: User.first.id).completed_face_to_face_course
   end
