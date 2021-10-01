466e36e0c12209e29c09a46e077a529644d73dbd
Dan Halson <dan.halson@raspberrypi.org>
2021-10-01 15:56:40 +0100

    1956: Invalidate the schema when a file is changed

--- app/services/cache_invalidator.rb
+++ app/services/cache_invalidator.rb
@@ -3,9 +3,11 @@ class CacheInvalidator
     @resource = resource
     @identifier = identifier
     @namespace = 'curriculum'
+    @schema = 'curriculum_schema'
   end
 
   def run
+    Rails.cache.delete(@schema)
     Rails.cache.delete(resource_single_key, namespace: @namespace)
     Rails.cache.delete(resource_list_key, namespace: @namespace) if resource_list_cached?
   end
