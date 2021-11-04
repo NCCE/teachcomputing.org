3253eacba5ae118a9a564f56a38c3c5519278a89
Dan Halson <dan.halson@raspberrypi.org>
2021-11-04 13:28:07 +0000

    1977: Update copy and add new hero image

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
+  def category
+    case params[:page_slug]
+    when 'gender-balance'
+      'GBIC'
+    end
+  end
+
+  def tracking(label)
+    return unless category
+
+    { event_action: 'click', event_category: category, event_label: label }
+  end
 end
