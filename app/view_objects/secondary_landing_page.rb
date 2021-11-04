3598d6452df68d86b2df964d1077afe05fe7e902
Dan Halson <danhalson@users.noreply.github.com>
2021-11-04 14:33:16 +0000

    1996: Swap testimonial (#1421)

--- app/view_objects/secondary_landing_page.rb
+++ app/view_objects/secondary_landing_page.rb
@@ -79,11 +79,11 @@ class SecondaryLandingPage
   def testimonials
     [
       {
-        text: '“The online courses let me go through the content at my own pace, while the face to face gave me more hands-on experience with practical programming.”',
-        image: 'media/images/landing-pages/kasim.png',
-        name: 'Kasim Rashid',
-        link_target: 'https://blog.teachcomputing.org/getting-with-the-programming/',
-        bio: 'Maths and Computer Science teacher, London',
+        text: '“We’re teaching kids to use technologies that don’t exist yet for problems that they don’t know. It’s going to be a completely different world when they leave school.”',
+        image: 'media/images/pages/careers-week/helen_brant.jpg',
+        name: 'Helen Brant',
+        link_target: 'https://blog.teachcomputing.org/music-computing',
+        bio: 'Music teacher',
         tracking_event: 'Testimonial 1'
       },
       {
