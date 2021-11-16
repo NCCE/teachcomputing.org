0bb797be8c3f4054f4df6e6d8e00b025be52783d
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2021-11-16 15:37:02 +0000

    corrects test

--- spec/views/pages/pedagogy.html_spec.rb
+++ spec/views/pages/pedagogy.html_spec.rb
@@ -5,7 +5,19 @@ RSpec.describe('pages/pedagogy', type: :view) do
     render
   end
 
-  it 'has a title' do
+  it 'render a page title' do
     expect(rendered).to have_css('.hero__heading', text: 'Promoting effective computing pedagogy')
+  end
+
+  context 'when main page section' do
+    it 'renders correct number of sections' do
+      expect(rendered).to have_css('.ways_to_teach__title', count: 12)
+    end
+  end
+
+  context 'when aside page section' do
+    it 'renders correct number of aside sections' do
+      expect(rendered).to have_css('.report-card-component__title', count: 5)
+    end
   end
 end
