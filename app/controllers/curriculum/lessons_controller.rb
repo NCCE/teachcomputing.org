d580b3bf79b81d66a5f27ee56bd1479a991c7051
Dan Halson <dan.halson@raspberrypi.org>
2021-11-19 09:32:10 +0000

    1995: Raise if lesson empty

--- app/controllers/curriculum/lessons_controller.rb
+++ app/controllers/curriculum/lessons_controller.rb
@@ -6,6 +6,9 @@ module Curriculum
 
     def show
       @lesson = CurriculumClient::Queries::Lesson.one(params[:lesson_slug], params[:unit_slug]).lesson
+
+      raise ActiveRecord::RecordNotFound if @lesson.nil?
+
       @unit = @lesson.unit
     end
 
