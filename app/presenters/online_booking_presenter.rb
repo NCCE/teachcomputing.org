637a8c554034edbf7e09e056f63a88a4d3cbd07b
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2021-11-19 02:11:16 +0000

    adds aside booking details section to course page

--- app/presenters/online_booking_presenter.rb
+++ app/presenters/online_booking_presenter.rb
@@ -2,11 +2,35 @@ class OnlineBookingPresenter
   include Rails.application.routes.url_helpers
 
   def title
-    'Join this course'
+    'Join this course on FutureLearn'
+  end
+
+  def enrolled_title
+    'You are enrolled on this course'
+  end
+
+  def completed_title
+    'You’ve completed this course'
+  end
+
+  def enrolled_introduction
+    'You will be taken to the FutureLearn website for further details.'
   end
 
   def introduction
     'You will be taken to the FutureLearn website to create an account and sign up for online courses.'
+  end
+
+  def enrolled_button_title
+    'Continue course on FutureLearn'
+  end
+
+  def completed_button_title
+    'View course on FutureLearn'
+  end
+
+  def completed_button_introduction
+    'You will be taken to the FutureLearn website for further details.'
   end
 
   def booking_path(course_id)
