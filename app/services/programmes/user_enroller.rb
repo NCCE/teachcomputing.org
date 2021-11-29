f64e67e04ce9f5c976c31b05071f7b631cf55717
Dan Halson <dan.halson@raspberrypi.org>
2021-11-29 13:12:18 +0000

    1971: Enable jobs

--- app/services/programmes/user_enroller.rb
+++ app/services/programmes/user_enroller.rb
@@ -10,21 +10,22 @@ module Programmes
       enrolment = UserProgrammeEnrolment.new(@enrolment_params)
       return false unless enrolment.save
 
-      # KickOffEmailsJob.perform_later(enrolment.id)
-      # Achiever::ScheduleCertificateSyncJob.perform_later(enrolment.id)
+      KickOffEmailsJob.perform_later(enrolment.id)
+      Achiever::ScheduleCertificateSyncJob.perform_later(enrolment.id)
 
       QuestionnaireResponse.create(user_id: @user_id, questionnaire: Questionnaire.cs_accelerator) if enrolment.programme.cs_accelerator?
       set_eligible_achievements_for_programme
       true
     end
+
     private
 
-    def set_eligible_achievements_for_programme
-      user_achievements = Achievement.where(user_id: @user_id, programme_id: nil)
-      user_achievements.each do |achievement|
-        programme_activity = ProgrammeActivity.find_by(activity_id: achievement.activity_id, programme_id: @programme_id)
-        achievement.update(programme_id: @programme_id) if programme_activity
+      def set_eligible_achievements_for_programme
+        user_achievements = Achievement.where(user_id: @user_id, programme_id: nil)
+        user_achievements.each do |achievement|
+          programme_activity = ProgrammeActivity.find_by(activity_id: achievement.activity_id, programme_id: @programme_id)
+          achievement.update(programme_id: @programme_id) if programme_activity
+        end
       end
-    end
   end
 end
