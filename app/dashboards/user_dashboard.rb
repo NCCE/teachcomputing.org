054dc1da829019bfa74def73c870c7a9bbf90a19
Damien Hogan <damienhogan87@gmail.com>
2021-11-30 11:54:51 +0000

    Add dashboards

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -38,10 +38,10 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-    achievements
-    activities
-    assessment_attempts
-    feedback_comments
+    email
+    first_name
+    last_name
+    stem_achiever_contact_no
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
---
+++ app/dashboards/user_programme_enrolment_dashboard.rb
@@ -0,0 +1,78 @@
+require "administrate/base_dashboard"
+
+class UserProgrammeEnrolmentDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    user: Field::BelongsTo,
+    programme: Field::BelongsTo,
+    pathway: Field::BelongsTo,
+    user_programme_enrolment_transitions: Field::HasMany,
+    id: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+    flagged: Field::Boolean,
+    auto_enrolled: Field::Boolean,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+    user
+    programme
+    pathway
+    user_programme_enrolment_transitions
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    user
+    programme
+    pathway
+    user_programme_enrolment_transitions
+    id
+    created_at
+    updated_at
+    flagged
+    auto_enrolled
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    user
+    programme
+    pathway
+    user_programme_enrolment_transitions
+    flagged
+    auto_enrolled
+  ].freeze
+
+  # COLLECTION_FILTERS
+  # a hash that defines filters that can be used while searching via the search
+  # field of the dashboard.
+  #
+  # For example to add an option to search for open resources by typing "open:"
+  # in the search field:
+  #
+  #   COLLECTION_FILTERS = {
+  #     open: ->(resources) { resources.where(open: true) }
+  #   }.freeze
+  COLLECTION_FILTERS = {}.freeze
+
+  # Overwrite this method to customize how user programme enrolments are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(user_programme_enrolment)
+  #   "UserProgrammeEnrolment ##{user_programme_enrolment.id}"
+  # end
+end
