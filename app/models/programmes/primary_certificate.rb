2374af5cf27f8c51868617ffb6d3e7fe7f3888c8
Damien Hogan <damienhogan87@gmail.com>
2021-12-09 15:57:57 +0000

    correct

--- app/models/programmes/primary_certificate.rb
+++ app/models/programmes/primary_certificate.rb
@@ -48,17 +48,17 @@ module Programmes
 
         complete_achievements.with_category('online')
                             .any? &&
-          complete_achievements.with_category('face-to-face')
-                              .any? &&
-          complete_achievements.with_category('community')
-                              .with_credit('5')
-                              .any? &&
-          complete_achievements.with_category('community')
-                              .with_credit('10')
-                              .any? &&
-          complete_achievements.with_category('community')
-                              .with_credit('20')
-                              .any?
+        complete_achievements.with_category('face-to-face')
+                            .any? &&
+        complete_achievements.with_category('community')
+                            .with_credit('5')
+                            .any? &&
+        complete_achievements.with_category('community')
+                            .with_credit('10')
+                            .any? &&
+        complete_achievements.with_category('community')
+                            .with_credit('20')
+                            .any?
       end
     end
 
