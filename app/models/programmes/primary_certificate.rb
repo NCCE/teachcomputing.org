63e0368a97d96b5f905ede778d2e62f4ed92848b
Dan Halson <dan.halson@raspberrypi.org>
2021-12-09 16:06:50 +0000

    2017: Make rubocop happy

--- app/models/programmes/primary_certificate.rb
+++ app/models/programmes/primary_certificate.rb
@@ -47,18 +47,18 @@ module Programmes
                                     .in_state('complete')
 
         complete_achievements.with_category('online')
-                            .any? &&
-        complete_achievements.with_category('face-to-face')
-                            .any? &&
-        complete_achievements.with_category('community')
-                            .with_credit('5')
-                            .any? &&
-        complete_achievements.with_category('community')
-                            .with_credit('10')
-                            .any? &&
-        complete_achievements.with_category('community')
-                            .with_credit('20')
-                            .any?
+                             .any? &&
+          complete_achievements.with_category('face-to-face')
+                               .any? &&
+          complete_achievements.with_category('community')
+                               .with_credit('5')
+                               .any? &&
+          complete_achievements.with_category('community')
+                               .with_credit('10')
+                               .any? &&
+          complete_achievements.with_category('community')
+                               .with_credit('20')
+                               .any?
       end
     end
 
