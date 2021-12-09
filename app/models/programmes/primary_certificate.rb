5186374e8592aab2316b1805a7f13d07d1022585
Damien Hogan <damienhogan87@gmail.com>
2021-12-09 15:31:03 +0000

    Refactor

--- app/models/programmes/primary_certificate.rb
+++ app/models/programmes/primary_certificate.rb
@@ -39,23 +39,27 @@ module Programmes
     end
 
     def user_meets_completion_requirement?(user)
-      complete_achievements = user.achievements
-                                  .for_programme(self)
-                                  .in_state('complete')
+      if FeatureFlagService.new.flags[:primary_redesign_enabled]
+        super
+      else
+        complete_achievements = user.achievements
+                                    .for_programme(self)
+                                    .in_state('complete')
 
-      complete_achievements.with_category('online')
-                           .any? &&
-        complete_achievements.with_category('face-to-face')
-                             .any? &&
-        complete_achievements.with_category('community')
-                             .with_credit('5')
-                             .any? &&
-        complete_achievements.with_category('community')
-                             .with_credit('10')
-                             .any? &&
-        complete_achievements.with_category('community')
-                             .with_credit('20')
-                             .any?
+        complete_achievements.with_category('online')
+                            .any? &&
+          complete_achievements.with_category('face-to-face')
+                              .any? &&
+          complete_achievements.with_category('community')
+                              .with_credit('5')
+                              .any? &&
+          complete_achievements.with_category('community')
+                              .with_credit('10')
+                              .any? &&
+          complete_achievements.with_category('community')
+                              .with_credit('20')
+                              .any?
+      end
     end
 
     def path
