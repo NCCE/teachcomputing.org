f76d5e48e215a3357064a581b2b1eb9c1db762cd
Greg Annandale <greg@raspberrypi.org>
2022-01-19 11:49:41 +0000

    Bumps loglevel to debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   config.log_tags = [
