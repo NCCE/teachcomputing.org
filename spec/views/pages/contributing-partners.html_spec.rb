fd2d99c033885803b076c58b2449d111d9782ca0
Ant Barnes <ant@arayb.com>
2022-01-21 09:57:52 +0000

    Add Bebras to contributing partners page

--- spec/views/pages/contributing-partners.html_spec.rb
+++ spec/views/pages/contributing-partners.html_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe('pages/contributing-partners', type: :view) do
     expect(rendered).to have_css('.govuk-body-l', count: 1)
   end
 
-  it 'has 10 cards' do
-    expect(rendered).to have_css('.image-link-card-component', count: 10)
+  it 'has 11 cards' do
+    expect(rendered).to have_css('.image-link-card-component', count: 11)
   end
 end
