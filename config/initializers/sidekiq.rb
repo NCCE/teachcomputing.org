528923f73b18c04329ef2ede7268fcf35e1447ef
Ant Barnes <ant@arayb.com>
2022-01-28 15:37:27 +0000

    Remove reference to removed class

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.default_worker_options = { retry: false }
-Sidekiq::Logging.logger.level = Logger::WARN
+Sidekiq.logger.level = Logger::WARN
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'], size: 2, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
