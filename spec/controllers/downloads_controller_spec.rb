96a622223cdc0d9cd29ee75d00e692feb06c1621
Nic <nic.shackle@raspberrypi.org>
2022-02-23 12:22:38 +0000

    clean+update tests

---
+++ spec/controllers/downloads_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe DownloadsController, type: :controller do
+  describe 'POST' do
+    let(:user) { create(:user, email: 'web@raspberrypi.org') }
+    let(:uri) { 'https://rubyonrails.org' }
+
+    it 'creates download, recording logged in user' do
+      allow(controller).to receive(:current_user) { user }
+      post :create, params: { uri: uri }
+      expect(Download.first.uri).to eq uri
+      expect(Download.first.user_id).to eq user.id
+    end
+
+    it 'creates download without logged in user' do
+      post :create, params: { uri: uri }
+      expect(Download.first.uri).to eq uri
+      expect(Download.first.user_id).to eq nil
+    end
+
+    it 'redirects to uri after creation' do
+      post :create, params: { uri: uri }
+      assert_redirected_to 'https://rubyonrails.org'
+    end
+  end
+end
---
+++ spec/models/download_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Download, type: :model do
+  # test "the truth" do
+  #   assert true
+  # end
+end
