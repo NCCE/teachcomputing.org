d0aa9052c7125cae635537bcc4a179b7d0a12ea8
Nic <nic.shackle@raspberrypi.org>
2022-02-28 16:41:21 +0000

    clean up downloadsController (per review comments)

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,6 +1,4 @@
 class DownloadsController < ApplicationController
-  before_action :set_download, only: [:show, :edit, :update, :destroy]
-
   # @name POST /downloads
   # @note Allows downloads of static assets to be recorded
   # @example
@@ -13,26 +11,14 @@ class DownloadsController < ApplicationController
       aggregate_download.save
     end
 
-    @download = Download.new
-    @download.user_id = current_user.id if current_user.present?
-    aggregate_download.downloads << @download
+    download = Download.new
+    download.user_id = current_user.id if current_user.present?
+    aggregate_download.downloads << download
 
-    if @download.save
+    if download.save
       redirect_to aggregate_download.uri
     else
       flash[:error] = 'There was a problem creating download. Please contact support'
     end
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_download
-    @download = Download.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def download_params
-    params.require(:download).permit(:uri)
   end
 end
