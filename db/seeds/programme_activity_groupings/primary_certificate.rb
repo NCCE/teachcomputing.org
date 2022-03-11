dd270918965ded7d902613a21c1481d39266189d
Dan Halson <dan.halson@raspberrypi.org>
2022-03-11 12:14:22 +0000

    2075: Activity groupings now created dynamically according to category

--- db/seeds/programme_activity_groupings/primary_certificate.rb
+++ db/seeds/programme_activity_groupings/primary_certificate.rb
@@ -34,61 +34,11 @@ end
 
 puts 'Seeding Programme Activity Groupings for Primary Certificate'
 
-group_one = primary_certificate.programme_activity_groupings.find_by(sort_key: 1)
-
 # Online/Future Learn courses
 
-if activity = Activity.find_by(future_learn_course_uuid: '7e5ae100-f4fc-425b-a53b-c81cb6eb4abc')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_one.id) unless group_one.programme_activities.include?(programme_activity)
-end
+group_one = primary_certificate.programme_activity_groupings.find_by(sort_key: 1)
 
-if activity = Activity.find_by(future_learn_course_uuid: 'bc7debb8-59a0-4d4a-8d86-082047bf155f')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_one.id) unless group_one.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(future_learn_course_uuid: 'b19646a7-d78b-4a92-ad36-d4b3a11a3df1')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_one.id) unless group_one.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(future_learn_course_uuid: '4ec560a3-6435-46bc-90b7-75cfdcf7e72d')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_one.id) unless group_one.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(future_learn_course_uuid: '147b3adf-9f26-4ffa-a95f-6c1720ca4d27')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_one.id) unless group_one.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(future_learn_course_uuid: '7f88c178-9538-4970-b438-ab80e6125d5e')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_one.id) unless group_one.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(future_learn_course_uuid: 'b2445d09-f3b3-45da-b4ec-6d33bb6cb89b')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_one.id) unless group_one.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(future_learn_course_uuid: 'c9fb59cc-6393-4a29-8136-7020128ca879')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_one.id) unless group_one.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(future_learn_course_uuid: '3ce9a624-6cc7-4d23-8f5f-95162e360178')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_one.id) unless group_one.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(future_learn_course_uuid: '26e9cd23-2d71-4964-9af3-751aa3fdc8e5')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_one.id) unless group_one.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(future_learn_course_uuid: 'd440d652-4128-4995-9ef7-662a0bc505ed')
+primary_certificate.activities.online.each do |activity|
   programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
   programme_activity.update(programme_activity_grouping_id: group_one.id) unless group_one.programme_activities.include?(programme_activity)
 end
@@ -97,80 +47,13 @@ end
 
 group_two = primary_certificate.programme_activity_groupings.find_by(sort_key: 2)
 
-if activity = Activity.find_by(stem_course_template_no: 'e5acc943-4926-ea11-a810-000d3a86d545')
+# The face_to_face category covers remote too (as we have no specific remote category)
+primary_certificate.activities.face_to_face.each do |activity|
   programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
   programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
 end
 
-if activity = Activity.find_by(stem_course_template_no: '68f5b6c5-556d-4b66-8159-7cd6019da6f3')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: '488bed9b-515b-4295-a488-62b5bb6bf852')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: '16aeecbd-d202-4f42-bad3-f86c8f671547')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: '34ff2768-a7fc-ea11-a813-000d3a86d545')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: 'e3c14378-3015-eb11-a813-000d3a86f6ce')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: '11f58c3f-3341-eb11-a813-000d3a86d545')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: 'bb7a0f31-5086-ea11-a811-000d3a86d545')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: '3bff03fd-256d-eb11-a812-000d3a872640')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: '6bc40e34-4c86-ea11-a811-000d3a86d545')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: 'e432e725-ba7f-ea11-a811-000d3a86d545')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: '5b694e80-ce7f-ea11-a811-000d3a86d545')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: 'a533bf46-d17f-ea11-a811-000d3a86d545')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: '5fdb7188-dd7f-ea11-a811-000d3a86f6ce')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
-
-if activity = Activity.find_by(stem_course_template_no: '88975226-811c-ec11-b6e7-0022481a8033')
-  programme_activity = primary_certificate.programme_activities.find_by(activity_id: activity.id)
-  programme_activity.update(programme_activity_grouping_id: group_two.id) unless group_two.programme_activities.include?(programme_activity)
-end
+# Community Achievements
 
 group_three = primary_certificate.programme_activity_groupings.find_by(sort_key: 3)
 
