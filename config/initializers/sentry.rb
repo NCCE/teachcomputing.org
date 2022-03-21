51f138b47792adceb55701aaded341a62bd4f627
Dan Halson <dan.halson@raspberrypi.org>
2022-03-21 15:10:22 +0000

    Add env name for review apps

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,6 +3,7 @@ Sentry.init do |config|
   config.breadcrumbs_logger = %i[sentry_logger active_support_logger http_logger]
   config.traces_sample_rate = 0.25
   config.enabled_environments = %w[staging production]
+  config.environment = ENV.fetch('HEROKU_APP_NAME') || Rails.env
 
   filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
   config.before_send = lambda do |event, _hint|
