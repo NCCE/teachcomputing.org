9ca3c9c0cfdb241710d019c5c241c1bd10c319be
Dan Halson <dan.halson@raspberrypi.org>
2022-03-21 14:08:39 +0000

    Fix dsn usage, add standard logger

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 Sentry.init do |config|
-  config.dsn = Rails.application.credentials[ENV.fetch('SENTRY_DSN')]
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.dsn = ENV.fetch('SENTRY_DSN')
+  config.breadcrumbs_logger = %i[sentry_logger active_support_logger http_logger]
   config.traces_sample_rate = 0.25
   config.enabled_environments = %w[staging production]
 
