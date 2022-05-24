ad174c66c2d1810329f5523e8d75f15a3e8546e5
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2022-05-24 10:00:48 +0100

    Add stub to test

--- spec/system/courses/show_spec.rb
+++ spec/system/courses/show_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe('Course Details page', type: :system) do
     stub_subjects
     stub_age_groups
     stub_course_templates
+    stub_delegate
   end
 
   context 'when using a desktop', js: true do
