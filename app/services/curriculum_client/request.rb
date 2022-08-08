ff6dc28298e578048b6b8379e893b87181a18376
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2022-08-08 13:09:32 +0100

    handles exception in the controller instead

--- app/services/curriculum_client/request.rb
+++ app/services/curriculum_client/request.rb
@@ -21,7 +21,12 @@ module CurriculumClient
           fetch_data(query, client, params)
         end
       rescue Graphlient::Errors::ExecutionError => e
+        # Graphlient does not support the graphql extensions hash. See: http://spec.graphql.org/June2018/#example-fce18
+        original_hash = e.response.original_hash
+        extensions = original_hash['errors'][0]['extensions']
+        raise ActiveRecord::RecordNotFound, e.message if extensions && extensions['code'] == :not_found.to_s
 
+        raise # Don't prevent other ExecutionErrors from being raised
       rescue Graphlient::Errors::ServerError => e
         case e.status_code
         when 404
