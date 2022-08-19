df36d1b9b0ac32fcfab3baad202945b00907c52d
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2022-08-19 16:10:34 +0100

    enforces integer

--- app/helpers/curriculum_helper.rb
+++ app/helpers/curriculum_helper.rb
@@ -36,7 +36,7 @@ module CurriculumHelper
   end
 
   def lesson_with_range_wording(lesson_order, lesson_range)
-    if lesson_range - lesson_order >= 2
+    if lesson_range.to_i - lesson_order.to_i >= 2
       "Lesson #{lesson_order} to #{lesson_range}"
     else
       "Lesson #{lesson_order} and #{lesson_range}"
