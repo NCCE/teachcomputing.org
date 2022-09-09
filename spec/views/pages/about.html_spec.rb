d502693fb55911ba04c712708e90567c5a77b530
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2022-09-09 14:28:14 +0100

    resolves pr comments

--- spec/views/pages/about.html_spec.rb
+++ spec/views/pages/about.html_spec.rb
@@ -35,6 +35,8 @@ RSpec.describe('pages/about', type: :view) do
   end
 
   it 'has the expected links in the offer section' do
+    expect(rendered).to have_link('resources', href: '/curriculum')
+    expect(rendered).to have_link('training courses', href: '/courses')
     expect(rendered).to have_link('Funding is available', href: '/funding')
     expect(rendered).to have_link('Primary teachers toolkit', href: '/primary-teachers')
     expect(rendered).to have_link('Secondary teachers toolkit', href: '/secondary-teachers')
