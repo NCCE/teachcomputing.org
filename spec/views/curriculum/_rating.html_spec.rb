1de2934c41bcccc1c89618d3559e3f5251b7ae48
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2022-11-10 13:26:04 +0000

    resolves ratings tests

--- spec/views/curriculum/_rating.html_spec.rb
+++ spec/views/curriculum/_rating.html_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe('curriculum/_rating', type: :view) do
   describe 'a lesson rating partial' do
     before do
       render partial: 'curriculum/rating',
-             locals: { path: :create_curriculum_lesson_rating_path, comment_path: :update_curriculum_lesson_rating, id: 'an_id',
+             locals: { path: :create_curriculum_lesson_rating_path, comment_path: :update_curriculum_lesson_rating, choices_path: :update_curriculum_unit_rating_choices, id: 'an_id',
                        user_id: 'user_id' }
     end
 
@@ -20,7 +20,7 @@ RSpec.describe('curriculum/_rating', type: :view) do
   describe 'a unit rating partial' do
     before do
       render partial: 'curriculum/rating',
-             locals: { path: :create_curriculum_unit_rating_path, comment_path: :update_curriculum_unit_rating, id: 'an_id',
+             locals: { path: :create_curriculum_unit_rating_path, comment_path: :update_curriculum_unit_rating, choices_path: :update_curriculum_unit_rating_choices, id: 'an_id',
                        user_id: 'user_id' }
     end
 
@@ -44,7 +44,7 @@ RSpec.describe('curriculum/_rating', type: :view) do
     end
 
     it 'renders with the expected message' do
-      expect(rendered).to have_text('You have already provided a rating, thanks!')
+      expect(rendered).to have_text('You have already provided a rating, thank you!')
     end
   end
 end
