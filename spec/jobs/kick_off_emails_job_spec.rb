208396a35d9c10941d32f5b02fa384e5df81e1f7
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2022-11-14 18:44:13 +0000

    switched to the kick of class instead

--- spec/jobs/kick_off_emails_job_spec.rb
+++ spec/jobs/kick_off_emails_job_spec.rb
@@ -2,14 +2,24 @@ require 'rails_helper'
 
 RSpec.describe KickOffEmailsJob, type: :job do
   let(:cs_accelerator) { create(:cs_accelerator) }
+  let(:primary) { create(:primary_certificate) }
   let(:secondary) { create(:secondary_certificate) }
   let(:cs_accelerator_enrolment) { create(:user_programme_enrolment, programme_id: cs_accelerator.id) }
+  let(:primary_enrolment) { create(:user_programme_enrolment, programme_id: primary.id) }
   let(:secondary_enrolment) { create(:user_programme_enrolment, programme_id: secondary.id) }
 
   describe '#perform' do
+  
     context 'when the programme is cs accelerator' do
       it 'sends an email' do
         expect { described_class.perform_now(cs_accelerator_enrolment.id) }
+        .to change { ActionMailer::Base.deliveries.count }.by(1)
+      end
+    end
+  
+    context 'when the programme is primary' do
+      it 'sends an email' do
+        expect { described_class.perform_now(primary_enrolment.id) }
           .to change { ActionMailer::Base.deliveries.count }.by(1)
       end
     end
