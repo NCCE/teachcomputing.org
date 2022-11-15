8d3efa47f42314223e4cb1858b51efae9166f81b
timregan <tim_regan82@hotmail.com>
2022-11-15 11:00:11 +0000

    Fixing preview syntax

--- previews/components/tick_list_collection_component_preview.rb
+++ previews/components/tick_list_collection_component_preview.rb
@@ -1,9 +1,9 @@
 class TickListCollectionComponentPreview < ViewComponent::Preview
   def default
-    tick_lists = [
+    tick_list_collection = [
       {
         class_name: 'tick-list-example-component',
-        title: 'Tick List Component',
+        title: 'Tick List Component 1',
         text: 'This is an example of a tick list component. This is the main ' \
               'body text and it may span multiple lines so our text should ' \
               'attempt to do this too. There will also be some bullet points ' \
@@ -12,20 +12,22 @@ class TickListCollectionComponentPreview < ViewComponent::Preview
         button: {
           button_title: 'Example button',
           button_url: '/'
+        }
       },
       {
-          class_name: 'tick-list-example-component',
-          title: 'Tick List Component',
-          text: 'This is an example of a tick list component. This is the main ' \
-                'body text and it may span multiple lines so our text should ' \
-                'attempt to do this too. There will also be some bullet points ' \
-                'and a button displayed. All are populated by parameters.',
-          bullets: ['Item 1', 'Item 2', 'Item 3'],
-          button: {
-            button_title: 'Example button',
-            button_url: '/'
+        class_name: 'tick-list-example-component',
+        title: 'Tick List Component 2',
+        text: 'This is another example of a tick list component. This is the ' \
+              'main body text and it may span multiple lines so our text ' \
+              'should attempt to do this too. There will also be some bullet ' \
+              'points and a button displayed. All are populated by parameters.',
+        bullets: ['Item 1', 'Item 2', 'Item 3'],
+        button: {
+          button_title: 'Example button',
+          button_url: '/'
+        }
       }
     ]
-    render(TickListCollectionComponent.new(tick_lists:))
+    render(TickListCollectionComponent.new(tick_list_collection:))
   end
 end
