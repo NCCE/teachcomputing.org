d9ea88f137028ef7eaedb1d12b9087415111e7d9
timregan <tim_regan82@hotmail.com>
2022-11-16 11:06:46 +0000

    Swapping to faker

--- spec/components/tick_list_component_spec.rb
+++ spec/components/tick_list_component_spec.rb
@@ -1,59 +1,58 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe TickListComponent, type: :component do
   let(:tick_list) do
     {
-      class_name: 'quality-framework-tick-list',
-      title: 'The Computing Quality Framework',
-      text: 'The Computing Quality Framework helps you and your leadership ' \
-            'team assess your computing offer. Use our guided journey to:',
+      class_name: Faker::Lorem.unique.words.join('-'),
+      title: Faker::Lorem.unique.sentence,
+      text: Faker::Lorem.unique.paragraph,
       bullets: [
-        'Identify strengths and weaknesses in your computing curriculum',
-        'Gain feedback and suggested actions, including relevant resources',
-        'Track progress towards achieving our accredited Computing Quality Mark'
+        Faker::Lorem.unique.sentence,
+        Faker::Lorem.unique.sentence,
+        Faker::Lorem.unique.sentence,
+        Faker::Lorem.unique.sentence
       ],
       button: {
-        button_title: 'Register for the Computing Quality Framework',
-        button_url: '/register'
+        button_title: Faker::Lorem.unique.sentence,
+        button_url: Faker::Internet.url(scheme: 'https')
       }
     }
   end
 
-  context 'with no date' do
-    before do
-      render_inline(described_class.new(tick_list:))
-    end
+  before do
+    render_inline(described_class.new(tick_list:))
+  end
 
-    it 'adds the wrapper class' do
-      expect(rendered_component).to have_css(".#{tick_list[:class_name]}")
-    end
+  it 'adds the wrapper class' do
+    expect(rendered_component).to have_css(".#{tick_list[:class_name]}")
+  end
 
-    it 'renders a title' do
-      expect(rendered_component).to have_css(
-        '.tick-list-component__title',
-        text: tick_list[:title]
-      )
-    end
+  it 'renders a title' do
+    expect(rendered_component).to have_css(
+      '.tick-list-component__title',
+      text: tick_list[:title]
+    )
+  end
 
-    it 'renders the body text' do
-      expect(rendered_component).to have_css(
-        '.tick-list-component__text',
-        text: tick_list[:text]
-      )
-    end
+  it 'renders the body text' do
+    expect(rendered_component).to have_css(
+      '.tick-list-component__text',
+      text: tick_list[:text]
+    )
+  end
 
-    it 'renders a button' do
-      expect(rendered_component).to have_link(
-        tick_list[:button][:button_title],
-        href: tick_list[:button][:button_url]
-      )
-    end
+  it 'renders a button' do
+    expect(rendered_component).to have_link(
+      tick_list[:button][:button_title],
+      href: tick_list[:button][:button_url]
+    )
+  end
 
-    it 'renders a list with the expected number of items' do
-      expect(rendered_component).to have_css(
-        '.tick-list-component__list li',
-        count: tick_list[:bullets].count
-      )
-    end
+  it 'renders a list with the expected number of items' do
+    expect(rendered_component).to have_css(
+      '.tick-list-component__list li',
+      count: tick_list[:bullets].count
+    )
   end
 end
