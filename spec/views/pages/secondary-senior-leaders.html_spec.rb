42d7d34d995620d5c14be530726b8475abc71afa
timregan <tim_regan82@hotmail.com>
2022-11-21 17:44:33 +0000

    Page tested

--- spec/views/pages/secondary-senior-leaders.html_spec.rb
+++ spec/views/pages/secondary-senior-leaders.html_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe('pages/secondary-senior-leaders') do
   # TODO: can we read values from /app/config/locales/views/pages/secondary-senior-leaders/en.yml ?
-  let(:title) { 'Support for Secondary Senior Leaders' }
+  let(:hero_title) { 'Support for Secondary Senior Leaders' }
+  let(:quality_framework_tick_list_title) { 'How we can help you and your school or college' }
+  let(:student_help_title) { 'How we can help you and your students' }
+  let(:stem_partner_title) { 'STEM Ambassadors' }
+  let(:code_club_partner_title) { 'How we can help you and your students' }
+  let(:bebras_partner_title) { 'How we can help you and your students' }
 
   before do
     render
@@ -10,10 +15,26 @@ RSpec.describe('pages/secondary-senior-leaders') do
 
   it 'has a title' do
     # TODO: either of the next two fail, why?
-    # expect(rendered).to have_css('title', text: title)
-    # expect(rendered).to have_title(title)
-    expect(rendered).to have_css('h1', text: title)
+    # expect(rendered).to have_css('title', text: hero_title)
+    # expect(rendered).to have_title(hero_title)
+    expect(rendered).to have_css('h1', text: hero_title)
   end
 
+  it 'has the TickListCollectionComponent' do
+    expect(rendered).to have_css('.tick-list-collection-component__title', text: quality_framework_tick_list_title)
+  end
 
+  it 'has the BorderedCardsComponent' do
+    expect(rendered).to have_css('.teacher-help-cards')
+  end
+
+  it 'has the LogoButtonImageCardComponent' do
+    expect(rendered).to have_css('.logo-button-image-card-component__title', text: student_help_title)
+  end
+
+  it 'has the partners listed' do
+    expect(rendered).to have_css('non-bordered-card__title', text: stem_partner_title)
+    expect(rendered).to have_css('non-bordered-card__title', text: code_club_partner_title)
+    expect(rendered).to have_css('non-bordered-card__title', text: bebras_partner_title)
+  end
 end
