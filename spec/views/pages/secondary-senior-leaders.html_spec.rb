e72621712b05351eb0e051ea1df676e203e4e9f0
timregan <tim_regan82@hotmail.com>
2022-11-21 17:20:40 +0000

    Tests for page h1

---
+++ spec/views/pages/secondary-senior-leaders.html_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe('pages/secondary-senior-leaders') do
+  let(:title) { 'Support for Secondary Senior Leaders' }
+
+  before do
+    render
+  end
+
+  it 'has a title' do
+    expect(rendered).to have_css('h1', text: title)
+  end
+end
