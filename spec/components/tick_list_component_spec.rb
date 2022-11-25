0b0af7501cc224dacb5f8f77ed7b00d93006ee11
timregan <tim_regan82@hotmail.com>
2022-11-25 15:39:47 +0000

    Test for exception from nil tick_list

--- spec/components/tick_list_component_spec.rb
+++ spec/components/tick_list_component_spec.rb
@@ -2,65 +2,73 @@ require 'rails_helper'
 require 'faker'
 
 RSpec.describe TickListComponent, type: :component do
-  let(:tick_list) do
-    {
-      class_name: Faker::Lorem.unique.words.join('-'),
-      title: Faker::Lorem.unique.sentence,
-      text: Faker::Lorem.unique.paragraph,
-      bullets: [
-        Faker::Lorem.unique.sentence,
-        Faker::Lorem.unique.sentence,
-        Faker::Lorem.unique.sentence,
-        Faker::Lorem.unique.sentence
-      ],
-      button: {
-        button_title: Faker::Lorem.unique.sentence,
-        button_url: Faker::Internet.url(scheme: 'https'),
-        tracking_page: Faker::Lorem.unique.word,
-        tracking_label: Faker::Lorem.unique.word
+  context 'with a tick_list provided' do
+    let(:tick_list) do
+      {
+        class_name: Faker::Lorem.unique.words.join('-'),
+        title: Faker::Lorem.unique.sentence,
+        text: Faker::Lorem.unique.paragraph,
+        bullets: [
+          Faker::Lorem.unique.sentence,
+          Faker::Lorem.unique.sentence,
+          Faker::Lorem.unique.sentence,
+          Faker::Lorem.unique.sentence
+        ],
+        button: {
+          button_title: Faker::Lorem.unique.sentence,
+          button_url: Faker::Internet.url(scheme: 'https'),
+          tracking_page: Faker::Lorem.unique.word,
+          tracking_label: Faker::Lorem.unique.word
+        }
       }
-    }
+    end
+
+    before do
+      render_inline(described_class.new(tick_list:))
+    end
+
+    it 'adds the wrapper class' do
+      expect(rendered_component).to have_css(".#{tick_list[:class_name]}")
+    end
+
+    it 'renders a title' do
+      expect(rendered_component).to have_css(
+        '.tick-list-component__title',
+        text: tick_list[:title]
+      )
+    end
+
+    it 'renders the body text' do
+      expect(rendered_component).to have_css(
+        '.tick-list-component__text',
+        text: tick_list[:text]
+      )
+    end
+
+    it 'renders a button' do
+      expect(rendered_component).to have_link(
+        tick_list[:button][:button_title],
+        href: tick_list[:button][:button_url]
+      )
+    end
+
+    it 'renders a list with the expected number of items' do
+      expect(rendered_component).to have_css(
+        '.tick-list-component__list li',
+        count: tick_list[:bullets].count
+      )
+    end
+
+    it 'adds the GA tag' do
+      expect(rendered_component).to have_selector("a[href='#{tick_list[:button][:button_url]}'][data-event-action='click']")
+      expect(rendered_component).to have_selector("a[href='#{tick_list[:button][:button_url]}'][data-event-category='#{tick_list[:button][:tracking_page]}']")
+      expect(rendered_component).to have_selector("a[href='#{tick_list[:button][:button_url]}'][data-event-label='#{tick_list[:button][:tracking_label]}']")
+    end
   end
 
-  before do
-    render_inline(described_class.new(tick_list:))
-  end
-
-  it 'adds the wrapper class' do
-    expect(rendered_component).to have_css(".#{tick_list[:class_name]}")
-  end
-
-  it 'renders a title' do
-    expect(rendered_component).to have_css(
-      '.tick-list-component__title',
-      text: tick_list[:title]
-    )
-  end
-
-  it 'renders the body text' do
-    expect(rendered_component).to have_css(
-      '.tick-list-component__text',
-      text: tick_list[:text]
-    )
-  end
-
-  it 'renders a button' do
-    expect(rendered_component).to have_link(
-      tick_list[:button][:button_title],
-      href: tick_list[:button][:button_url]
-    )
-  end
-
-  it 'renders a list with the expected number of items' do
-    expect(rendered_component).to have_css(
-      '.tick-list-component__list li',
-      count: tick_list[:bullets].count
-    )
-  end
-
-  it 'adds the GA tag' do
-    expect(rendered_component).to have_selector("a[href='#{tick_list[:button][:button_url]}'][data-event-action='click']")
-    expect(rendered_component).to have_selector("a[href='#{tick_list[:button][:button_url]}'][data-event-category='#{tick_list[:button][:tracking_page]}']")
-    expect(rendered_component).to have_selector("a[href='#{tick_list[:button][:button_url]}'][data-event-label='#{tick_list[:button][:tracking_label]}']")
+  context 'without a tick-list provided' do
+    it 'raises an exception' do
+      expect { described_class.new(tick_list: nil) }.to raise_error(ArgumentError)
+    end
   end
 end
