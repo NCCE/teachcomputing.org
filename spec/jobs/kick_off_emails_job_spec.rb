ffcc2f76d318ab4f3dd429c8300c6fa8033b9b75
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2022-11-29 15:06:26 +0000

    pr test edit

--- spec/jobs/kick_off_emails_job_spec.rb
+++ spec/jobs/kick_off_emails_job_spec.rb
@@ -9,37 +9,46 @@ RSpec.describe KickOffEmailsJob, type: :job do
   let(:primary_enrolment) { create(:user_programme_enrolment, programme_id: primary.id) }
   let(:secondary_enrolment) { create(:user_programme_enrolment, programme_id: secondary.id) }
   let(:primary_certificate_enrolment) { create(:user_programme_enrolment, programme_id: primary_certificate.id) }
-
+  
+  
   describe '#perform' do
-
-    context 'when the programme is cs accelerator' do
-      it 'sends an email' do
-        expect { described_class.perform_now(cs_accelerator_enrolment.id) }
-        .to change { ActionMailer::Base.deliveries.count }.by(1)
-      end
-    end
-
-    context 'when the programme is primary' do
-      it 'sends an email' do
-        expect { described_class.perform_now(primary_enrolment.id) }
-          .to change { ActionMailer::Base.deliveries.count }.by(1)
-      end
-    end
-
-    context 'when the programme is secondary' do
-      it 'sends an email' do
-        expect { described_class.perform_now(secondary_enrolment.id) }
-          .to change { ActionMailer::Base.deliveries.count }.by(1)
-      end
-    end
-
-    it 'sends jobs to be delivered later' do
-      expect do
-        described_class.perform_now(cs_accelerator_enrolment.id)
-      end.to have_enqueued_job(ScheduleProgrammeGettingStartedPromptJob).with(cs_accelerator_enrolment.id)
-      expect do
-        described_class.perform_now(primary_certificate_enrolment.id)
-      end.to have_enqueued_job(ScheduleProgrammeGettingStartedPromptJob).with(primary_certificate_enrolment.id)
+  
+  context 'when the programme is cs accelerator' do
+    it 'sends an email' do
+      expect { described_class.perform_now(cs_accelerator_enrolment.id) }
+      .to change { ActionMailer::Base.deliveries.count }.by(1)
     end
   end
+  
+  context 'when the programme is primary' do
+    it 'sends an email' do
+      expect { described_class.perform_now(primary_enrolment.id) }
+      .to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+  end
+  
+  context 'when the programme is secondary' do
+    it 'sends an email' do
+      expect { described_class.perform_now(secondary_enrolment.id) }
+      .to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+  end
+  
+  describe 'jobs to be delivered a month after enrolment' do 
+    it 'enques jobs to be delivered later' do
+      let(:time) { Time.new(2020, 11, 11, 12, 22) }
+
+      before do
+        allow(Time).to receive(:now).and_return(time)
+      end
+
+      expect do
+        described_class.perform_now(cs_accelerator_enrolment.id)
+      end.to have_enqueued_job(ScheduleProgrammeGettingStartedPromptJob).with(cs_accelerator_enrolment.id).at(time)
+      expect do
+        described_class.perform_now(primary_certificate_enrolment.id)
+      end.to have_enqueued_job(ScheduleProgrammeGettingStartedPromptJob).with(primary_certificate_enrolment.id).at(time)
+    end
+  end 
+end
 end
