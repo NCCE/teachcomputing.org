803cb7eaf146c84e93429916b36dc563ee0d4a68
Tim Regan <tim.regan@raspberrypi.org>
2022-11-30 14:49:03 +0000

    Wrong thing

--- app/services/support/user_utilities.rb
+++ app/services/support/user_utilities.rb
@@ -4,5 +4,10 @@ module Support
       user = User.find(user_id)
       Achiever::FetchUsersCompletedCoursesFromAchieverJob.perform_now(user)
     end
+
+    def self.reset_tests(user_id)
+      user = User.find(user_id)
+      user&.achievements&.each { |ach| ach.transition_to(:enrolled) }
+    end
   end
 end
