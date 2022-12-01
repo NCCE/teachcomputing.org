91e32494afd7ad0a9500920dc6b7dc287deb7385
dumbledad <tim.regan@raspberrypi.org>
2022-12-01 16:19:54 +0000

    another refactoring prior to adding audit test

--- spec/system/admin/reset_tests_user_spec.rb
+++ spec/system/admin/reset_tests_user_spec.rb
@@ -6,16 +6,20 @@ RSpec.describe 'User reset tests' do
 
     before do
       create_list(:assessment_attempt, 5, user:)
-      visit admin_users_path(user.id)
-      click_link user.email
-      click_link 'Remove Assessment Attempts'
     end
 
     it 'allows resetting assesment tests for a user' do
+      click_remove_attempts
       # TODO: Why do the next two tests pass? Shouldn't we be taken to an audit page?
       expect(page).to have_text('Assessment attempts removed')
       expect(page).to have_text("Show #{user.email}")
       expect(user.assessment_attempts.count).to eq(0)
+    end
+
+    def click_remove_attempts
+      visit admin_users_path(user.id)
+      click_link user.email
+      click_link 'Remove Assessment Attempts'
     end
   end
 end
