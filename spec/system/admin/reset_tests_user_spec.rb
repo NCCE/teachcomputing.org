b2e332a56bd470e7ae308ecdb4987a9152e4ddc9
dumbledad <tim.regan@raspberrypi.org>
2022-12-01 14:41:17 +0000

    Still failing :-(

--- spec/system/admin/reset_tests_user_spec.rb
+++ spec/system/admin/reset_tests_user_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe 'User reset tests' do
   context 'with two assesment attempts' do
     let(:user) { create(:user) }
-    let(:attempts) { create_list(:assesment_attempts, 5, user:) }
+    let(:attempts) { create_list(:assessment_attempt, 5, user:) }
 
     it 'allows resetting assesment tests for a user' do
+      user.reload
+      expect(user.assessment_attempts.count).to eq(5)
       visit admin_users_path(user.id)
       click_link user.email
-      expect(user.assessment_attempts.count).to eq(5)
       click_link 'Remove Assessment Attempts'
       user.reload
       expect(user.assessment_attempts.count).to eq(0)
