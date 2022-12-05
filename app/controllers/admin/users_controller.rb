063b87502b1bc7572490055ca1ac8589376ef60c
dumbledad <tim.regan@raspberrypi.org>
2022-12-05 14:29:55 +0000

    didn't work:
    ActiveRecord::RecordNotFound in Admin::UsersController#perform_reset_tests
    Couldn't find User without an ID

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,15 +14,18 @@ module Admin
       user_id = params[:user_id]
       Support::UserUtilities.reset_tests(user_id)
 
-      redirect_back(
-        fallback_location: admin_users_path(user_id:),
-        flash: { notice: I18n.t('admin.users.actions.reset.complete') }
+      redirect_to(
+        edit_admin_support_audit_path(id: requested_resource.support_audits.last.id)
       )
+      # redirect_back(
+      #   fallback_location: admin_users_path(user_id:),
+      #   flash: { notice: I18n.t('admin.users.actions.reset.complete') }
+      # )
     end
 
-    def after_resource_updated_path(requested_resource)
-      edit_admin_support_audit_path(id: requested_resource.support_audits.last.id)
-    end
+    # def after_resource_updated_path(requested_resource)
+    #   edit_admin_support_audit_path(id: requested_resource.support_audits.last.id)
+    # end
 
     def authorized_action?(resource, action_name)
       return false if resource.instance_of?(SupportAudit) && action_name == :edit
