84329d3eebf04176a36eb889dfebb63690845d5e
Nyasha Maphosa <nyashamaphosa@yahoo.co.uk>
2022-12-05 16:17:42 +0000

    tests

---
+++ spec/views/pages/primary-senior-leaders.html_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe('pages/primary-senior-leaders') do
+  # TODO: can we read values from /app/config/locales/views/pages/primary-senior-leaders/en.yml ?
+  let(:hero_title) { 'Support for Primary Senior Leaders' }
+  let(:quality_framework_tick_list_title) { 'How we can help you and your school' }
+  let(:stem_partner_title) { 'STEM Ambassadors' }
+  let(:code_club_partner_title) { 'Code Club' }
+  let(:bebras_partner_title) { 'Barefoot' }
+
+  before do
+    render
+  end
+
+  it 'has a title' do
+    expect(rendered).to have_css('h1', text: hero_title)
+  end
+
+  it 'has the TickListCollectionComponent' do
+    expect(rendered).to have_css('.tick-list-collection-component__title', text: quality_framework_tick_list_title)
+  end
+
+  it 'has the BorderedCardsComponent' do
+    expect(rendered).to have_css('.teacher-help-cards')
+  end
+
+  it 'has the partners listed' do
+    expect(rendered).to have_css('.non-bordered-card__title', text: stem_partner_title)
+    expect(rendered).to have_css('.non-bordered-card__title', text: code_club_partner_title)
+    expect(rendered).to have_css('.non-bordered-card__title', text: bebras_partner_title)
+  end
+end
