5ad2b0583d0b2ce5b01404cfd014d55356fe20e7
David Corking <2813357+dcorking@users.noreply.github.com>
2023-02-16 16:38:38 +0000

    Show the activity always_on field in its admin show page

--- previews/components/course_component_preview.rb
+++ previews/components/course_component_preview.rb
@@ -12,4 +12,23 @@ class CourseComponentPreview < ViewComponent::Preview
 
     render CourseComponent.new(course:, filter: nil)
   end
+
+  def online_always_on_with_occurrences
+    course = FactoryBot.build(:achiever_course_template, online_cpd: true, always_on: true)
+    course.occurrences = FactoryBot.build_list(:achiever_course_occurrence, 2)
+
+    render CourseComponent.new(course:, filter: nil)
+  end
+
+  def online_not_always_on_with_occurrences
+    course = FactoryBot.build(
+      :achiever_course_template,
+      online_cpd: true,
+      always_on: false,
+      title: 'This should not happen as online courses should always be on',
+    )
+    course.occurrences = FactoryBot.build_list(:achiever_course_occurrence, 2)
+
+    render CourseComponent.new(course:, filter: nil)
+  end
 end
