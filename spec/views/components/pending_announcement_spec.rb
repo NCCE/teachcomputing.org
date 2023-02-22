898ace9f805aaf016f705b5997bb13240f8729d0
Dan Halson <dan.halson@raspberrypi.org>
2023-02-22 11:40:51 +0000

    Add empty announcement component

---
+++ spec/views/components/pending_announcement_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe('components/_announcement') do
+  before do
+    render
+  end
+
+  it 'has a tag' do
+    expect(rendered).to have_css('.ncce-announcement__tag', text: 'ANNOUNCEMENT')
+  end
+
+  it 'has the text' do
+    expect(rendered).to have_css('.ncce-announcement__copy', text: '')
+  end
+end
