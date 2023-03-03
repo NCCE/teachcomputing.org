b63db8b6b4e1002b7888cd2f525ef0ee229eab21
Dan Halson <dan.halson@raspberrypi.org>
2023-03-03 16:48:05 +0000

    Revert migration change

--- db/migrate/20230216090745_stem_achiever_contact_no_change_column_type.rb
+++ db/migrate/20230216090745_stem_achiever_contact_no_change_column_type.rb
@@ -1,6 +1,6 @@
 class StemAchieverContactNoChangeColumnType < ActiveRecord::Migration[6.1]
   def up
-    change_column :users, :stem_achiever_contact_no, 'uuid USING CAST(stem_achiever_contact_no AS uuid)'
+    change_column :users, :stem_achiever_contact_no, :uuid, using: 'stem_achiever_contact_no::uuid'
   end
 
   def down
