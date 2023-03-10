787b7f26a9c3d1ebdf5e6a75fd7a75d1ffce671c
David Corking <dcorking@users.noreply.github.com>
2023-03-10 09:49:50 +0000

    Fix typo in dev-nginx suggestion to dev

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -37,7 +37,7 @@ if [ $OK_TO_PROCEED = 1 ]; then
   printf %s "- Open ${URL} in your default browser (y/n)? "
   read -r RESP
   if [ "$RESP" != "${RESP#[Yy]}" ]; then
-    echo "- Opening page, if the page fails to load ensure you've run 'yarn run setup' and 'yarn dev-nginx restart'"
+    echo "- Opening page, if the page fails to load ensure you've run 'yarn run setup' and 'sudo dev-nginx restart'"
     open "$URL"
   fi
 else
