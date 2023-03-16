8a493981976410819b6b5254e957eb1f03cd89a5
David Corking <dcorking@users.noreply.github.com>
2023-03-16 11:51:00 +0000

    Instrument auth callback 404s (#1677)
    * Report auth/callback 404s to Sentry (hopefully)
    
    * Turn on Sentry in qa environment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,7 @@ Sentry.init do |config|
   config.environment = ENV.fetch('SENTRY_CURRENT_ENV', nil) || ENV.fetch('RAILS_ENV', nil)
   config.breadcrumbs_logger = %i[sentry_logger active_support_logger http_logger]
   config.traces_sample_rate = 0.25
-  config.enabled_environments = %w[staging production]
+  config.enabled_environments = %w[qa staging production]
 
   filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
   config.before_send = lambda do |event, _hint|
