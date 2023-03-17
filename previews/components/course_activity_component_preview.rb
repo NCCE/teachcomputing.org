6c51af09513a168b8583e57a55fce08f2bfb2a0e
David Corking <2813357+dcorking@users.noreply.github.com>
2023-03-17 10:46:06 +0000

    Add web component preview for CourseActivityComponent

---
+++ previews/components/course_activity_component_preview.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class CourseActivityComponentPreview < ViewComponent::Preview
+  def default
+    objective = 'Complete in <strong>at least one</strong> <em>certificate</em> course.'.html_safe
+    booking = {
+      path: 'https://example.com/book',
+      tracking_label: 'book'
+    }
+    render CourseActivityComponent.new(objective:, booking: )
+  end
+
+  def with_achievements
+    objective = 'Participate in <strong>at least one</strong> <em>certificate</em> course'.html_safe
+    booking = {
+      path: 'https://example.com/book',
+      tracking_label: 'book'
+    }
+    achievements = [
+      FactoryBot.build(:achievement),
+      FactoryBot.build(:achievement, :online),
+      FactoryBot.build(:achievement, :remote),
+      Achievement.in_state(:completed).first
+    ].compact
+    render CourseActivityComponent.new(objective:, booking:, achievements: )
+  end
+end
