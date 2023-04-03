6627183b34c938db4adc78627721ea664f9a90f0
David Corking <2813357+dcorking@users.noreply.github.com>
2023-04-03 12:39:27 +0100

    Update hubs for NCCE2 (4 new, 6 destroyed) (#1699)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_23_173657) do
+ActiveRecord::Schema.define(version: 2023_03_28_121836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
