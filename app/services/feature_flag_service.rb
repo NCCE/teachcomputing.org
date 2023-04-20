6e005f342a48bddae66d8f8174dad6ab4e3020c9
David Corking <2813357+dcorking@users.noreply.github.com>
2023-04-20 12:18:53 +0100

    Rm ncce2_live feature flag

--- app/services/feature_flag_service.rb
+++ app/services/feature_flag_service.rb
@@ -6,7 +6,7 @@
 #
 class FeatureFlagService
   FLAGS = {
-    ncce2: 'FLAG_NCCE2'
+    # example_feature: 'FLAG_EXAMPLE_FEATURE'
   }.freeze
 
   def initialize(dependencies = {})
@@ -19,20 +19,6 @@ class FeatureFlagService
     @flags ||= begin
       @flags_to_define.transform_values { |v| cast_boolean(ENV[v]) }
     end
-  end
-
-  # only true when ncce2 flag set, and NCCE2_START_DATE has past; this is a one-off flag that turns on automatically on a date
-  # determined by the environment variable
-  #
-  # TODO: rm this method when remove ncce2 flag
-  #
-  # note: set NCCE2_START_DATE to 2nd April "2023-04-02" to allow the jobs to run a final time on 1st April and
-  # pick up the last end of the month course completions.
-  def ncce2_live
-    start_date_string = ENV['NCCE2_START_DATE']
-    return false unless start_date_string
-
-    flags[:ncce2] && (Date.today >= Date.parse(start_date_string))
   end
 
   private
