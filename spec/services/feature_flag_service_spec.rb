80d635f85bd64cef61a48191fea00c1748e65697
David Corking <2813357+dcorking@users.noreply.github.com>
2023-04-20 12:18:53 +0100

    Rm ncce2_live feature flag

--- spec/services/feature_flag_service_spec.rb
+++ spec/services/feature_flag_service_spec.rb
@@ -3,13 +3,9 @@ require 'rails_helper'
 RSpec.describe FeatureFlagService do
   around do |example|
     orig_test_flag_var = ENV['TEST_FLAG_VAR']
-    orig_flag_ncce2 = ENV['FLAG_NCCE2']
-    orig_flag_ncce2_start_date = ENV['NCCE2_START_DATE']
     example.run
   ensure
     ENV['TEST_FLAG_VAR'] = orig_test_flag_var
-    ENV['FLAG_NCCE2'] = orig_flag_ncce2
-    ENV['NCCE2_START_DATE'] = orig_flag_ncce2_start_date
   end
 
   describe '#flags' do
@@ -58,43 +54,7 @@ RSpec.describe FeatureFlagService do
 
   describe 'FLAGS' do
     it 'defines hash of flags correctly' do
-      expect(FeatureFlagService::FLAGS.count).to eq 1
-    end
-  end
-
-  describe '#ncce2_live' do
-    it 'is true when flag set on start date' do
-      ENV['NCCE2_START_DATE'] = '2023-04-02'
-      ENV['FLAG_NCCE2'] = 'on'
-      Timecop.freeze(2023, 4, 2) do
-        expect(described_class.new.ncce2_live).to be true
-      end
-    end
-
-    it 'is false when flag unset' do
-      ENV['FLAG_NCCE2'] = nil
-      expect(described_class.new.ncce2_live).to be false
-    end
-
-    it 'is false when flag set before start date' do
-      ENV['NCCE2_START_DATE'] = '2023-04-02'
-      ENV['FLAG_NCCE2'] = 'on'
-      Timecop.freeze(2023, 4, 1, 23, 59) do
-        expect(described_class.new.ncce2_live).to be false
-      end
-    end
-
-    it 'is false when flag unset on start date' do
-      ENV['NCCE2_START_DATE'] = '2023-04-02'
-      ENV['FLAG_NCCE2'] = nil
-      Timecop.freeze(2023, 4, 2) do
-        expect(described_class.new.ncce2_live).to be false
-      end
-    end
-
-    it 'is false when start date unset' do
-      ENV['NCCE2_START_DATE'] = nil
-      expect(described_class.new.ncce2_live).to be false
+      expect(FeatureFlagService::FLAGS.count).to eq 0
     end
   end
 end
