cf300bae0322d5c5a60af2fe83cbf3a0f1e3f427
David Corking <2813357+dcorking@users.noreply.github.com>
2023-04-21 10:11:20 +0100

    Rm one-off script to retire 33 FutureLearn activity codes

--- scripts/retire_activity_codes.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# One off task to change the activity codes of 33 retired activities
-bin/rails runner ./scripts/change_activity_codes.rb scripts/retired_template_codes_template_uuids.tsv
--- scripts/undo_retired_activity_codes.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# Undo retire_activity_codes.sh
-bin/rails runner ./scripts/change_activity_codes.rb scripts/undo_retired_template_codes_template_uuids.tsv
