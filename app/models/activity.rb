6447de93233d7566bf53f162d883c253e80e1fcf
= <=>
2023-04-21 14:20:27 +0100

    resolves activity display for retired courses

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -54,4 +54,8 @@ class Activity < ApplicationRecord
   def online?
     category == ONLINE_CATEGORY
   end
+
+  def is_not_retired?
+    self.stem_activity_code && self.retired == false
+  end 
 end
