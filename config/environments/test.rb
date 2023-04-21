056794d30bc735ae19e8db1ccc2c846a1aa64882
David Corking <2813357+dcorking@users.noreply.github.com>
2023-04-21 08:36:13 +0100

    Raise missing translation errors in dev and test
    This helps us catch regressions, as our translations setup is quite
    complex

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 
   config.assets.css_compressor = nil
   config.sass.style = :compressed
