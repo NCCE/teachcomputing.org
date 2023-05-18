605b5125da0816ca5f141554cc7a1c5b5a76e9f7
= <=>
2023-05-18 17:20:10 +0100

    removes uncessary tracking config, use rails convention to rename files

--- app/components/secondary_question_banks_component.rb
+++ app/components/secondary_question_banks_component.rb
@@ -3,15 +3,10 @@ class SecondaryQuestionBanksComponent < ViewComponent::Base
   
     def initialize(
       text: nil,
-      link_title: nil,
-      tracking_event_category: nil,
-      tracking_event_label: nil,
       bottom_margin: true,
       class_name: nil
     )
       @text = text
-      @link_title = link_title
-      @tracking_event_category = tracking_event_category
       @bottom_margin = bottom_margin
       @class_name = class_name
     end
