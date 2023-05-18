2f43488ce4992b97c0aa9e1fd87d2ad71f4ec281
David Corking <2813357+dcorking@users.noreply.github.com>
2023-05-18 17:50:41 +0100

    Use collection rendering for question bank topics

--- config/routes.rb
+++ config/routes.rb
@@ -141,7 +141,7 @@ Rails.application.routes.draw do
   get '/gender-balance', to: 'pages#page', as: :gender_balance, defaults: { page_slug: 'gender-balance' }
   get '/get-involved', to: 'pages#page', as: :get_involved, defaults: { page_slug: 'get-involved' }
   get '/secondary-early-careers', to: 'pages#page', as: :secondary_early_careers, defaults: { page_slug: 'secondary-early-careers' }
-  get '/secondary-question-banks', to: 'pages#page', as: :secondary_question_banks, defaults: { page_slug: 'secondary-question-banks' }
+  get '/secondary-question-banks', to: 'pages#page', as: :secondary_question_banks, defaults: { page_slug: 'secondary_question_banks' }
   get '/primary-early-careers', to: 'pages#page', as: :primary_early_careers, defaults: { page_slug: 'primary-early-careers' }
   get '/powerupthedigitalgeneration', to: redirect('/supporting-partners')
   get '/hero-demo', to: 'pages#page', as: :hero_demo, defaults: { page_slug: 'hero-demo' }
