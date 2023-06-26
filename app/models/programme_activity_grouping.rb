07aa810b0da5c58dfb9052677b80f78c5401cf31
Caleb Owens <caleb@storm-consultancy.com>
2023-06-26 12:10:41 +0100

    Fix ProgrammeActivityGrouping achievement caching issue, rm includes
    We found when debugging a rake task that called user_complete on the
    same ProgrammeActivityGrouping that the instance variable was the same,
    IE containing user information for other users, because it was the same
    PAG instance. We've removed the IVAR memoization to fix this.
    
    We also noticed that with the way its currently implemented, the
    benefit that was trying to be derived by moving it into an IVAR was
    being missed as it was still an active record collection and so
    calling find_by was still doing a DB query. We've changed this to
    use ruby's Array#find
    
    We've also removed the excessive #includes on the user.achievements
    query as it seems it was never used
    
    Restore contract of ProgrammeActivityGrouping#achievements(user)
    
    Rm N+1 - no need to load activity when we are only comparing activity_id

--- app/models/programme_activity_grouping.rb
+++ app/models/programme_activity_grouping.rb
@@ -3,14 +3,14 @@ class ProgrammeActivityGrouping < ApplicationRecord
   belongs_to :programme
 
   def achievements(user)
-    @achievements ||= user.achievements.in_state(:complete).for_programme(programme).includes(:activity)
+    user.achievements.in_state(:complete).for_programme(programme)
   end
 
   def user_complete?(user)
     completed_activity_count = 0
-    user_achievements = achievements(user)
+    user_achievements = achievements(user).to_a
     programme_activities.each do |programme_activity|
-      completed_activity = user_achievements.find_by(activity_id: programme_activity.activity.id)
+      completed_activity = user_achievements.find { _1.activity_id == programme_activity.activity_id }
       completed_activity_count += 1 if completed_activity
       return true if completed_activity_count >= required_for_completion
     end
