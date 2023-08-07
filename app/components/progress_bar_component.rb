b10e614a822fecd8f925dee174acbd210fdbc19b
= <nyashamaphosa@yahoo.co.uk>
2023-08-07 14:21:13 +0100

    refactors based on pr comments

--- app/components/progress_bar_component.rb
+++ app/components/progress_bar_component.rb
@@ -2,13 +2,5 @@ class ProgressBarComponent < ViewComponent::Base
     def initialize(current_user, programme)
       @programme = programme
       @current_user = current_user
-      index = 0
-  
-      grouping_titles = programme.programme_activity_groupings.map(&:title).drop(1)
-  
-      grouping_titles.each do |title|
-        index += 1
-        activity_grouping = programme.programme_activity_groupings.find_by_title(title)
-      end
     end
   end
--- spec/components/progress_bar_component_spec.rb
+++ spec/components/progress_bar_component_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ProgressBarComponent, type: :component do
     let(:user) { create(:user) }
+    let(:primary_certificate) { create(:primary_certificate) }
+    let(:secondary_certificate) { create(:secondary_certificate) }
     let(:primary_programme_activity_groupings) do
-      create_list(:programme_activity_grouping, 4, programme: create(:primary_certificate) ).tap do |groupings|
+      create_list(:programme_activity_grouping, 4, programme: primary_certificate).tap do |groupings|
         groupings[0].update(title: 'All courses')
         groupings[1].update(title: 'Contribute to an online discussion')
         groupings[2].update(title: 'Develop your teaching practice')
@@ -11,8 +13,8 @@ RSpec.describe ProgressBarComponent, type: :component do
       end
     end
 
-    let!(:secondary_rogramme_activity_groupings) do
-      create_list(:programme_activity_grouping, 4, programme: programme).tap do |groupings|
+    let(:secondary_programme_activity_groupings) do
+      create_list(:programme_activity_grouping, 4, programme: secondary_certificate).tap do |groupings|
         groupings[0].update(title: 'All courses')
         groupings[1].update(title: 'Develop your subject knowledge')
         groupings[2].update(title: 'Develop your teaching practice')
@@ -20,31 +22,56 @@ RSpec.describe ProgressBarComponent, type: :component do
       end
     end
   
-    before(:each) do 
-      render_inline(described_class.new(user, programme))
-    end 
-  
-    it 'has a title' do
-      expect(page).to have_css('h4', text: 'Your certificate progress')
-    end 
-  
-    it 'has a description' do
-      expect(page).to have_css('.ncce-progress-section--description', count: 1)
-    end 
+    
+    # it 'has a title' do
+    #   expect(page).to have_css('h4', text: 'Your certificate progress')
+    # end 
+    
+    # it 'has a description' do
+    #   expect(page).to have_css('.ncce-progress-section--description', count: 1)
+    # end 
     
     context "when primary certificate" do
+
+      before(:each) do 
+        primary_programme_activity_groupings
+        render_inline(described_class.new(user, primary_certificate))
+      end 
+      
+      it 'has a title' do
+        expect(page).to have_css('h4', text: 'Your certificate progress')
+      end 
+    
+      it 'has a description' do
+        expect(page).to have_css('.ncce-progress-section--description', count: 1)
+      end 
+      
       it 'has 3 completion steps' do
-          expect(rendered_component).to have_css('.ncce-progress-section') do
-              expect(rendered_component).to have_css('.ncce-progress-section--text', text: 'Contribute to an online discussion')
-              expect(rendered_component).to have_css('.ncce-progress-section--text', text: 'Develop your teaching practice')
-              expect(rendered_component).to have_css('.ncce-progress-section--text', text: 'Develop computing in your community')
-            end
+        expect(rendered_component).to have_css('.ncce-progress-section') do
+          expect(rendered_component).to have_css('.ncce-progress-section--text', text: 'Contribute to an online discussion')
+          expect(rendered_component).to have_css('.ncce-progress-section--text', text: 'Develop your teaching practice')
+          expect(rendered_component).to have_css('.ncce-progress-section--text', text: 'Develop computing in your community')
+        end
       end 
     end
-
+    
     context "when se certificate" do
-      it 'has 3 completion steps' do
+
+      before(:each) do 
         secondary_programme_activity_groupings
+        render_inline(described_class.new(user, secondary_certificate))
+      end 
+      
+      it 'has a title' do
+        expect(page).to have_css('h4', text: 'Your certificate progress')
+      end 
+    
+      it 'has a description' do
+        expect(page).to have_css('.ncce-progress-section--description', count: 1)
+      end 
+
+      it 'has 3 completion steps' do
+        render_inline(described_class.new(user, secondary_certificate))
           expect(rendered_component).to have_css('.ncce-progress-section') do
               expect(rendered_component).to have_css('.ncce-progress-section--text', text: 'Develop your subject knowledge')
               expect(rendered_component).to have_css('.ncce-progress-section--text', text: 'Develop your teaching practice')
