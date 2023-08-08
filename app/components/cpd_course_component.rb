7a9ab4b728299bc7629bedc809b2ed556ae2214b
Caleb Owens <caleb@storm-consultancy.com>
2023-08-08 11:50:13 +0100

    CPD component testing

--- app/components/cpd_course_component.rb
+++ app/components/cpd_course_component.rb
@@ -1,16 +1,16 @@
 class CpdCourseComponent < ViewComponent::Base
-  attr_reader :programme_activity, :current_user, :course, :last_margin, :achievement
+  attr_reader :activity, :current_user, :course, :last_margin, :achievement
 
   delegate :activity_icon_class,
            :activity_type,
            to: :helpers
 
-  def initialize(programme_activity:, current_user:, last_margin: true)
-    @programme_activity = programme_activity
+  def initialize(activity:, current_user:, last_margin: true)
+    @activity = activity
     @current_user = current_user
     @last_margin = last_margin
 
-    @course = Achiever::Course::Template.maybe_find_by_activity_code(programme_activity.stem_activity_code)
-    @achievement = current_user.achievements.find_by_activity_id(programme_activity.activity_id)
+    @course = Achiever::Course::Template.maybe_find_by_activity_code(activity.stem_activity_code) if activity.stem_activity_code.present?
+    @achievement = current_user.achievements.find_by_activity_id(activity.id)
   end
 end
--- app/models/programme_activity.rb
+++ app/models/programme_activity.rb
@@ -4,6 +4,4 @@ class ProgrammeActivity < ApplicationRecord
   belongs_to :programme_activity_grouping, optional: true
 
   validates :programme_id, uniqueness: { scope: [:activity_id] }
-
-  delegate :title, :stem_activity_code, :slug, :category, :online?, :remote_delivered_cpd?, to: :activity
 end
