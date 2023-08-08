2f5c1a55bf6789dcaa7339739482317eed134f6b
Caleb Owens <caleb@storm-consultancy.com>
2023-08-08 11:50:13 +0100

    CPD component testing

---
+++ spec/components/cpd_course_component_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe CpdCourseComponent, type: :component do
+  let(:user) { create(:user) }
+  let(:stem_activity_code) { 'CPXXX' }
+  let(:activity) { create(:activity, stem_activity_code:) }
+  let(:achievement) { nil }
+  let(:last_margin) { true }
+
+  before do
+    stub_course_templates
+    stub_duration_units
+
+    achievement
+
+    render_inline(
+      described_class.new(
+        current_user: user,
+        activity:,
+        last_margin:
+      )
+    )
+  end
+
+  context 'when last_margin is true' do
+    it 'shouldn\'t have the no-last-margin class' do
+      expect(page).not_to have_css('.courses__course-no-last-margin')
+    end
+  end
+
+  context 'when last_margin is false' do
+    let(:last_margin) { false }
+
+    it 'shouldn\'t have the no-last-margin class' do
+      expect(page).to have_css('.ncce-pathway-courses__course-no-last-margin')
+    end
+  end
+
+  context 'when stem_activity_code is not found' do
+    it 'shouldn\'t display the date' do
+      expect(page).not_to have_css('.icon-clock')
+    end
+  end
+
+  context 'when stem_activity_code is found' do
+    let(:stem_activity_code) { 'CP228' }
+    it 'shouldn\'t display the date' do
+      expect(page).to have_css('.icon-clock')
+    end
+  end
+
+  context 'when there is no achievement found' do
+    it 'shouldn\'t display a status' do
+      expect(page).not_to have_css('.ncce-pathway-courses__status')
+    end
+  end
+
+  context 'when there is an in progress achievement' do
+    let(:achievement) { create(:achievement, user:, activity:) }
+
+    it 'should display an in progress status' do
+      expect(page).to have_css('.ncce-pathway-courses__status', text: 'In progress')
+    end
+  end
+
+  context 'when there is no stem_activity_code' do
+    let(:stem_activity_code) { nil }
+
+    it 'should not display the link' do
+      expect(page).not_to have_css('.ncce-link')
+    end
+  end
+
+  context 'when there is a stem_activity_code' do
+    it 'should display the link' do
+      expect(page).to have_css('.ncce-link')
+    end
+  end
+end
