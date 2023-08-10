e00e1fce9844e3ecf10082305d79d99f6b20f27e
Caleb Owens <caleb@storm-consultancy.com>
2023-08-10 12:33:39 +0100

    Renamed seeds to be after seeds currently in production

--- db/migrate/20230802091207_add_legacy_flag_to_pathways.rb
+++
@@ -1,5 +0,0 @@
-class AddLegacyFlagToPathways < ActiveRecord::Migration[6.1]
-  def change
-    add_column :pathways, :legacy, :boolean, default: false
-  end
-end
---
+++ db/migrate/20230810112735_add_web_copy_and_legacy_to_pathways.rb
@@ -0,0 +1,6 @@
+class AddWebCopyAndLegacyToPathways < ActiveRecord::Migration[6.1]
+  def change
+    add_column :pathways, :web_copy, :jsonb
+    add_column :pathways, :legacy, :boolean, default: false
+  end
+end
