45dfcdaabfc772bfdd9c621cd035430260b383df
Caleb Owens <caleb@storm-consultancy.com>
2023-08-21 16:14:28 +0100

    Introduced dashboard page and models for A-Level

---
+++ db/seeds/programmes/a_level.rb
@@ -0,0 +1,15 @@
+a_level = Programmes::ALevel.find_or_initialize_by(slug: 'a-level').tap do |programme|
+  programme.title = 'A level subject knowledge'
+  programme.slug = 'a-level'
+  programme.description = 'A level subject knowledege'
+  programme.enrollable = true
+
+  programme.save
+end
+
+puts "Created Programme: #{a_level.title} (#{a_level})"
+
+ProgrammeCompleteCounter.find_or_create_by(programme_id: a_level.id) do |programme_complete_counter|
+  programme_complete_counter.programme_id = a_level.id
+  programme_complete_counter.counter = 0
+end
