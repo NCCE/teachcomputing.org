c99fa049fbada1a107ac929bf37c9142bb0220e1
Caleb Owens <caleb@storm-consultancy.com>
2023-08-22 16:05:04 +0100

    Renumbered seeds after rebase and made boolean fields not null

--- db/migrate/20230822113148_add_legacy_to_programme_activities.rb
+++ db/migrate/20230822113148_add_legacy_to_programme_activities.rb
@@ -1,5 +1,5 @@
 class AddLegacyToProgrammeActivities < ActiveRecord::Migration[6.1]
   def change
-    add_column :programme_activities, :legacy, :boolean, default: false
+    add_column :programme_activities, :legacy, :boolean, default: false, null: false
   end
 end
