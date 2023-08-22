d8e56f12cf8de6aa3403b869bd066892a5d41a58
Caleb Owens <caleb@storm-consultancy.com>
2023-08-22 10:40:45 +0100

    Removed debugging logging from js

--- app/webpacker/javascript/controllers/modal_controller.js
+++ app/webpacker/javascript/controllers/modal_controller.js
@@ -3,10 +3,6 @@ import ApplicationController from "./application_controller";
 export default class extends ApplicationController {
   static targets = ['modal']
 
-  connect() {
-    console.log('hello world')
-  }
-
   toggle() {
     this.modalTarget.classList.toggle('ncce-modal--expanded')
   }
