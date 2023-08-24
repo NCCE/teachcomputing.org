136d69b192270fce627240cb7de1bd984aec5d71
Caleb Owens <caleb@storm-consultancy.com>
2023-08-24 08:27:49 +0100

    Alter name of IB CPD and change dashboard copy

--- db/seeds/activities/stem_learning.rb
+++ db/seeds/activities/stem_learning.rb
@@ -1059,7 +1059,7 @@ end
 a.programmes << cs_accelerator unless a.programmes.include?(cs_accelerator)
 
 Activity.find_or_initialize_by(stem_course_template_no: '22ee67ba-4f8d-eb11-b1ac-0022481a6ad5').tap do |activity|
-  activity.title = 'Encouraging girls into GCSE computer science'
+  activity.title = 'Encouraging girls into GCSE computer science - remote - short course'
   activity.credit = 10
   activity.slug = 'encouraging-girls-into-gcse-computer-science'
   activity.stem_course_template_no = '22ee67ba-4f8d-eb11-b1ac-0022481a6ad5'
