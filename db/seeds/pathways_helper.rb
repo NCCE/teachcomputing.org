18b13dc2f087f5ad69955a505d4e182956beab38
Caleb Owens <caleb@storm-consultancy.com>
2023-08-25 14:22:14 +0100

    Removed unrelased CPDs from new pathways

--- db/seeds/pathways_helper.rb
+++ db/seeds/pathways_helper.rb
@@ -8,3 +8,9 @@ def maybe_attach_activity_to_pathway(pathway, slug: nil, stem_activity_code: nil
 
   pathway.pathway_activities.find_or_create_by(activity_id: activity.id) if activity && !pathway.pathway_activities.include?(activity)
 end
+
+def maybe_detach_activity_from_pathway(pathway, stem_activity_code:)
+  activity = Activity.find_by(stem_activity_code:)
+
+  pathway.pathway_activities.find_by(activity:)&.destroy
+end
