86d98896a904721590d3eea2f1a458a4edc9116c
Caleb Owens <hello@calebowens.com>
2023-09-13 18:39:24 +0100

    Add Post 16 as an age group

--- app/services/achiever/course/age_group.rb
+++ app/services/achiever/course/age_group.rb
@@ -6,8 +6,9 @@ class Achiever::Course::AgeGroup
   def self.all
     age_groups = Achiever::Request.option_sets(RESOURCE_PATH, QUERY_STRINGS)
     parsed_response = JSON.parse(age_groups)
-    parsed_response.reduce({}, :merge)
-                   .select { |k| k.starts_with?('Key') }
+    parsed_response
+      .reduce({}, :merge)
+      .select { |k| k.starts_with?('Key') || k == 'Post 16' }
   end
 
   def self.primary_certificate
