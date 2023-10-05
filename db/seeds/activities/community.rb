11b0d9eaa89ffbbbbcc90d8ec1fa0dbd8e36d417
= <nyashamaphosa@yahoo.co.uk>
2023-10-05 17:09:16 +0100

    updates primary and secondary pathways

--- db/seeds/activities/community.rb
+++ db/seeds/activities/community.rb
@@ -388,12 +388,27 @@ Activity.find_or_initialize_by(slug: 'support-other-teachers-and-earn-a-stem-com
   activity.category = 'community'
   activity.provider = 'stem-learning'
   activity.self_certifiable = true
-  activity.description = 'You’ll earn points for your activities on the <a href="https://community.stem.org.uk/helpfaqs/points">STEM Community</a>. Your points add up, and over time you will be <a href="https://community.stem.org.uk/helpfaqs/points">rewarded with badges</a> in recognition of your activity and participation in the community.'
+  activity.description = 'You’ll earn points for your activities on the <a href="https://community.stem.org.uk/communities/community-home?communitykey=1238654a-fe3d-4173-99e1-4ecf11c98cdc">STEM Community</a>. Your points add up, and over time you will be <a href="https://community.stem.org.uk/communities/community-home?communitykey=1238654a-fe3d-4173-99e1-4ecf11c98cdc">rewarded with badges</a> in recognition of your activity and participation in the community.'
   activity.public_copy_description = 'You’ll earn points for your activities on the STEM Community. Your points add up, and over time you will be rewarded with badges in recognition of your activity and participation in the community.'
-  activity.public_copy_title_url = 'https://community.stem.org.uk/helpfaqs/points'
+  activity.public_copy_title_url = 'https://community.stem.org.uk/communities/community-home?communitykey=1238654a-fe3d-4173-99e1-4ecf11c98cdc'
   activity.self_verification_info = 'Please provide us with evidence of participation'
 
-  activity.programmes = [primary_certificate, secondary_certificate]
+  activity.programmes = [primary_certificate]
+end.save
+
+Activity.find_or_initialize_by(slug: 'support-other-teachers-and-earn-a-stem-community-participation-badge').tap do |activity|
+  activity.title = 'Support other teachers and earn a STEM Community participation badge'
+  activity.credit = 10
+  activity.slug = 'support-other-teachers-and-earn-a-stem-community-participation-badge'
+  activity.category = 'community'
+  activity.provider = 'stem-learning'
+  activity.self_certifiable = true
+  activity.description = 'You’ll earn points for your activities on the <a href="https://community.stem.org.uk/communities/community-home?CommunityKey=f5098030-41d2-43f9-8f5d-67b536b18b97">STEM Community</a>. Your points add up, and over time you will be <a href="https://community.stem.org.uk/communities/community-home?CommunityKey=f5098030-41d2-43f9-8f5d-67b536b18b97">rewarded with badges</a> in recognition of your activity and participation in the community.'
+  activity.public_copy_description = 'You’ll earn points for your activities on the STEM Community. Your points add up, and over time you will be rewarded with badges in recognition of your activity and participation in the community.'
+  activity.public_copy_title_url = 'https://community.stem.org.uk/communities/community-home?CommunityKey=f5098030-41d2-43f9-8f5d-67b536b18b97'
+  activity.self_verification_info = 'Please provide us with evidence of participation'
+
+  activity.programmes = [secondary_certificate]
 end.save
 
 Activity.find_or_initialize_by(slug: 'run-or-support-a-code-club-in-your-school').tap do |activity|
