c818d24d429ee19db2ca684b87ce6b9c65dc0468
Caleb Owens <hello@calebowens.com>
2023-10-23 11:53:35 +0100

    Added event listener to allow closing modals with Esc

--- app/webpacker/javascript/controllers/modal_controller.js
+++ app/webpacker/javascript/controllers/modal_controller.js
@@ -3,7 +3,26 @@ import ApplicationController from "./application_controller";
 export default class extends ApplicationController {
   static targets = ['modal']
 
+  connect() {
+    this.element.addEventListener('keydown', this.onKeyDown.bind(this))
+  }
+
+  disconnect() {
+    this.element.removeEventListener(this.onKeyDown)
+  }
+
   toggle() {
     this.modalTarget.classList.toggle('ncce-modal--expanded')
+  }
+
+  onKeyDown(event) {
+    if (event.key === 'Escape') {
+      if (this.modalTarget.classList.contains('ncce-modal--expanded')) {
+        if (!this.modalTarget.querySelector('ncce-modal--expanded')) {
+          event.stopPropagation()
+          this.toggle()
+        }
+      }
+    }
   }
 }
