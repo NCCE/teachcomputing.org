8a759831309e61bbeaf6ac9be27834424dfbc4a0
Caleb Owens <hello@calebowens.com>
2023-11-14 14:42:39 +0000

    I Belong acordion no longer tries to collapse required activities

--- app/components/community_activity_list_component.rb
+++ app/components/community_activity_list_component.rb
@@ -1,8 +1,8 @@
 class CommunityActivityListComponent < ViewComponent::Base
   attr_reader :first_activities, :second_activities, :tracking_category
 
-  def initialize(programme_activities:, community_achievements:, tracking_category:)
-    activities_with_achievements = programme_activities.map do |programme_activity|
+  def initialize(programme_activity_grouping:, community_achievements:, tracking_category:)
+    activities_with_achievements = programme_activity_grouping.programme_activities.not_legacy.map do |programme_activity|
       {
         programme_activity:,
         achievement: community_achievements&.find { _1.activity_id == programme_activity.activity_id }
@@ -11,9 +11,10 @@ class CommunityActivityListComponent < ViewComponent::Base
 
     complete, non_complete = activities_with_achievements.partition { _1[:achievement]&.in_state? :complete }
 
-    if complete.empty?
-      @first_activities = non_complete.first(4)
-      @second_activities = non_complete[4..]
+    if complete.size < programme_activity_grouping.required_for_completion
+      sorted_activities = complete + non_complete
+      @first_activities = sorted_activities.first(4)
+      @second_activities = sorted_activities[4..]
     else
       @first_activities = complete
       @second_activities = non_complete
