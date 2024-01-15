6f64b46f33e604b3e81d9a0fa2070d6089ebaac5
Caleb Owens <hello@calebowens.com>
2024-01-15 16:24:20 +0000

    Add inactivity mailer previews

--- previews/mailers/i_belong_mailer_preview.rb
+++ previews/mailers/i_belong_mailer_preview.rb
@@ -12,22 +12,22 @@ class IBelongMailerPreview < ActionMailer::Preview
   end
 
   def inactive_not_completed_any_sections
-    IBelongMailer.with(user: User.first).inactive_not_completed_any_sections
+    IBelongMailer.with(user: User.first, preview: true).inactive_not_completed_any_sections
   end
 
   def inactive_only_completed_one_section
-    IBelongMailer.with(user: User.first).inactive_only_completed_one_section
+    IBelongMailer.with(user: User.first, preview: true).inactive_only_completed_one_section
   end
 
   def inactive_everything_but_understanding_factors
-    IBelongMailer.with(user: User.first).inactive_everything_but_understanding_factors
+    IBelongMailer.with(user: User.first, preview: true).inactive_everything_but_understanding_factors
   end
 
   def inactive_everything_but_access_resources
-    IBelongMailer.with(user: User.first).inactive_everything_but_access_resources
+    IBelongMailer.with(user: User.first, preview: true).inactive_everything_but_access_resources
   end
 
   def inactive_everything_but_increase_engagement
-    IBelongMailer.with(user: User.first).inactive_everything_but_increase_engagement
+    IBelongMailer.with(user: User.first, preview: true).inactive_everything_but_increase_engagement
   end
 end
