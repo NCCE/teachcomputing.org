4639a7d83547d7221f7376eb91542d5f15b38a8d
Michael Squance <m.squance@stem.org.uk>
2024-01-26 16:24:45 +0000

    Adding in filter for programme

--- app/views/fields/achievement_list_field/achievement_list_field_controller.js
+++ app/views/fields/achievement_list_field/achievement_list_field_controller.js
@@ -1,25 +1,31 @@
 import ApplicationController from "../../../webpacker/javascript/controllers/application_controller";
 
 export default class extends ApplicationController {
-  static targets = ['stateFilter', 'achievement'];
+  static targets = ['stateFilter', 'programmeFilter', 'achievement'];
   currentStateFilter = null;
+  currentProgrammeFilter = null;
 
   initialize() {
   }
   
   filter() {
     this.currentStateFilter = this.stateFilterTarget.value;
-    console.log(this.currentStateFilter)
+    this.currentProgrammeFilter = this.programmeFilterTarget.value;
     this.toggleAchievements();
   }
 
   toggleAchievements(){
+    console.log(this.currentProgrammeFilter);
     this.achievementTargets.forEach(element => {
-      let hide = false
+      let stateHide = false
+      let progHide = false
       if(this.currentStateFilter){
-        hide = element.dataset.currentState !== this.currentStateFilter
+        stateHide = element.dataset.currentState !== this.currentStateFilter
       } 
-      element.hidden = hide
+      if(this.currentProgrammeFilter){
+        progHide = !element.dataset.programmes.includes(this.currentProgrammeFilter)
+      }
+      element.hidden = stateHide || progHide
     });
   }
 
