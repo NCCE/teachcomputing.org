729bc7853526b8ee81c42a695f027f940561c918
Michael Squance <m.squance@stem.org.uk>
2024-02-09 16:57:56 +0000

    Tests suite fixes and few minor tweaks to views

--- spec/views/pages/secondary-senior-leaders.html_spec.rb
+++ spec/views/pages/secondary-senior-leaders.html_spec.rb
@@ -1,13 +1,10 @@
 require "rails_helper"
 
 RSpec.describe("pages/secondary-senior-leaders") do
-  # TODO: can we read values from /app/config/locales/views/pages/secondary-senior-leaders/en.yml ?
-  let(:hero_title) { "Support for Secondary Senior Leaders" }
-  let(:quality_framework_tick_list_title) { "How we can help you and your school or college" }
-  let(:student_help_title) { "How we can help you and your students" }
-  let(:stem_partner_title) { "STEM Ambassadors" }
-  let(:code_club_partner_title) { "Code Club" }
-  let(:bebras_partner_title) { "Bebras UK" }
+  let(:hero_title) { I18n.t("pages.secondary-senior-leaders.hero.title") }
+  let(:invest_title) { I18n.t("pages.secondary-senior-leaders.objectives.cards.invest.title") }
+  let(:certification_title) { I18n.t("pages.secondary-senior-leaders.objectives.cards.certification.title") }
+  let(:download_brochure_title) { I18n.t("pages.secondary-senior-leaders.objectives.cards.download-brochure.title") }
 
   before do
     render
@@ -17,21 +14,25 @@ RSpec.describe("pages/secondary-senior-leaders") do
     expect(rendered).to have_css("h1", text: hero_title)
   end
 
-  it "has the TickListCollectionComponent" do
-    expect(rendered).to have_css(".tick-list-collection-component__title", text: quality_framework_tick_list_title)
+  it "has the IconRowComponent" do
+    expect(rendered).to have_css(".icon-row-component-icon_content", text: "Curriculum design")
   end
 
   it "has the BorderedCardsComponent" do
-    expect(rendered).to have_css(".teacher-help-cards")
+    expect(rendered).to have_css(".resources-help-cards")
   end
 
-  it "has the LogoButtonImageCardComponent" do
-    expect(rendered).to have_css(".logo-button-image-card-component__title", text: student_help_title)
+  it "has i belong banner" do
+    expect(rendered).to have_css(".i-belong-banner")
   end
 
-  it "has the partners listed" do
-    expect(rendered).to have_css(".non-bordered-card__title", text: stem_partner_title)
-    expect(rendered).to have_css(".non-bordered-card__title", text: code_club_partner_title)
-    expect(rendered).to have_css(".non-bordered-card__title", text: bebras_partner_title)
+  it "has the ambassador banner" do
+    expect(rendered).to have_css(".inspire-with-comp-ambassador-banner")
+  end
+
+  it "has the objectives listed" do
+    expect(rendered).to have_css(".non-bordered-card__title", text: invest_title)
+    expect(rendered).to have_css(".non-bordered-card__title", text: certification_title)
+    expect(rendered).to have_css(".non-bordered-card__title", text: download_brochure_title)
   end
 end
