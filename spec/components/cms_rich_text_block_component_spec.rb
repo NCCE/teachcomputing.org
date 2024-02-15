80df03909f422a1c2426043b2dd5462c2eccd4a5
Alex Brook <90186562+abr-storm@users.noreply.github.com>
2024-02-15 14:46:00 +0000

    implement text styles, heading types, quotes

--- spec/components/cms_rich_text_block_component_spec.rb
+++ spec/components/cms_rich_text_block_component_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe CmsRichTextBlockComponent, type: :component do
     expect(page).to have_css("p", text: "Hello world!")
   end
 
-  it "renders a heading" do
+  it "renders a large heading" do
     render_inline(described_class.new({blocks: [
       type: "heading",
       level: 1,
@@ -21,7 +21,31 @@ RSpec.describe CmsRichTextBlockComponent, type: :component do
       ]
     ]}))
 
-    expect(page).to have_css("h1", text: "Heading world!")
+    expect(page).to have_css(".govuk-heading-l", text: "Heading world!")
+  end
+
+  it "renders a medium heading" do
+    render_inline(described_class.new({blocks: [
+      type: "heading",
+      level: 2,
+      children: [
+        {type: "text", text: "Heading world!"}
+      ]
+    ]}))
+
+    expect(page).to have_css(".govuk-heading-m", text: "Heading world!")
+  end
+
+  it "renders a small heading" do
+    render_inline(described_class.new({blocks: [
+      type: "heading",
+      level: 3,
+      children: [
+        {type: "text", text: "Heading world!"}
+      ]
+    ]}))
+
+    expect(page).to have_css(".govuk-heading-s", text: "Heading world!")
   end
 
   it "renders some text" do
@@ -30,6 +54,46 @@ RSpec.describe CmsRichTextBlockComponent, type: :component do
     ]}))
 
     expect(page).to have_text("Just text")
+  end
+
+  it "renders bold text" do
+    render_inline(described_class.new({blocks: [
+      {type: "text", text: "Bold text", bold: true}
+    ]}))
+
+    expect(page).to have_css(".cms-rich-text-block-component__text--bold", text: "Bold text")
+  end
+
+  it "renders italic text" do
+    render_inline(described_class.new({blocks: [
+      {type: "text", text: "Italic text", italic: true}
+    ]}))
+
+    expect(page).to have_css(".cms-rich-text-block-component__text--italic", text: "Italic text")
+  end
+
+  it "renders underlined text" do
+    render_inline(described_class.new({blocks: [
+      {type: "text", text: "Underlined text", underline: true}
+    ]}))
+
+    expect(page).to have_css(".cms-rich-text-block-component__text--underline", text: "Underlined text")
+  end
+
+  it "renders strikethrough text" do
+    render_inline(described_class.new({blocks: [
+      {type: "text", text: "Strikethrough text", strikethrough: true}
+    ]}))
+
+    expect(page).to have_css(".cms-rich-text-block-component__text--strikethrough", text: "Strikethrough text")
+  end
+
+  it "renders code text" do
+    render_inline(described_class.new({blocks: [
+      {type: "text", text: "Code text", code: true}
+    ]}))
+
+    expect(page).to have_css(".cms-rich-text-block-component__text--code", text: "Code text")
   end
 
   it "renders a link" do
@@ -89,5 +153,18 @@ RSpec.describe CmsRichTextBlockComponent, type: :component do
     ]}))
 
     expect(page).to have_css("img[src='/an-image.png']")
+  end
+
+  it "renders a quote" do
+    render_inline(described_class.new({blocks: [
+      {
+        type: "quote",
+        children: [
+          {type: "text", text: "Quoted"}
+        ]
+      }
+    ]}))
+
+    expect(page).to have_css("blockquote", text: "Quoted")
   end
 end
