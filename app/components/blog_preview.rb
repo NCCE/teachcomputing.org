0272c7def43b40e0746eab2afcdf80365842193e
Michael Squance <m.squance@stem.org.uk>
2024-02-15 09:52:41 +0000

    Updating routing for single blog instances Adding in RecordNotFound for missing pages

--- app/components/blog_preview.rb
+++ app/components/blog_preview.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class BlogPreview < ViewComponent::Base
-  def initialize(title:)
+  def initialize(title:, excerpt:)
     @title = title
+    @excerpt = excerpt
   end
 
   def call
