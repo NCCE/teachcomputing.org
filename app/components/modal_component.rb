1ed59226130a87006850c67e756a6e3d3c206d65
Michael Squance <m.squance@stem.org.uk>
2024-02-16 09:49:58 +0000

    Changing variable name as requested

--- app/components/modal_component.rb
+++ app/components/modal_component.rb
@@ -4,12 +4,12 @@ class ModalComponent < ViewComponent::Base
 
   attr_reader :expanded, :title, :reopen_button_text, :show_corner_decoration, :class_name, :button_class
 
-  def initialize(title:, expanded: false, reopen_button_text: nil, show_corner_decoration: true, class_name: nil, button_class: nil)
+  def initialize(title:, expanded: false, reopen_button_text: nil, show_corner_decoration: true, class_name: nil, reopen_button_class: nil)
     @expanded = expanded
     @title = title
     @reopen_button_text = reopen_button_text
     @show_corner_decoration = show_corner_decoration
     @class_name = class_name
-    @button_class = button_class
+    @reopen_button_class = reopen_button_class
   end
 end
