f9fb392a385aef387c64775cf2eb65b22ff6c399
Michael Squance <m.squance@stem.org.uk>
2024-02-16 11:28:23 +0000

    Updating the modal component to new parameter name

--- app/components/modal_component.rb
+++ app/components/modal_component.rb
@@ -2,7 +2,7 @@ class ModalComponent < ViewComponent::Base
   renders_one :body
   renders_one :close_button
 
-  attr_reader :expanded, :title, :reopen_button_text, :show_corner_decoration, :class_name, :button_class
+  attr_reader :expanded, :title, :reopen_button_text, :show_corner_decoration, :class_name, :reopen_button_class
 
   def initialize(title:, expanded: false, reopen_button_text: nil, show_corner_decoration: true, class_name: nil, reopen_button_class: nil)
     @expanded = expanded
