a802c12b7401af3328d13b6be311c247c0d112bc
Michael Squance <m.squance@stem.org.uk>
2024-03-20 09:10:08 +0000

    Moved CP408 into stem_learning seeds and reconfigured as a remote course

--- db/seeds/activities/stem_learning.rb
+++ db/seeds/activities/stem_learning.rb
@@ -2464,6 +2464,18 @@ Activity.find_or_initialize_by(stem_course_template_no: "ee22f2ed-60dd-ee11-904d
   activity.programmes = [secondary_certificate, a_level]
 end.save!
 
+Activity.find_or_initialize_by(stem_course_template_no: "072b18fe-19e2-ee11-904d-002248c6f9ce").tap do |activity|
+  activity.title = "AI in primary computing"
+  activity.slug = "ai-in-primary-computing"
+  activity.category = "face-to-face"
+  activity.remote_delivered_cpd = true
+  activity.provider = "stem-learning"
+  activity.stem_activity_code = "CP408"
+  activity.credit = 15
+
+  activity.programmes = [primary_certificate]
+end.save!
+
 Activity.find_or_initialize_by(stem_course_template_no: "9d4bb5fd-fde5-ee11-904c-0022481b6d82").tap do |activity|
   activity.title = "Help! How do I lead primary computing? - residential"
   activity.slug = "help-how-do-i-lead-primary-computing-residential"
