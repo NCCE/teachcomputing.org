7c9dc2e55220609a236b7af835c5c135f7570f45
Michael Squance <m.squance@stem.org.uk>
2024-03-28 14:46:14 +0000

    Brought in the banner component from the toolkit branch and improved it Added in the various banners as requested

---
+++ app/components/banner_component.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class BannerComponent < ViewComponent::Base
+  def initialize(title:, text:, padding: 3, link: nil, background_color: "white", media_column_size: :third, image: nil, video: nil, media_side: :left)
+    @title = title
+    @text = text
+    @padding = padding
+    @image = image
+    @video = video
+    @background_color = background_color
+    @media_column_size = media_column_size
+    @media_side = media_side
+    @link = link
+  end
+
+  def image_column_class
+    classes = ["banner-component banner-component__media-section govuk-!-padding-right-0 govuk-!-padding-left-0"]
+    classes << case @media_column_size
+    when :third
+      "govuk-grid-column-one-third"
+    when :half
+      "govuk-grid-column-one-half"
+    else
+      "govuk-grid-column-one-third"
+    end
+  end
+
+  def text_column_class
+    classes = ["banner-component banner-component__content-section"]
+    classes << case @media_column_size
+    when :third
+      "govuk-grid-column-two-thirds"
+    when :half
+      "govuk-grid-column-one-half"
+    else
+      "govuk-grid-column-two-thirds"
+    end
+    classes.join(" ")
+  end
+
+  def media_block
+    content_tag(:div, class: media_column_class) do
+      image_pack_tag @image[:url], alt: @image[:alt] if @image
+      if @video
+        content_tag(:video, width: "100%", height: "auto", controls: :controls) do
+          content_tag(:source, nil, src: @video[:url])
+        end
+      end
+    end
+  end
+end
---
+++ spec/components/banner_component_spec.rb
@@ -0,0 +1,75 @@
+require "rails_helper"
+
+RSpec.describe BannerComponent, type: :component do
+  context "with left align image" do
+    before do
+      render_inline(described_class.new(
+        title: "Banner title",
+        text: "Some random text",
+        image: {
+          url: "media/images/test/image.png",
+          alt: ""
+        },
+        background_color: "purple",
+        link: "http://www.example.com"
+      ))
+    end
+
+    it "should have container" do
+      expect(page).to have_css(".govuk-width-container")
+    end
+
+    it "should default to two thirds for content" do
+      expect(page).to have_css(".banner-component__content-section.govuk-grid-column-two-thirds")
+    end
+  end
+
+  context "with half column" do
+    before do
+      render_inline(described_class.new(
+        title: "Banner title",
+        text: "Some random text",
+        media_column_size: :half,
+        image: {
+          url: "media/images/test/image.png",
+          alt: ""
+        },
+        background_color: "purple",
+        link: "http://www.example.com"
+      ))
+    end
+
+    it "should have container" do
+      expect(page).to have_css(".govuk-width-container")
+    end
+
+    it "should set content to be one half" do
+      expect(page).to have_css(".banner-component__content-section.govuk-grid-column-one-half")
+    end
+  end
+
+  context "with right align image" do
+    before do
+      render_inline(described_class.new(
+        title: "Banner title",
+        text: "Some random text",
+        media_side: :right,
+        image: {
+          url: "media/images/test/image.png",
+          alt: ""
+        },
+        background_color: "purple",
+        link: "http://www.example.com"
+      ))
+    end
+
+    it "should have container" do
+      expect(page).to have_css(".govuk-width-container")
+    end
+
+    it "should set background colour class" do
+      expect(page).to have_css(".purple-bg")
+    end
+
+  end
+end
