c4c5743125f2eb48141077bce73954d801a55a05
Michael Squance <m.squance@stem.org.uk>
2024-05-13 15:56:12 +0000

    Fixing the vital model checker which appeared to not be monitoring what it claimed to be

--- spec/lib/tasks/vital_model_checker_spec.rb
+++ spec/lib/tasks/vital_model_checker_spec.rb
@@ -9,4 +9,50 @@ RSpec.describe "rake vital_model_checker", type: :task do
     task.execute
     expect(Sentry).to have_received(:capture_message).twice
   end
+
+  describe "with face to face achievement" do
+    describe "created recently" do
+      before do
+        create(:achievement, :face_to_face)
+      end
+
+      it "should only call sentry once for online" do
+        task.execute
+        expect(Sentry).to have_received(:capture_message).once.with("No online achievements created at in the last 24 hours.")
+      end
+    end
+    describe "created a while ago" do
+      before do
+        create(:achievement, :face_to_face, created_at: 2.days.ago)
+      end
+
+      it "should call sentry twice" do
+        task.execute
+        expect(Sentry).to have_received(:capture_message).twice
+      end
+    end
+  end
+
+  describe "with online achievement" do
+    describe "created recently" do
+      before do
+        create(:achievement, :online)
+      end
+
+      it "should only call sentry once for face to face" do
+        task.execute
+        expect(Sentry).to have_received(:capture_message).once.with("No face to face achievements created at in the last 24 hours.")
+      end
+    end
+    describe "created a while ago" do
+      before do
+        create(:achievement, :online, created_at: 2.days.ago)
+      end
+
+      it "should call sentry twice" do
+        task.execute
+        expect(Sentry).to have_received(:capture_message).twice
+      end
+    end
+  end
 end
