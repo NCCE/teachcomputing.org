6f9791dfe1d636824cb54dab29cdd3541b007201
Adam Wheatley <adamwheatley7@gmail.com>
2024-05-14 13:01:55 +0000

    Updated YouTube embed component to handle exceptions Added title option onto bordered_cards_wrapper_component Updated careers_video_card_component SCSS to be inline with BEM Added preview for bordered card wrapper Added preview for careers video card Added preview for youtube embed Added tests for 3 new components

---
+++ spec/components/careers_video_card_component_spec.rb
@@ -0,0 +1,39 @@
+require "faker"
+require "rails_helper"
+
+RSpec.describe CareersVideoCardComponent, type: :component do
+  let(:title) { Faker::Lorem.sentence }
+  let(:name) { Faker::Name.name }
+  let(:job_title) { Faker::Company.name }
+  let(:description) { Faker::Lorem.sentence }
+
+  before do
+    render_inline(described_class.new(
+      video_url: "https://www.youtube.com/watch?v=t0ojrm0fMoE&list=PLwcV67XMdDdJn_6BFs2h1yBeyZDArab7J&index=3",
+      title:,
+      name:,
+      job_title:,
+      description:
+    ))
+  end
+
+  it "embeds a youtube video" do
+    expect(page).to have_css("iframe")
+  end
+
+  it "has a title" do
+    expect(page).to have_css("h2", text: title)
+  end
+
+  it "has a name" do
+    expect(page).to have_css(".govuk-body-s", text: name)
+  end
+
+  it "has a job title" do
+    expect(page).to have_css(".govuk-body-s", text: job_title)
+  end
+
+  it "has a description" do
+    expect(page).to have_css(".govuk-body-s", text: description)
+  end
+end
