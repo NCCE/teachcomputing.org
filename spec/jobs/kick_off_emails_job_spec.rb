22652cd61611a0b0550dea533b759698f3654d75
Michael Squance <m.squance@stem.org.uk>
2024-05-20 10:16:45 +0000

    Adding in extra test to ensure its not scheduling the i_belong email under other programmes Also removing some duplicate definitions in the test

--- spec/jobs/kick_off_emails_job_spec.rb
+++ spec/jobs/kick_off_emails_job_spec.rb
@@ -3,12 +3,10 @@ require "rails_helper"
 RSpec.describe KickOffEmailsJob, type: :job do
   let(:primary_certificate) { create(:primary_certificate) }
   let(:cs_accelerator) { create(:cs_accelerator) }
-  let(:primary) { create(:primary_certificate) }
   let(:secondary) { create(:secondary_certificate) }
   let(:i_belong) { create(:i_belong) }
   let(:cs_accelerator_enrolment) { create(:user_programme_enrolment, programme: cs_accelerator) }
   let(:i_belong_enrolment) { create(:user_programme_enrolment, programme: i_belong) }
-  let(:primary_enrolment) { create(:user_programme_enrolment, programme: primary) }
   let(:secondary_enrolment) { create(:user_programme_enrolment, programme: secondary) }
   let(:primary_certificate_enrolment) { create(:user_programme_enrolment, programme: primary_certificate) }
 
@@ -30,7 +28,7 @@ RSpec.describe KickOffEmailsJob, type: :job do
 
     context "when the programme is primary" do
       it "sends an email" do
-        expect { described_class.perform_now(primary_enrolment.id) }
+        expect { described_class.perform_now(primary_certificate_enrolment.id) }
           .to change { ActionMailer::Base.deliveries.count }.by(1)
       end
     end
@@ -53,12 +51,26 @@ RSpec.describe KickOffEmailsJob, type: :job do
         expect do
           described_class.perform_now(cs_accelerator_enrolment.id)
         end.to have_enqueued_job(ScheduleProgrammeGettingStartedPromptJob).with(cs_accelerator_enrolment.id).at(Time.new(2020, 12, 11, 22, 51, 6))
+
         expect do
           described_class.perform_now(primary_certificate_enrolment.id)
         end.to have_enqueued_job(ScheduleProgrammeGettingStartedPromptJob).with(primary_certificate_enrolment.id).at(Time.new(2020, 12, 11, 22, 51, 6))
+
         expect do
           described_class.perform_now(i_belong_enrolment.id)
         end.to have_enqueued_job(ScheduleIBelongStudentSurveyPromptJob).with(i_belong_enrolment.user).at(now_time + 1.week)
+      end
+
+      it "when not i_belong does not enqueue the i_belong job" do
+        expect do
+          described_class.perform_now(primary_certificate_enrolment.id)
+        end.not_to have_enqueued_job(ScheduleIBelongStudentSurveyPromptJob)
+        expect do
+          described_class.perform_now(cs_accelerator_enrolment.id)
+        end.not_to have_enqueued_job(ScheduleIBelongStudentSurveyPromptJob)
+        expect do
+          described_class.perform_now(secondary_enrolment.id)
+        end.not_to have_enqueued_job(ScheduleIBelongStudentSurveyPromptJob)
       end
     end
   end
