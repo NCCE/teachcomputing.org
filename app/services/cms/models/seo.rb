4b82c837469643cedef3421c41cd2b02015b84a9
Michael Squance <m.squance@stem.org.uk>
2024-05-23 11:44:23 +0000

    Removing keywords keyword and fixing typo

--- app/services/cms/models/seo.rb
+++ app/services/cms/models/seo.rb
@@ -3,7 +3,7 @@ module Cms
     class Seo
       attr_accessor :title, :description
 
-      def initialize(title:, description:, featured_image:, keywords:)
+      def initialize(title:, description:, featured_image:)
         @title = title
         @description = description
         @featured_image = featured_image
