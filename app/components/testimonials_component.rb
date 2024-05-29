01dca20c8337f64ec8e5ccfd36587f7d3360648d
Benjamin Croad <ben@storm-consultancy.com>
2024-05-29 12:07:37 +0100

    Rework of get involved page as per designs
    Redirect supporting and contributing partner pages to get involved
    Address accessibility issues

--- app/components/testimonials_component.rb
+++ app/components/testimonials_component.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 class TestimonialsComponent < ViewComponent::Base
-  def initialize(testimonials:, event_tracking_category:, testimonials_per_row:, light: false)
+  def initialize(testimonials:, event_tracking_category:, testimonials_per_row:, wrap_text: false, light: false, show_all_on_mobile: false)
     @testimonials = testimonials
     @event_tracking_category = event_tracking_category
     @testimonials_per_row = testimonials_per_row
+    @wrap_text = wrap_text
     @light = light
+    @show_all_on_mobile = show_all_on_mobile
   end
 
   def background_class
-    @light ? "testimonial--light" : "testimonial--dark"
+    light_css = @light ? "testimonial--light" : "testimonial--dark"
+    wrap_text_css = @wrap_text ? "testimonial--wrap-text" : ""
+    show_all_on_mobile_css = @show_all_on_mobile ? "testimonial--show-all-on-mobile" : ""
+
+    "#{light_css} #{wrap_text_css} #{show_all_on_mobile_css}".strip
   end
 
   def custom_properties
--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -2,4 +2,12 @@ module VideosHelper
   def cs_topic_video_1_url
     "https://static.teachcomputing.org/computer_science_topics/Sabrina_Singh_Virtual_Storage.mp4"
   end
+
+  def ambassador_video_url
+    "https://www.youtube.com/watch?v=OpdLaUGXhOw"
+  end
+
+  def governor_video_url
+    "https://www.youtube.com/watch?v=6H3vzOTILvg"
+  end
 end
