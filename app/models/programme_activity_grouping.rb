2ba959afc1834adf9cc92e1531c3a049319ea506
Benjamin Croad <ben@storm-consultancy.com>
2024-05-30 14:39:44 +0100

    Method to perform move of i belong activity grouping

--- app/models/programme_activity_grouping.rb
+++ app/models/programme_activity_grouping.rb
@@ -58,6 +58,34 @@ class ProgrammeActivityGrouping < ApplicationRecord
     end.to_h
   end
 
+  # Move "Implement selected key stage 3 Teach Computing Curriculum resources" from section 2
+  # to section 3 and update the #required_for_completion value on the old grouping
+  def self.move_i_belong_section_from_2_to_3
+    # Perform the move of the activity to the new grouping
+    programme = Programme.find_by(slug: "i-belong")
+    raise "Programme could not be found" if programme.blank?
+
+    potential_groupings = programme.programme_activity_groupings.community
+    raise "There are more than 2 available groupings for the moved activity" unless potential_groupings.size == 2
+
+    activity = programme.activities.find_by(title: "Implement selected key stage 3 Teach Computing Curriculum resources")
+    raise "Activity could not be found" if activity.blank?
+
+    programme_activity = ProgrammeActivity.find_by(activity_id: activity.id)
+    raise "Programme activity could not be found" if programme_activity.blank?
+
+    original_grouping = programme_activity.programme_activity_grouping
+    raise "Current grouping could not be found" if original_grouping.blank?
+
+    new_grouping = potential_groupings.where.not(id: original_grouping.id).first
+    raise "New grouping could not be determined" if new_grouping.blank?
+
+    programme_activity.update!(programme_activity_grouping_id: new_grouping.id)
+
+    # Update the original grouping to only require 2 completed activities
+    original_grouping.update!(required_for_completion: 2)
+  end
+
   protected def fetch_users_achievement_activity_ids(users:)
     Achievement
       .in_state(:complete)
