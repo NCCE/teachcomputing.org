681c8e0eb75932d1e9549492379ed309ad370846
Benjamin Croad <ben@storm-consultancy.com>
2024-06-06 09:13:39 +0100

    Improve coverage for UserProgrammeEnrolmentTransition

---
+++ spec/factories/user_programme_enrolment_transition.rb
@@ -0,0 +1,24 @@
+FactoryBot.define do
+  factory :user_programme_enrolment_transition do
+    association :user_programme_enrolment
+    to_state { "enrolled" }
+    sort_key { 1 }
+    most_recent { false }
+
+    trait :most_recent do
+      most_recent { true }
+    end
+
+    trait :unenrolled do
+      to_state { "unenrolled" }
+    end
+
+    trait :pending do
+      to_state { "pending" }
+    end
+
+    trait :complete do
+      to_state { "complete" }
+    end
+  end
+end
