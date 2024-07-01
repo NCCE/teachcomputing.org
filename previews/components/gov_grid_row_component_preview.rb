07d067fa7bb029a8935b61f191f0fe746f95197f
Michael Squance <m.squance@stem.org.uk>
2024-07-01 15:10:43 +0000

    Adding some previews for the new gov_grid_row_component Testing it on the careers support page Improving testing Moved padding and margin to the wrapper not the grid row

---
+++ previews/components/gov_grid_row_component_preview.rb
@@ -0,0 +1,42 @@
+class GovGridRowComponentPreview < ViewComponent::Preview
+  def two_thirds_one_third
+    component = GovGridRowComponent.new.tap do |c|
+      c.with_column("two-thirds") do
+        "Two thirds column"
+      end
+      c.with_column("one-third") do
+        "One third column"
+      end
+    end
+    render(component)
+  end
+
+  def two_thirds_one_third_purple_bg
+    component = GovGridRowComponent.new(
+      background_color: "purple"
+    ).tap do |c|
+      c.with_column("two-thirds") do
+        "Two thirds column"
+      end
+      c.with_column("one-third") do
+        "One third column"
+      end
+    end
+    render(component)
+  end
+
+  def grey_with_padding
+    component = GovGridRowComponent.new(
+      background_color: "light-grey",
+      padding: {all: 5}
+    ).tap do |c|
+      c.with_column("one-third") do
+        "One third column"
+      end
+      c.with_column("two-thirds") do
+        "Two thirds column"
+      end
+    end
+    render(component)
+  end
+end
