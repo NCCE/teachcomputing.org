4f915df85d973228fc21acd76c093e3ed579ee45
Michael Squance <m.squance@stem.org.uk>
2024-07-31 11:16:14 +0000

    Sorting the ENV references in Credly files

--- app/services/credly/connection.rb
+++ app/services/credly/connection.rb
@@ -1,7 +1,7 @@
 module Credly
   class Connection
-    API_TOKEN = ENV["CREDLY_AUTH_TOKEN"]
-    URL = ENV["CREDLY_URL"]
+    API_TOKEN = Rails.application.config.credly_auth_token.freeze
+    URL = Rails.application.config.credly_url.freeze
 
     def self.connect
       Faraday.new(url: URL) do |conn|
