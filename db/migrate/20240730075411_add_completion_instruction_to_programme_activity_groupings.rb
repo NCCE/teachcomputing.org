8c340c52a972d407cfe619c550df5d5f35411532
Adam Wheatley <adamwheatley7@gmail.com>
2024-07-31 12:42:53 +0000

    Remove completion_instruction from activity grouping

--- db/migrate/20240730075411_add_completion_instruction_to_programme_activity_groupings.rb
+++
@@ -1,5 +0,0 @@
-class AddCompletionInstructionToProgrammeActivityGroupings < ActiveRecord::Migration[6.1]
-  def change
-    add_column :programme_activity_groupings, :completion_instruction, :string
-  end
-end
