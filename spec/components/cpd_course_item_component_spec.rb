c223f063160421205e7ad89aa9f53ef8283badbc
Michael Squance <m.squance@stem.org.uk>
2024-08-12 12:34:05 +0000

    Adding addtional testing for cpd_course_item_compenent Few final css tweaks Removing old no longer needed tracking code from asides

--- spec/components/cpd_course_item_component_spec.rb
+++ spec/components/cpd_course_item_component_spec.rb
@@ -3,13 +3,60 @@
 require "rails_helper"
 
 RSpec.describe CpdCourseItemComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:current_user) { create(:user) }
+  let(:activity) { create(:activity, public_copy_additional_icons: ["Primary"]) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  before do
+    stub_course_templates
+  end
+
+  context "without achievement" do
+    before do
+      render_inline(described_class.new(activity:, current_user:))
+    end
+
+    it "should render the element" do
+      expect(page).to have_css(".cpd-course-item")
+    end
+
+    it "should render title" do
+      expect(page).to have_css(".govuk-body", text: activity.title)
+    end
+
+    it "should render the book now button" do
+      expect(page).to have_link("Book now", href: "/courses/#{activity.stem_activity_code}/#{activity.slug}")
+    end
+
+    it "should show activity_type icon" do
+      expect(page).to have_css("p.icon-map-pin")
+    end
+
+    it "should show addtional icon" do
+      expect(page).to have_css("p.icon-person", text: "Primary")
+    end
+  end
+
+  context "when complete" do
+    let!(:achievement) { create(:completed_achievement, user: current_user, activity:) }
+
+    before do
+      render_inline(described_class.new(activity:, current_user:))
+    end
+
+    it "should show completed flag" do
+      expect(page).to have_css(".govuk-tag", text: "Completed")
+    end
+  end
+
+  context "when in progress" do
+    let!(:achievement) { create(:achievement, user: current_user, activity:) }
+
+    before do
+      render_inline(described_class.new(activity:, current_user:))
+    end
+
+    it "should show completed flag" do
+      expect(page).to have_css(".govuk-tag", text: "In progress")
+    end
+  end
 end
