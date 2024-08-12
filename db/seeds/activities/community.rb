1d9bc6dc4473728d4b7d9f07b4420a9857736b88
Michael Squance <m.squance@stem.org.uk>
2024-08-12 14:08:34 +0000

    Community activity evidence copy updated

--- db/seeds/activities/community.rb
+++ db/seeds/activities/community.rb
@@ -423,7 +423,7 @@ Activity.find_or_initialize_by(slug: "request-your-i-belong-in-computer-science-
 end.save!
 
 Activity.find_or_initialize_by(slug: "implement-selected-key-stage-3-teach-computing-curriculum-resources").tap do |activity|
-  activity.title = "Implement selected key stage 3 Teach Computing Curriculum resources"
+  activity.title = "Implement selected Teach Computing Curriculum resources"
   activity.credit = 10
   activity.slug = "implement-selected-key-stage-3-teach-computing-curriculum-resources"
   activity.category = "community"
@@ -436,14 +436,20 @@ Activity.find_or_initialize_by(slug: "implement-selected-key-stage-3-teach-compu
       brief: "Tell us which units you’ve been teaching, to which year group, and when."
     },
     {
-      brief: "Tell us how you added approaches from the 'Encouraging Girls' course into your teaching. Examples may include increasing students’ sense of ‘belonging’ with different ways of working together; using near-peer and relatable role models to link careers into the computing curriculum; or boosting girls’ self-efficacy when teaching units such as programming."
+      brief: "Tell us how you added approaches from the 'Encouraging Girls' or ‘Empowering girls’ course into your teaching. Examples may include:",
+      bullets: [
+        "increasing pupils’ sense of ‘belonging’ with different ways of working together",
+        "using near-peer and relatable role models to link careers into the computing curriculum",
+        "boosting girls’ self-efficacy when teaching units such as programming by implementing effective pedagogies such as pair programming."
+      ]
     },
     {
-      brief: "Tell us the impact of what you did. Share your reflections and add examples of evidence, such as:",
+      brief: "Tell us the impact of what you did. Share your reflections and add examples of evidence, such as: ",
       bullets: [
-        "documenting changes in girls’ participation levels after implementing the resources (may include student voice).",
-        "examples where collaborative working or team-based activities have increased girls’ participation.",
-        "sharing programming units as a link from your school website and reflect on strategies to boost girls' self-efficacy, such as targeted feedback, peer collaboration, showcasing successful projects by female students, or family engagement."
+        "documenting changes in girls’ participation levels after implementing the resources (may include student voice)",
+        "examples where collaborative working or project-based activities have increased girls’ participation",
+        "reflect on the use of strategies to boost girls' self-efficacy, such as targeted feedback, peer collaboration",
+        "showcase successful projects by female pupils, on the school website and share them with family to support family engagement."
       ]
     }
   ]
@@ -460,13 +466,13 @@ Activity.find_or_initialize_by(slug: "participate-in-a-ncce-student-enrichment-a
   activity.self_verification_info = "Please provide us with evidence of participation"
   activity.public_copy_evidence = [
     {
-      brief: "Tell us which event your students participated in. Describe the activity including when it took place. Include the age range of students involved, the total number and the proportion of female and male students. "
+      brief: "Tell us which event your students participated in. Describe the activity including when it took place. Include the age range of students involved, the total number and the proportion of female and male pupils."
     },
     {
-      brief: "Tell us which approaches from the 'Encouraging Girls' course were used. Examples might include:",
+      brief: "Tell us which approaches from the 'Encouraging Girls' or ‘Empowering girls’ course were used. Examples might include:",
       bullets: [
         "Being part of a supportive team at the event – increasing the proportion of girls participating.",
-        "Using near-peer mentors, such as older students - increasing identity.",
+        "Using near-peer mentors, such as older pupils - increasing identity.",
         "Family engagement – increasing social capital.",
         "Considering prior experiences and access to computing at home – advancing equity."
       ]
@@ -474,8 +480,8 @@ Activity.find_or_initialize_by(slug: "participate-in-a-ncce-student-enrichment-a
     {
       brief: "What was the impact of the event? Share your reflections and add examples of evidence, such as:",
       bullets: [
-        "Positive feedback from students or a blogpost from the school website.",
-        "A link to an online news story or social media post from your school’s account to summarise the event.",
+        "Positive feedback from pupils, a link to a newsletter covering the event or a blogpost from the school website.",
+        "A link to an online news story or social media post from your school’s account to summarise the event",
         "Option data showing an increase in the proportion of girls considering GCSE Computer Science in KS4."
       ]
     }
@@ -491,26 +497,29 @@ Activity.find_or_initialize_by(slug: "provide-access-to-a-computing-related-extr
   activity.self_certifiable = true
   activity.description = %(Provide access to a computing related STEM lunchtime or after school club. Consider involving older female students to support. Resources to help you get started are available via the <a href="#{i_belong_handbook_url}">handbook</a>.)
   activity.self_verification_info = "Please provide us with evidence of delivery"
-  activity.public_copy_evidence = [{
-    brief: "Tell us what you did and when. Describe your computing club in school and include timescales, particularly if you’ve run the club for longer than a year. Include the age range of students involved, and the proportion of female and male students. How do you promote a higher proportion of girls participating? Finally, reflect and tell us why you chose this activity."
-  },
+  activity.public_copy_evidence = [
     {
-      brief: "Which approaches from the 'Encouraging Girls' course have you considered or used? Examples might include:",
+      brief: "Tell us what you did and when. Describe your computing club in school and include timescales, particularly if you’ve run the club for longer than a year. Include the age range of pupils involved, and the proportion of female and male pupils. How do you promote a higher proportion of girls participating? Finally, reflect and tell us why you chose this activity."
+    },
+    {
+      brief: "Which approaches from the 'Encouraging Girls' or ‘Empowering girls’ course have you considered or used? Examples might include:",
       bullets: [
-        "Inviting students during lessons or family events to increase the proportion of girls taking part.",
-        "Having a choice of activities including physical computing or themed projects such as climate challenges or creating music.",
-        "Including competitive activities in a supportive setting such as the CyberFirst Girls or CanSat challenges.",
-        "Using near-peer mentors (such as older students or computing ambassadors)."
+        "Inviting female pupils during lessons or family events to increase the proportion of girls taking part",
+        "Having a choice of activities including physical computing or themed projects such as climate challenges or creating music",
+        "Setting up girls-only clubs to empower them to question, experiment and lead",
+        "Including competitive activities in a supportive setting such as the CyberFirst Girls or CanSat challenges",
+        "Using near-peer mentors (such as older pupils or computing ambassadors)."
       ]
     },
     {
       brief: "What is the impact of the club? Write your reflections and add examples of evidence, which might include:",
       bullets: [
-        "Positive feedback from students or sharing a blogpost from the school website.",
-        "Sharing a link to an online news story or social media post from your school’s account about a success in a competition or club activities.",
+        "Positive feedback from pupils, a link to a newsletter covering the event or a blogpost from the school website",
+        "Sharing a link to an online news story or social media post from your school’s account about a success in a competition or club activities",
         "Option data showing an increase in the proportion of girls considering GCSE Computer Science in KS4."
       ]
-    }]
+    }
+  ]
 end.save!
 
 Activity.find_or_initialize_by(slug: "host-a-computing-stem-ambassador-activity").tap do |activity|
@@ -522,24 +531,27 @@ Activity.find_or_initialize_by(slug: "host-a-computing-stem-ambassador-activity"
   activity.self_certifiable = true
   activity.description = "Host a Computing Ambassador in your school to support raising gender and career aspirations and to help students understand the real-world applications of their learning. Evidence must showcase the <a href=\"#{stem_request_ambassador_url}\">Computing Ambassador</a> visit to your school from April 2023 onwards."
   activity.self_verification_info = "Please provide us with evidence of delivery"
-  activity.public_copy_evidence = [{
-    brief: "Tell us what you did and when. Was it a one-off visit from a computing ambassador or part of a series of events you’ve organised in school? Include the age range of students involved, and the proportion of female and male students. How did you promote a higher proportion of girls participating?"
-  },
+  activity.public_copy_evidence = [
     {
-      brief: "Which approaches from the 'Encouraging Girls' course can you share in how you planned the activity? Examples might include:",
+      brief: "Tell us what you did and when. Was it a one-off visit from a computing ambassador or part of a series of events you’ve organised in school? Include the age range of students involved, and the proportion of female and male pupils. How did you promote a higher proportion of girls participating?"
+    },
+    {
+      brief: "Which approaches from the 'Encouraging Girls' or ‘Empowering girls’ course can you share in how you planned the activity? Examples might include: ",
       bullets: [
-        "Inviting near-peer and relatable role models to link careers into the computing curriculum.",
-        "Bringing in relevance such as AI, health or environmental themes for activities that are about ‘people not things’."
+        "Linking the visit to the curriculum to add real world contexts",
+        "Inviting relatable role models to link careers into the computing curriculum.",
+        "Choosing a topic such as AI, health or environmental themes for activities that are about ‘people not things’."
       ]
     },
     {
       brief: "What was the impact? What have you observed following the interventions you’ve put in place? Examples might include:",
       bullets: [
-        "Positive feedback from students or sharing a blogpost from the school website.",
+        "Positive feedback from pupils, a link to a newsletter covering the event or a blogpost from the school website.",
         "A link to an online news story or social media post from your school’s account summarising the activity.",
         "Option data showing an increase in the proportion of girls considering GCSE Computer Science in KS4."
       ]
-    }]
+    }
+  ]
 end.save!
 
 Activity.find_or_initialize_by(slug: "participate-in-a-computing-related-competition").tap do |activity|
@@ -553,21 +565,22 @@ Activity.find_or_initialize_by(slug: "participate-in-a-computing-related-competi
   activity.self_verification_info = "Please provide us with evidence of delivery"
   activity.public_copy_evidence = [
     {
-      brief: "Tell us which competition your students entered into and when. Include the age range of students involved, and the proportion of girls taking part."
+      brief: "Tell us which competition your students entered into and when. Include the age range of pupils involved, and the proportion of girls taking part."
     },
     {
-      brief: "Which approaches from the 'Encouraging Girls' course helped to increase the proportion of girls participating in the competition? Examples might include:",
+      brief: "Which approaches from the 'Encouraging Girls' or ‘Empowering girls’ course helped to increase the proportion of girls participating in the competition? Examples might include:",
       bullets: [
-        "Inviting students during lessons or family engagement events to promote self-efficacy and participation.",
-        "Building a supportive team with competitions in a club setting.",
-        "Involving near-peer mentors such as older students or computing ambassadors."
+        "Inviting female pupils during lessons or family engagement events to promote self-efficacy and participation",
+        "Building a supportive team with competitions in a club setting",
+        "Setting up girls-only teams to empower them to question, experiment and lead",
+        "Involving near-peer mentors such as older pupils or computing ambassadors."
       ]
     },
     {
       brief: "What was the impact? What have you observed following the interventions you’ve put in place? Examples might include:",
       bullets: [
-        "Positive feedback from students or a blogpost from the school website.",
-        "A link to an online news story or school social media post about a success in a competition or club activity.",
+        "Positive feedback from pupils, a link to a newsletter covering the event or a blogpost from the school website",
+        "A link to an online news story or school social media post about a success in a competition or club activity",
         "Option data showing an increase in the proportion of girls considering GCSE Computer Science in KS4."
       ]
     }
@@ -585,20 +598,22 @@ Activity.find_or_initialize_by(slug: "any-other-activity-which-aligns-with-recom
   activity.self_verification_info = "Please provide us with evidence of delivery"
   activity.public_copy_evidence = [
     {
-      brief: "Tell us which activity your students have got involved with and when it took place. Include the age range of all the students involved, the total number and the proportion of girls and boys."
+      brief: "Tell us which activity your students have got involved with and when it took place. Include the age range of all the pupils involved, the total number and the proportion of girls and boys."
     },
     {
-      brief: "Which approaches from the 'Encouraging Girls' course can you share in the way you planned the activity? Examples might include:",
+      brief: "Which approaches from the 'Encouraging Girls' or ‘Empowering girls’ course can you share in the way you planned the activity? Examples might include:",
       bullets: [
-        "Inviting near-peer and relatable role models to link careers into the computing curriculum.",
+        "Creating opportunities for family engagement",
+        "Setting up girls-only opportunities to empower them to question, experiment and lead",
+        "Inviting near-peer and relatable role models to link careers into the computing curriculum",
         "Bringing in relevance such as AI, health or environmental themes for activities that are about ‘people not things’."
       ]
     },
     {
       brief: "What was the impact? What have you observed following the interventions you’ve put in place? Examples might include:",
       bullets: [
-        "Positive feedback from students or a blogpost from the school website.",
-        "A link to an online news story or school social media post about a success in a competition or club activities.",
+        "Positive feedback from pupils, a link to a newsletter covering the event or a blogpost from the school website",
+        "A link to an online news story or school social media post about a success in a competition or club activities",
         "Option data showing an increase in the proportion of girls considering GCSE Computer Science in KS4."
       ]
     }
