dfddc4568114a2336421a17761cca26a364d418f
Michael Squance <m.squance@stem.org.uk>
2024-08-15 09:15:13 +0000

    Adding in some dynamic zone testing

--- app/components/cms_dynamic_zone_component.rb
+++ app/components/cms_dynamic_zone_component.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class CmsDynamicZoneComponent < ViewComponent::Base
-  def initialize(components)
-    @components = components
+  def initialize(cms_models)
+    @cms_models = cms_models
   end
 
   erb_template <<~ERB
     <div class="cms-dynamic-zone">
-      <% @components.each do |comp| %>
-        <%= render comp.render %>
+      <% @cms_models.each do |model| %>
+        <%= render model.render %>
       <% end %>
     <div>
   ERB
