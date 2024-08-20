85ae1c74712a5baaafed801faa6156048a9d45f1
Michael Squance <m.squance@stem.org.uk>
2024-08-20 08:12:11 +0000

    Fixing issue where book now button still showed when in progress or completed. Added some improved error handling for the cpd badge job

--- app/jobs/issue_cpd_badge_job.rb
+++ app/jobs/issue_cpd_badge_job.rb
@@ -12,8 +12,12 @@ class IssueCpdBadgeJob < ApplicationJob
       next unless programme.user_qualifies_for_credly_cpd_badge?(user)
       next if Credly::Badge.user_has_programme_cpd_badge?(user, programme)
 
-      Credly::Badge.issue(user.id, badge.credly_badge_template_id)
-      NewBadgeMailer.new_badge_email(user, programme).deliver_now
+      begin
+        Credly::Badge.issue(user.id, badge.credly_badge_template_id)
+        NewBadgeMailer.new_badge_email(user, programme).deliver_now
+      rescue Credly::Error => e
+        Sentry.capture_exception(e)
+      end
     end
   end
 end
