6288b6fb477044f1ee74ee90922ca89cb90eb4de
Michael Squance <m.squance@stem.org.uk>
2024-08-30 15:59:33 +0000

    Fixing mobile design Adding in new filter counter and clear option

--- app/components/cms_enrichment_list_component/cms_enrichment_list_component_controller.js
+++ app/components/cms_enrichment_list_component/cms_enrichment_list_component_controller.js
@@ -2,25 +2,45 @@ import ApplicationController from "../../webpacker/javascript/controllers/applic
 
 export default class CmsEnrichmentListComponentController extends ApplicationController {
 
-  static targets = ["termSelect", "ageGroupSelect", "typeSelect", "enrichment"];
+  static targets = ["termSelect", "ageGroupSelect", "typeSelect", "enrichment", "filterCount", "filterWrapper"];
 
   connect(){
   }
 
-  filterChanged(event){
+  filterChanged(){
     const termFilter = this.termSelectTarget.value;
     const ageGroupFilter = this.ageGroupSelectTarget.value;
     const typeFilter = this.typeSelectTarget.value;
+    console.log(termFilter == "")
+    let count = 0;
     this.enrichmentTargets.forEach(enrichment => {
       const termMatch = termFilter != "" ? JSON.parse(enrichment.dataset.enrichmentTerms).includes(termFilter) : true;
       const ageGroupMatch = ageGroupFilter != "" ? JSON.parse(enrichment.dataset.enrichmentAgeGroups).includes(ageGroupFilter) : true;
       const typeMatch = typeFilter != "" ? enrichment.dataset.enrichmentType === typeFilter : true;
       if(termMatch && ageGroupMatch && typeMatch) {
+        console.log("showing")
         enrichment.classList.add("show")
+        count++;
       } else {
         enrichment.classList.remove("show")
       }
     });
+    let message; 
+    if (count == 1){
+      message = "1 activity found";
+    } else {
+      message = `${count} activities found`;
+    }
+    this.filterCountTarget.innerText = message;
+    this.filterWrapperTarget.classList.remove("hide");
+  }
+
+  clearFilter() {
+    this.termSelectTarget.selectedIndex = 0;
+    this.ageGroupSelectTarget.selectedIndex = 0;
+    this.typeSelectTarget.selectedIndex = 0;
+    this.filterChanged();
+    this.filterWrapperTarget.classList.add("hide");
   }
   
 }
