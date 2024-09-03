f0c388757c6a482c59dcb2581af78002cff434a4
Michael Squance <m.squance@stem.org.uk>
2024-09-03 16:03:18 +0000

    Adding in some testing for the enrichment system Not sure I like the new strapi generators, will think about them and maybe have a better solution Need more testing for the list component to make sure filters and featured are processed correctly

--- spec/components/cms_enrichment_component_spec.rb
+++ spec/components/cms_enrichment_component_spec.rb
@@ -3,13 +3,31 @@
 require "rails_helper"
 
 RSpec.describe CmsEnrichmentComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    render_inline(CmsEnrichmentComponent.new(
+      title: Cms::Models::RichHeader.new(blocks: generate_strapi_content_block),
+      details: generate_strapi_content_block,
+      link: "https:://www.teachcomputing.org/test-enrichment",
+      i_belong: false,
+      type: Cms::Models::EnrichmentType.new(
+        name: "Challenge",
+        icon: Cms::Providers::Strapi::Factories::ModelFactory.to_image(generate_strapi_image_attributes)
+      ),
+      terms: ["Spring", "Autumn"],
+      age_groups: ["KS1", "KS3"],
+      partner_icon: nil
+    ))
+  end
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it "renders the link to enrichment" do
+    expect(page).to have_link(href: "https:://www.teachcomputing.org/test-enrichment")
+  end
+
+  it "render the terms in a comma seperated alphabetical ordered list" do
+    expect(page).to have_css(".enrichment__term-details", text: "Autumn, Spring")
+  end
+
+  it "render the terms in a comma seperated list of age groups" do
+    expect(page).to have_css(".enrichment__term-details", text: "KS1, KS3")
+  end
 end
--- spec/components/cms_enrichment_list_component_spec.rb
+++ spec/components/cms_enrichment_list_component_spec.rb
@@ -3,13 +3,34 @@
 require "rails_helper"
 
 RSpec.describe CmsEnrichmentListComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    render_inline CmsEnrichmentListComponent.new(
+      enrichments: [],
+      featured_title: "Featured enrichments",
+      all_title: "Search for enrichment",
+      type_filter_placeholder: "Type (all)",
+      age_group_filter_placeholder: "Age group (all)",
+      term_filter_placeholder: "Term (all)"
+    )
+  end
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it "renders featured heading" do
+    expect(page).to have_css(".govuk-heading-m", text: "Featured enrichments")
+  end
+
+  it "renders all sections heading" do
+    expect(page).to have_css(".govuk-heading-m", text: "Search for enrichment")
+  end
+
+  it "renders term select with place holder" do
+    expect(page).to have_css("select[data-cms-enrichment-list-component-target='termSelect'] option", text: "Term (all)")
+  end
+
+  it "renders ageGroup select with place holder" do
+    expect(page).to have_css("select[data-cms-enrichment-list-component-target='ageGroupSelect'] option", text: "Age group (all)")
+  end
+
+  it "renders type select with place holder" do
+    expect(page).to have_css("select[data-cms-enrichment-list-component-target='typeSelect'] option", text: "Type (all)")
+  end
 end
