0f196e4f506db26d9273f721c63238e676ea9f6e
Adam Wheatley <adamwheatley7@gmail.com>
2024-09-13 07:25:10 +0000

    Refactor of code to use colour theme from Strapi Updated global css colours for standard, isaac and i belong

--- app/components/cms_horizontal_card_component.rb
+++ app/components/cms_horizontal_card_component.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class CmsHorizontalCardComponent < ViewComponent::Base
-  def initialize(title:, body_blocks:, image:, image_link:, ribbon_colour:, icon_block:)
+  def initialize(title:, body_blocks:, image:, image_link:, colour_theme:, icon_block:)
     @title = title
     @body_blocks = body_blocks
     @image = image
     @image_link = image_link
-    @ribbon_colour = ribbon_colour
+    @colour_theme = colour_theme
     @icon_block = icon_block
   end
 end
