055b2b0251282be5734eb6b67f6c944f303970c3
Adam Wheatley <adamwheatley7@gmail.com>
2024-09-16 09:29:05 +0000

    Added initial testing for component

---
+++ spec/components/cms_horizontal_card_component_spec.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe CmsHorizontalCardComponent, type: :component do
+  context "with only a title and body block" do
+    before do
+      render_inline(described_class.new(
+        title: "Page title",
+        body_blocks: [
+          type: "paragraph",
+          children: [
+            {type: "text", text: "Hello world!"}
+          ]
+        ],
+        image: nil,
+        image_link: nil,
+        colour_theme: nil,
+        icon_block: nil
+      ))
+    end
+
+    it "renders the title" do
+      expect(page).to have_text("Page title")
+    end
+
+    it "renders the body block text" do
+      expect(page).to have_text("Hello world!")
+    end
+
+    it "has no image" do
+      expect(page).to_not have_css(".horizontal-card-component__image")
+    end
+
+    it "has no colour theme" do
+      expect(page).to_not have_css("[class^='cms-colour-theme__border-']")
+    end
+
+    it "has no icon block" do
+      expect(page).to_not have_css("horizontal-card-component__icon-wrapper")
+    end
+  end
+
+  context "with an image" do
+    before do
+      render_inline(described_class.new(
+        title: "Page title",
+        body_blocks: [
+          type: "paragraph",
+          children: [
+            {type: "text", text: "Hello world!"}
+          ]
+        ],
+        image: nil,
+        image_link: nil,
+        colour_theme: nil,
+        icon_block: nil
+      ))
+    end
+  end
+
+  context "with a colour theme" do
+    before do
+      render_inline(described_class.new(
+        title: "Page title",
+        body_blocks: [
+          type: "paragraph",
+          children: [
+            {type: "text", text: "Hello world!"}
+          ]
+        ],
+        image: nil,
+        image_link: nil,
+        colour_theme: "standard",
+        icon_block: nil
+      ))
+    end
+  end
+end
