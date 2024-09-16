9e8d46822f5d5ee7e821ab6e8729335fcf4bdbe6
Adam Wheatley <adamwheatley7@gmail.com>
2024-09-16 10:10:22 +0000

    Added default nil values to component Refactor of component tests

--- app/components/cms_horizontal_card_component.rb
+++ app/components/cms_horizontal_card_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CmsHorizontalCardComponent < ViewComponent::Base
-  def initialize(title:, body_blocks:, image:, image_link:, colour_theme:, icon_block:)
+  def initialize(title:, body_blocks:, image: nil, image_link: nil, colour_theme: nil, icon_block: nil)
     @title = title
     @body_blocks = body_blocks
     @image = image
