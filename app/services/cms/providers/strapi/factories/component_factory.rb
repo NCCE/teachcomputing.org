c8b60af7118af19a018955db4694fbcea01f31a8
Michael Squance <m.squance@stem.org.uk>
2024-09-17 13:53:11 +0000

    Updating the test suite using mocks, and adding some tests for dynamic components

--- app/services/cms/providers/strapi/factories/component_factory.rb
+++ app/services/cms/providers/strapi/factories/component_factory.rb
@@ -30,9 +30,13 @@ module Cms
             when "buttons.ncce-button"
               DynamicComponents::NcceButton.new(title: strapi_data[:title], link: strapi_data[:link])
             when "blocks.question-and-answer"
-              DynamicComponents::QuestionAndAnswer.new(question: strapi_data[:question], answer: strapi_data[:answer],
-                aside_sections: extract_aside_sections(strapi_data))
+              to_question_and_answer(strapi_data)
             end
+          end
+
+          def self.to_question_and_answer(strapi_data)
+            DynamicComponents::QuestionAndAnswer.new(question: strapi_data[:question], answer: strapi_data[:answer],
+              aside_sections: extract_aside_sections(strapi_data))
           end
 
           def self.extract_aside_sections(strapi_data)
