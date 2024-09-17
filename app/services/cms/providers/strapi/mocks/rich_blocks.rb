b783106729fbfdb3fb6b3a54d4a42da5df5908f5
Michael Squance <m.squance@stem.org.uk>
2024-09-17 09:57:13 +0000

    Setting up mock structure for CMS data Updating the enrichment component test to use the new mocks

---
+++ app/services/cms/providers/strapi/mocks/rich_blocks.rb
@@ -0,0 +1,20 @@
+module Cms
+  module Providers
+    module Strapi
+      module Mocks
+        class RichBlocks
+          def self.generate
+            [paragraph(2)]
+          end
+
+          def self.paragraph(sentences)
+            {
+              type: "paragraph",
+              children: Faker::Lorem.sentences(number: sentences).map { {text: _1, type: "text"} }
+            }
+          end
+        end
+      end
+    end
+  end
+end
