1c9c315943619dd05c204bd63143c97ee0c17af1
Adam Wheatley <adamwheatley7@gmail.com>
2024-09-17 09:16:20 +0000

    Added testing for horizontal card component Added testing for icon block component Initial work on previews for both components

---
+++ previews/components/cms_horizontal_card_component_preview.rb
@@ -0,0 +1,85 @@
+class CmsHorizontalCardComponentPreview < ViewComponent::Preview
+  layout "full-width"
+
+  def default
+    render(CmsHorizontalCardComponent.new(
+      title: "Page title",
+      body_blocks: [{
+        type: "paragraph",
+        children: [
+          {
+            text: Faker::Lorem.paragraph,
+            type: "text"
+          }
+        ]
+      }],
+      image: nil,
+      image_link: nil,
+      colour_theme: nil,
+      icon_block: nil
+    ))
+  end
+
+  def with_image_and_image_link
+    render(CmsHorizontalCardComponent.new(
+      title: "Page title",
+      body_blocks: [{
+        type: "paragraph",
+        children: [
+          {
+            text: Faker::Lorem.paragraph,
+            type: "text"
+          }
+        ]
+      }],
+      image: nil,
+      image_link: nil,
+      colour_theme: nil,
+      icon_block: nil
+    ))
+  end
+
+  def with_colour_theme
+    render(CmsHorizontalCardComponent.new(
+      title: "Page title",
+      body_blocks: [{
+        type: "paragraph",
+        children: [
+          {
+            text: Faker::Lorem.paragraph,
+            type: "text"
+          }
+        ]
+      }],
+      image: nil,
+      image_link: nil,
+      colour_theme: "standard",
+      icon_block: nil
+    ))
+  end
+
+  def with_icon_block
+    render(CmsHorizontalCardComponent.new(
+      title: "Page title",
+      body_blocks: [{
+        type: "paragraph",
+        children: [
+          {
+            text: Faker::Lorem.paragraph,
+            type: "text"
+          }
+        ]
+      }],
+      image: nil,
+      image_link: nil,
+      colour_theme: "standard",
+      icon_block: Cms::DynamicComponents::IconBlock.new(icons:
+        [
+          Cms::DynamicComponents::Icon.new(
+            text: "Face to face",
+            image: Cms::Providers::Strapi::Factories::ModelFactory.to_image(generate_strapi_image_attributes)
+          )
+        ])
+    ))
+  end
+end
---
+++ previews/components/cms_icon_block_component_preview.rb
@@ -0,0 +1,5 @@
+class CmsIconBlockComponentPreview < ViewComponent::Preview
+  def default
+    # TODO ADD COMPONENT PREVIEW
+  end
+end
