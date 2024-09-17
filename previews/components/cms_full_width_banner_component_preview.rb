93880a647a6ec2ef0802b74d706b245f66956d05
Michael Squance <m.squance@stem.org.uk>
2024-09-17 15:20:07 +0000

    Adding view for buttons, but I need them from my other work. Updating preview to work with mocks after merge of mock system

--- previews/components/cms_full_width_banner_component_preview.rb
+++ previews/components/cms_full_width_banner_component_preview.rb
@@ -3,12 +3,23 @@
 class CmsFullWidthBannerComponentPreview < ViewComponent::Preview
   def default
     render(CmsFullWidthBannerComponent.new(
-      text_content: [], # Cms::Mocks::RichBlocks.generate,
+      text_content: Cms::Mocks::RichBlocks.generate,
       background_color: :white,
-      image: nil,  # Cms::Mocks::Image.as_model,
+      image: Cms::Mocks::Image.as_model,
       image_side: :left,
       image_link: nil,
       buttons: []
+    ))
+  end
+
+  def with_button
+    render(CmsFullWidthBannerComponent.new(
+      text_content: Cms::Mocks::RichBlocks.generate,
+      background_color: :white,
+      image: Cms::Mocks::Image.as_model,
+      image_side: :left,
+      image_link: nil,
+      buttons: [] # TODO after merge
     ))
   end
 end
