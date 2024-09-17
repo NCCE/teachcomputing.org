b0049dd66dc27a3badee43e351df4decf142d049
Michael Squance <m.squance@stem.org.uk>
2024-09-17 09:57:13 +0000

    Setting up mock structure for CMS data Updating the enrichment component test to use the new mocks

--- spec/components/cms_enrichment_component_spec.rb
+++ spec/components/cms_enrichment_component_spec.rb
@@ -5,13 +5,13 @@ require "rails_helper"
 RSpec.describe CmsEnrichmentComponent, type: :component do
   before do
     render_inline(CmsEnrichmentComponent.new(
-      title: Cms::Models::RichHeader.new(blocks: generate_strapi_content_block),
-      details: generate_strapi_content_block,
+      title: Cms::Models::RichHeader.new(blocks: Cms::Mocks::RichBlocks.generate),
+      details: Cms::Mocks::RichBlocks.generate,
       link: "https:://www.teachcomputing.org/test-enrichment",
       i_belong: false,
       type: Cms::Models::EnrichmentType.new(
         name: "Challenge",
-        icon: Cms::Providers::Strapi::Factories::ModelFactory.to_image(generate_strapi_image_attributes)
+        icon: Cms::Mocks::Image.as_model
       ),
       terms: ["Spring", "Autumn"],
       age_groups: ["KS1", "KS3"],
