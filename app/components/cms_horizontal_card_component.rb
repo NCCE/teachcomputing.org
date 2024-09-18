3a48204b6b434474aacf87e8f1c4169f380e0f8e
Adam Wheatley <adamwheatley7@gmail.com>
2024-09-18 08:02:59 +0000

    Updated cms helper method name to make it clearer it's returning a class name Moved wrapper classes into the component Added optional parameter onto CmsImageComponent to allow a link to be passed in Updated image mock to allow a caption to be passed in, nil by default Added preview for CmsImageBlockComponent

--- app/components/cms_horizontal_card_component.rb
+++ app/components/cms_horizontal_card_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CmsHorizontalCardComponent < ViewComponent::Base
-  delegate :cms_colour_theme, to: :helpers
+  delegate :cms_colour_theme_class, to: :helpers
 
   def initialize(title:, body_blocks:, image: nil, image_link: nil, colour_theme: nil, icon_block: nil)
     @title = title
@@ -10,5 +10,11 @@ class CmsHorizontalCardComponent < ViewComponent::Base
     @image_link = image_link
     @colour_theme = colour_theme
     @icon_block = icon_block
+  end
+
+  def wrapper_classes
+    classes = ["horizontal-card-component__wrapper"]
+    classes << cms_colour_theme_class(@colour_theme, "left") if @colour_theme
+    classes
   end
 end
