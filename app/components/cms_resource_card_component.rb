3bc94a60f529490ee8057f7608c5b3c3a1c04789
Adam Wheatley <adamwheatley7@gmail.com>
2024-09-23 09:55:20 +0000

    Updates to card wrapper component to allow cards per row to be specified via Strapi Added buttons and classes to resource cards Created card wrapper and styling Created resource cards and styling

--- app/components/cms_resource_card_component.rb
+++ app/components/cms_resource_card_component.rb
@@ -1,8 +1,18 @@
 class CmsResourceCardComponent < ViewComponent::Base
-  def initialize(title:, icon:, colour_theme:, body_text:)
+  delegate :cms_colour_theme_class, to: :helpers
+
+  def initialize(title:, icon:, colour_theme:, body_text:, button_text:, button_link:)
     @title = title
     @icon = icon
     @colour_theme = colour_theme
     @body_text = body_text
+    @button_text = button_text
+    @button_link = button_link
+  end
+
+  def wrapper_classes
+    classes = ["cms-resource-card_wrapper"]
+    classes << cms_colour_theme_class(@colour_theme, "top") if @colour_theme
+    classes
   end
 end
--- app/services/cms/dynamic_components/card_wrapper.rb
+++ app/services/cms/dynamic_components/card_wrapper.rb
@@ -1,17 +1,17 @@
 module Cms
   module DynamicComponents
     class CardWrapper
-      attr_accessor :title, :cards_block, :cards_per_row, :background_colour
+      attr_accessor :title, :cards_block, :cards_per_row, :background_color
 
-      def initialize(title:, cards_block:, cards_per_row:, background_colour:)
+      def initialize(title:, cards_block:, cards_per_row:, background_color:)
         @title = title
         @cards_block = cards_block
         @cards_per_row = cards_per_row
-        @background_colour = background_colour
+        @background_color = background_color
       end
 
       def render
-        CmsCardWrapperComponent.new(title:, cards_block:, cards_per_row:, background_colour:)
+        CmsCardWrapperComponent.new(title:, cards_block:, cards_per_row:, background_color:)
       end
     end
   end
