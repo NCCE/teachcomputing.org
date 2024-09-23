960da6b60c80e483e63df61f5c4a780a35c6cd34
Adam Wheatley <adamwheatley7@gmail.com>
2024-09-23 09:55:20 +0000

    Updates to card wrapper component to allow cards per row to be specified via Strapi Added buttons and classes to resource cards Created card wrapper and styling Created resource cards and styling

--- app/services/cms/models/resource_card.rb
+++ app/services/cms/models/resource_card.rb
@@ -1,17 +1,19 @@
 module Cms
   module Models
     class ResourceCard
-      attr_accessor :title, :icon, :colour_theme, :body_text
+      attr_accessor :title, :icon, :colour_theme, :body_text, :button_text, :button_link
 
-      def initialize(title:, icon:, colour_theme:, body_text:)
+      def initialize(title:, icon:, colour_theme:, body_text:, button_text:, button_link:)
         @title = title
         @icon = icon
         @colour_theme = colour_theme
         @body_text = body_text
+        @button_text = button_text
+        @button_link = button_link
       end
 
       def render
-        CmsResourceCardComponent.new(title:, icon:, colour_theme:, body_text:)
+        CmsResourceCardComponent.new(title:, icon:, colour_theme:, body_text:, button_text:, button_link:)
       end
     end
   end
