6ba0291228ffcdc862c541bb67d60dc014f0590a
Michael Squance <m.squance@stem.org.uk>
2024-09-24 09:24:46 +0000

    Fixing test suite issue after adding in answer_icon_block

--- app/components/cms_question_and_answer_component.rb
+++ app/components/cms_question_and_answer_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class CmsQuestionAndAnswerComponent < CmsWithAsidesComponent
-  def initialize(question:, answer:, aside_sections:)
+  def initialize(question:, answer:, aside_sections:, answer_icon_block:)
     super(aside_sections:)
     @question = question
     @answer = answer
+    @answer_icon_block = answer_icon_block
   end
 end
--- app/services/cms/providers/strapi/mocks/question_and_answer.rb
+++ app/services/cms/providers/strapi/mocks/question_and_answer.rb
@@ -7,20 +7,20 @@ module Cms
             Factories::ComponentFactory.to_question_and_answer(generate_data)
           end
 
-          def self.generate_data(aside_sections: [])
+          def self.generate_data(aside_sections: {})
             {
               question: Faker::Lorem.sentence,
               answer: RichBlocks.generate,
-              asideSections: aside_sections
+              asideSections: aside_sections,
+              answerIcons: {}
             }
           end
 
           def self.generate_raw_data
             {
               __component: "blocks.question-and-answer",
-              id: 1,
-              attributes: generate_data
-            }
+              id: 1
+            }.merge(generate_data)
           end
         end
       end
