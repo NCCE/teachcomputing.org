d6e1e6b0d9356f4620443dc7424d7610bd1f62fd
Adam Wheatley <adamwheatley7@gmail.com>
2024-09-30 09:23:54 +0000

    Refactor component Added logic for school and individual achievements in programme model Added ribbon colour based on programme and tooltip over icon

--- app/components/dashboard_certificate_component.rb
+++ app/components/dashboard_certificate_component.rb
@@ -2,11 +2,26 @@
 
 class DashboardCertificateComponent < ViewComponent::Base
   PROGRAMMES = {
-    Programmes::PrimaryCertificate => {title: "Teach primary computing", programme_type: Programme.primary_certificate},
-    Programmes::SecondaryCertificate => {title: "Teach secondary computing", programme_type: Programme.secondary_certificate},
-    Programmes::IBelong => {title: "I Belong", programme_type: Programme.i_belong},
-    Programmes::CSAccelerator => {title: "Key Stage 3 and GCSE subject knowledge", programme_type: Programme.cs_accelerator},
-    Programmes::ALevel => {title: "A level subject knowledge", programme_type: Programme.a_level}
+    Programmes::PrimaryCertificate => {
+      title: "Teach primary computing",
+      programme_type: Programme.primary_certificate
+    },
+    Programmes::SecondaryCertificate => {
+      title: "Teach secondary computing",
+      programme_type: Programme.secondary_certificate
+    },
+    Programmes::IBelong => {
+      title: "I Belong",
+      programme_type: Programme.i_belong
+    },
+    Programmes::CSAccelerator => {
+      title: "Key Stage 3 and GCSE subject knowledge",
+      programme_type: Programme.cs_accelerator
+    },
+    Programmes::ALevel => {
+      title: "A level subject knowledge",
+      programme_type: Programme.a_level
+    }
   }
 
   def initialize(certificate:, button_text:)
@@ -14,15 +29,23 @@ class DashboardCertificateComponent < ViewComponent::Base
     @button_text = button_text
   end
 
-  def certificate_data
+  def programme
     PROGRAMMES[@certificate.class]
   end
 
   def title
-    certificate_data[:title]
+    programme[:title]
   end
 
-  def programme
-    certificate_data[:programme_type]
+  def programme_type
+    programme[:programme_type]
+  end
+
+  def tool_tip_text
+    if programme_type.school_achievement?
+      "School achievement"
+    else
+      "Individual achievement"
+    end
   end
 end
