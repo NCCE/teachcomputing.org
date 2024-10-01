c054face43bbbe0c7fb1f74de0e9ecd6d541b2c6
Adam Wheatley <adamwheatley7@gmail.com>
2024-10-01 08:55:19 +0000

    Tidy up course component css and dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,13 +4,7 @@ class DashboardController < ApplicationController
   after_action :discourage_caching
 
   def show
-    @incomplete_achievements = current_user.achievements.not_in_state(:dropped, :complete).with_courses.order("created_at DESC")
-    @completed_achievements = current_user.achievements.in_state(:complete).with_courses.order("updated_at DESC")
-
     @user_achievements = current_user.achievements.with_courses.order("updated_at DESC")
-
-    user_course_info = Achiever::Course::Delegate.find_by_achiever_contact_number(current_user.stem_achiever_contact_no)
-    @user_course_info = user_course_info.select { |course| %w[enrolled attended].include?(course.attendance_status) }
 
     @enrolled_certificates = []
 
