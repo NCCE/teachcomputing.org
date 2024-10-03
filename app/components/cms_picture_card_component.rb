b22c085ef3bc289a675f4a859f43270eff36dbaa
Adam Wheatley <adamwheatley7@gmail.com>
2024-10-03 10:20:34 +0000

    Added ribbon colour options onto cards Card layout and styling Updated component factory to work with a resource card section or picture card section

--- app/components/cms_picture_card_component.rb
+++ app/components/cms_picture_card_component.rb
@@ -1,8 +1,15 @@
 class CmsPictureCardComponent < ViewComponent::Base
-  def initialize(image:, title:, body_text:, link:)
+  delegate :cms_colour_theme_class, to: :helpers
+
+  def initialize(image:, title:, body_text:, link:, colour_theme:)
     @image = image
     @title = title
     @body_text = body_text
     @link = link
+    @colour_theme = colour_theme
+  end
+
+  def ribbon_class
+    cms_colour_theme_class(@colour_theme, "bottom") if @colour_theme
   end
 end
