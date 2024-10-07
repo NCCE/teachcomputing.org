b2090ef26be577772ebdb0e8451311bdf462e6af
Adam Wheatley <adamwheatley7@gmail.com>
2024-10-07 09:08:49 +0000

    Created colour scheme mock Updated picture card mocks to allow you to pass in number of cards, cards per row and colour theme Updated picture card previews

--- app/services/cms/providers/strapi/mocks/picture_card_section.rb
+++ app/services/cms/providers/strapi/mocks/picture_card_section.rb
@@ -3,38 +3,46 @@ module Cms
     module Strapi
       module Mocks
         class PictureCardSection
-          def self.as_model
-            Factories::ComponentFactory.process_component(generate_raw_data)
+          def self.as_model(number_of_cards: 3, cards_per_row: 3, colour_scheme: nil)
+            Factories::ComponentFactory.process_component(generate_raw_data(
+              number_of_cards: number_of_cards,
+              cards_per_row: cards_per_row,
+              colour_scheme: colour_scheme
+            ))
           end
 
-          def self.generate_data
+          def self.generate_data(number_of_cards: 3, cards_per_row: 3, colour_scheme: nil)
             {
               sectionTitle: Faker::Lorem.words(number: 5),
-              pictureCard: Array.new(3) { PictureCard.generate_data },
-              cardsPerRow: 3,
+              pictureCard: Array.new(number_of_cards) { PictureCard.generate_data(colour_scheme: colour_scheme) },
+              cardsPerRow: cards_per_row,
               colourTheme: nil
             }
           end
 
-          def self.generate_raw_data
+          def self.generate_raw_data(number_of_cards: 3, cards_per_row: 3, colour_scheme: nil)
             {
               __component: "blocks.picture-card-section",
               id: 1
-            }.merge(generate_data)
+            }.merge(generate_data(
+              number_of_cards: number_of_cards,
+              cards_per_row: cards_per_row,
+              colour_scheme: colour_scheme
+            ))
           end
         end
 
         class PictureCard
-          def self.as_model
-            Factories::ComponentFactory.picture_card_block(generate_data)
+          def self.as_model(colour_scheme: nil)
+            Factories::ComponentFactory.picture_card_block(generate_data(colour_scheme: colour_scheme))
           end
 
-          def self.generate_data
+          def self.generate_data(colour_scheme: nil)
             {
               title: Faker::Lorem.words(number: 5),
               image: {data: Image.generate_raw_data},
               bodyText: RichBlocks.generate,
-              colourTheme: nil,
+              colourTheme: ColourScheme.generate_data(name: colour_scheme),
               link: Faker::Internet.url
             }
           end
