806383642000e4601625c15a579724736cc87371
Michael Squance <m.squance@stem.org.uk>
2024-10-07 11:12:30 +0000

    Adding in border bottom for banner Adding background colour for text blocks Adding in check for valid columns in grid row, and then fixing my invalid column elsewhere Updating tests

--- app/services/cms/dynamic_components/full_width_text.rb
+++ app/services/cms/dynamic_components/full_width_text.rb
@@ -1,14 +1,15 @@
 module Cms
   module DynamicComponents
     class FullWidthText
-      attr_accessor :blocks
+      attr_accessor :blocks, :background_color
 
-      def initialize(blocks:)
+      def initialize(blocks:, background_color:)
         @blocks = blocks
+        @background_color = background_color
       end
 
       def render
-        CmsFullWidthTextBlockComponent.new(blocks:)
+        CmsFullWidthTextBlockComponent.new(blocks:, background_color:)
       end
     end
   end
