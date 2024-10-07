5689c61caaa090ae91e7139218e30b8c2f5f6f48
Adam Wheatley <adamwheatley7@gmail.com>
2024-10-07 10:56:50 +0000

    Fix to CSS on resource card to keep image size Updated resource card mock Updated previews for cards

---
+++ app/services/cms/providers/strapi/mocks/resource_card_section.rb
@@ -0,0 +1,94 @@
+module Cms
+  module Providers
+    module Strapi
+      module Mocks
+        class ResourceCardSection
+          def self.as_model(
+            number_of_cards: 3,
+            cards_per_row: 3,
+            colour_scheme: nil,
+            button_text: nil,
+            button_link: nil,
+            icon: nil
+          )
+
+            Factories::ComponentFactory.process_component(generate_raw_data(
+              number_of_cards: number_of_cards,
+              cards_per_row: cards_per_row,
+              colour_scheme: colour_scheme,
+              button_text: button_text,
+              button_link: button_link,
+              icon: icon
+            ))
+          end
+
+          def self.generate_data(
+            number_of_cards: 3,
+            cards_per_row: 3,
+            colour_scheme: nil,
+            button_text: nil,
+            button_link: nil,
+            icon: nil
+          )
+            {
+              sectionTitle: Faker::Lorem.words(number: 5),
+              resourceCard: Array.new(number_of_cards) {
+                ResourceCard.generate_data(
+                  colour_scheme: colour_scheme,
+                  button_text: button_text,
+                  button_link: button_link,
+                  icon: icon
+                )
+              },
+              cardsPerRow: cards_per_row,
+              colourTheme: nil
+            }
+          end
+
+          def self.generate_raw_data(
+            number_of_cards: 3,
+            cards_per_row: 3,
+            colour_scheme: nil,
+            button_text: nil,
+            button_link: nil,
+            icon: nil
+          )
+            {
+              __component: "blocks.resource-card-section",
+              id: 1
+            }.merge(generate_data(
+              number_of_cards: number_of_cards,
+              cards_per_row: cards_per_row,
+              colour_scheme: colour_scheme,
+              button_text: button_text,
+              button_link: button_link,
+              icon: icon
+            ))
+          end
+        end
+
+        class ResourceCard
+          def self.as_model(colour_scheme: nil, button_text: nil, button_link: nil, icon: nil)
+            Factories::ComponentFactory.resource_card_block(generate_data(
+              colour_scheme: colour_scheme,
+              button_text: button_text,
+              button_link: button_link,
+              icon: icon
+            ))
+          end
+
+          def self.generate_data(colour_scheme: nil, button_text: nil, button_link: nil, icon: nil)
+            {
+              title: Faker::Lorem.words(number: 5),
+              icon: icon,
+              bodyText: RichBlocks.generate,
+              colourTheme: {data: ColourScheme.generate_data(name: colour_scheme)},
+              buttonText: button_text,
+              buttonLink: button_link
+            }
+          end
+        end
+      end
+    end
+  end
+end
--- previews/components/cms_resource_card_component_preview.rb
+++ previews/components/cms_resource_card_component_preview.rb
@@ -2,13 +2,47 @@ class CmsResourceCardComponentPreview < ViewComponent::Preview
   layout "full-width"
 
   def default
-    render(CmsResourceCardComponent.new(
-      title: "Page title",
-      body_text: Cms::Mocks::RichBlocks.generate,
-      icon: Cms::Mocks::Image.as_model,
-      colour_theme: "standard",
-      button_text: Faker::Lorem.word,
-      button_link: Faker::Internet.url
+    card_section = Cms::Mocks::ResourceCardSection.as_model(
+      number_of_cards: 3,
+      button_text: "Click here",
+      button_link: "https://www.example.com/"
+    )
+
+    render(CmsCardWrapperComponent.new(
+      title: card_section.title,
+      cards_block: card_section.cards_block,
+      background_color: "light-grey",
+      cards_per_row: 3
+    ))
+  end
+
+  def two_cards_per_row_no_button
+    card_section = Cms::Mocks::ResourceCardSection.as_model(
+      number_of_cards: 2
+    )
+
+    render(CmsCardWrapperComponent.new(
+      title: card_section.title,
+      cards_block: card_section.cards_block,
+      background_color: card_section.background_color,
+      cards_per_row: 2
+    ))
+  end
+
+  def with_colour_theme_and_icon
+    card_section = Cms::Mocks::ResourceCardSection.as_model(
+      number_of_cards: 3,
+      colour_scheme: "standard",
+      button_text: Faker::Lorem.words(number: 2),
+      button_link: Faker::Internet.url,
+      icon: {data: Cms::Mocks::Image.generate_raw_data}
+    )
+
+    render(CmsCardWrapperComponent.new(
+      title: card_section.title,
+      cards_block: card_section.cards_block,
+      background_color: nil,
+      cards_per_row: 3
     ))
   end
 end
