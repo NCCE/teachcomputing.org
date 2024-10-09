3a9fb62a5de135d20a93502287accf488d0109aa
Michael Squance <m.squance@stem.org.uk>
2024-10-09 15:55:52 +0000

    Fixing broken test cases

--- spec/views/courses/programme_courses.html_spec.rb
+++ spec/views/courses/programme_courses.html_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 
 RSpec.describe("courses/programme_courses", type: :view) do
+  let!(:programme) { create(:primary_certificate) }
+  let(:title) { "Teach primary computing certificate courses" }
+
   let(:courses) do
     build_list(
       :achiever_course_template,
@@ -20,14 +23,16 @@ RSpec.describe("courses/programme_courses", type: :view) do
 
   context "with a hub with no courses" do
     before do
+      @programme = programme
       @certificate_filter = "primary-certificate"
       @filter_params = {}
       @course_filter = filter_stub
+      @title = title
       render
     end
 
     it "has the clear filters link" do
-      expect(rendered).to have_css(".govuk-heading-l", text: "Teach primary computing certificate courses")
+      expect(rendered).to have_css(".govuk-heading-l", text: title)
     end
   end
 end
