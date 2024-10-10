4c653fdaa3a02115151d09b5c9c271f358fbe9c0
Michael Squance <m.squance@stem.org.uk>
2024-10-10 17:45:17 +0000

    Updating cards to new mock system Moving file to more appropriately named file_link Adding mock for text with asides

---
+++ app/services/cms/providers/strapi/mocks/file_link.rb
@@ -0,0 +1,20 @@
+module Cms
+  module Providers
+    module Strapi
+      module Mocks
+        class FileLink < StrapiMock
+          strapi_component "content-blocks.file-link"
+
+          attribute(:file) { {data: File.generate_raw_data} }
+        end
+
+        class File < StrapiMock
+          attribute(:url) { Faker::Internet.slug }
+          attribute(:filename) { "test.pdf" }
+          attribute(:size) { 50 }
+          attribute(:updatedAt) { Faker::Date.backward.to_s }
+        end
+      end
+    end
+  end
+end
