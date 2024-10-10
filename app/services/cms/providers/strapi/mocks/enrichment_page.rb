46001d12f26c26ba1e171d1d3a56aae53e9989b6
Michael Squance <m.squance@stem.org.uk>
2024-10-10 15:07:28 +0000

    Migrating enrichment to new mock system

--- app/services/cms/providers/strapi/mocks/enrichment_page.rb
+++ app/services/cms/providers/strapi/mocks/enrichment_page.rb
@@ -2,90 +2,39 @@ module Cms
   module Providers
     module Strapi
       module Mocks
-        class EnrichmentPage
-          def self.generate_data(slug:)
-            {
-              pageTitle: PageTitle.generate_raw_data,
-              seo: Seo.generate_raw_data,
-              slug:,
-              featuredSectionTitle: Faker::Lorem.sentence,
-              allSectionTitle: Faker::Lorem.sentence,
-              typeFilterPlaceholder: Faker::Lorem.sentence,
-              termFilterPlaceholder: Faker::Lorem.sentence,
-              ageGroupFilterPlaceholder: Faker::Lorem.sentence,
-              content: [
-                Cms::Mocks::FullWidthBanner.generate_raw_data
-              ],
-              enrichments: {
-                data: Array.new(5) { Enrichment.generate_raw_data }
-              },
-              publishedAt: Faker::Date.backward,
-              createdAt: Faker::Date.backward,
-              updatedAt: Faker::Date.backward
-            }
-          end
-
-          def self.generate_raw_data(slug:)
-            {
-              id: Faker::Number.number,
-              attributes: generate_data(slug:)
-            }
-          end
-        end
-      end
-
-      class Enrichment
-        def self.generate_data
-          {
-            i_belong: true,
-            link: nil,
-            featured: false,
-            rich_title: Mocks::RichBlocks.generate_data,
-            rich_details: Mocks::RichBlocks.generate_data,
-            terms: {data: Array.new(1) { EnrichmentCategory.generate_raw_data }},
-            age_groups: {data: Array.new(2) { EnrichmentCategory.generate_raw_data }},
-            type: {data: EnrichmentType.generate_raw_data},
-            partner_icon: Mocks::Image.generate_raw_data
-          }
+        class EnrichmentPage < StrapiMock
+          attribute(:pageTitle) { PageTitle.generate_raw_data }
+          attribute(:seo) { Seo.generate_raw_data }
+          attribute(:slug) { Faker::Internet.slug }
+          attribute(:featuredSectionTitle) { Faker::Lorem.sentence }
+          attribute(:allSectionTitle) { Faker::Lorem.sentence }
+          attribute(:typeFilterPlaceholder) { Faker::Lorem.sentence }
+          attribute(:termFilterPlaceholder) { Faker::Lorem.sentence }
+          attribute(:ageGroupFilterPlaceholder) { Faker::Lorem.sentence }
+          attribute(:content) { [Cms::Mocks::FullWidthBanner.generate_raw_data] }
+          attribute(:enrichments) { {data: Array.new(5) { Enrichment.generate_raw_data }} }
         end
 
-        def self.generate_raw_data
-          {
-            id: Faker::Number.number,
-            attributes: generate_data
-          }
+        class Enrichment < StrapiMock
+          attribute(:i_belong) { true }
+          attribute(:link) { nil }
+          attribute(:featured) { false }
+          attribute(:rich_title) { Mocks::RichBlocks.generate_data }
+          attribute(:rich_details) { Mocks::RichBlocks.generate_data }
+          attribute(:terms) { {data: Array.new(1) { EnrichmentCategory.generate_raw_data }} }
+          attribute(:age_groups) { {data: Array.new(2) { EnrichmentCategory.generate_raw_data }} }
+          attribute(:type) { {data: EnrichmentType.generate_raw_data} }
+          attribute(:partner_icon) { Mocks::Image.generate_raw_data }
         end
-      end
-    end
 
-    class EnrichmentCategory # For terms and age groups
-      def self.generate_data
-        {
-          name: Faker::Lorem.word
-        }
-      end
+        class EnrichmentCategory < StrapiMock # For terms and age groups
+          attribute(:name) { Faker::Lorem.word }
+        end
 
-      def self.generate_raw_data
-        {
-          id: Faker::Number.number,
-          attributes: generate_data
-        }
-      end
-    end
-
-    class EnrichmentType
-      def self.generate_data
-        {
-          name: Faker::Lorem.word,
-          icon: Mocks::Image.generate_raw_data
-        }
-      end
-
-      def self.generate_raw_data
-        {
-          id: Faker::Number.number,
-          attributes: generate_data
-        }
+        class EnrichmentType < StrapiMock # For terms and age groups
+          attribute(:name) { Faker::Lorem.word }
+          attribute(:name) { Mocks::Image.generate_raw_data }
+        end
       end
     end
   end
