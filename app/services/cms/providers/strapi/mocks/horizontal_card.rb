a7d7619ab2ecb9adf0bee095a85fe14d86a10edc
Michael Squance <m.squance@stem.org.uk>
2024-10-10 14:07:39 +0000

    Creating new StrapiMock system that centralises strapi data generation through a shared model

--- app/services/cms/providers/strapi/mocks/horizontal_card.rb
+++ app/services/cms/providers/strapi/mocks/horizontal_card.rb
@@ -2,28 +2,15 @@ module Cms
   module Providers
     module Strapi
       module Mocks
-        class HorizontalCard
-          def self.as_model
-            Factories::ComponentFactory.to_horizontal_card(generate_data)
-          end
+        class HorizontalCard < StrapiMock
+          strapi_component "blocks.horizontal-card"
 
-          def self.generate_data
-            {
-              title: Faker::Lorem.words(number: 5),
-              bodyText: RichBlocks.generate_data,
-              image: Image.generate_data,
-              imageLink: nil,
-              colourTheme: nil,
-              iconBlock: IconBlocks.generate_data
-            }
-          end
-
-          def self.generate_raw_data
-            {
-              __component: "blocks.horizontal-card",
-              id: 1
-            }.merge(generate_data)
-          end
+          attribute(:title) { Faker::Lorem.words(number: 5) }
+          attribute(:bodyText) { RichBlocks.generate_data }
+          attribute(:image) { Image.generate_data }
+          attribute(:imageLink) { nil }
+          attribute(:colourTheme) { nil }
+          attribute(:iconBlock) { IconBlocks.generate_data }
         end
       end
     end
--- app/services/cms/providers/strapi/mocks/page_title.rb
+++ app/services/cms/providers/strapi/mocks/page_title.rb
@@ -2,26 +2,9 @@ module Cms
   module Providers
     module Strapi
       module Mocks
-        class PageTitle
-          def self.as_model(key: nil)
-            Factories::ModelFactory.process_model(
-              {model: Models::PageTitle, key:},
-              generate_data
-            )
-          end
-
-          def self.generate_data(intro_text: nil)
-            {
-              title: Faker::Lorem.words(number: 5),
-              introText: intro_text
-            }
-          end
-
-          def self.generate_raw_data
-            {
-              id: Faker::Number.number
-            }.merge(generate_data)
-          end
+        class PageTitle < StrapiMock
+          attribute(:title) { Faker::Lorem.words(number: 5) }
+          attribute(:introText) { nil }
         end
       end
     end
