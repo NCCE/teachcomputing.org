aa8a2f9d9ccea65bab7a39ade3143ec7912689ec
Adam Wheatley <adamwheatley7@gmail.com>
2024-10-11 14:55:08 +0000

    Updated the course component to show course details such as address, date etc. Using global programme tags in component now

--- app/components/dashboard_course_component.rb
+++ app/components/dashboard_course_component.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 class DashboardCourseComponent < ViewComponent::Base
-  delegate :course_icon_class, :course_category, to: :helpers
+  delegate :course_icon_class,
+    :course_category,
+    :stem_course_details,
+    :get_date_string,
+    :course_start_date,
+    :user_course_info,
+    :course_start_date,
+    :start_date,
+    :online_course_availability,
+    :started?,
+    to: :helpers
 
-  def initialize(achievement:)
+  def initialize(achievement:, user_course_info:)
     @achievement = achievement
+    @user_course_info = user_course_info
   end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,6 +7,9 @@ class DashboardController < ApplicationController
     @incomplete_achievements = incomplete_achievements
     @completed_achievements = completed_achievements
     @enrolled_certificates, @unenrolled_certificates = enrolments
+
+    user_course_info = Achiever::Course::Delegate.find_by_achiever_contact_number(current_user.stem_achiever_contact_no)
+    @user_course_info = user_course_info.select { |course| %w[enrolled attended].include?(course.attendance_status) }
   end
 
   private
