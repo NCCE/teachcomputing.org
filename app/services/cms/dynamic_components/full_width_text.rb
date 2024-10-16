a7cd80c87012ef4c54508a2d105f8b21151ebb1f
Michael Squance <m.squance@stem.org.uk>
2024-10-16 10:14:17 +0000

    Adding border bottom option to full width text

--- app/services/cms/dynamic_components/full_width_text.rb
+++ app/services/cms/dynamic_components/full_width_text.rb
@@ -1,15 +1,16 @@
 module Cms
   module DynamicComponents
     class FullWidthText
-      attr_accessor :blocks, :background_color
+      attr_accessor :blocks, :background_color, :show_border_bottom
 
-      def initialize(blocks:, background_color:)
+      def initialize(blocks:, background_color:, show_border_bottom:)
         @blocks = blocks
         @background_color = background_color
+        @show_border_bottom = show_border_bottom
       end
 
       def render
-        CmsFullWidthTextBlockComponent.new(blocks:, background_color:)
+        CmsFullWidthTextBlockComponent.new(blocks:, background_color:, show_border_bottom:)
       end
     end
   end
