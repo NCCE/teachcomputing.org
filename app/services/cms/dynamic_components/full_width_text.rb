1828e28b357874dc82cfdc484be2d460c2fa3da1
Michael Squance <m.squance@stem.org.uk>
2024-10-16 10:25:38 +0000

    Fixing typo in parameter name

--- app/services/cms/dynamic_components/full_width_text.rb
+++ app/services/cms/dynamic_components/full_width_text.rb
@@ -1,16 +1,16 @@
 module Cms
   module DynamicComponents
     class FullWidthText
-      attr_accessor :blocks, :background_color, :show_border_bottom
+      attr_accessor :blocks, :background_color, :show_bottom_border
 
-      def initialize(blocks:, background_color:, show_border_bottom:)
+      def initialize(blocks:, background_color:, show_bottom_border:)
         @blocks = blocks
         @background_color = background_color
-        @show_border_bottom = show_border_bottom
+        @show_bottom_border = show_bottom_border
       end
 
       def render
-        CmsFullWidthTextBlockComponent.new(blocks:, background_color:, show_border_bottom:)
+        CmsFullWidthTextBlockComponent.new(blocks:, background_color:, show_bottom_border:)
       end
     end
   end
