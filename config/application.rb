dcd560efe86fb581bcb4f023e739c589e7fd1aeb
Michael Squance <m.squance@stem.org.uk>
2024-10-22 15:07:21 +0000

    Going to do redirect to /v2/logout

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,7 @@ module TeachComputing
     # Auth Settings
     config.bypass_oauth = ENV["BYPASS_OAUTH"] == "true"
     config.stem_account_site = ENV["STEM_ACCOUNT_SITE"]
+    config.auth0_domain = ENV["STEM_AUTH0_DOMAIN"]
     config.stem_credentials_access_token = ENV["STEM_CREDENTIALS_ACCESS_TOKEN_KEY"]
     config.stem_credentials_refresh_token = ENV["STEM_CREDENTIALS_REFRESH_TOKEN_KEY"]
 
