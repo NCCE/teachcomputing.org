ef1521e3c2df5d58d5332a619c5da4cdd8eb011b
Adam Wheatley <adamwheatley7@gmail.com>
2024-10-22 09:19:17 +0000

    Updated unit show page to iterate through url_links and pass into external link component Added graphql definitions for url links Added file_type to external link card component and tweaked some css

--- app/components/external_link_card_component.rb
+++ app/components/external_link_card_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class ExternalLinkCardComponent < ViewComponent::Base
-  def initialize(external_link_card:, title: nil, link: nil)
+  def initialize(external_link_card:, title: nil, link: nil, file_type: nil)
     @title = title || external_link_card[:title]
     @link = link || external_link_card[:link]
+    @file_type = file_type || external_link_card[:file_type]
   end
 
   def render?
