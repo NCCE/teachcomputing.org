985bf67367ab14ba3cda56ff48298bd755e76274
Michael Squance <m.squance@stem.org.uk>
2024-10-23 15:48:21 +0000

    Adding numeric card from strapi

--- app/services/cms/models/text_block.rb
+++ app/services/cms/models/text_block.rb
@@ -3,13 +3,14 @@ module Cms
     class TextBlock
       attr_accessor :blocks, :with_wrapper
 
-      def initialize(blocks:, with_wrapper: false)
+      def initialize(blocks:, with_wrapper: false, **options)
         @blocks = blocks
         @with_wrapper = with_wrapper
+        @options = options
       end
 
       def render
-        CmsRichTextBlockComponent.new(blocks:, with_wrapper:)
+        CmsRichTextBlockComponent.new(blocks:, with_wrapper:, **@options)
       end
     end
   end
