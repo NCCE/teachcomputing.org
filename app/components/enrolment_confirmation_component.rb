53fa753b2283620de81a6252875cc2c35b9ac849
Adam Wheatley <adamwheatley7@gmail.com>
2024-10-24 12:48:51 +0000

    Added method to choose from full width button to calculated size in component Refactor component code as no need to check for pathways

--- app/components/enrolment_confirmation_component.rb
+++ app/components/enrolment_confirmation_component.rb
@@ -3,9 +3,16 @@
 class EnrolmentConfirmationComponent < ViewComponent::Base
   delegate :auth_url, to: :helpers
 
-  def initialize(programme:, current_user:, enrol_text: nil)
+  def initialize(programme:, current_user:, full_width: true, enrol_button_text: nil)
     @current_user = current_user
     @programme = programme
-    @enrol_text = enrol_text
+    @full_width = full_width
+    @enrol_button_text = enrol_button_text
+  end
+
+  def button_classes
+    classes = ["govuk-button button"]
+    classes << "button--full-width" if @full_width
+    classes
   end
 end
