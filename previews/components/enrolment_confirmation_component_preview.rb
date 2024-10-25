c44497821e7830a202f822c354f917d2c8801fbd
Adam Wheatley <adamwheatley7@gmail.com>
2024-10-25 07:41:02 +0000

    Added tests on programme and primarty certiciate models for enrolment_confirmation_required method Created previews for component

---
+++ previews/components/enrolment_confirmation_component_preview.rb
@@ -0,0 +1,25 @@
+class EnrolmentConfirmationComponentPreview < ViewComponent::Preview
+  def default
+    render(EnrolmentConfirmationComponent.new(
+      current_user: User.last.presence,
+      programme: Programme.primary_certificate,
+      full_width: false
+    ))
+  end
+
+  def full_width
+    render(EnrolmentConfirmationComponent.new(
+      current_user: User.last.presence,
+      programme: Programme.primary_certificate
+    ))
+  end
+
+  def custom_button_text
+    render(EnrolmentConfirmationComponent.new(
+      current_user: User.last.presence,
+      programme: Programme.primary_certificate,
+      full_width: false,
+      enrol_button_text: "Click here to enrol"
+    ))
+  end
+end
