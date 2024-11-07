295c442e8eac79d7e9df3c7b528f5cd69af519aa
Michael Squance <m.squance@stem.org.uk>
2024-11-07 13:33:32 +0000

    Moved email content to use a dynamic zone, and creating new email specific models to handle the fact we need to render text and html Setting up methods to get the latest CPD completed, and creating mail merge for that. Setting up new course lists components. Still need to sort out the logic for not showing sections that contain completed courses

---
+++ app/services/cms/email_components/cta.rb
@@ -0,0 +1,33 @@
+module Cms
+  module EmailComponents
+    class Cta
+      attr_accessor :text, :link
+
+      def initialize(text:, link:)
+        @text = text
+        @link = link
+      end
+
+      def render(email_template, user)
+        CmsNcceButtonComponent.new(title: @text, link:)
+      end
+
+      def render_text(email_template, user)
+        CtaText.new(@text, @link)
+      end
+    end
+
+    class CtaText
+      def initialize(text, link)
+        @text = text
+        @link = link
+      end
+
+      def render_in(view_context)
+        view_context.render inline: "#{@text} (#{@link})\n"
+      end
+
+      def format = :text
+    end
+  end
+end
