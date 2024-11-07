8b8cd2f7619bacbba89931eb583ce4c903185176
Adam Wheatley <adamwheatley7@gmail.com>
2024-11-07 12:04:41 +0000

    Updating dashboard programme activity group section component with strapi asides

--- app/components/dashboard_programme_activity_group_section_component.rb
+++ app/components/dashboard_programme_activity_group_section_component.rb
@@ -1,20 +1,11 @@
 # frozen_string_literal: true
 
-class DashboardProgrammeActivityGroupSectionComponent < ViewComponent::Base
+class DashboardProgrammeActivityGroupSectionComponent < CmsWithAsidesComponent
   def initialize(anchor_id:, title: nil, completed: false, aside_slug: nil, subtitle: nil)
+    super(aside_sections: [{slug: aside_slug}])
     @title = title
-    @aside_slug = aside_slug
     @subtitle = subtitle
     @completed = completed
     @anchor_id = anchor_id
-
-    @aside_model = if @aside_slug
-      begin
-        Cms::Collections::AsideSection.get(@aside_slug)
-      rescue ActiveRecord::RecordNotFound => e
-        raise(e) if Rails.env.development?
-        nil
-      end
-    end
   end
 end
