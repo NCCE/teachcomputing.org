4257014da57a8e6eb689c4d996f3423ba01b3039
Michael Squance <m.squance@stem.org.uk>
2024-11-07 13:33:32 +0000

    Moved email content to use a dynamic zone, and creating new email specific models to handle the fact we need to render text and html Setting up methods to get the latest CPD completed, and creating mail merge for that. Setting up new course lists components. Still need to sort out the logic for not showing sections that contain completed courses

---
+++ app/components/cms_email_course_list_component.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class CmsEmailCourseListComponent < ViewComponent::Base
+  erb_template <<~ERB
+    <table>
+      <% if @section_title %>
+        <tr><td><h2><%= @section_title %></h2></td></tr>
+      <% end %>
+      <% @course_list.each do |course| %>
+        <tr>
+          <td>
+            <%= link_to display_name(course), course_link(course) %>
+          </td>
+        </tr>
+      <% end %>
+    </table>
+  ERB
+
+  def initialize(course_list:, section_title:)
+    @course_list = course_list
+    @section_title = section_title
+  end
+
+  def display_name(course)
+    course.display_name.presence || course.activity.title
+  end
+
+  def course_link(course)
+    course_url(id: course.activity.stem_activity_code, name: course.activity.title.parameterize)
+  end
+end
