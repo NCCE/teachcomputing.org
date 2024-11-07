5a0a20a292ed2a811d43c43eabe692496b199702
Michael Squance <m.squance@stem.org.uk>
2024-11-07 13:33:32 +0000

    Moved email content to use a dynamic zone, and creating new email specific models to handle the fact we need to render text and html Setting up methods to get the latest CPD completed, and creating mail merge for that. Setting up new course lists components. Still need to sort out the logic for not showing sections that contain completed courses

--- app/services/cms/models/email_template.rb
+++ app/services/cms/models/email_template.rb
@@ -1,17 +1,18 @@
 module Cms
   module Models
     class EmailTemplate
-      attr_accessor :slug, :subject, :ctas
+      attr_accessor :slug, :subject, :email_content, :programme
 
-      def initialize(slug:, subject:, email_content:, ctas:)
+      def initialize(slug:, subject:, email_content:, programme_slug:)
         @slug = slug
         @subject = subject
         @email_content = email_content
-        @ctas = ctas
+        @programme_slug = programme_slug
+        @programme = Programme.find_by(slug: @programme_slug)
       end
 
-      def email_content(user)
-        content = @email_content.deep_dup
+      def process_blocks(blocks, user)
+        content = blocks.deep_dup
         content.each { search_for_text(_1, user) }
         content
       end
@@ -30,11 +31,26 @@ module Cms
         merges = [
           ["{first_name}", user.first_name]
         ]
+        achievements = user.sorted_completed_cpd_achievements_by(programme: @programme)
+        if achievements.any?
+          achievement = achievements.last
+          merges += [
+            ["{last_cpd_completed_ago}", time_diff_words(achievement.updated_at)],
+            ["{last_cpd_title}", achievement.activity.title]
+          ]
+        end
         merges.each { text.gsub!(_1[0], _1[1]) }
         text
       end
 
-      def render
+      def time_diff_words(date)
+        diff = DateTime.now.to_i - date.to_i
+        months = diff / (60 * 60 * 24 * 30)
+        if months >= 12
+          "#{months / 12} #{"year".pluralize(months / 12)}"
+        else
+          "#{months} #{"month".pluralize(months)}"
+        end
       end
     end
   end
