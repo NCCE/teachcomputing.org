bdb74757299a63a2cf69c20a3a6524983dca024f
Adam Wheatley <adamwheatley7@gmail.com>
2024-11-07 15:55:21 +0000

    Added course duration and icons

--- app/components/user_programme_course_bookings_with_asides_component.rb
+++ app/components/user_programme_course_bookings_with_asides_component.rb
@@ -6,4 +6,21 @@ class UserProgrammeCourseBookingsWithAsidesComponent < CmsWithAsidesComponent
     @courses = courses
     @programme = programme
   end
+
+  def course_icon_class(activity)
+    case activity.category.to_sym
+    when :online
+      "icon-online"
+    else
+      if activity.remote_delivered_cpd
+        "icon-remote"
+      else
+        "icon-map-pin"
+      end
+    end
+  end
+
+  def course_duration(course)
+    Achiever::Course::Template.maybe_find_by_activity_code(course.activity.stem_activity_code) if course.activity.stem_activity_code.present?
+  end
 end
