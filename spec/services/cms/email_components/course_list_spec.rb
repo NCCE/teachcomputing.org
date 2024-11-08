b3f6394bb510b86fb48d83ccea32e9a16ff20744
Michael Squance <m.squance@stem.org.uk>
2024-11-08 16:27:37 +0000

    Creating suite of tests for the new cms email components the new email render

---
+++ spec/services/cms/email_components/course_list_spec.rb
@@ -0,0 +1,124 @@
+require "rails_helper"
+
+RSpec.describe Cms::EmailComponents::CourseList do
+  let(:user) { create(:user) }
+  let(:programme) { create(:primary_certificate) }
+  let(:email_template) {
+    Cms::Models::EmailTemplate.new(
+      slug: "test",
+      subject: "Test email",
+      email_content: Cms::Mocks::RichBlocks.generate_data,
+      programme_slug: programme.slug
+    )
+  }
+  let!(:activity1) { create(:activity, stem_activity_code: "CP123", programmes: [programme]) }
+  let!(:activity2) { create(:activity, stem_activity_code: "CP223", programmes: [programme]) }
+  let!(:sub_activity) { create(:activity, stem_activity_code: "CP323", programmes: [programme]) }
+  let(:courses) {
+    [
+      Cms::Mocks::EmailComponents::Course.generate_data(activity_code: "CP123"),
+      Cms::Mocks::EmailComponents::Course.generate_data(activity_code: "CP223")
+    ]
+  }
+  let(:courses_with_sub) {
+    [
+      Cms::Mocks::EmailComponents::Course.generate_data(activity_code: "CP123"),
+      Cms::Mocks::EmailComponents::Course.generate_data(activity_code: "CP223"),
+      Cms::Mocks::EmailComponents::Course.generate_data(activity_code: "CP323", substitute: true)
+    ]
+  }
+
+  context "without substitutes" do
+    before do
+      @course_list = Cms::Providers::Strapi::Factories::EmailComponentFactory.process_component(Cms::Mocks::EmailComponents::CourseList.generate_raw_data(courses:))
+    end
+
+    context "with no achievements" do
+      it "should generate correct activity list" do
+        expect(@course_list.activity_list(email_template, user).count).to eq(2)
+      end
+
+      it "should render as CmsRichTextBlockComponent" do
+        expect(@course_list.render(email_template, user)).to be_a(CmsEmailCourseListComponent)
+      end
+
+      it "should render text as CmsRichTextBlockTextComponent" do
+        expect(@course_list.render_text(email_template, user)).to be_a(Cms::EmailComponents::CourseListText)
+      end
+    end
+
+    context "user has completed course" do
+      let!(:completed_achievement) {
+        create(:completed_achievement, user:, activity: activity1)
+      }
+
+      it "should generate correct activity list" do
+        activity_list = @course_list.activity_list(email_template, user)
+        expect(activity_list.count).to eq(1)
+        expect(activity_list.first.activity).to eq(activity2)
+      end
+    end
+  end
+
+  context "with substitutes" do
+    before do
+      @course_list_with_sub = Cms::Providers::Strapi::Factories::EmailComponentFactory.process_component(Cms::Mocks::EmailComponents::CourseList.generate_raw_data(courses: courses_with_sub))
+    end
+
+    context "with no achievements" do
+      it "should generate correct activity list" do
+        expect(@course_list_with_sub.activity_list(email_template, user).count).to eq(2)
+      end
+
+      it "should not include substitute" do
+        activities = @course_list_with_sub.activity_list(email_template, user).collect { _1.activity }
+        expect(activities).not_to include(sub_activity)
+      end
+    end
+
+    context "user has completed course" do
+      let!(:completed_achievement) {
+        create(:completed_achievement, user:, activity: activity1)
+      }
+
+      it "should generate correct activity list" do
+        activity_list = @course_list_with_sub.activity_list(email_template, user)
+        expect(activity_list.count).to eq(2)
+        expect(activity_list.first.activity).to eq(activity2)
+        expect(activity_list.second.activity).to eq(sub_activity)
+      end
+    end
+  end
+
+  context "with remove_on_match" do
+    before do
+      @course_list_with_remove = Cms::Providers::Strapi::Factories::EmailComponentFactory.process_component(
+        Cms::Mocks::EmailComponents::CourseList.generate_raw_data(courses:, remove_on_match: true)
+      )
+    end
+
+    context "no achievements" do
+      it "should render as CmsRichTextBlockComponent" do
+        expect(@course_list_with_remove.render(email_template, user)).to be_a(CmsEmailCourseListComponent)
+      end
+
+      it "should render text as CmsRichTextBlockTextComponent" do
+        expect(@course_list_with_remove.render_text(email_template, user)).to be_a(Cms::EmailComponents::CourseListText)
+      end
+    end
+
+    context "when user has achievement" do
+      let!(:completed_achievement) {
+        create(:completed_achievement, user:, activity: activity1)
+      }
+
+      it "should render as nil" do
+        expect(@course_list_with_remove.render(email_template, user)).to be_nil
+      end
+
+      it "should render text as nil" do
+        expect(@course_list_with_remove.render_text(email_template, user)).to be_nil
+      end
+    end
+  end
+end
