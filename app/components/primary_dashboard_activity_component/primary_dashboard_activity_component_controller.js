0322fa4c393c2cef66fdaed0db8a4d3ec4c9292d
Adam Wheatley <adamwheatley7@gmail.com>
2024-11-13 10:16:28 +0000

    Updating components to allow the user to select an activity and create an achivement

---
+++ app/components/primary_dashboard_activity_component/primary_dashboard_activity_component_controller.js
@@ -0,0 +1,39 @@
+import ApplicationController from "../../webpacker/javascript/controllers/application_controller"
+
+export default class extends ApplicationController {
+  static values = {
+    createPath: String,
+    updatePath: String,
+    achievementId: String,
+    activityId: String,
+  }
+
+  static targets = ["activityDetails", "activity"];
+
+  connect(){
+  }
+
+  selectActivity(event) {
+    this.activityIdValue = event.target.value;
+  }
+
+  submitActivitySelection(event) {
+    event.preventDefault()
+
+    fetch(this.createPathValue, {
+        method: 'POST',
+        headers: {
+            'X-CSRF-Token': document.querySelector('meta[name=csrf-token]').content,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            achievement: {
+                activity_id: this.activityIdValue
+            }
+        })
+    })
+    .then(() => {
+        location.reload()
+    })
+  }
+}
