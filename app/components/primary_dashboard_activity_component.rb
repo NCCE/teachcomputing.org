5b22f295d7094163e31c81c8ee2cea6dcb5cd183
Adam Wheatley <adamwheatley7@gmail.com>
2024-11-13 10:16:28 +0000

    Updating components to allow the user to select an activity and create an achivement

--- app/components/primary_dashboard_activity_component.rb
+++ app/components/primary_dashboard_activity_component.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class PrimaryDashboardActivityComponent < CmsWithAsidesComponent
-  def initialize(programme_activities:)
+  def initialize(programme_activities:, current_user:)
     @programme_activities = programme_activities
+    @current_user = current_user
+  end
+
+  def user_programme_activities
+    user_activity_ids = @current_user.achievements.pluck(:activity_id)
+
+    @programme_activities
+      .where(activity_id: user_activity_ids)
+      .includes(:activity)
   end
 end
