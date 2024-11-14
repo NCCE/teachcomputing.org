1733c4d7ce3fea14c9e125eb00a8979c17a43312
Michael Squance <m.squance@stem.org.uk>
2024-11-14 10:03:30 +0000

    Adding faker to searchable page factory

--- spec/factories/searchable_pages/cms_web_page.rb
+++ spec/factories/searchable_pages/cms_web_page.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :searchable_pages_cms_web_page, class: "SearchablePages::CmsWebPage" do
     slug { Random.uuid }
-    title { "I'm a title" }
-    excerpt { "I'm a description" }
+    title { Faker::Lorem.sentence }
+    excerpt { Faker::Lorem.paragraph }
     published_at { DateTime.now }
   end
 end
