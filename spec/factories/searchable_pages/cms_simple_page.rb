074cf6fe21d0476d97632284d4e75c2f18dade7c
Michael Squance <m.squance@stem.org.uk>
2024-11-20 11:56:39 +0000

    Remove ghost code. Also removing references to the old Simple Page model from Strapi

--- spec/factories/searchable_pages/cms_simple_page.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :searchable_pages_cms_simple_page, class: "SearchablePages::CmsSimplePage" do
-    slug { Random.uuid }
-    title { "I'm a title" }
-    excerpt { "I'm a description" }
-    published_at { DateTime.now }
-  end
-end
