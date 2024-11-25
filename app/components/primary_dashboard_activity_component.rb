edec90f3655a5561ebd6d96c4f68a6286716d9de
Adam Wheatley <adamwheatley7@gmail.com>
2024-11-25 11:39:50 +0000

    Added tick icon for completed activities Logic for completed activity section Adding complete and incomplete activities into the component

--- app/components/primary_dashboard_activity_component.rb
+++ app/components/primary_dashboard_activity_component.rb
@@ -1,17 +1,26 @@
 # frozen_string_literal: true
 
 class PrimaryDashboardActivityComponent < CmsWithAsidesComponent
-  def initialize(programme_activities:, current_user:)
+  def initialize(programme_activities:, current_user:, programme_activity_group:)
     @programme_activities = programme_activities
     @current_user = current_user
+    @programme_activity_group = programme_activity_group
   end
 
   def user_programme_activities
-    user_activity_ids = @current_user.achievements.pluck(:activity_id)
+    user_achievements = @current_user.achievements
 
-    @programme_activities
-      .where(activity_id: user_activity_ids)
-      .includes(:activity)
+    complete_activity_ids = user_achievements.select { |a| a.complete? }.map(&:activity_id)
+    incomplete_activity_ids = user_achievements.select { |a| !a.complete? }.map(&:activity_id)
+
+    {
+      complete: @programme_activities
+        .where(activity_id: complete_activity_ids)
+        .includes(:activity),
+      incomplete: @programme_activities
+        .where(activity_id: incomplete_activity_ids)
+        .includes(:activity)
+    }
   end
 
   def heading_text
