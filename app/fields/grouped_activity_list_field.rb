09d609a13db6ec20eaa577b798dec9fc6bdc9bb9
Michael Squance <m.squance@stem.org.uk>
2024-12-03 16:15:04 +0000

    Adding activity code to the administrate drop down

--- app/fields/grouped_activity_list_field.rb
+++ app/fields/grouped_activity_list_field.rb
@@ -5,7 +5,7 @@ class GroupedActivityListField < Administrate::Field::Base
     Activity.includes([:programmes]).all.group_by(&:category).each_with_object([]) do |(cat, acts), arr|
       arr << [cat, acts.sort_by(&:title).map {
                      [
-                       "#{_1.title} #{_1.remote_delivered_cpd ? "(remote)" : ""} -- (#{_1.programmes.collect(&:slug).join(", ")})",
+                       "#{_1.stem_activity_code} #{_1.title} #{_1.remote_delivered_cpd ? "(remote)" : ""} -- (#{_1.programmes.collect(&:slug).join(", ")})",
                        _1.id
                      ]
                    }]
