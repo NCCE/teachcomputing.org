c570367755548d9ccaeff3035783cb1f1224fe32
Michael Squance <m.squance@stem.org.uk>
2024-12-04 15:43:59 +0000

    Undoing the schema thats auto migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_26_112120) do
+ActiveRecord::Schema.define(version: 2024_11_01_083738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -317,8 +317,6 @@ ActiveRecord::Schema.define(version: 2024_11_26_112120) do
     t.jsonb "web_copy"
     t.jsonb "metadata"
     t.string "type"
-    t.string "cms_slug"
-    t.index ["cms_slug"], name: "index_programme_activity_groupings_on_cms_slug", unique: true
     t.index ["programme_id"], name: "index_programme_activity_groupings_on_programme_id"
   end
 
