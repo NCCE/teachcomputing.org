291c171989a0be0db2e93096d56c541c9ed1484b
Adam Wheatley <adamwheatley7@gmail.com>
2024-12-05 10:46:40 +0000

    Asides optional on DashboardProgrammeActivityGroupSectionComponent Refactoring components Styling for mobile Fix typo in component preview Updating testing for community activity component and evidence submission component Testing for course bookings component

--- spec/views/certificates/primary-certificate/show.html_spec.rb
+++ spec/views/certificates/primary-certificate/show.html_spec.rb
@@ -3,80 +3,38 @@ require "rails_helper"
 RSpec.describe("certificates/primary_certificate/show", type: :view) do
   let(:user) { create(:user) }
   let(:primary_certificate) { create(:primary_certificate) }
-  let(:pathway) { create(:pathway, programme: primary_certificate, title: "Developing", pdf_link: "developing.pdf") }
-  let(:pathway_2) { create(:pathway, programme: primary_certificate, title: "Specialising", pdf_link: "specialising.pdf") }
-  let(:pathways) { [pathway, pathway_2] }
-  let(:professional_development_groups) { create_list(:programme_activity_grouping, 2, :with_activities, sort_key: 1, programme: primary_certificate) }
-  let(:online_development_group) { create(:programme_activity_grouping, :with_activities, sort_key: 3, community: true, programme: primary_certificate) }
+
+  let(:user_courses) { create_list(:programme_activity_grouping, 2, :with_activities, sort_key: 4, community: false, programme: primary_certificate) }
   let(:community_groups) { create_list(:programme_activity_grouping, 2, :with_activities, sort_key: 4, community: true, programme: primary_certificate) }
-  let(:community_activity) { create(:activity, :community) }
-  let(:upe) { create(:user_programme_enrolment, user_id: user.id, programme_id: primary_certificate.id, pathway_id: pathway.id) }
 
   before do
     assign(:programme, primary_certificate)
+    assign(:current_user, user)
+    assign(:user_courses, user_courses)
+    assign(:community_groups, community_groups)
+
+    allow_any_instance_of(AuthenticationHelper)
+      .to receive(:current_user).and_return(user)
+
+    stub_strapi_aside_section("primary-certificate-need-help")
+    stub_strapi_aside_section("primary-dashboard-cpd-section")
+
+    render
   end
 
-  describe "when the user has a pathway" do
-    before do
-      assign(:current_user, user)
+  it "renders the page title component" do
+    expect(rendered).to have_css(".page-title__wrapper")
+  end
 
-      assign(:face_to_face_achievements, [])
-      assign(:online_achievements, [])
+  it "renders the progress component" do
+    expect(rendered).to have_css(".progress-bar-component")
+  end
 
-      assign(:professional_development_groups, professional_development_groups)
-      assign(:online_discussion_activity, community_activity)
-      assign(:community_groups, community_groups)
+  it "renders the user courses component" do
+    expect(rendered).to have_css(".user-programme-course-bookings-with-asides")
+  end
 
-      recommended_activities = upe.pathway.pathway_activities
-      assign(:recommended_activities, recommended_activities.filter { |pa| pa.activity.category != :community.to_s })
-      assign(:recommended_community_activities, recommended_activities.filter { |pa| pa.activity.category == :community.to_s })
-      assign(:user_pathway, upe.pathway)
-      assign(:pathways, pathways)
-      assign(:available_pathways_for_user, [pathways[1]])
-      assign(:user_enrolment, upe)
-      render
-    end
-
-    it "has a recommendation list" do
-      expect(rendered).to have_css(".recommended-courses-wrapper .expander__button", text: "Courses based on your pathway")
-    end
-
-    it "has hidden activities" do
-      expect(rendered).not_to have_text("View more activity options")
-    end
-
-    describe "the pathway selector" do
-      it "shows the current pathway" do
-        expect(rendered).to have_css(".ncce-pathway-prompt", text: "Developing (PDF)")
-      end
-
-      it "has the details expander" do
-        expect(rendered).to have_css(".ncce-details__summary-text", text: "Not sure this is the right pathway for you?")
-      end
-
-      it "has a list of pathways" do
-        expect(rendered).to have_link("Specialising", href: "specialising.pdf", visible: :hidden)
-      end
-
-      it "has an select list" do
-        expect(rendered).to have_css(".ncce-pathway-aside__select", visible: :hidden)
-      end
-
-      it "has a button" do
-        expect(rendered).to have_button("Change pathway", visible: :hidden)
-      end
-    end
-
-    context "when the user has a primary_pathway_migrated messages flag" do
-      let(:upe) { create(:user_programme_enrolment, user_id: user.id, programme_id: primary_certificate.id, pathway_id: pathway.id, message_flags_primary_pathway_migrated: true) }
-
-      it "has in info modal with the message on it" do
-        expect(rendered).to have_css(".govuk-heading-m", text: "Updates to your Teach primary computing certificate pathway")
-      end
-
-      it "has a close button" do
-        expect(rendered).to have_css('a[href="/user_programme_enrolments/destroy_message_flags_primary_pathway_migrated"]')
-      end
-    end
+  it "renders the user programme activities components" do
+    expect(rendered).to have_css(".user-programme-activities-with-asides", count: 2)
   end
 end
