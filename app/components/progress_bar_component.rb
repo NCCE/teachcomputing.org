115a6e25796fb50fabdbdd52ba56b18a9f2ff1df
Adam Wheatley <adamwheatley7@gmail.com>
2024-12-10 08:02:01 +0000

    Intial work on progress bar changes Added pending icon svg Updated progress bar titles in primary seed file

--- app/components/progress_bar_component.rb
+++ app/components/progress_bar_component.rb
@@ -1,7 +1,8 @@
 class ProgressBarComponent < ViewComponent::Base
-  def initialize(current_user, programme, title: nil, body: nil, top_padding: true, steps_to_accreditation: true)
+  delegate :current_user, to: :helpers
+
+  def initialize(current_user, programme, title: nil, body: nil, steps_to_accreditation: true)
     @programme = programme
-    @current_user = current_user
     @title = title
     @body = body
     @top_padding = top_padding
@@ -10,9 +11,32 @@ class ProgressBarComponent < ViewComponent::Base
     @programme_objectives = programme.programme_objectives_displayed_in_progress_bar
   end
 
-  def top_padding_class
-    if @top_padding
-      "govuk-!-padding-top-3"
+  def before_render
+    @current_user = current_user
+
+    in_progress_achievements = current_user.achievements.in_state(:in_progress, :enrolled).with_courses.order("created_at DESC")
+    complete_achievements = current_user.achievements.in_state(:complete).with_courses.order("created_at DESC")
+
+    @courses =
+      {
+        in_progress: in_progress_achievements.belonging_to_programme(@programme),
+        complete: complete_achievements.belonging_to_programme(@programme)
+      }
+  end
+
+  def user_enrolled_class
+    if @programme.user_enrolled?(@current_user)
+      "icon-ticked-circle"
+    else
+      "icon-blank-circle"
+    end
+  end
+
+  def course_status_class(status)
+    if @courses[status].blank?
+      "icon-blank-circle"
+    elsif @courses[status].present?
+      "icon-pending-circle"
     end
   end
 end
