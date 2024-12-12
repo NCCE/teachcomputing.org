1a0535c4a5b75bf4075630c76e80adc09357a9fc
Adam Wheatley <adamwheatley7@gmail.com>
2024-12-12 15:16:01 +0000

    Added boolean column onto programme activity groupings for multi stage activity groups Added logic for handling of multi stage group Method to get user courses and sort by in progress, complete etc

--- app/lib/programme_objectives/programme_completion_required.rb
+++ app/lib/programme_objectives/programme_completion_required.rb
@@ -1,11 +1,12 @@
 # Plays role "ProgrammeObjective" and "ProgressBarItem"
 class ProgrammeObjectives::ProgrammeCompletionRequired
-  attr_reader :progress_bar_title, :progress_bar_path
+  attr_reader :progress_bar_title, :progress_bar_path, :multi_stage_group
 
-  def initialize(required_programme:, progress_bar_title:, progress_bar_path:)
+  def initialize(required_programme:, progress_bar_title:, progress_bar_path:, multi_stage_group: false)
     @required_programme = required_programme
     @progress_bar_title = progress_bar_title
     @progress_bar_path = progress_bar_path
+    @multi_stage_group = multi_stage_group
   end
 
   def user_complete?(user)
@@ -17,6 +18,10 @@ class ProgrammeObjectives::ProgrammeCompletionRequired
   end
 
   def objective_displayed_in_body?
+    false
+  end
+
+  def multi_stage_group?
     false
   end
 
