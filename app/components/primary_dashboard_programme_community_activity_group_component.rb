1b86387e74868b03d1acf9d30c7186672a9b0fcb
Adam Wheatley <adamwheatley7@gmail.com>
2024-12-16 13:28:32 +0000

    Moved achievement complete and rejected methods into the model Refactored activities and courses into variables instead of hashes Updated activity group component to get title and aside slug from the model Removed duplicated code in the course bookings component and combined completed and in progress together

--- app/components/primary_dashboard_programme_community_activity_group_component.rb
+++ app/components/primary_dashboard_programme_community_activity_group_component.rb
@@ -3,15 +3,14 @@
 class PrimaryDashboardProgrammeCommunityActivityGroupComponent < CmsWithAsidesComponent
   delegate :current_user, to: :helpers
 
-  def initialize(title:, programme_activity_group:, current_user:, aside_slug: nil)
-    aside_sections = if aside_slug.nil?
+  def initialize(programme_activity_group:, current_user:)
+    aside_sections = if programme_activity_group.web_copy_aside_slug.nil?
       nil
     else
-      [{slug: aside_slug}]
+      [{slug: programme_activity_group.web_copy_aside_slug}]
     end
 
     super(aside_sections:)
-    @title = title
     @programme_activity_group = programme_activity_group
   end
 
@@ -19,14 +18,21 @@ class PrimaryDashboardProgrammeCommunityActivityGroupComponent < CmsWithAsidesCo
     @current_user = current_user
   end
 
-  def user_programme_activities
-    {
-      complete: @current_user.achievements.in_state(:complete).belonging_to_programme_activity_grouping(@programme_activity_group).includes([:activity]),
-      incomplete: @current_user.achievements.not_in_state(:complete).belonging_to_programme_activity_grouping(@programme_activity_group).includes([:activity])
-    }
+  def completed_activities
+    @current_user.achievements
+      .in_state(:complete)
+      .belonging_to_programme_activity_grouping(@programme_activity_group)
+      .includes([:activity])
+  end
+
+  def incomplete_activities
+    @current_user.achievements
+      .not_in_state(:complete)
+      .belonging_to_programme_activity_grouping(@programme_activity_group)
+      .includes([:activity])
   end
 
   def complete?
-    user_programme_activities[:complete].size >= @programme_activity_group.required_for_completion
+    completed_activities.size >= @programme_activity_group.required_for_completion
   end
 end
