5e734cba8e1005e3a860333ccc9e79edf2b67d09
Adam Wheatley <adamwheatley7@gmail.com>
2024-12-18 11:12:29 +0000

    Refactored components to reduce the number of queries to the database when getting activities and achievements Using helper method to get icon class Fix and update testing

--- app/components/primary_dashboard_programme_community_activity_group_component.rb
+++ app/components/primary_dashboard_programme_community_activity_group_component.rb
@@ -17,22 +17,4 @@ class PrimaryDashboardProgrammeCommunityActivityGroupComponent < CmsWithAsidesCo
   def before_render
     @current_user = current_user
   end
-
-  def completed_activities
-    @current_user.achievements
-      .in_state(:complete)
-      .belonging_to_programme_activity_grouping(@programme_activity_group)
-      .includes([:activity])
-  end
-
-  def incomplete_activities
-    @current_user.achievements
-      .not_in_state(:complete)
-      .belonging_to_programme_activity_grouping(@programme_activity_group)
-      .includes([:activity])
-  end
-
-  def complete?
-    completed_activities.size >= @programme_activity_group.required_for_completion
-  end
 end
