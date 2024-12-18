6e2542a4a8d4b8f39735f34e75223041a311beba
Adam Wheatley <adamwheatley7@gmail.com>
2024-12-18 11:12:29 +0000

    Refactored components to reduce the number of queries to the database when getting activities and achievements Using helper method to get icon class Fix and update testing

--- spec/components/user_programme_course_bookings_with_asides_component_spec.rb
+++ spec/components/user_programme_course_bookings_with_asides_component_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe UserProgrammeCourseBookingsWithAsidesComponent, type: :component do
   let(:user) { create(:user) }
-  let(:activity) { create(:activity, stem_activity_code: "CP100", category: :online) }
+  let(:activity) { create(:activity, stem_activity_code: "CP199", category: :online) }
   let(:activity_two) { create(:activity, stem_activity_code: "CP228") }
   let(:activity_three) { create(:activity, stem_activity_code: "CP101", remote_delivered_cpd: true) }
   let(:programme) { create(:primary_certificate) }
