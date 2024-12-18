2dac933aa753c244b56384f5d8ff5ad7f3b18835
Adam Wheatley <adamwheatley7@gmail.com>
2024-12-18 15:49:34 +0000

    Fix padding on component Adding testing for component variations Updating component render on certificate pages

--- spec/components/progress_bar_component_spec.rb
+++ spec/components/progress_bar_component_spec.rb
@@ -3,9 +3,12 @@ require "rails_helper"
 RSpec.describe ProgressBarComponent, type: :component do
   let(:user) { create(:user) }
   let(:primary_certificate) { create(:primary_certificate) }
+  let(:activity_not_enough_credits) { create(:activity, credit: 10) }
+  let(:activity_enough_credits) { create(:activity, credit: 100) }
+
   let!(:primary_programme_activity_groupings) do
     create_list(:programme_activity_groupings_credit_counted, 3, programme: primary_certificate).tap do |groupings|
-      groupings[0].update(progress_bar_title: "Attend required CPD", multi_stage_group: true, required_credit_count: 10)
+      groupings[0].update(progress_bar_title: "Attend required CPD", multi_stage_group: true, required_credit_count: 50)
       groupings[1].update(progress_bar_title: "Put into practice")
       groupings[2].update(progress_bar_title: "Share with others")
     end
@@ -54,16 +57,160 @@ RSpec.describe ProgressBarComponent, type: :component do
       end
     end
 
-    context "with booked cpd not enough credits" do
+    context "with booked cpd" do
+      let(:in_progress_achievement) { create(:in_progress_achievement, activity: activity_not_enough_credits) }
+      context "with not enough credits" do
+        let(:not_enough_credits_programme_activity) {
+          create(:programme_activity,
+            programme: primary_certificate,
+            activity: activity_not_enough_credits)
+        }
+
+        before do
+          allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
+          allow(primary_certificate).to receive(:user_enrolled?).with(user).and_return(true)
+
+          user.achievements << in_progress_achievement
+          primary_programme_activity_groupings[0].programme_activities << not_enough_credits_programme_activity
+
+          render_inline(
+            described_class.new(
+              programme: primary_certificate
+            )
+          )
+        end
+
+        it "renders the correct icon status images" do
+          expect(page).to have_css(".icon-ticked-circle", count: 1)
+          expect(page).to have_css(".icon-pending-circle", count: 1)
+          expect(page).to have_css(".icon-blank-circle", count: 3)
+        end
+      end
+
+      context "with enough credits" do
+        let(:enough_credits_programme_activity) {
+          create(:programme_activity,
+            programme: primary_certificate,
+            activity: activity_enough_credits)
+        }
+        let(:in_progress_achievement) { create(:in_progress_achievement, activity: activity_enough_credits) }
+
+        before do
+          allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
+          allow(primary_certificate).to receive(:user_enrolled?).with(user).and_return(true)
+
+          user.achievements << in_progress_achievement
+          primary_programme_activity_groupings[0].programme_activities << enough_credits_programme_activity
+
+          render_inline(
+            described_class.new(
+              programme: primary_certificate
+            )
+          )
+        end
+
+        it "renders the correct icon status images" do
+          expect(page).to have_css(".icon-ticked-circle", count: 2)
+          expect(page).to_not have_css(".icon-pending-circle")
+          expect(page).to have_css(".icon-blank-circle", count: 3)
+        end
+      end
+    end
+
+    context "with completed cpd" do
+      context "with not enough credits" do
+        let(:not_enough_credits_programme_activity) {
+          create(:programme_activity,
+            programme: primary_certificate,
+            activity: activity_not_enough_credits)
+        }
+        let(:completed_achievement) { create(:completed_achievement, activity: activity_not_enough_credits) }
+
+        before do
+          allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
+          allow(primary_certificate).to receive(:user_enrolled?).with(user).and_return(true)
+
+          user.achievements << completed_achievement
+          primary_programme_activity_groupings[0].programme_activities << not_enough_credits_programme_activity
+
+          render_inline(
+            described_class.new(
+              programme: primary_certificate
+            )
+          )
+        end
+
+        it "renders the correct icon status images" do
+          expect(page).to have_css(".icon-ticked-circle", count: 1)
+          expect(page).to have_css(".icon-pending-circle", count: 1)
+          expect(page).to have_css(".icon-blank-circle", count: 3)
+        end
+      end
+
+      context "with enough credits" do
+        let(:enough_credits_programme_activity) {
+          create(:programme_activity,
+            programme: primary_certificate,
+            activity: activity_enough_credits)
+        }
+        let(:completed_achievement) { create(:completed_achievement, activity: activity_enough_credits) }
+
+        before do
+          allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
+          allow(primary_certificate).to receive(:user_enrolled?).with(user).and_return(true)
+
+          user.achievements << completed_achievement
+          primary_programme_activity_groupings[0].programme_activities << enough_credits_programme_activity
+
+          render_inline(
+            described_class.new(
+              programme: primary_certificate
+            )
+          )
+        end
+
+        it "renders the correct icon status images" do
+          expect(page).to have_css(".icon-ticked-circle", count: 2)
+          expect(page).to_not have_css(".icon-pending-circle")
+          expect(page).to have_css(".icon-blank-circle", count: 3)
+        end
+      end
+    end
+
+    context "with booked and completed cpd" do
+      let(:programme_activity) {
+        create(:programme_activity,
+          programme: primary_certificate,
+          activity: activity_enough_credits)
+      }
+      let(:activity_two) { create(:activity, credit: 100) }
+      let(:programme_activity_two) {
+        create(:programme_activity,
+          programme: primary_certificate,
+          activity: activity_two)
+      }
+
+      let(:completed_achievement) { create(:completed_achievement, activity: activity_enough_credits) }
+      let(:in_progress_achievement) { create(:in_progress_achievement, activity: activity_two) }
+
       before do
         allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
         allow(primary_certificate).to receive(:user_enrolled?).with(user).and_return(true)
+
+        user.achievements << completed_achievement << in_progress_achievement
+        primary_programme_activity_groupings[0].programme_activities << programme_activity << programme_activity_two
 
         render_inline(
           described_class.new(
             programme: primary_certificate
           )
         )
+      end
+
+      it "renders the correct icon status images" do
+        expect(page).to have_css(".icon-ticked-circle", count: 3)
+        expect(page).to_not have_css(".icon-pending-circle")
+        expect(page).to have_css(".icon-blank-circle", count: 2)
       end
     end
   end
