efc52d8dc6f0971529a7bf006430b72d24aebe3a
Adam Wheatley <adamwheatley7@gmail.com>
2024-12-31 11:01:50 +0000

    Added sub text option onto cms card wrapper Updated testing to cover this

--- app/components/cms_card_wrapper_component.rb
+++ app/components/cms_card_wrapper_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class CmsCardWrapperComponent < ViewComponent::Base
-  def initialize(cards_block:, cards_per_row:, title: nil, background_color: nil)
+  def initialize(cards_block:, cards_per_row:, title: nil, sub_text: nil, background_color: nil)
     @title = title
+    @sub_text = sub_text
     @cards_block = cards_block
     @cards_per_row = cards_per_row
     @background_color = background_color
--- app/services/cms/dynamic_components/card_wrapper.rb
+++ app/services/cms/dynamic_components/card_wrapper.rb
@@ -1,17 +1,18 @@
 module Cms
   module DynamicComponents
     class CardWrapper
-      attr_accessor :title, :cards_block, :cards_per_row, :background_color
+      attr_accessor :title, :sub_text, :cards_block, :cards_per_row, :background_color
 
-      def initialize(title:, cards_block:, cards_per_row:, background_color:)
+      def initialize(title:, sub_text:, cards_block:, cards_per_row:, background_color:)
         @title = title
+        @sub_text = sub_text
         @cards_block = cards_block
         @cards_per_row = cards_per_row
         @background_color = background_color
       end
 
       def render
-        CmsCardWrapperComponent.new(title:, cards_block:, cards_per_row:, background_color:)
+        CmsCardWrapperComponent.new(title:, sub_text:, cards_block:, cards_per_row:, background_color:)
       end
     end
   end
