4adb193616e40da518100e993c67696ea79fd835
Adam Wheatley <adamwheatley7@gmail.com>
2025-01-15 14:11:00 +0000

    Defining constant classes in progress bar to remove duplicates Showing states for enrolled and completed in the booked cpd section for better user experience

--- db/seeds/programme_activity_groupings/primary_certificate.rb
+++ db/seeds/programme_activity_groupings/primary_certificate.rb
@@ -11,13 +11,13 @@ primary_certificate.programme_activity_groupings.find_or_initialize_by(title: "A
   group.required_for_completion = 1
   group.programme_id = primary_certificate.id
   group.progress_bar_title = "Attend required CPD"
-  group.web_copy_course_requirements = "Complete one full day face-to-face, remote or online course, or a combination of short courses that amounts to 6+ hours of professional development."
+  group.web_copy_course_requirements = "Complete one full day face-tI geto-face, remote or online course, or a combination of short courses that amounts to 6+ hours of professional development."
   group.required_credit_count = 50
   group.multi_stage_group = true
   group.objectives_progress_bar_stages = [
     {
       title: "Book required CPD",
-      state: :enrolled
+      state: [:enrolled, :complete]
     },
     {
       title: "Attend required CPD",
