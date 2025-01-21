de8bba4e981968b25e9a3085d500474ba37d4a41
Adam Wheatley <adamwheatley7@gmail.com>
2025-01-21 15:09:38 +0000

    Adding missing previews Tidy up exisiting previews Fix broken previews

--- previews/components/cms/rich_text_block_component_preview.rb
+++ previews/components/cms/rich_text_block_component_preview.rb
@@ -1,1 +1,30 @@
-# TO DO
+class Cms::RichTextBlockComponentPreview < ViewComponent::Preview
+  def default
+    render Cms::RichTextBlockComponent.new(blocks:
+      [
+        {type: "heading",
+         level: 1,
+         children: [
+           {type: "text", text: "Heading large"}
+         ]},
+        {type: "heading",
+         level: 2,
+         children: [
+           {type: "text", text: "Heading medium"}
+         ]},
+        {type: "paragraph",
+         children: [
+           {type: "text", text: "Standard paragraph"}
+         ]},
+        {type: "text", text: "Code text", code: true},
+        {
+          type: "list",
+          format: "ordered",
+          children: [
+            {type: "list-item", children: [{type: "text", text: "Item 1"}]},
+            {type: "list-item", children: [{type: "text", text: "Item 2"}]}
+          ]
+        }
+      ])
+  end
+end
--- previews/components/cms/text_with_asides_component_preview.rb
+++ previews/components/cms/text_with_asides_component_preview.rb
@@ -1,1 +1,9 @@
-# TO DO
+class Cms::TextWithAsidesComponentPreview < ViewComponent::Preview
+  def default
+    render Cms::TextWithAsidesComponent.new(
+      blocks: Cms::Mocks::RichBlocks.as_model,
+      asides: Cms::Mocks::AsideSection.as_model,
+      background_color: "nil"
+    )
+  end
+end
