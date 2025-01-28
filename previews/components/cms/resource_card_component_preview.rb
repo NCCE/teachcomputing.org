7a18c46241f2c7799952d2788bfc930baac5dd0d
Adam Wheatley <adamwheatley7@gmail.com>
2025-01-28 12:03:52 +0000

    Updates to mocks and previews

--- previews/components/cms/resource_card_component_preview.rb
+++ previews/components/cms/resource_card_component_preview.rb
@@ -2,12 +2,54 @@ class Cms::ResourceCardComponentPreview < ViewComponent::Preview
   layout "full-width"
 
   def default
-    card_section = Cms::Mocks::ResourceCardSection.as_model
+    card_section = Cms::Mocks::ResourceCardSection.as_model(
+      resource_cards: Array.new(3) {
+                       Cms::Mocks::ResourceCard.generate_data(
+                         color_theme: nil
+                       )
+                     }
+    )
 
     render(Cms::CardWrapperComponent.new(
       title: card_section.title,
       cards_block: card_section.cards_block,
-      background_color: "light-grey",
+      background_color: nil,
+      cards_per_row: 3
+    ))
+  end
+
+  def with_color_theme
+    card_section = Cms::Mocks::ResourceCardSection.as_model(
+      resource_cards: Array.new(3) {
+                       Cms::Mocks::ResourceCard.generate_data(
+                        color_theme: {data: Cms::Mocks::ColorScheme.generate_data(name: "standard")}
+                       )
+                     }
+    )
+
+    render(Cms::CardWrapperComponent.new(
+      title: card_section.title,
+      cards_block: card_section.cards_block,
+      background_color: nil,
+      cards_per_row: 3
+    ))
+  end
+
+  def with_button
+    card_section = Cms::Mocks::ResourceCardSection.as_model(
+      resource_cards: Array.new(3) {
+                       Cms::Mocks::ResourceCard.generate_data(
+                        color_theme: {data: Cms::Mocks::ColorScheme.generate_data(name: "standard")},
+                        button_text: "Click here",
+                        button_link: "https://www.google.com"
+                       )
+                     }
+    )
+
+    render(Cms::CardWrapperComponent.new(
+      title: card_section.title,
+      cards_block: card_section.cards_block,
+      background_color: nil,
       cards_per_row: 3
     ))
   end
--- previews/components/cms/text_with_asides_component_preview.rb
+++ previews/components/cms/text_with_asides_component_preview.rb
@@ -2,7 +2,7 @@ class Cms::TextWithAsidesComponentPreview < ViewComponent::Preview
   def default
     render Cms::TextWithAsidesComponent.new(
       blocks: Cms::Mocks::RichBlocks.as_model,
-      asides: Cms::Mocks::AsideSection.as_model,
+      asides: [],
       background_color: "nil"
     )
   end
