85962e2420fbb2ef33b8c8cc1bd1b3a23efccd99
Adam Wheatley <adamwheatley7@gmail.com>
2025-01-28 12:03:52 +0000

    Updates to mocks and previews

--- previews/components/cms/picture_card_component_preview.rb
+++ previews/components/cms/picture_card_component_preview.rb
@@ -2,12 +2,52 @@ class Cms::PictureCardComponentPreview < ViewComponent::Preview
   layout "full-width"
 
   def default
-    card_section = Cms::Mocks::PictureCardSection.as_model
+    card_section = Cms::Mocks::PictureCardSection.as_model(
+      picture_cards: Array.new(3) {
+                       Cms::Mocks::PictureCard.generate_data(
+                         color_theme: nil
+                       )
+                     }
+    )
 
     render(Cms::CardWrapperComponent.new(
       title: card_section.title,
       cards_block: card_section.cards_block,
       background_color: nil,
+      cards_per_row: 3
+    ))
+  end
+
+  def with_color_theme
+    card_section = Cms::Mocks::PictureCardSection.as_model(
+      picture_cards: Array.new(3) {
+                       Cms::Mocks::PictureCard.generate_data(
+                         color_theme: {data: Cms::Mocks::ColorScheme.generate_data(name: "standard")}
+                       )
+                     }
+    )
+
+    render(Cms::CardWrapperComponent.new(
+      title: card_section.title,
+      cards_block: card_section.cards_block,
+      background_color: nil,
+      cards_per_row: 3
+    ))
+  end
+
+  def with_background_color
+    card_section = Cms::Mocks::PictureCardSection.as_model(
+      picture_cards: Array.new(3) {
+                       Cms::Mocks::PictureCard.generate_data(
+                         color_theme: {data: Cms::Mocks::ColorScheme.generate_data(name: "standard")}
+                       )
+                     }
+    )
+
+    render(Cms::CardWrapperComponent.new(
+      title: card_section.title,
+      cards_block: card_section.cards_block,
+      background_color: "light-grey",
       cards_per_row: 3
     ))
   end
--- previews/components/cms/question_and_answer_component_preview.rb
+++ previews/components/cms/question_and_answer_component_preview.rb
@@ -4,18 +4,11 @@ class Cms::QuestionAndAnswerComponentPreview < ViewComponent::Preview
   def default
     render(Cms::QuestionAndAnswerComponent.new(
       question: "What is the meaning of life?",
-      answer: [{
-        type: "paragraph",
-        children: [
-          {
-            text: "42",
-            type: "text"
-          }
-        ]
-      }],
-      aside_sections: [{
-        slug: "intro-to-primary-computing"
-      }]
+      answer: Cms::Mocks::RichBlocks.as_model,
+      aside_sections: [],
+      answer_icon_block: Cms::Mocks::IconBlocks.as_model,
+      aside_alignment: "top",
+      show_background_triangle: true
     ))
   end
 end
