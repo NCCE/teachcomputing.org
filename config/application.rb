440b98eadf5bd192d95bc135c421ef58cf50e446
Adam Wheatley <adamwheatley7@gmail.com>
2025-01-30 11:32:16 +0000

    Fix deprecation warnings

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module TeachComputing
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.active_support.cache_format_version = 7.1
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
