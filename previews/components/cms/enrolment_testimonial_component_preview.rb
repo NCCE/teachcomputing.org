4cc53cc022bc3b87066b04d3b8bb1c65d499a9be
Adam Wheatley <adamwheatley7@gmail.com>
2025-02-04 12:31:34 +0000

    Fixing previews

--- previews/components/cms/enrolment_testimonial_component_preview.rb
+++ previews/components/cms/enrolment_testimonial_component_preview.rb
@@ -2,6 +2,13 @@
 
 class Cms::EnrolmentTestimonialComponentPreview < ViewComponent::Preview
   def default
-    render(Cms::EnrolmentTestimonialComponent.new(testimonial: "testimonial", enrolled_aside_slug: "enrolled_aside_slug", programme_slug: "programme_slug"))
+    render(Cms::EnrolmentTestimonialComponent.new(
+      title: "Testimonial Title",
+      testimonial: Cms::Mocks::Testimonial.as_model,
+      enrol_aside: [{slug: "i-belong-dashboard-resources"}],
+      enrolled_aside: [],
+      programme_slug: "primary-certificate",
+      background_color: Cms::Mocks::ColorScheme.generate_data(name: "standard")
+    ))
   end
 end
