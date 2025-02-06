43ba94ad7642aedefdd3d4d7e20ba9515838865c
Michael Squance <m.squance@stem.org.uk>
2025-02-06 11:39:44 +0000

    Updating schema order

--- db/schema.rb
+++ db/schema.rb
@@ -318,9 +318,9 @@ ActiveRecord::Schema.define(version: 2024_12_19_151246) do
     t.jsonb "metadata"
     t.string "type"
     t.string "cms_slug"
-    t.index ["cms_slug"], name: "index_programme_activity_groupings_on_cms_slug", unique: true
     t.boolean "multi_stage_group", default: false, null: false
     t.jsonb "objectives"
+    t.index ["cms_slug"], name: "index_programme_activity_groupings_on_cms_slug", unique: true
     t.index ["programme_id"], name: "index_programme_activity_groupings_on_programme_id"
   end
 
