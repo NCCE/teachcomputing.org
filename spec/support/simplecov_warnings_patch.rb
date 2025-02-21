c6a3faa38d2a295458207f7d38ed10ff33c22db5
Michael Squance <m.squance@stem.org.uk>
2025-02-21 12:19:49 +0000

    standard fixes!

--- spec/support/simplecov_warnings_patch.rb
+++ spec/support/simplecov_warnings_patch.rb
@@ -6,9 +6,9 @@ module SimpleCov
     # it also shows up for views when we `enable_coverage_for_eval`
     # patching here so that we can turn this off via ENV VAR
     def coverage_exceeding_source_warn
-      return unless ENV['SHOW_EXCESS_LINE_WARNING']
+      return unless ENV["SHOW_EXCESS_LINE_WARNING"]
 
-      message = "Warning: coverage data provided by Coverage [#{coverage_data['lines'].size}] "
+      message = "Warning: coverage data provided by Coverage [#{coverage_data["lines"].size}] "
       message += "exceeds number of lines in #{filename} [#{src.size}]"
       warn message
     end
