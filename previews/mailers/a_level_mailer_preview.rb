09b4e5017299bb897339be83d0f63cfc1fe3052f
Michael Squance <m.squance@stem.org.uk>
2025-02-21 11:54:11 +0000

    Updating email to remove references to subisidies and hubs as per instructions from comms

--- previews/mailers/a_level_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-class ALevelMailerPreview < ActionMailer::Preview
-  def enrolled
-    ALevelMailer.with(user: User.first, preview: true).enrolled
-  end
-
-  def completed
-    ALevelMailer.with(user: User.first, preview: true).completed
-  end
-end
