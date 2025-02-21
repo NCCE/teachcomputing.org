3fd5bd674702db254095846202b63aec24831b88
Michael Squance <m.squance@stem.org.uk>
2025-02-21 11:54:11 +0000

    Updating email to remove references to subisidies and hubs as per instructions from comms

--- app/mailers/new_badge_mailer.rb
+++
@@ -1,9 +0,0 @@
-class NewBadgeMailer < ApplicationMailer
-  def new_badge_email(user, programme)
-    @user = user
-    @programme = programme
-    @subject = "You’ve been awarded a new digital badge"
-
-    mail(to: @user, subject: @subject)
-  end
-end
