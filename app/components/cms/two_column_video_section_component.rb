8b53906995662d6320f56c6751c6b4bf6b1d5857
Adam Wheatley <adamwheatley7@gmail.com>
2025-02-25 10:01:26 +0000

    Refactored component to use flex Added background colour classes Added ncce button Return nil on content block and ncce button if non

--- app/components/cms/two_column_video_section_component.rb
+++ app/components/cms/two_column_video_section_component.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 class Cms::TwoColumnVideoSectionComponent < ViewComponent::Base
-  def initialize(left_column_content:, video:, right_column_content:, background_color:)
+  def initialize(left_column_content:, video:, right_column_content:, background_color:, left_column_button:)
     @left_column_content = left_column_content
     @video = video
     @right_column_content = right_column_content
     @background_color = background_color
+    @left_column_button = left_column_button
+  end
+
+  def background_color_classes
+    if @background_color == "isaac"
+      "cms-two-column-video-section-component--wrapper__white"
+    else
+      "cms-two-column-video-section-component--wrapper"
+    end
   end
 end
