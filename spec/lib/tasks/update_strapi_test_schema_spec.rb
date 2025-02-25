c32d9edf46fabbce41815e399a2eed1dd6577f52
Michael Squance <m.squance@stem.org.uk>
2025-02-25 15:31:07 +0000

    Addressing comments Adding testing for the header graphql query Fixing typo in the graphql query examples

--- spec/lib/tasks/update_strapi_test_schema_spec.rb
+++ spec/lib/tasks/update_strapi_test_schema_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "rake strapi:update_graphql_schema", type: :task do
     stub_strapi_schema
   end
 
-  it "should call SendInactivityEmailsJob.perform_now" do
+  it "should write the schema to the file defined by STRAPI_TEST_SCHEMA_PATH" do
     expect(File).to receive(:write).with(ENV["STRAPI_TEST_SCHEMA_PATH"], @schema)
     task.execute
   end
