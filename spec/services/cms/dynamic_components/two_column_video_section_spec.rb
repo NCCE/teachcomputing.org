a6452d3c404d736db55618853c58295dea160332
Adam Wheatley <adamwheatley7@gmail.com>
2025-02-26 11:34:22 +0000

    Removing some unnecessary code in cms tests

--- spec/services/cms/dynamic_components/two_column_video_section_spec.rb
+++ spec/services/cms/dynamic_components/two_column_video_section_spec.rb
@@ -1,12 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Cms::DynamicComponents::TwoColumnVideoSection do
-  let(:video_url) { "https://static.teachcomputing.org/How_important_is_the_I_Belong_programme.mp4" }
-
   before do
     @section = Cms::Providers::Strapi::Factories::ComponentFactory.process_component(
       Cms::Mocks::DynamicComponents::TwoColumnVideoSection.generate_raw_data(
-        video: Cms::Mocks::DynamicComponents::EmbeddedVideo.generate_raw_data(url: video_url)
+        video: Cms::Mocks::DynamicComponents::EmbeddedVideo.generate_raw_data
       )
     )
   end
