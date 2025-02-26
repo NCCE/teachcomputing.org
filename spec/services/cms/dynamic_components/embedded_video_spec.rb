8692c69760cbdf952ac2939ec428874dfb0ab565
Adam Wheatley <adamwheatley7@gmail.com>
2025-02-26 11:34:22 +0000

    Removing some unnecessary code in cms tests

--- spec/services/cms/dynamic_components/embedded_video_spec.rb
+++ spec/services/cms/dynamic_components/embedded_video_spec.rb
@@ -1,11 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Cms::DynamicComponents::EmbeddedVideo do
-  let(:video_url) { "https://static.teachcomputing.org/How_important_is_the_I_Belong_programme.mp4" }
-
   before do
     @video = Cms::Providers::Strapi::Factories::ComponentFactory.process_component(
-      Cms::Mocks::DynamicComponents::EmbeddedVideo.generate_raw_data(url: video_url)
+      Cms::Mocks::DynamicComponents::EmbeddedVideo.generate_raw_data
     )
   end
 
