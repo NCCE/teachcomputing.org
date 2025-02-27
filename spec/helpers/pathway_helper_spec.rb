6bee2c189f0aa505476623fb42c56754b913cbbe
Adam Wheatley <adamwheatley7@gmail.com>
2025-02-27 15:32:09 +0000

    Fix pathway helper test

--- spec/helpers/pathway_helper_spec.rb
+++ spec/helpers/pathway_helper_spec.rb
@@ -18,22 +18,4 @@ RSpec.describe PathwayHelper do
       end
     end
   end
-
-  describe "#should_show_funding?" do
-    context "when provided a primary certificate programme" do
-      let(:programme) { create(:primary_certificate) }
-
-      it "should return true" do
-        expect(helper.should_show_funding?(programme)).to be true
-      end
-    end
-
-    context "when provided a secondary certificate programme" do
-      let(:programme) { create(:secondary_certificate) }
-
-      it "should return false" do
-        expect(helper.should_show_funding?(programme)).to be false
-      end
-    end
-  end
 end
