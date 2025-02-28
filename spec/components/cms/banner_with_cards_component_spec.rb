4d259521aa86d14cd71922ca6ee5ffbf746b32a1
Michael Squance <m.squance@stem.org.uk>
2025-02-28 10:04:42 +0000

    Creating new options for ncce button that allow for different behaviour if the user is logged in.

--- spec/components/cms/banner_with_cards_component_spec.rb
+++ spec/components/cms/banner_with_cards_component_spec.rb
@@ -9,24 +9,49 @@ RSpec.describe Cms::BannerWithCardsComponent, type: :component do
     )
   }
 
-  before do
-    render_inline(described_class.new(
-      title: banner_with_cards.title,
-      text_content: banner_with_cards.text_content,
-      cards: banner_with_cards.cards,
-      background_color: "purple"
-    ))
+  context "with dark background" do
+    before do
+      render_inline(described_class.new(
+        title: banner_with_cards.title,
+        text_content: banner_with_cards.text_content,
+        cards: banner_with_cards.cards,
+        background_color: "purple"
+      ))
+    end
+
+    it "should add background color" do
+      expect(page).to have_css(".tc-gov-grid-wrapper.purple-bg")
+    end
+
+    it "should render title" do
+      expect(page).to have_css("h2", text: "Learn about computing")
+    end
+
+    it "should render 2 cards" do
+      expect(page).to have_css(".cms-horizontal-link-card", count: 2)
+    end
+
+    it "should render light text" do
+      expect(page).to have_css(".text-content--light")
+    end
   end
 
-  it "should add background color" do
-    expect(page).to have_css(".tc-gov-grid-wrapper.purple-bg")
-  end
+  context "with no background" do
+    before do
+      render_inline(described_class.new(
+        title: banner_with_cards.title,
+        text_content: banner_with_cards.text_content,
+        cards: banner_with_cards.cards,
+        background_color: nil
+      ))
+    end
 
-  it "should render title" do
-    expect(page).to have_css("h2", text: "Learn about computing")
-  end
+    it "should render grid wrapper" do
+      expect(page).to have_css(".tc-gov-grid-wrapper")
+    end
 
-  it "should render 2 cards" do
-    expect(page).to have_css(".cms-horizontal-link-card", count: 2)
+    it "should render standard text" do
+      expect(page).to have_css(".text-content")
+    end
   end
 end
