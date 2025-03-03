5e482dad93c08f2a3ad3bb2e8fc9b3a57fbe5b8c
Michael Squance <m.squance@stem.org.uk>
2025-03-03 15:34:05 +0000

    Adding previews for home page

---
+++ previews/components/cms/homepage_hero_component_preview.rb
@@ -0,0 +1,13 @@
+class Cms::HomepageHeroComponentPreview < ViewComponent::Preview
+  layout "full-width"
+
+  def default
+    render(Cms::HomepageHeroComponent.new(
+      title: "Helping you teach computing",
+      house_content: Cms::Mocks::RichBlocks.as_model,
+      buttons: [
+        Cms::Mocks::NcceButton.as_model
+      ]
+    ))
+  end
+end
