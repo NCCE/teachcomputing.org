b397ef2fd2b0910f1ff8d85894b6eace2b586b03
Adam Wheatley <adamwheatley7@gmail.com>
2025-03-06 13:49:59 +0000

    Removing comments

--- app/components/cpd_course_component.rb
+++ app/components/cpd_course_component.rb
@@ -12,16 +12,12 @@ class CpdCourseComponent < ViewComponent::Base
     if activity.replaced_by
       begin
         @activity = activity
-        # This is a replaced course, is it in Achiever?
         @course = Achiever::Course::Template.find_by_activity_code(@activity.stem_activity_code)
       rescue ActiveRecord::RecordNotFound
-        # Not in achiever, means we need to load replace by
         @activity = activity.replaced_by
-        # Try and get this one in achiever
         @course = Achiever::Course::Template.maybe_find_by_activity_code(@activity.stem_activity_code)
       end
     else
-      # Course is not replaced by, use original logic
       @activity = activity
       @course = Achiever::Course::Template.maybe_find_by_activity_code(@activity.stem_activity_code) if @activity.stem_activity_code.present?
     end
