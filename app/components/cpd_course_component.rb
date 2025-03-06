0f714086439a8c8673289b02a140f26ab1570379
Adam Wheatley <adamwheatley7@gmail.com>
2025-03-06 13:49:03 +0000

    Added logic in cpd course component for replaced_by Added replaced by onto activities in administrate Testing for component, courses controller and dashboard

--- app/components/cpd_course_component.rb
+++ app/components/cpd_course_component.rb
@@ -6,11 +6,26 @@ class CpdCourseComponent < ViewComponent::Base
     to: :helpers
 
   def initialize(activity:, current_user:, last_margin: true)
-    @activity = activity
     @current_user = current_user
     @last_margin = last_margin
 
-    @course = Achiever::Course::Template.maybe_find_by_activity_code(activity.stem_activity_code) if activity.stem_activity_code.present?
-    @achievement = current_user.achievements.to_a.find { _1.activity_id == activity.id }
+    if activity.replaced_by
+      begin
+        @activity = activity
+        # This is a replaced course, is it in Achiever?
+        @course = Achiever::Course::Template.find_by_activity_code(@activity.stem_activity_code)
+      rescue ActiveRecord::RecordNotFound
+        # Not in achiever, means we need to load replace by
+        @activity = activity.replaced_by
+        # Try and get this one in achiever
+        @course = Achiever::Course::Template.maybe_find_by_activity_code(@activity.stem_activity_code)
+      end
+    else
+      # Course is not replaced by, use original logic
+      @activity = activity
+      @course = Achiever::Course::Template.maybe_find_by_activity_code(@activity.stem_activity_code) if @activity.stem_activity_code.present?
+    end
+
+    @achievement = current_user.achievements.to_a.find { _1.activity_id == @activity.id }
   end
 end
