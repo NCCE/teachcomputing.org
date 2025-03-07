d34832b611eb5a516c1d603065a51b3ebdea5294
Michael Squance <m.squance@stem.org.uk>
2025-03-07 11:24:04 +0000

    Creating new text_with_testimonial component

--- app/services/cms/providers/strapi/queries/dynamic_zone.rb
+++ app/services/cms/providers/strapi/queries/dynamic_zone.rb
@@ -21,6 +21,7 @@ module Cms
             Components::Blocks::StickyDashboardBar,
             Components::Blocks::TestimonialRow,
             Components::Blocks::TextWithAsides,
+            Components::Blocks::TextWithTestimonial,
             Components::Blocks::TwoColumnVideoSection
           ]
 
