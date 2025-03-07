080cabb85fbf907f9e971df08cc9371341608bcf
Adam Wheatley <adamwheatley7@gmail.com>
2025-03-07 11:05:10 +0000

    Updated page title with background color and i belong flag Updated mocks Added graphql query test

--- spec/components/cms/page_title_component_spec.rb
+++ spec/components/cms/page_title_component_spec.rb
@@ -23,6 +23,14 @@ RSpec.describe Cms::PageTitleComponent, type: :component do
     it "doesnt render status_message container when non present" do
       expect(page).not_to have_css(".page-title__status-message")
     end
+
+    it "doesnt apply background color class when not present" do
+      expect(page).not_to have_css("[class$='-bg']")
+    end
+
+    it "doesnt render i belong flag when false" do
+      expect(page).to_not have_css(".i-belong-flag")
+    end
   end
 
   context "With Media" do
@@ -80,6 +88,26 @@ RSpec.describe Cms::PageTitleComponent, type: :component do
 
     it "render status_message container" do
       expect(page).to have_css(".page-title__status-message", text: "You completed")
+    end
+  end
+
+  context "with background color" do
+    before do
+      render_inline(described_class.new(title: "Page title", background_color: "orange"))
+    end
+
+    it "applies the background class" do
+      expect(page).to have_css(".orange-bg")
+    end
+  end
+
+  context "with i belong flag" do
+    before do
+      render_inline(described_class.new(title: "Page title", i_belong_flag: true))
+    end
+
+    it "renders the i belong image" do
+      expect(page).to have_css(".i-belong-flag")
     end
   end
 end
