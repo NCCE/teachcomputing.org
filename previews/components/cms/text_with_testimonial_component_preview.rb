bc39e38cf8874f69c5ec6ee92e898967dcae31d0
Michael Squance <m.squance@stem.org.uk>
2025-03-10 15:46:47 +0000

    Updating schema and adding preview

---
+++ previews/components/cms/text_with_testimonial_component_preview.rb
@@ -0,0 +1,31 @@
+class Cms::TextWithTestimonialComponentPreview < ViewComponent::Preview
+  def right_side
+    render Cms::TextWithTestimonialComponent.new(
+      text_content: Cms::Mocks::RichBlocks.as_model,
+      buttons: [],
+      testimonial: Cms::Mocks::Testimonial.as_model,
+      testimonial_side: "right",
+      background_color: nil
+    )
+  end
+
+  def left_side
+    render Cms::TextWithTestimonialComponent.new(
+      text_content: Cms::Mocks::RichBlocks.as_model,
+      buttons: [],
+      testimonial: Cms::Mocks::Testimonial.as_model,
+      testimonial_side: "left",
+      background_color: nil
+    )
+  end
+
+  def left_side_with_button
+    render Cms::TextWithTestimonialComponent.new(
+      text_content: Cms::Mocks::RichBlocks.as_model,
+      buttons: [Cms::Mocks::NcceButton.as_model],
+      testimonial: Cms::Mocks::Testimonial.as_model,
+      testimonial_side: "left",
+      background_color: nil
+    )
+  end
+end
