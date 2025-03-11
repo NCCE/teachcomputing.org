b95f02a7d6a54906410a02bb81caa934c0177f67
Adam Wheatley <adamwheatley7@gmail.com>
2025-03-11 15:45:08 +0000

    Standardrb fixes Removing generator files not needed

--- spec/services/cms/dynamic_components/i_belong_picture_card_spec.rb
+++ spec/services/cms/dynamic_components/i_belong_picture_card_spec.rb
@@ -3,7 +3,8 @@ require "rails_helper"
 RSpec.describe Cms::DynamicComponents::ContentBlocks::IBelongPictureCard do
   before do
     @i_belong_card = Cms::Providers::Strapi::Factories::BlocksFactory.to_i_belong_picture_card_section(
-      Array.wrap(Cms::Mocks::DynamicComponents::ContentBlocks::IBelongPictureCard.generate_data)).first
+      Array.wrap(Cms::Mocks::DynamicComponents::ContentBlocks::IBelongPictureCard.generate_data)
+    ).first
   end
 
   it "should render as CmsIBelongPictureCardComponent" do
