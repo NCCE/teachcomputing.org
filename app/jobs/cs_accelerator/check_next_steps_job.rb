88c0c2d49741ef54ed43c9cd4edb34ef31d8e7ac
Michael Squance <m.squance@stem.org.uk>
2025-03-17 16:01:19 +0000

    Adding logic to prevent email being scheduled if user can start exam

--- app/jobs/cs_accelerator/check_next_steps_job.rb
+++ app/jobs/cs_accelerator/check_next_steps_job.rb
@@ -3,11 +3,13 @@ module CSAccelerator
     queue_as :default
 
     def perform(achievement_id)
+      programme = Programme.cs_accelerator
       achievement = Achievement.find(achievement_id)
       user = achievement.user
-      enrolment = user.user_programme_enrolments.find_by(programme_id: Programme.cs_accelerator.id)
+      enrolment = user.user_programme_enrolments.find_by(programme_id: programme.id)
 
       return unless enrolment
+      return if programme.enough_credits_for_test?(user)
       return if enrolment.current_state == "complete"
 
       AchievementMailer.with(user_id: user.id).completed_course.deliver_now
