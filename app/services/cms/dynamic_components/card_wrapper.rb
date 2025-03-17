63584fa60d2fcf096f46d14141591138814a3e33
Adam Wheatley <adamwheatley7@gmail.com>
2025-03-17 14:55:59 +0000

    Adding multi state model and component Refatoring programme card wrapper in the block factory including new method to find programme slug Moved programme wrapper into child component Added mocks

--- app/services/cms/dynamic_components/card_wrapper.rb
+++ app/services/cms/dynamic_components/card_wrapper.rb
@@ -1,20 +1,19 @@
 module Cms
   module DynamicComponents
     class CardWrapper
-      attr_accessor :title, :intro_text, :cards_block, :cards_per_row, :background_color, :title_as_paragraph, :programme
+      attr_accessor :title, :intro_text, :cards_block, :cards_per_row, :background_color, :title_as_paragraph
 
-      def initialize(title:, intro_text:, cards_block:, cards_per_row:, background_color:, title_as_paragraph:, programme:)
+      def initialize(title:, intro_text:, cards_block:, cards_per_row:, background_color:, title_as_paragraph:)
         @title = title
         @intro_text = intro_text
         @cards_block = cards_block
         @cards_per_row = cards_per_row
         @background_color = background_color
         @title_as_paragraph = title_as_paragraph
-        @programme = Programme.find_by(slug: programme[:data][:attributes][:slug]) if programme
       end
 
       def render
-        Cms::CardWrapperComponent.new(title:, intro_text:, cards_block:, cards_per_row:, background_color:, title_as_paragraph:, programme:)
+        Cms::CardWrapperComponent.new(title:, intro_text:, cards_block:, cards_per_row:, background_color:, title_as_paragraph:)
       end
     end
   end
