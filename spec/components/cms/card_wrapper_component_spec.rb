c471788d6df63e00f8717809da12c3fb5e9fadf9
Adam Wheatley <adamwheatley7@gmail.com>
2025-03-18 12:28:08 +0000

    Standardrb fixes

--- spec/components/cms/card_wrapper_component_spec.rb
+++ spec/components/cms/card_wrapper_component_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Cms::CardWrapperComponent, type: :component do
         title: nil,
         intro_text: nil,
         cards_per_row: 3,
-        cards_block: resource_cards(1).cards_block,
+        cards_block: resource_cards(1).cards_block
       ))
     end
 
