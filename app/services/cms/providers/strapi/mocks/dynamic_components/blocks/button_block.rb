d1c41f8415932b79ccafb06d90a5d0f521a467c7
Michael Squance <m.squance@stem.org.uk>
2025-03-21 14:50:22 +0000

    Creating the button block component

---
+++ app/services/cms/providers/strapi/mocks/dynamic_components/blocks/button_block.rb
@@ -0,0 +1,21 @@
+module Cms
+  module Providers
+    module Strapi
+      module Mocks
+        module DynamicComponents
+          module Blocks
+            class ButtonBlock < StrapiMock
+              strapi_component "blocks.button-block"
+
+              attribute(:buttons) { Array.new(3) { NcceButton.generate_data } }
+              attribute(:bkColor) { ColorScheme.generate_data(name: "light_grey") }
+              attribute(:padding) { false }
+              attribute(:alignment) { "left" }
+              attribute(:fullWidth) { false }
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ previews/components/cms/button_block_component_preview.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+class Cms::ButtonBlockComponentPreview < ViewComponent::Preview
+  def with_padding_not_full_width_left_align
+    render(Cms::ButtonBlockComponent.new(
+      buttons: Array.new(2) { Cms::Mocks::NcceButton.as_model },
+      background_color: nil,
+      padding: true,
+      full_width: false,
+      alignment: "left"
+      ))
+  end
+
+  def with_padding_full_width_center
+    render(Cms::ButtonBlockComponent.new(
+      buttons: Array.new(2) { Cms::Mocks::NcceButton.as_model },
+      background_color: nil,
+      padding: true,
+      full_width: true,
+      alignment: "center"
+      ))
+  end
+
+  def with_padding_not_full_width_center
+    render(Cms::ButtonBlockComponent.new(
+      buttons: Array.new(2) { Cms::Mocks::NcceButton.as_model },
+      background_color: nil,
+      padding: true,
+      full_width: false,
+      alignment: "center"
+      ))
+  end
+
+  def with_padding_full_width_right
+    render(Cms::ButtonBlockComponent.new(
+      buttons: Array.new(2) { Cms::Mocks::NcceButton.as_model },
+      background_color: nil,
+      padding: true,
+      full_width: true,
+      alignment: "right"
+      ))
+  end
+
+  def with_padding_not_full_width_right
+    render(Cms::ButtonBlockComponent.new(
+      buttons: Array.new(2) { Cms::Mocks::NcceButton.as_model },
+      background_color: nil,
+      padding: true,
+      full_width: false,
+      alignment: "right"
+      ))
+  end
+end
