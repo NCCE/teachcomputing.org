1dff93a47506221cddc47e4b1a59851306958c8d
Michael Squance <m.squance@stem.org.uk>
2025-03-21 11:48:36 +0000

    Removing routes to hard coded pages, removing associated tests

--- spec/system/pages/static_pages_spec.rb
+++ spec/system/pages/static_pages_spec.rb
@@ -35,57 +35,6 @@ RSpec.describe("Static pages", type: :system) do
     end
   end
 
-  describe "I Belong page", js: false do
-    let(:user) { create(:user, email: "web@teachcomputing.org") }
-    let(:programme) { create(:i_belong) }
-    let(:enrolment) { create(:user_programme_enrolment, user:, programme:) }
-
-    context "as a guest" do
-      it "has guest content" do
-        visit "/i-belong"
-        expect(page).to have_css("h1", text: "I Belong: encouraging girls into computer science")
-        expect(page).to have_css("li", text: "Enrol on the programme")
-        expect(page).to have_link("Log in to enrol")
-        expect(page).to have_link("Log in to request")
-      end
-
-      it "is accessible" do
-        expect(page).to be_accessible.within("#main-content")
-      end
-    end
-
-    context "when logged in" do
-      before do
-        programme
-        allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
-      end
-
-      it "has logged in content" do
-        visit "/i-belong"
-        expect(page).to have_css("li", text: "Complete our ‘Encouraging Girls into GCSE Computer Science’ short course")
-        expect(page).to have_css("li", text: "Participate in a range of recommended activity")
-        expect(page).to have_link("Enrol on the programme")
-        expect(page).to have_link("Enrol to request")
-      end
-    end
-
-    context "when enrolled" do
-      before do
-        enrolment
-        allow_any_instance_of(AuthenticationHelper).to receive(:current_user).and_return(user)
-      end
-
-      it "has enrolled content" do
-        visit "/i-belong"
-        expect(page).to have_link("Visit dashboard")
-        expect(page).to have_link("Request your posters")
-        expect(page).to have_css("li", text: "Complete the I Belong short course for your key stage")
-        expect(page).to have_css("li", text: "Participate in a range of recommended activity")
-        expect(page).to have_css("li", text: "Claim your school certificate")
-      end
-    end
-  end
-
   context "Terms page" do
     before do
       visit terms_conditions_path
