02719a20f827ed853e68eb837b7c98a0fda2e51a
Michael Squance <m.squance@stem.org.uk>
2025-03-21 13:34:11 +0000

    Cleaning up old non-longer needed code

--- spec/system/pages/static_pages_spec.rb
+++ spec/system/pages/static_pages_spec.rb
@@ -35,20 +35,6 @@ RSpec.describe("Static pages", type: :system) do
     end
   end
 
-  context "Terms page" do
-    before do
-      visit terms_conditions_path
-    end
-
-    it "is the correct page" do
-      expect(page).to have_content("National Centre for Computing Education Terms and Conditions")
-    end
-
-    it "main is accessible" do
-      expect(page).to be_accessible.within("#main-content")
-    end
-  end
-
   context "404 page" do
     before do
       visit "/404"
