98c7c08656aed66daecffabcaf476c5cd58cb809
Michael Squance <m.squance@stem.org.uk>
2025-03-21 12:49:56 +0000

    Removing terms and conditions from routes

--- config/routes.rb
+++ config/routes.rb
@@ -236,7 +236,7 @@ Rails.application.routes.draw do
   get "/secondary-certification", to: "pages#secondary-certification", as: :secondary_certification
   get "/signup-confirmation", to: "pages#page", as: :signup_confirmation, defaults: {page_slug: "signup-confirmation"}
   get "/supporting-partners", to: redirect("/get-involved")
-  get "/terms-conditions", to: "pages#page", as: :terms_conditions, defaults: {page_slug: "terms-conditions"}
+  # get "/terms-conditions", to: "pages#page", as: :terms_conditions, defaults: {page_slug: "terms-conditions"}
 
   resource :search, only: :show
 
