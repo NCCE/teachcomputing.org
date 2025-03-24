e75a7d13b471708d752edeed6dc6ac4b38523a52
Adam Wheatley <adamwheatley7@gmail.com>
2025-03-24 14:46:24 +0000

    Added new feedback banner cms component Adding to block factory, queries and dynamic zone Testing for all the above

---
+++ spec/components/cms/feedback_banner_component_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Cms::FeedbackBannerComponent, type: :component do
+  before do
+    render_inline(described_class.new(
+      title: "Help us make these resources better",
+      button: Cms::Mocks::NcceButton.as_model
+    ))
+  end
+
+  it "renders the title" do
+    expect(page).to have_text("Help us make these resources better")
+  end
+
+  it "renders the ncce button" do
+    expect(page).to have_css(".govuk-button")
+  end
+end
