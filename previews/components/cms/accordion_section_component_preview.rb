ed9fa1ac88c211c6ab6cf0ccb66c166db2328b6a
Adam Wheatley <adamwheatley7@gmail.com>
2025-03-28 14:01:11 +0000

    Fixing preview

--- previews/components/cms/accordion_section_component_preview.rb
+++ previews/components/cms/accordion_section_component_preview.rb
@@ -3,9 +3,14 @@
 class Cms::AccordionSectionComponentPreview < ViewComponent::Preview
   def default
     render(Cms::AccordionSectionComponent.new(
+      id: 1,
       title: Faker::Lorem.sentence,
       background_color: nil,
-      accordion_block: Array.new(2) { Cms::Mocks::DynamicComponents::ContentBlocks::AccordionBlock.as_model }
+      accordion_block: Array.new(2) {
+        Cms::Mocks::DynamicComponents::ContentBlocks::AccordionBlock.as_model(
+          text_content: Cms::Mocks::RichBlocks.as_model
+        )
+      }
     ))
   end
 end
