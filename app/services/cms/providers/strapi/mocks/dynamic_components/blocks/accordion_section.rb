7961db17363286b92c3127b5540a9bd068ec5274
Adam Wheatley <adamwheatley7@gmail.com>
2025-03-28 13:49:15 +0000

    Fixing some tests and mocks

--- app/services/cms/providers/strapi/mocks/dynamic_components/blocks/accordion_section.rb
+++ app/services/cms/providers/strapi/mocks/dynamic_components/blocks/accordion_section.rb
@@ -10,6 +10,7 @@ module Cms
               attribute(:title) { Faker::Lorem.sentence }
               attribute(:bkColor) { nil }
               attribute(:accordionBlock) { Array.new(2) { ContentBlocks::AccordionBlock.generate_data } }
+              attribute(:id) { 1 }
             end
           end
         end
