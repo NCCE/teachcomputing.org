96f2ecbfed68c251ff1f498b2c2facf1b8db53a0
Michael Squance <m.squance@stem.org.uk>
2025-03-31 14:32:15 +0000

    Making link no longer required on benchmark cards

--- spec/components/benchmark_bordered_cards_component_spec.rb
+++ spec/components/benchmark_bordered_cards_component_spec.rb
@@ -1,31 +1,69 @@
 require "rails_helper"
 
 RSpec.describe BenchmarkBorderedCardsComponent, type: :component do
-  before do
-    render_inline(
-      described_class.new(
-        cards: [
-          {
-            title: "Testing",
-            text: "Pellentesque commodo eros a enim. Sed fringilla mauris sit amet nibh.",
-            benchmark_icons: [
-              "media/images/pages/careers-support/benchmark_1.svg"
-            ]
-          }
-        ]
+  context "without link" do
+    before do
+      render_inline(
+        described_class.new(
+          cards: [
+            {
+              title: "Testing",
+              text: "Pellentesque commodo eros a enim. Sed fringilla mauris sit amet nibh.",
+              benchmark_icons: [
+                "media/images/pages/careers-support/benchmark_1.svg"
+              ]
+            }
+          ]
+        )
       )
-    )
+    end
+
+    it "should have benchmark banner component" do
+      expect(page).to have_css("div.benchmark-banner-wrapper")
+    end
+
+    it "should render title" do
+      expect(page).to have_text("Testing")
+    end
+
+    it "should render text" do
+      expect(page).to have_text("Pellentesque")
+    end
   end
 
-  it "should have benchmark banner component" do
-    expect(page).to have_css("div.benchmark-banner-wrapper")
+  context "with link" do
+    before do
+      render_inline(
+        described_class.new(
+          cards: [
+            {
+              title: "Testing",
+              title_link: "/courses",
+              text: "Pellentesque commodo eros a enim. Sed fringilla mauris sit amet nibh.",
+              benchmark_icons: [
+                "media/images/pages/careers-support/benchmark_1.svg"
+              ]
+            }
+          ]
+        )
+      )
+    end
+
+    it "should have benchmark banner component" do
+      expect(page).to have_css("div.benchmark-banner-wrapper")
+    end
+
+    it "should render title" do
+      expect(page).to have_text("Testing")
+    end
+
+    it "should have link" do
+      expect(page).to have_link("Testing", href: "/courses")
+    end
+
+    it "should render text" do
+      expect(page).to have_text("Pellentesque")
+    end
   end
 
-  it "should render title" do
-    expect(page).to have_text("Testing")
-  end
-
-  it "should render text" do
-    expect(page).to have_text("Pellentesque")
-  end
 end
