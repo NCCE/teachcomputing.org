360166b4455d6dd0458dd5054be1c87abf51c9f8
Adam Wheatley <adamwheatley7@gmail.com>
2025-04-23 14:32:28 +0000

    Adding and updating tests

---
+++ spec/services/cms/providers/strapi/queries/site_wide_banner_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe Cms::Providers::Strapi::Queries::SiteWideBanner do
+  it_should_behave_like "a strapi graphql embed", {
+    required_fields: %w[
+      startTime
+      endTime
+      textContent
+    ]}
+end
