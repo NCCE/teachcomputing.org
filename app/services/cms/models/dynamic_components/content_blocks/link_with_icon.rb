70338d5c6c8d5dff63555cf0822a5978a79c5a65
Adam Wheatley <adamwheatley7@gmail.com>
2025-05-29 12:34:03 +0000

    Refactored factory and component logic

--- app/services/cms/models/dynamic_components/content_blocks/link_with_icon.rb
+++ app/services/cms/models/dynamic_components/content_blocks/link_with_icon.rb
@@ -3,17 +3,21 @@ module Cms
     module DynamicComponents
       module ContentBlocks
         class LinkWithIcon
-          attr_accessor :link_text, :url, :icon, :additional_classes
+          attr_accessor :link_text, :url, :icon
 
-          def initialize(link_text:, url:, icon:, additional_classes:)
+          def initialize(link_text:, url:, icon:)
             @link_text = link_text
             @url = url
             @icon = icon
-            @additional_classes = additional_classes
           end
 
-          def render
-            Cms::LinkWithIconComponent.new(link_text:, url:, icon:, additional_classes:)
+          def render(additional_classes: nil)
+            Cms::LinkWithIconComponent.new(
+              link_text: @link_text,
+              url: @url,
+              icon: @icon,
+              additional_classes: additional_classes
+            )
           end
         end
       end
