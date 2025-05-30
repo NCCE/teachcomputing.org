91c637020d40ac881b9248d17b2a3a94d9da09b5
Adam Wheatley <adamwheatley7@gmail.com>
2025-05-30 07:26:17 +0000

    Site wide banner mock was always showing and causing tests to fail

--- app/services/cms/providers/strapi/mocks/collections/site_wide_banner.rb
+++ app/services/cms/providers/strapi/mocks/collections/site_wide_banner.rb
@@ -6,7 +6,7 @@ module Cms
           class SiteWideBanner < StrapiMock
             attribute(:textContent) { Text::RichBlocks.generate_data }
             attribute(:startTime) { Faker::Time.backward(days: 2) }
-            attribute(:endTime) { Faker::Time.forward(days: 2) }
+            attribute(:endTime) { Faker::Time.backward(days: 1) }
           end
         end
       end
