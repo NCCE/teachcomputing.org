1219f700eb57688d1a1f54edf66a9a0c77b146be
Adam Wheatley <adamwheatley7@gmail.com>
2025-06-04 13:50:09 +0000

    Added a get_model method into cms resource to pull out specific models by key Refactored footer component Updated and added additional tests

--- app/components/cms/footer_component.rb
+++ app/components/cms/footer_component.rb
@@ -7,33 +7,9 @@ class Cms::FooterComponent < ViewComponent::Base
     @data = data
   end
 
-  def company_logo
-    data_models_by_type(Cms::Models::Images::Image).first
-  end
-
-  def funding_logo
-    data_models_by_type(Cms::Models::Images::Image).second
-  end
-
-  def company_url
-    find_text_field(0)&.value
-  end
-
-  def funding_url
-    find_text_field(1)&.value
-  end
-
-  def link_blocks
-    data_models_by_type(Cms::Models::Meta::FooterLinkBlock).first&.process_blocks
-  end
-
   private
 
-  def data_models_by_type(type)
-    data.select { |model| model.is_a?(type) }
-  end
-
-  def find_text_field(index)
-    data_models_by_type(Cms::Models::Data::TextField)[index]
+  def get_model(key)
+    @data.get_model(key)
   end
 end
