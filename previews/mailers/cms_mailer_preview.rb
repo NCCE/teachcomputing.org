0ab719c379d53da9942c379a5f0f9647532105e7
Adam Wheatley <adamwheatley7@gmail.com>
2025-06-12 11:51:52 +0000

    Small updates to administrate Updating preview to use first template

--- previews/mailers/cms_mailer_preview.rb
+++ previews/mailers/cms_mailer_preview.rb
@@ -1,6 +1,6 @@
 class CmsMailerPreview < ActionMailer::Preview
   def send_template
-    template_slug = params[:template_slug].presence || "primary-cert-1a.2"
+    template_slug = params[:template_slug].presence || "primary-cert-1a.1"
     user_id = params[:user_id].presence || User.first.id
     CmsMailer.with(user_id:, template_slug:, preview: true).send_template
   end
