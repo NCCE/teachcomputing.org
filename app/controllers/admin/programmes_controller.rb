8b71b9be3088ad2e8566eb76b21b2e95e74e750c
Adam Wheatley <adamwheatley7@gmail.com>
2025-06-16 14:44:34 +0000

    First pass at programme dashboard

---
+++ app/controllers/admin/programmes_controller.rb
@@ -0,0 +1,17 @@
+module Admin
+  class ProgrammesController < Admin::ApplicationController
+    def index
+      @programmes = [
+        Programme.cs_accelerator,
+        Programme.primary_certificate,
+        Programme.secondary_certificate,
+        Programme.i_belong,
+        Programme.a_level
+      ].compact.sort_by(&:title)
+    end
+
+    def show
+      @programme = Programme.find(params[:id])
+    end
+  end
+end
