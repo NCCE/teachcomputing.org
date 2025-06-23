8092210f3992e8731069496c032ae05811ba5eea
Adam Wheatley <adamwheatley7@gmail.com>
2025-06-23 08:13:16 +0000

    Tidy up methods in the view component

--- app/components/cms/footer_component.rb
+++ app/components/cms/footer_component.rb
@@ -12,4 +12,24 @@ class Cms::FooterComponent < ViewComponent::Base
   def get_model(key)
     @data.get_model(key)
   end
+
+  def company_logo
+    get_model(:company_logo).render
+  end
+
+  def company_logo_link
+    get_model(:company_logo_link).value
+  end
+
+  def funder_logo
+    get_model(:funder_logo).render
+  end
+
+  def funder_logo_link
+    get_model(:funder_logo_link).value
+  end
+
+  def link_blocks
+    get_model(:link_block).link_blocks
+  end
 end
