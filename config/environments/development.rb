ef5f4be56856d54e1cf09659bc49c484590a1bf8
Adam Wheatley <adamwheatley7@gmail.com>
2026-06-11 09:54:02 +0100

    Update environment configs for Rails 8 and ViewComponent 4
    Remove config.sass, update mailer preview_path to preview_paths and update ViewComponent preview config to the 4 API
    
    ENG-1743

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_mailer.preview_path = "#{Rails.root}/previews/mailers"
+  config.action_mailer.preview_paths = ["#{Rails.root}/previews/mailers"]
   config.action_mailer.asset_host = "http://localhost:3000"
   config.action_mailer.default_url_options = {host: "http://localhost:3000"}
 
@@ -106,8 +106,8 @@ Rails.application.configure do
   config.hosts << "teachcomputing.test"
   config.autoload_paths << "lib"
 
-  config.view_component.preview_paths << "#{Rails.root}/previews/components"
-  config.view_component.preview_route = "/rails/components"
+  config.view_component.previews.paths << "#{Rails.root}/previews/components"
+  config.view_component.previews.route = "/rails/components"
   config.view_component.generate.sidecar = true
 
   config.action_mailer.default_url_options = {host: "teachcomputing.rpfdev.com"}
